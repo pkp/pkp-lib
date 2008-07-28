af93959cd78054628b055881fd45d8a4f424431b
asmecher <asmecher>
2008-07-28 18:39:29 +0000

    #3696# #3603# Fixed TinyMCE cache dir; extracted javascript

--- lib/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php
+++ lib/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php
@@ -22,7 +22,7 @@
 	$compress = getParam("compress", "true") == "true";
 	$core = getParam("core", "true") == "true";
 	$suffix = getParam("suffix", "_src") == "_src" ? "_src" : "";
-	$cachePath = realpath("."); // Cache path, this is where the .gz files will be stored
+	$cachePath = realpath(dirname(dirname(dirname(dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME']))))))) . '/cache'); // Cache path, this is where the .gz files will be stored
 	$expiresOffset = 3600 * 24 * 10; // Cache for 10 days in browser cache
 	$content = "";
 	$encodings = array();

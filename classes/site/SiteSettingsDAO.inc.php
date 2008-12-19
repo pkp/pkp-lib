b17d2a1b7f8a3d179c8fd2186f3a7d1ccea95707
asmecher <asmecher>
2008-12-19 23:39:47 +0000

    #3935# Fixed site settings install

--- classes/site/SiteSettingsDAO.inc.php
+++ classes/site/SiteSettingsDAO.inc.php
@@ -161,7 +161,7 @@ class SiteSettingsDAO extends DAO {
 	 * @returns string
 	 */
 	function _performReplacement($rawInput, $paramArray = array()) {
-		$value = preg_replace_callback('{{translate key="([^"]+)"}}', '_installer_regexp_callback', $rawInput);
+		$value = preg_replace_callback('{{translate key="([^"]+)"}}', array(&$this, '_installer_regexp_callback'), $rawInput);
 		foreach ($paramArray as $pKey => $pValue) {
 			$value = str_replace('{$' . $pKey . '}', $pValue, $value);
 		}
@@ -231,13 +231,13 @@ class SiteSettingsDAO extends DAO {
 		$xmlParser->destroy();
 
 	}
-}
 
-/**
- * Used internally by site setting installation code to perform translation function.
- */
-function _installer_regexp_callback($matches) {
-	return Locale::translate($matches[1]);
+	/**
+	 * Used internally by site setting installation code to perform translation function.
+	 */
+	function _installer_regexp_callback($matches) {
+		return Locale::translate($matches[1]);
+	}
 }
 
 ?>

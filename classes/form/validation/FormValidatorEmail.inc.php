190937debefac24524af2b4885468b4721bf2fa6
asmecher <asmecher>
2009-01-08 17:28:12 +0000

    #3989# Allowed numeric domains in email addresses

--- classes/form/validation/FormValidatorEmail.inc.php
+++ classes/form/validation/FormValidatorEmail.inc.php
@@ -21,7 +21,7 @@ import('form.validation.FormValidatorRegExp');
 class FormValidatorEmail extends FormValidatorRegExp {
 	function getRegexp() {
 		$atom = '[-a-z0-9!#\$%&\'\*\+\/=\?\^_\`\{\|\}~]'; // allowed characters for part before "at" character
-		$domain = '([a-z]([-a-z0-9]*[a-z0-9]+)?)'; // allowed characters for part after "at" character
+		$domain = '([a-z0-9]([-a-z0-9]*[a-z0-9]+)?)'; // allowed characters for part after "at" character
 		$regex = '^' . $atom . '+' . // One or more atom characters.
 		'(\.' . $atom . '+)*'. // Followed by zero or more dot separated sets of one or more atom characters.
 		'@'. // Followed by an "at" character.

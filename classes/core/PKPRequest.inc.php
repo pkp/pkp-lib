c53db81e2a079a78fadd404145b831930102ae60
michael <michael>
2010-01-29 22:38:12 +0000

    #5115# IP handling always takes local IP, misses proxies

--- classes/core/PKPRequest.inc.php
+++ classes/core/PKPRequest.inc.php
@@ -341,7 +341,7 @@ class PKPRequest {
 			}
 
 			// If multiple addresses are listed, take the last. (Supports ipv6.)
-			if (preg_match_all('/^([0-9.a-fA-F:]+)/', $ipaddr, $matches)) {
+			if (preg_match_all('/([0-9.a-fA-F:]+)/', $ipaddr, $matches)) {
 				$ipaddr = $matches[0][count($matches[0])-1];
 			}
 			HookRegistry::call('Request::getRemoteAddr', array(&$ipaddr));

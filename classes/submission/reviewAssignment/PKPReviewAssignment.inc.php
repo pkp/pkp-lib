4c28bce0b27e09b8e4b784eeb90adf0e46af5504
Matthew Crider <mattcrider@gmail.com>
2010-05-19 17:37:08 -0700

    *5444* Back-end code needed for OMP review assignments

--- classes/submission/reviewAssignment/PKPReviewAssignment.inc.php
+++ classes/submission/reviewAssignment/PKPReviewAssignment.inc.php
@@ -29,6 +29,11 @@ define('SUBMISSION_REVIEWER_RATING_AVERAGE', 3);
 define('SUBMISSION_REVIEWER_RATING_POOR', 2);
 define('SUBMISSION_REVIEWER_RATING_VERY_POOR', 1);
 
+define('SUBMISSION_REVIEW_METHOD_BLIND', 1);
+define('SUBMISSION_REVIEW_METHOD_DOUBLEBLIND', 2);
+define('SUBMISSION_REVIEW_METHOD_OPEN', 3);
+
+
 class PKPReviewAssignment extends DataObject {
 	/** @var array The revisions of the reviewer file */
 	var $reviewerFileRevisions;
@@ -158,6 +163,22 @@ class PKPReviewAssignment extends DataObject {
 		return $this->setData('reviewType', $type);
 	}
 
+	/**
+	 * Get the method of the review (open, blind, or double-blind).
+	 * @return int
+	 */
+	function getReviewMethod() {
+		return $this->getData('reviewMethod');
+	}
+
+	/**
+	 * Set the type of review.
+	 * @param $method int
+	 */
+	function setReviewMethod($method) {
+		return $this->setData('reviewMethod', $method);
+	}
+	
 	/**
 	 * Get regret message.
 	 * @return string
@@ -341,6 +362,22 @@ class PKPReviewAssignment extends DataObject {
 		return $this->setData('dateDue', $dateDue);
 	}
 
+	/**
+	 * Get the reviewer's response due date.
+	 * @return string
+	 */
+	function getDateResponseDue() {
+		return $this->getData('dateResponseDue');
+	}
+
+	/**
+	 * Set the reviewer's response due date.
+	 * @param $dateResponseDue string
+	 */
+	function setDateResponseDue($dateResponseDue) {
+		return $this->setData('dateResponseDue', $dateResponseDue);
+	}
+	
 	/**
 	 * Get the declined value.
 	 * @return boolean

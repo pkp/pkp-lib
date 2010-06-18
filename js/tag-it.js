8119b12cb04660e508822af6a2e0d1c0f6c8f0d8
Matthew Crider <mattcrider@gmail.com>
2010-06-18 16:25:44 -0700

    *5478* Implemented advanced reviewer search

--- js/tag-it.js
+++ js/tag-it.js
@@ -92,7 +92,7 @@
 			el  = "<li class=\"tagit-choice\">\n";
 			el += value + "\n";
 			el += "<a class=\"close\">x</a>\n";
-			el += "<input type=\"hidden\" style=\"display:none;\" value=\""+value+"\" name=\"interests[]\">\n";
+			el += "<input type=\"hidden\" class=\"interestValue\" style=\"display:none;\" value=\""+value+"\" name=\"interests[]\">\n";
 			el += "</li>\n";
 			var li_search_tags = this.tag_input.parent();
 			$(el).insertBefore (li_search_tags);

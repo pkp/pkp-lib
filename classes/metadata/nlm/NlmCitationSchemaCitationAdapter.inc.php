e7da75b6f8709e2f273e3898f80f2a7e3cd2dd88
jerico.dev <jerico.dev@gmail.com>
2010-08-24 18:09:33 -0300

    *5796* Add NLM citation support to the Article object

--- classes/metadata/nlm/NlmCitationSchemaCitationAdapter.inc.php
+++ classes/metadata/nlm/NlmCitationSchemaCitationAdapter.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @file classes/metadata/NlmCitationSchemaCitationAdapter.inc.php
+ * @file classes/metadata/nlm/NlmCitationSchemaCitationAdapter.inc.php
  *
  * Copyright (c) 2000-2010 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.

933dc1a56b51f08e4599a60bcd76ab6846e9d507
jerico.dev <jerico.dev@gmail.com>
2010-10-29 19:24:33 -0200

    *6076* Plug-In version entries are not properly being installed/updated

--- classes/install/Installer.inc.php
+++ classes/install/Installer.inc.php
@@ -701,6 +701,39 @@ class Installer {
 		}
 		return false;
 	}
+
+	/**
+	 * Insert or update plugin data in versions
+	 * and plugin_settings tables.
+	 * @return boolean
+	 */
+	function addPluginVersions() {
+		$versionDao =& DAORegistry::getDAO('VersionDAO');
+		import('lib.pkp.classes.site.VersionCheck');
+		$categories = PluginRegistry::getCategories();
+		foreach ($categories as $category) {
+			PluginRegistry::loadCategory($category);
+			$plugins = PluginRegistry::getPlugins($category);
+			if (is_array($plugins)) {
+				foreach ($plugins as $plugin) {
+					$versionFile = $plugin->getPluginPath() . '/version.xml';
+
+					if (FileManager::fileExists($versionFile)) {
+						$versionInfo =& VersionCheck::parseVersionXML($versionFile);
+						$pluginVersion = $versionInfo['version'];
+					} else {
+						$pluginVersion = new Version(
+							1, 0, 0, 0, Core::getCurrentDate(), 1,
+							'plugins.'.$category, basename($plugin->getPluginPath()), '', 0
+						);
+					}
+					$versionDao->insertVersion($pluginVersion, true);
+				}
+			}
+		}
+
+		return true;
+	}
 }
 
 ?>

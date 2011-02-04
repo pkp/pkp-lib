a6e3f2e377b624eba85565ab1873daf85e676c79
jerico.dev <jerico.dev@gmail.com>
2011-02-04 14:52:28 -0200

    *6382* Cleaning up JSON class - added additional object test.

--- tests/classes/core/JSONTest.inc.php
+++ tests/classes/core/JSONTest.inc.php
@@ -22,14 +22,17 @@ class JSONTest extends PKPTestCase {
 	 */
 	public function testGetString() {
 		// Create a test object.
+		$testObject = new stdClass();
+		$testObject->someInt = 5;
+		$testObject->someFloat = 5.5;
 		$json = new JSON($status = true, $content = 'test content',
 				$isScript = false, $elementId = '0',
-				$additionalAttributes = array('someInt' => 5, 'someFloat' => 5.5));
+				$additionalAttributes = array('testObj' => $testObject));
 		$json->setEvent('someEvent', array('eventDataKey' => array('item1', 'item2')));
 
 		// Render the JSON message.
 		$expectedString = '{"status":true,"content":"test content",'.
-				'"isScript":false,"elementId":"0","someInt":5,"someFloat":5.5,'.
+				'"isScript":false,"elementId":"0","testObj":{"someInt":5,"someFloat":5.5},'.
 				'"event":{"name":"someEvent","data":{"eventDataKey":["item1","item2"]}}}';
 		self::assertEquals($expectedString, $json->getString());
 

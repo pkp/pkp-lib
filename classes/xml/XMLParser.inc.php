04ec02fcc64a36dd421b16d5e154297e4bb61742
Alec Smecher <alec@smecher.bc.ca>
2011-03-15 10:42:46 -0700

    *6502* Added data dump capability to harvester

--- classes/xml/XMLParser.inc.php
+++ classes/xml/XMLParser.inc.php
@@ -92,9 +92,10 @@ class XMLParser {
 	 * Parse an XML file using the specified handler.
 	 * If no handler has been specified, XMLParserDOMHandler is used by default, returning a tree structure representing the document.
 	 * @param $file string full path to the XML file
+	 * @param $dataCallback mixed Optional callback for data handling: function dataCallback($operation, $wrapper, $data = null)
 	 * @return object actual return type depends on the handler
 	 */
-	function &parse($file) {
+	function &parse($file, $dataCallback = null) {
 		$parser =& $this->createParser();
 
 		if (!isset($this->handler)) {
@@ -133,6 +134,8 @@ class XMLParser {
 			return $result;
 		}
 
+		if ($dataCallback) call_user_func($dataCallback, 'open', $wrapper);
+
 		while (!$wrapper->eof() && ($data = $wrapper->read()) !== false) {
 
 			// if the string contains non-UTF8 characters, convert it to UTF-8 for parsing
@@ -159,11 +162,13 @@ class XMLParser {
 			// strip any invalid ASCII control characters
 			$data = String::utf8_strip_ascii_ctrl($data);
 
+			if ($dataCallback) call_user_func($dataCallback, 'parse', $wrapper, $data);
 			if (!xml_parse($parser, $data, $wrapper->eof())) {
 				$this->addError(xml_error_string(xml_get_error_code($parser)));
 			}
 		}
 
+		if ($dataCallback) call_user_func($dataCallback, 'close', $wrapper);
 		$wrapper->close();
 		$result =& $this->handler->getResult();
 		$this->destroyParser($parser);

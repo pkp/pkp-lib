112e349bc2dd6a363c088e140dbe96f0a67e1e7c
jerico.dev <jerico.dev@gmail.com>
2011-03-17 21:01:36 -0300

    *6515* OMP installer doesn't redirect after installation

--- pages/install/PKPInstallHandler.inc.php
+++ pages/install/PKPInstallHandler.inc.php
@@ -67,7 +67,7 @@ class PKPInstallHandler extends Handler {
 		$installForm->readInputData();
 
 		if ($installForm->validate()) {
-			$installForm->execute();
+			return $installForm->execute();
 
 		} else {
 			$installForm->display();

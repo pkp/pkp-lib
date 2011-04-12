205f7d402504f630c9b3c94b204f112c78bba7d7
Alec Smecher <alec@smecher.bc.ca>
2011-04-12 10:17:08 -0700

    *6578* Permit null link action

---
+++ classes/linkAction/request/NullAction.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @file classes/linkAction/request/NullAction.inc.php
+ *
+ * Copyright (c) 2000-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class NullAction
+ * @ingroup linkAction_request
+ *
+ * @brief This action does nothing.
+ */
+
+
+import('lib.pkp.classes.linkAction.request.LinkActionRequest');
+
+class NullAction extends LinkActionRequest {
+	/**
+	 * Constructor
+	 */
+	function NullAction() {
+		parent::LinkActionRequest();
+	}
+
+
+	//
+	// Overridden protected methods from LinkActionRequest
+	//
+	/**
+	 * @see LinkActionRequest::getJSLinkActionRequest()
+	 */
+	function getJSLinkActionRequest() {
+		return '$.pkp.classes.linkAction.NullRequest';
+	}
+}
+
+?>
---
+++ js/classes/linkAction/NullAction.js
@@ -0,0 +1,49 @@
+/**
+ * @file js/classes/linkAction/NullAction.js
+ *
+ * Copyright (c) 2000-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class NullAction
+ * @ingroup js_classes_linkAction
+ *
+ * @brief A simple action request that doesn't.
+ */
+(function($) {
+
+
+	/**
+	 * @constructor
+	 *
+	 * @extends $.pkp.classes.linkAction.LinkActionRequest
+	 *
+	 * @param {jQuery} $linkActionElement The element the link
+	 *  action was attached to.
+	 * @param {Object} options Configuration of the link action
+	 *  request.
+	 */
+	$.pkp.classes.linkAction.NullAction =
+			function($linkActionElement, options) {
+
+		this.parent($linkActionElement, options);
+	};
+	$.pkp.classes.Helper.inherits(
+			$.pkp.classes.linkAction.NullAction,
+			$.pkp.classes.linkAction.LinkActionRequest);
+
+
+	//
+	// Public methods
+	//
+	/**
+	 * @inheritDoc
+	 */
+	$.pkp.classes.linkAction.NullAction.prototype.activate =
+			function(element, event) {
+
+		return this.parent('activate', element, event);
+	};
+
+
+/** @param {jQuery} $ jQuery closure. */
+})(jQuery);

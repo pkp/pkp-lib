e19bf284388dc373097ad53462443954a272d468
Matthew Crider <mattcrider@gmail.com>
2011-06-27 12:25:46 -0700

    *6723* Some ICO file types don't upload for favicon

--- classes/file/FileManager.inc.php
+++ classes/file/FileManager.inc.php
@@ -424,6 +424,7 @@ class FileManager {
 				return '.png';
 			case 'image/vnd.microsoft.icon':
 			case 'image/x-icon':
+			case 'image/x-ico':
 			case 'image/ico':
 				return '.ico';
 			case 'application/x-shockwave-flash':

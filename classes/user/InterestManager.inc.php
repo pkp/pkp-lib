a1de96b1e8b2ebb4a17a093332f896c7082751ed
Jason Nugent <jason.nugent@gmail.com>
2011-07-06 09:31:12 -0300

    *6738* Remove extra argument for insertInterests, allow submission of empty interests field.

--- classes/user/InterestManager.inc.php
+++ classes/user/InterestManager.inc.php
@@ -25,15 +25,12 @@ class InterestManager {
 	 * @param $interests array
 	 * @param $interestsTextOnly string Will be populated if the user's javascript is disabled and is using the old-style interest field
 	 */
-	function insertInterests($userId, $interests = array(), $interestsTextOnly = null) {
-		if(isset($interestsTextOnly)) {
-			// If JS is disabled, this will be the input to read
-			$interestsTextOnly = explode(",", $interestsTextOnly);
-		} else $interestsTextOnly = null;
-		if ($interestsTextOnly && empty($interests)) {
-			$interests = $interestsTextOnly;
-		} elseif (isset($interests) && !is_array($interests)) {
+	function insertInterests($userId, $interests = array()) {
+
+		if (isset($interests) && !is_array($interests)) {
 			$interests = array($interests);
+		} elseif ($interests == null) {
+			$interests = array();
 		}
 
 		$interestDao =& DAORegistry::getDAO('InterestDAO');

61d357e6740c152c3e83cfc07ebd6811d8e8d8b7
michael-pkp <michael.pkp@gmail.com>
2011-09-14 11:23:59 -0700

    *6818* Update copyright dates

--- classes/mail/MassMail.inc.php
+++ classes/mail/MassMail.inc.php
@@ -3,7 +3,7 @@
 /**
  * @file classes/mail/MassMail.inc.php
  *
- * Copyright (c) 2000-2010 John Willinsky
+ * Copyright (c) 2000-2011 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class MassMail

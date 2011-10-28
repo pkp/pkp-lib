856f80bd4f9d09e0cfc84ddaccb5abf334b1bac0
Alec Smecher <alec@smecher.bc.ca>
2011-10-28 10:01:55 -0700

    Hard-coded router names

--- tests/classes/core/DispatcherTest.inc.php
+++ tests/classes/core/DispatcherTest.inc.php
@@ -58,11 +58,12 @@ class DispatcherTest extends PKPTestCase {
 	public function testUrl() {
 		$baseUrl = $this->request->getBaseUrl();
 
-		$url = $this->dispatcher->url($this->request, 'page', array('context1', 'context2'), 'somepage', 'someop');
+		$url = $this->dispatcher->url($this->request, ROUTER_PAGE, array('context1', 'context2'), 'somepage', 'someop');
 		self::assertEquals($baseUrl.'/phpunit.php/context1/context2/somepage/someop', $url);
 
-		$url = $this->dispatcher->url($this->request, 'component', array('context1', 'context2'), 'some.ComponentHandler', 'someOp');
+		$url = $this->dispatcher->url($this->request, ROUTER_COMPONENT, array('context1', 'context2'), 'some.ComponentHandler', 'someOp');
 		self::assertEquals($baseUrl.'/phpunit.php/context1/context2/$$$call$$$/some/component/some-op', $url);
 	}
 }
+
 ?>

ed0d18f139f70c1ef6a29731e65acf4672ceabe0
Alec Smecher <alec@smecher.bc.ca>
2012-01-04 11:16:39 -0800

    *7006* Update copyright dates

--- classes/xml/XMLCustomWriter.inc.php
+++ classes/xml/XMLCustomWriter.inc.php
@@ -3,7 +3,7 @@
 /**
  * @file classes/xml/XMLCustomWriter.inc.php
  *
- * Copyright (c) 2000-2010 John Willinsky
+ * Copyright (c) 2000-2012 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class XMLCustomWriter

10927144e448e87f02f79d3a42977000025a099c
Alec Smecher <alec@smecher.bc.ca>
2012-03-12 11:27:08 -0700

    *7097* Captcha for mailing list

--- classes/notification/form/NotificationMailingListForm.inc.php
+++ classes/notification/form/NotificationMailingListForm.inc.php
@@ -20,13 +20,24 @@ import('lib.pkp.classes.form.Form');
 import('classes.notification.Notification');
 
 class NotificationMailingListForm extends Form {
+	
+	/** @var boolean Whether or not Captcha support is enabled */
+	var $captchaEnabled;
+	
 	/**
 	 * Constructor.
 	 */
 	function NotificationMailingListForm() {
 		parent::Form('notification/maillist.tpl');
 
+		import('lib.pkp.classes.captcha.CaptchaManager');
+		$captchaManager = new CaptchaManager();
+		$this->captchaEnabled = ($captchaManager->isEnabled() && Config::getVar('captcha', 'captcha_on_mailinglist'))?true:false;		
+
 		// Validation checks for this form
+		if ($this->captchaEnabled) {
+			$this->addCheck(new FormValidatorCaptcha($this, 'captcha', 'captchaId', 'common.captchaField.badCaptcha'));
+		}
 		$this->addCheck(new FormValidatorPost($this));
 		$this->addCheck(new FormValidatorEmail($this, 'email', 'required', 'notification.mailList.emailInvalid'));
 		$this->addCheck(new FormValidatorCustom($this, 'email', 'required', 'user.register.form.emailsDoNotMatch', create_function('$email,$form', 'return $email == $form->getData(\'confirmEmail\');'), array(&$this)));
@@ -36,7 +47,14 @@ class NotificationMailingListForm extends Form {
 	 * Assign form data to user-submitted data.
 	 */
 	function readInputData() {
-		$this->readUserVars(array('email', 'confirmEmail'));
+		$userVars = array('email', 'confirmEmail');
+		
+		if ($this->captchaEnabled) {
+			$userVars[] = 'captchaId';
+			$userVars[] = 'captcha';
+		}
+		
+		$this->readUserVars($userVars);
 	}
 
 	/**
@@ -45,6 +63,16 @@ class NotificationMailingListForm extends Form {
 	function display(&$request) {
 		$templateMgr =& TemplateManager::getManager();
 		$templateMgr->assign('new', true);
+		
+		if ($this->captchaEnabled) {
+			import('lib.pkp.classes.captcha.CaptchaManager');
+			$captchaManager = new CaptchaManager();
+			$captcha =& $captchaManager->createCaptcha();
+			if ($captcha) {
+				$templateMgr->assign('captchaEnabled', $this->captchaEnabled);
+				$this->setData('captchaId', $captcha->getId());
+			}
+		}
 
 		$context =& $request->getContext();
 		if ($context) {

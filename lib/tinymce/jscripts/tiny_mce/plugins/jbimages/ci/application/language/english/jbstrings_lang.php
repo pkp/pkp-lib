ce78dc7223426b24f0f0aafce93fc7f7e6655fbb
Alec Smecher <alec@smecher.bc.ca>
2012-03-13 14:27:55 -0700

    *7207* ibrowser/jbimages swap

---
+++ lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/language/english/jbstrings_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['jb_blankpage_message'] = "The Upload process had not started, or started, but had not finished yet, or your browser could not reach the remote server.";
+$lang['jb_delete'] = "Delete";
+$lang['jb_no_files'] = "You have not uploaded any files";
+$lang['jb_go_back'] = "Go back";
+$lang['jb_list_disabled'] = "This server has disabled file listing for uploaded images";
+$lang['jb_uploaded_images'] = "Uploaded Images";
+
+/* End of file jbstrings_lang.php */
+/* Location: ./application/language/english/jbstrings_lang.php */
---
+++ lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/views/ajax_upload_result.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>JustBoil's Result Page</title>
+<script language="javascript" type="text/javascript">
+	window.parent.window.jbImagesDialog.uploadFinish({
+		filename:'<?php echo html_escape($file_name); ?>',
+		result: '<?php echo html_escape($result); ?>',
+		resultCode: '<?php echo html_escape($resultcode); ?>'
+	});
+</script>
+</head>
+
+<body>
+
+Result: <?php echo html_escape($result); ?>
+
+</body>
+</html>

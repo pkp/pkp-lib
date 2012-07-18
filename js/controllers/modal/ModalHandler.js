b4ea890253f8e2df3e9d12fc440cf324d353babf
Jason Nugent <jason.nugent@gmail.com>
2012-07-18 10:57:16 -0300

    *7660* style modal corner close icon as per Evoke spec

--- js/controllers/modal/ModalHandler.js
+++ js/controllers/modal/ModalHandler.js
@@ -231,7 +231,7 @@ jQuery.pkp.controllers.modal = jQuery.pkp.controllers.modal || { };
 		var canClose = options.canClose || '1';
 		if (canClose) {
 			var $closeButton = $(['<a class="close ui-corner-all" href="#">',
-				'<span class="ui-icon ui-icon-closethick">',
+				'<span class="closeIcon">',
 				'close</span></a>"'].join(''));
 			$closeButton.click(this.callbackWrapper(this.modalClose));
 			$titleBar.append($closeButton);

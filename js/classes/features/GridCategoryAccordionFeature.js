6c4d3767259020d38060d765b7e85ef03cd512d0
Jason Nugent <jason.nugent@gmail.com>
2012-07-26 15:29:06 -0300

    *7719* hide row actions when collapsing all rows

--- js/classes/features/GridCategoryAccordionFeature.js
+++ js/classes/features/GridCategoryAccordionFeature.js
@@ -121,6 +121,7 @@
 		$('.category .' + this.getCollapseClass(),
 				this.getGridHtmlElement()).click();
 
+		$('.row_controls :visible').closest('tr').prev().find('.row_actions > a:first').click();
 		return false;
 	};
 

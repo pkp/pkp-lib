0a8f055680fce8ce164c311c4ae621acbd3a6c11
Jason Nugent <jason.nugent@gmail.com>
2012-10-30 13:21:50 -0300

    Introduce PKPSubmissionHandler for ONIX codelist autocomplete support

---
+++ pages/submission/PKPSubmissionHandler.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file pages/submission/PKPSubmissionHandler.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPSubmissionHandler
+ * @ingroup pages_submission
+ *
+ * @brief Base handler for submission requests.
+ */
+
+import('classes.handler.Handler');
+import('lib.pkp.classes.core.JSONMessage');
+
+class PKPSubmissionHandler extends Handler {
+	/**
+	 * Constructor
+	 */
+	function PKPSubmissionHandler() {
+		parent::Handler();
+	}
+
+
+	//
+	// Public Handler Methods
+	//
+	/**
+	 * Retrieves a JSON list of available choices for a tagit metadata input field.
+	 * @param $args array
+	 * @param $request Request
+	 */
+	function fetchChoices($args, &$request) {
+		$codeList = (int) $request->getUserVar('codeList');
+		$term =& $request->getUserVar('term');
+
+		$onixCodelistItemDao =& DAORegistry::getDAO('ONIXCodelistItemDAO');
+		$codes =& $onixCodelistItemDao->getCodes('List' . $codeList, array(), $term); // $term is escaped in the getCodes method.
+		import('lib.pkp.classes.core.JSONManager');
+		$jsonManager = new JSONManager();
+		header('Content-Type: text/json');
+		echo $jsonManager->encode(array_values($codes));
+	}
+
+	//
+	// Protected helper methods
+	//
+	/**
+	 * Setup common template variables.
+	 * @param $request Request
+	 */
+	function setupTemplate(&$request) {
+		parent::setupTemplate();
+	}
+}
+?>
---
+++ pages/submission/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @defgroup pages_submission
+ */
+
+/**
+ * @file pages/submission/index.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup pages_submission
+ * @brief Handle requests for the submission wizard.
+ *
+ */
+
+switch ($op) {
+	//
+	// PKP Submission
+	//
+	case 'fetchChoices':
+		import('lib.pkp.pages.submission.PKPSubmissionHandler');
+		define('HANDLER_CLASS', 'PKPSubmissionHandler');
+		break;
+}
+
+?>

ba2f28fa7177bbbe738abd61aa85385ff9da1f0c
Alec Smecher <alec@smecher.bc.ca>
2012-11-21 17:14:22 -0800

    *8030* Move language admin to PKP lib

---
+++ controllers/grid/languages/form/InstallLanguageForm.inc.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file controllers/grid/languages/form/InstallLanguageForm.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class InstallLanguageForm
+ * @ingroup controllers_grid_languages_form
+ *
+ * @brief Form for installing languages.
+ */
+
+// Import the base Form.
+import('lib.pkp.classes.form.Form');
+
+class InstallLanguageForm extends Form {
+
+	/**
+	 * Constructor.
+	 */
+	function InstallLanguageForm($wizardMode = false) {
+		parent::Form('controllers/grid/languages/installLanguageForm.tpl');
+	}
+
+	//
+	// Overridden methods from Form.
+	//
+	/**
+	 * @see Form::initData()
+	 */
+	function initData(&$request) {
+		parent::initData($request);
+
+		$site =& $request->getSite();
+		$this->setData('installedLocales', $site->getInstalledLocales());
+	}
+
+	/**
+	 * @see Form::fetch()
+	 */
+	function fetch(&$request) {
+		$site =& $request->getSite();
+		$allLocales = AppLocale::getAllLocales();
+		$installedLocales = $this->getData('installedLocales');
+		$notInstalledLocales = array_diff(array_keys($allLocales), $installedLocales);
+
+		$templateMgr =& TemplateManager::getManager($request);
+		$templateMgr->assign('allLocales', $allLocales);
+		$templateMgr->assign('notInstalledLocales', $notInstalledLocales);
+
+		import('classes.i18n.LanguageAction');
+		$languageAction = new LanguageAction();
+		if ($languageAction->isDownloadAvailable()) {
+			$downloadableLocales = $languageAction->getDownloadableLocales();
+			$downloadableLocaleLinks = array();
+
+			import('lib.pkp.classes.linkAction.request.AjaxAction');
+			$router =& $request->getRouter();
+			foreach ($downloadableLocales as $locale => $name) {
+				$downloadableLocaleLinks[$locale] = new LinkAction($locale,
+					new AjaxAction($router->url($request, null, null, 'downloadLocale', array('locale' => $locale))),
+					$name . ' (' . $locale . ')');
+			}
+
+			$templateMgr->assign('downloadAvailable', true);
+			$templateMgr->assign('downloadableLocaleLinks', $downloadableLocaleLinks);
+		}
+
+		return parent::fetch($request);
+	}
+
+	/**
+	 * @see Form::readInputData()
+	 */
+	function readInputData(&$request) {
+		parent::readInputData($request);
+
+		$localesToInstall = $request->getUserVar('localesToInstall');
+		$this->setData('localesToInstall', $localesToInstall);
+	}
+
+	/**
+	 * @see Form::execute()
+	 */
+	function execute(&$request) {
+		$site =& $request->getSite();
+		$localesToInstall = $this->getData('localesToInstall');
+
+		if (isset($localesToInstall) && is_array($localesToInstall)) {
+			$installedLocales = $site->getInstalledLocales();
+			$supportedLocales = $site->getSupportedLocales();
+
+			foreach ($localesToInstall as $locale) {
+				if (AppLocale::isLocaleValid($locale) && !in_array($locale, $installedLocales)) {
+					array_push($installedLocales, $locale);
+					// Activate/support by default.
+					if (!in_array($locale, $supportedLocales)) array_push($supportedLocales, $locale);
+					AppLocale::installLocale($locale);
+				}
+			}
+
+			$site->setInstalledLocales($installedLocales);
+			$site->setSupportedLocales($supportedLocales);
+			$siteDao =& DAORegistry::getDAO('SiteDAO');
+			$siteDao->updateObject($site);
+		}
+	}
+}
+
+?>
---
+++ templates/controllers/grid/languages/installLanguageForm.tpl
@@ -0,0 +1,49 @@
+{**
+ * controllers/grid/languages/installLanguageForm.tpl
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Form to install languages.
+ *}
+
+ <script type="text/javascript">
+	$(function() {ldelim}
+		// Attach the form handler.
+		$('#installLanguageForm').pkpHandler('$.pkp.controllers.form.AjaxFormHandler');
+	{rdelim});
+</script>
+
+<form class="pkp_form" id="installLanguageForm" method="post" action="{url router=$smarty.const.ROUTE_COMPONENT component="grid.admin.languages.AdminLanguageGridHandler" op="saveInstallLocale"}">
+	{include file="controllers/notification/inPlaceNotification.tpl" notificationId="installLanguageFormNotification"}
+
+	{fbvFormArea id="availableLocalesFormArea" title="admin.languages.availableLocales" class="border"}
+		{fbvFormSection list="true" description="admin.languages.installNewLocalesInstructions"}
+			{foreach name=locales from=$notInstalledLocales item=locale}
+				{fbvElement type="checkbox" id="locale-$locale" name="localesToInstall[$locale]" value=$locale label=$allLocales.$locale translate=false}
+			{foreachelse}
+				<p>{translate key="admin.languages.noLocalesAvailable"}</p>
+			{/foreach}
+		{/fbvFormSection}
+	{/fbvFormArea}
+
+	{fbvFormArea id="downloadLocaleFormArea" title="admin.languages.downloadLocales" class="border"}
+		{fbvFormSection list="true"}
+			{if $downloadAvailable}
+				<ul>
+				{foreach name=downloadableLocaleLinks from=$downloadableLocaleLinks item=localeLink}
+					<li>{include file="linkAction/linkAction.tpl" action=$localeLink}</li>
+				{foreachelse}
+					<li><p>{translate key="admin.languages.noLocalesToDownload"}</p></li>
+				{/foreach}
+				</ul>
+			{else}
+				<p>{translate key="admin.languages.downloadUnavailable"}</p>
+			{/if}
+		{/fbvFormSection}
+	{/fbvFormArea}
+
+	{if not empty($notInstalledLocales)}
+		{fbvFormButtons id="mastheadFormSubmit" submitText="common.save"}
+	{/if}
+</form>

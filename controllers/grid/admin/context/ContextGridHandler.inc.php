2c92c28a3db7b4f843f07a406fdaad519f1dfdcc
Alec Smecher <alec@smecher.bc.ca>
2012-11-29 11:55:49 -0800

    *8046* Partial port of context grid to PKP lib

---
+++ controllers/grid/admin/context/ContextGridHandler.inc.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * @file controllers/grid/admin/context/ContextGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ContextGridHandler
+ * @ingroup controllers_grid_admin_context
+ *
+ * @brief Handle context grid requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.GridHandler');
+
+class ContextGridHandler extends GridHandler {
+	/**
+	 * Constructor
+	 */
+	function ContextGridHandler() {
+		parent::GridHandler();
+		$this->addRoleAssignment(array(
+			ROLE_ID_SITE_ADMIN),
+			array('fetchGrid', 'fetchRow', 'createContext', 'editContext', 'updateContext',
+				'deleteContext', 'saveSequence')
+		);
+	}
+
+
+	//
+	// Implement template methods from PKPHandler.
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 */
+	function authorize(&$request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.PolicySet');
+		$rolePolicy = new PolicySet(COMBINING_PERMIT_OVERRIDES);
+
+		import('lib.pkp.classes.security.authorization.RoleBasedHandlerOperationPolicy');
+		foreach($roleAssignments as $role => $operations) {
+			$rolePolicy->addPolicy(new RoleBasedHandlerOperationPolicy($request, $role, $operations));
+		}
+		$this->addPolicy($rolePolicy);
+
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * @see PKPHandler::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+
+		// Load user-related translations.
+		AppLocale::requireComponents(
+			LOCALE_COMPONENT_PKP_USER
+		);
+
+		// Grid actions.
+		$router =& $request->getRouter();
+
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+		$this->addAction(
+			new LinkAction(
+				'createContext',
+				new AjaxModal(
+					$router->url($request, null, null, 'createContext', null, null),
+					__($this->_getAddContextKey()),
+					'modal_add_item',
+					true
+					),
+				__($this->_getAddContextKey()),
+				'add_item')
+		);
+
+		//
+		// Grid columns.
+		//
+		import('lib.pkp.controllers.grid.admin.context.ContextGridCellProvider');
+		$contextGridCellProvider = new ContextGridCellProvider();
+
+		// Context name.
+		$this->addColumn(
+			new GridColumn(
+				'name',
+				$this->_getContextNameKey(),
+				null,
+				'controllers/grid/gridCell.tpl',
+				$contextGridCellProvider
+			)
+		);
+
+		// Context path.
+		$this->addColumn(
+			new GridColumn(
+				'path',
+				'context.path',
+				null,
+				'controllers/grid/gridCell.tpl',
+				$contextGridCellProvider
+			)
+		);
+	}
+
+
+	//
+	// Implement methods from GridHandler.
+	//
+	/**
+	 * @see lib/pkp/classes/controllers/grid/GridHandler::getDataElementSequence()
+	 */
+	function getDataElementSequence($context) {
+		return $context->getSequence();
+	}
+
+	/**
+	 * @see GridHandler::addFeatures()
+	 */
+	function initFeatures($request, $args) {
+		import('lib.pkp.classes.controllers.grid.feature.OrderGridItemsFeature');
+		return array(new OrderGridItemsFeature());
+	}
+
+	/**
+	 * Get the list of "publish data changed" events.
+	 * Used to update the site context switcher upon create/delete.
+	 * @return array
+	 */
+	function getPublishChangeEvents() {
+		return array('updateHeader');
+	}
+
+
+	//
+	// Public grid actions.
+	//
+	/**
+	 * Add a new context.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 */
+	function createContext($args, &$request) {
+		// Calling editContext with an empty row id will add a new context.
+		return $this->editContext($args, $request);
+	}
+
+
+	//
+	// Protected helper methods.
+	//
+	/**
+	 * Get the "add context" locale key
+	 * @return string
+	 */
+	protected function _getAddContextKey() {
+		assert(false); // Should be overridden by subclasses
+	}
+
+	/**
+	 * Get the context name locale key
+	 * @return string
+	 */
+	protected function _getContextNameKey() {
+		assert(false); // Should be overridden by subclasses
+	}
+}
+
+?>
---
+++ controllers/grid/admin/context/ContextGridRow.inc.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @file controllers/grid/admin/context/ContextGridRow.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ContextGridRow
+ * @ingroup controllers_grid_admin_context
+ *
+ * @brief Context grid row definition
+ */
+
+import('lib.pkp.classes.controllers.grid.GridRow');
+import('lib.pkp.classes.linkAction.request.RemoteActionConfirmationModal');
+
+class ContextGridRow extends GridRow {
+	/**
+	 * Constructor
+	 */
+	function ContextGridRow() {
+		parent::GridRow();
+	}
+
+
+	//
+	// Overridden methods from GridRow
+	//
+	/**
+	 * @see GridRow::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+
+		// Is this a new row or an existing row?
+		$element =& $this->getData();
+		assert(is_a($element, 'Context'));
+
+		$rowId = $this->getId();
+
+		if (!empty($rowId) && is_numeric($rowId)) {
+			// Only add row actions if this is an existing row
+			$router =& $request->getRouter();
+			$actionArgs = array(
+				'gridId' => $this->getGridId(),
+				'rowId' => $rowId
+			);
+			$this->addAction(
+				new LinkAction(
+					'edit',
+					new AjaxModal(
+						$router->url($request, null, null, 'editContext', null, $actionArgs),
+						__('grid.action.edit'),
+						'modal_edit',
+						true
+						),
+					__('grid.action.edit'),
+					'edit')
+			);
+			$this->addAction(
+				new LinkAction(
+					'delete',
+					new RemoteActionConfirmationModal(
+						__($this->getConfirmDeleteKey()),
+						null,
+						$router->url($request, null, null, 'deleteContext', null, $actionArgs)
+						),
+					__('grid.action.remove'),
+					'delete')
+			);
+		}
+	}
+
+	/**
+	 * Get the delete context row locale key.
+	 * @return string
+	 */
+	function getConfirmDeleteKey() {
+		assert(false); // Should be overridden by subclasses.
+	}
+}
+
+?>

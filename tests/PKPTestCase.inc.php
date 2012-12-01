bcec3d5c1f15de9e42de71b896fc39849d9efdb6
jerico.dev <jerico.dev@gmail.com>
2012-12-01 13:13:32 -0200

    [OJS.de lucene - part III - win7] Upgrade tests to PHPUnit 3.7 compatibility.

--- tests/PKPTestCase.inc.php
+++ tests/PKPTestCase.inc.php
@@ -19,12 +19,9 @@
  */
 
 // Include PHPUnit
-require_once('PHPUnit/Extensions/OutputTestCase.php');
 import('lib.pkp.tests.PKPTestHelper');
 
-// FIXME: PHPUnit_Extensions_OutputTestCase is now deprecated but we have to
-// use it until we upgrade our test server from phpunit 3.4 to at least 3.6.
-abstract class PKPTestCase extends PHPUnit_Extensions_OutputTestCase {
+abstract class PKPTestCase extends PHPUnit_Framework_TestCase {
 	private $daoBackup = array(), $registryBackup = array();
 
 	/**

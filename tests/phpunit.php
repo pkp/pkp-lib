99b9e95b02f83ac841c23e33153ee7e5348ac2c7
jerico.dev <jerico.dev@gmail.com>
2012-12-01 13:13:32 -0200

    [OJS.de lucene - part III - win7] Upgrade tests to PHPUnit 3.7 compatibility.

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -6,6 +6,9 @@
  * Integrates PHPUnit with the PKP application environment
  * and enables running/debugging tests from within Eclipse or
  * other CASE tools.
+ *
+ * Requires PEAR to be on the path (e.g. installed as a user
+ * library in Eclipse).
  */
 
 // This script may not be executed remotely.
@@ -17,11 +20,8 @@ if (extension_loaded('xdebug')) {
     xdebug_disable();
 }
 
-if (strpos('/usr/bin/php', '@php_bin') === 0) {
-    set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
-}
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
 
-require 'PHPUnit/Autoload.php';
+require 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
 
 PHPUnit_TextUI_Command::main();
-?>

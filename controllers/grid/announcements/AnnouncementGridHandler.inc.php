87e18d83810f91b7492a6541a4a17e4c4d2e715d
Alec Smecher <alec@smecher.bc.ca>
2012-12-03 16:20:03 -0800

    *8051* Move announcements grids to PKP lib

---
+++ controllers/grid/announcements/AnnouncementGridHandler.inc.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * @file controllers/grid/announcements/AnnouncementGridHandler.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AnnouncementGridHandler
+ * @ingroup controllers_grid_announcements
+ *
+ * @brief Handle announcements grid requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.GridHandler');
+import('lib.pkp.classes.controllers.grid.DataObjectGridCellProvider');
+
+class AnnouncementGridHandler extends GridHandler {
+	/**
+	 * Constructor
+	 */
+	function AnnouncementGridHandler() {
+		parent::GridHandler();
+	}
+
+
+	//
+	// Overridden template methods
+	//
+	/**
+	 * @see GridHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 * @param $requireAnnouncementsEnabled Iff true, allow access only if context settings enable announcements
+	 */
+	function authorize($request, &$args, $roleAssignments, $requireAnnouncementsEnabled = true) {
+
+		import('lib.pkp.classes.security.authorization.ContextRequiredPolicy');
+		$this->addPolicy(new ContextRequiredPolicy($request));
+
+		$returner = parent::authorize($request, $args, $roleAssignments);
+
+		// Ensure announcements are enabled.
+		$context =& $request->getContext();
+		if ($requireAnnouncementsEnabled && !$context->getSetting('enableAnnouncements')) {
+			return false;
+		}
+
+		$announcementId = $request->getUserVar('announcementId');
+		if ($announcementId) {
+			// Ensure announcement is valid and for this context
+			$announcementDao =& DAORegistry::getDAO('AnnouncementDAO'); /* @var $announcementDao AnnouncementDAO */
+			if ($announcementDao->getAnnouncementAssocType($announcementId) != $context->getAssocType() &&
+				$announcementDao->getAnnouncementAssocId($announcementId) != $context->getId()) {
+				return false;
+			}
+		}
+
+		return $returner;
+	}
+
+	/**
+	 * @see GridHandler::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+
+		// Set the no items row text
+		$this->setEmptyRowText('announcement.noneExist');
+
+		$context =& $request->getContext();
+
+		// Columns
+		import('controllers.grid.content.announcements.AnnouncementGridCellProvider');
+		$announcementCellProvider = new AnnouncementGridCellProvider();
+		$this->addColumn(
+			new GridColumn('title',
+				'common.title',
+				null,
+				'controllers/grid/gridCell.tpl',
+				$announcementCellProvider,
+				array('width' => 60)
+			)
+		);
+
+		$this->addColumn(
+			new GridColumn('type',
+				'common.type',
+				null,
+				'controllers/grid/gridCell.tpl',
+				$announcementCellProvider
+			)
+		);
+
+		$cellProvider = new DataObjectGridCellProvider();
+		$this->addColumn(
+			new GridColumn(
+				'datePosted',
+				'announcement.posted',
+				null,
+				'controllers/grid/gridCell.tpl',
+				$cellProvider
+			)
+		);
+	}
+
+	/**
+	 * @see GridHandler::loadData()
+	 */
+	function loadData($request, $filter) {
+		$context = $request->getContext();
+		$announcementDao =& DAORegistry::getDAO('AnnouncementDAO');
+		$rangeInfo = $this->getGridRangeInfo($request, $this->getId());
+		$announcements = $announcementDao->getAnnouncementsNotExpiredByAssocId($context->getAssocType(), $context->getId(), $rangeInfo);
+
+		return $announcements;
+	}
+
+
+	//
+	// Public grid actions.
+	//
+	/**
+	 * Load and fetch the announcement form in read-only mode.
+	 * @param $args array
+	 * @param $request Request
+	 * @return string
+	 */
+	function moreInformation($args, &$request) {
+		$announcementId = (int)$request->getUserVar('announcementId');
+		$context =& $request->getContext();
+		$contextId = $context->getId();
+
+		import('controllers.grid.content.announcements.form.AnnouncementForm');
+		$announcementForm = new AnnouncementForm($contextId, $announcementId, true);
+
+		$announcementForm->initData($args, $request);
+
+		$json = new JSONMessage(true, $announcementForm->fetch($request));
+		return $json->getString();
+	}
+}
+
+?>

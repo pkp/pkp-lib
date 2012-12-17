cf9a24001aa5dfea49670e3de7323170540bbb59
Alec Smecher <alec@smecher.bc.ca>
2012-12-17 12:41:25 -0800

    *8062* Added missing constructor

--- classes/file/PKPPublicFileManager.inc.php
+++ classes/file/PKPPublicFileManager.inc.php
@@ -18,6 +18,12 @@
 import('lib.pkp.classes.file.FileManager');
 
 class PKPPublicFileManager extends FileManager {
+	/**
+	 * Constructor
+	 */
+	function PKPPublicFileManager() {
+		parent::FileManager();
+	}
 
 	/**
 	 * Get the path to the site public files directory.

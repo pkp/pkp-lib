6d08037f0de687ec9334ca0b04755b60098257f2
Jason Nugent <jason.nugent@gmail.com>
2013-01-08 08:40:17 -0400

    *8063* prevent datepickers from becoming orphaned on modal close

--- js/controllers/modal/ModalHandler.js
+++ js/controllers/modal/ModalHandler.js
@@ -183,6 +183,8 @@
 		// unregister this form with SiteHandler.
 
 		if ($form.length == 1) {
+			// prevent orphaned date pickers that may be still open.
+			$form.find('.hasDatepicker').datepicker('hide');
 			handler = $.pkp.classes.Handler.getHandler($('#' + $form.attr('id')));
 			if (handler.formChangesTracked) {
 				if (!confirm($.pkp.locale.form_dataHasChanged)) {

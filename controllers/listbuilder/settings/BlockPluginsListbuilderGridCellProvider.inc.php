d7b27cf1357d88d0825b1ba4deef8850fca4c6aa
Alec Smecher <alec@smecher.bc.ca>
2013-01-30 13:16:52 -0800

    *8094* Move block controls into pkp-lib

---
+++ controllers/listbuilder/settings/BlockPluginsListbuilderGridCellProvider.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file classes/controllers/listbuilder/settings/BlockPluginsListbuilderGridCellProvider.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class BlockPluginsListbuilderGridCellProvider
+ * @ingroup controllers_listbuilder_settings
+ *
+ * @brief Block plugins listbuilder cell provider.
+ */
+
+import('lib.pkp.classes.controllers.grid.GridCellProvider');
+
+class BlockPluginsListbuilderGridCellProvider extends GridCellProvider {
+	/**
+	 * Constructor
+	 */
+	function BlockPluginsListbuilderGridCellProvider() {
+		parent::GridCellProvider();
+	}
+
+	//
+	// Template methods from GridCellProvider
+	//
+	/**
+	 * This implementation assumes a simple data element array that
+	 * has column ids as keys.
+	 * @see GridCellProvider::getTemplateVarsFromRowColumn()
+	 * @param $row GridRow
+	 * @param $column GridColumn
+	 * @return array
+	 */
+	function getTemplateVarsFromRowColumn(&$row, $column) {
+		$plugin =& $row->getData();
+		$columnId = $column->getId();
+		assert((is_a($plugin, 'Plugin')) && !empty($columnId));
+
+		return array('label' => $plugin->getDisplayName());
+	}
+}
+
+?>
---
+++ controllers/listbuilder/settings/BlockPluginsListbuilderHandler.inc.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @file controllers/listbuilder/settings/BlockPluginsListbuilderHandler.inc.php
+ *
+ * Copyright (c) 2000-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class BlockPluginsListbuilderHandler
+ * @ingroup controllers_listbuilder_settings
+ *
+ * @brief Class for block plugins administration.
+ */
+
+import('lib.pkp.classes.controllers.listbuilder.MultipleListsListbuilderHandler');
+
+class BlockPluginsListbuilderHandler extends MultipleListsListbuilderHandler {
+	/**
+	 * Constructor
+	 */
+	function BlockPluginsListbuilderHandler() {
+		parent::MultipleListsListbuilderHandler();
+		$this->addRoleAssignment(
+			ROLE_ID_MANAGER,
+			array('fetch')
+		);
+	}
+
+	/**
+	 * @see GridHandler::authorize($request, $args, $roleAssignments)
+	 */
+	function authorize(&$request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.PkpContextAccessPolicy');
+		$this->addPolicy(new PkpContextAccessPolicy($request, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * @see ListbuilderHandler::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+		AppLocale::requireComponents(LOCALE_COMPONENT_PKP_MANAGER);
+
+		// Basic configuration
+		$this->setTitle('manager.setup.layout.blockManagement');
+		$this->setSaveFieldName('blocks');
+
+		// Name column
+		$nameColumn = new ListbuilderGridColumn($this, 'name', 'common.name');
+
+		// Add lists.
+		$this->addList(new ListbuilderList('leftContext', 'manager.setup.layout.leftSidebar'));
+		$this->addList(new ListbuilderList('unselected', 'manager.setup.layout.unselected'));
+		$this->addList(new ListbuilderList('rightContext', 'manager.setup.layout.rightSidebar'));
+
+		import('lib.pkp.controllers.listbuilder.settings.BlockPluginsListbuilderGridCellProvider');
+		$nameColumn->setCellProvider(new BlockPluginsListbuilderGridCellProvider());
+		$this->addColumn($nameColumn);
+	}
+
+
+	//
+	// Overridden template methods
+	//
+	/**
+	 * @see MultipleListsListbuilderHandler::setListsData()
+	 */
+	function setListsData($request, $filter) {
+		$leftBlockPlugins = $disabledBlockPlugins = $rightBlockPlugins = array();
+		$plugins =& PluginRegistry::loadCategory('blocks');
+		foreach ($plugins as $key => $junk) {
+			if (!$plugins[$key]->getEnabled() || $plugins[$key]->getBlockContext() == '') {
+				if (count(array_intersect($plugins[$key]->getSupportedContexts(), array(BLOCK_CONTEXT_LEFT_SIDEBAR, BLOCK_CONTEXT_RIGHT_SIDEBAR))) > 0) $disabledBlockPlugins[$key] =& $plugins[$key];
+			} else switch ($plugins[$key]->getBlockContext()) {
+				case BLOCK_CONTEXT_LEFT_SIDEBAR:
+					$leftBlockPlugins[$key] =& $plugins[$key];
+					break;
+				case BLOCK_CONTEXT_RIGHT_SIDEBAR:
+					$rightBlockPlugins[$key] =& $plugins[$key];
+					break;
+			}
+		}
+
+		$lists =& $this->getLists();
+		$lists['leftContext']->setData($leftBlockPlugins);
+		$lists['rightContext']->setData($rightBlockPlugins);
+		$lists['unselected']->setData($disabledBlockPlugins);
+	}
+}
+
+?>

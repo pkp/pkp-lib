db3abeccb64fb607efcc8d4bdc884a436f650f44
Alec Smecher <alec@smecher.bc.ca>
2013-01-31 09:23:01 -0800

    *8094* Move sponsors to PKP lib

---
+++ controllers/grid/settings/sponsor/SponsorGridRow.inc.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @file controllers/grid/settings/sponsor/SponsorGridRow.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SponsorGridRow
+ * @ingroup controllers_grid_settings_sponsor
+ *
+ * @brief Handle sponsor grid row requests.
+ */
+
+import('lib.pkp.classes.controllers.grid.GridRow');
+
+class SponsorGridRow extends GridRow {
+	/**
+	 * Constructor
+	 */
+	function SponsorGridRow() {
+		parent::GridRow();
+	}
+
+	//
+	// Overridden template methods
+	//
+	/*
+	 * Configure the grid row
+	 * @param $request PKPRequest
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+		// add Grid Row Actions
+
+		// Is this a new row or an existing row?
+		$rowId = $this->getId();
+		if (!empty($rowId)) {
+			// Actions
+			$router = $request->getRouter();
+			$actionArgs = array(
+				'gridId' => $this->getGridId(),
+				'rowId' => $rowId
+			);
+			$this->addAction(
+				new LinkAction(
+					'editSponsor',
+					new AjaxModal(
+						$router->url($request, null, null, 'editSponsor', null, $actionArgs),
+						__('grid.action.edit'),
+						'modal_edit',
+						true
+						),
+					__('grid.action.edit'),
+					'edit')
+			);
+
+			import('lib.pkp.classes.linkAction.request.RemoteActionConfirmationModal');
+
+			$this->addAction(
+				new LinkAction(
+					'deleteSponsor',
+					new RemoteActionConfirmationModal(
+						__('common.confirmDelete'),
+						__('common.delete'),
+						$router->url($request, null, null, 'deleteSponsor', null, $actionArgs),
+						'modal_delete'
+					),
+					__('grid.action.delete'),
+					'delete')
+			);
+		}
+	}
+}
+
+?>
---
+++ templates/controllers/grid/settings/sponsor/form/sponsorForm.tpl
@@ -0,0 +1,39 @@
+
+{**
+ * templates/controllers/grid/settings/sponsor/form/sponsorForm.tpl
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Sponsors grid form
+ *}
+ <script type="text/javascript">
+	$(function() {ldelim}
+		// Attach the form handler.
+		$('#editSponsorForm').pkpHandler('$.pkp.controllers.form.AjaxFormHandler');
+	{rdelim});
+</script>
+
+<form class="pkp_form" id="editSponsorForm" method="post" action="{url router=$smarty.const.ROUTE_COMPONENT component="grid.settings.sponsor.SponsorGridHandler" op="updateSponsor"}">
+{include file="controllers/notification/inPlaceNotification.tpl" notificationId="sponsorFormNotification"}
+
+{fbvFormArea id="mastheadInfo"}
+{fbvFormSection}
+	{fbvElement type="text" label="manager.setup.institution" id="institution" value=$institution|escape maxlength="90" required=true}
+{/fbvFormSection}
+{fbvFormSection}
+	{fbvElement type="text" label="common.url" id="url" value=$url|escape maxlength="255" required=true}
+{/fbvFormSection}
+{/fbvFormArea}
+
+{if $gridId}
+	<input type="hidden" name="gridId" value="{$gridId|escape}" />
+{/if}
+{if $rowId}
+	<input type="hidden" name="rowId" value="{$rowId|escape}" />
+{/if}
+{if $sponsorId}
+	<input type="hidden" name="sponsorId" value="{$sponsorId|escape}" />
+{/if}
+{fbvFormButtons}
+</form>

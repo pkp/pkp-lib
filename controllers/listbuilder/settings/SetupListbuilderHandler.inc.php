9c09421f2acf80f43fc4969cf62847c9b9d2aace
Alec Smecher <alec@smecher.bc.ca>
2013-02-13 11:20:52 -0800

    *8094* Move SetupListbuilderHandler to PKP lib

---
+++ controllers/listbuilder/settings/SetupListbuilderHandler.inc.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file controllers/listbuilder/settings/SetupListbuilderHandler.inc.php
+ *
+ * Copyright (c) 2003-2012 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SetupListbuilderHandler
+ * @ingroup listbuilder
+ *
+ * @brief Base class for setup listbuilders
+ */
+
+import('lib.pkp.classes.controllers.listbuilder.ListbuilderHandler');
+
+class SetupListbuilderHandler extends ListbuilderHandler {
+	/** @var $_context Context */
+	var $_context;
+
+	/**
+	 * Constructor
+	 */
+	function SetupListbuilderHandler() {
+		parent::ListbuilderHandler();
+		$this->addRoleAssignment(
+			ROLE_ID_MANAGER,
+			array('fetch', 'fetchRow', 'save')
+		);
+	}
+
+	/**
+	 * Set the current context
+	 * @param $context Context
+	 */
+	function setContext($context) {
+		$this->_context = $context;
+	}
+
+	/**
+	 * Get the current context
+	 * @return Context
+	 */
+	function getContext() {
+		return $this->_context;
+	}
+
+	/**
+	 * @see PKPHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 */
+	function authorize(&$request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.PkpContextAccessPolicy');
+		$this->addPolicy(new PkpContextAccessPolicy($request, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * @see ListbuilderHandler::initialize
+	 */
+	function initialize(&$request) {
+		$this->setContext($request->getContext());
+		return parent::initialize($request);
+	}
+}
+
+?>

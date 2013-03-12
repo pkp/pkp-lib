96c6ccc0fcf6d5ce8d742b65291d01f26c1f51c2
Jason Nugent <jason.nugent@gmail.com>
2013-03-12 15:42:20 -0300

    *8148* introduce PKP file notes link action

---
+++ controllers/informationCenter/linkAction/PKPFileNotesLinkAction.inc.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @file controllers/informationCenter/linkAction/PKPFileNotesLinkAction.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPFileNotesLinkAction
+ * @ingroup controllers_informationCenter_linkAction
+ *
+ * @brief An action to open up the notes IC for a file.
+ */
+
+import('lib.pkp.controllers.api.file.linkAction.FileLinkAction');
+
+class PKPFileNotesLinkAction extends FileLinkAction {
+
+	/**
+	 * Constructor
+	 * @param $request Request
+	 * @param $submissionFile SubmissionFile the submission file
+	 *  to show information about.
+	 * @param $user User
+	 * @param $stageId int (optional) The stage id that user is looking at.
+	 * @param $removeHistoryTab boolean (optional) Open the information center
+	 * without the history tab.
+	 */
+	function PKPFileNotesLinkAction(&$request, &$submissionFile, $user, $stageId = null, $removeHistoryTab = false) {
+		// Instantiate the information center modal.
+		$ajaxModal = $this->getModal($request, $submissionFile, $stageId, $removeHistoryTab);
+
+		// Configure the file link action.
+		parent::FileLinkAction(
+			'moreInformation', $ajaxModal,
+			'', $this->getNotesState($submissionFile, $user),
+			__('common.notes.tooltip')
+		);
+	}
+
+	function getNotesState($submissionFile, $user) {
+		$noteDao =& DAORegistry::getDAO('NoteDAO');
+
+		// If no notes exist, display a dimmed icon.
+		if (!$noteDao->notesExistByAssoc(ASSOC_TYPE_SUBMISSION_FILE, $submissionFile->getFileId())) {
+			return 'notes_none';
+		}
+
+		// If new notes exist, display a bold icon.
+		if ($noteDao->unreadNotesExistByAssoc(ASSOC_TYPE_SUBMISSION_FILE, $submissionFile->getFileId(), $user->getId())) {
+			return 'notes_new';
+		}
+
+		// Otherwise, notes exist but not new ones.
+		return 'notes';
+	}
+
+	/**
+	 * returns the modal for this link action.
+	 * Must be overridden in subclasses.
+	 * @param $request PKPRequest
+	 * @param $submissionFile SubmissionFile
+	 * @param $stageId int
+	 * @param $removeHistoryTab boolean
+	 * @return AjaxModal
+	 */
+	function getModal($request, $submissionFile, $stageId, $removeHistoryTab) {
+		assert(false);
+	}
+}
+
+?>

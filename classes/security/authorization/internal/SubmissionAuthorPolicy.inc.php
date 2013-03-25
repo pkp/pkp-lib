8ba6b2c406e66410c6ef93daa59b857afd6bf472
Jason Nugent <jason.nugent@gmail.com>
2013-03-25 20:20:40 -0300

    *8148* access policies to PKP-lib

---
+++ classes/security/authorization/internal/SubmissionAuthorPolicy.inc.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @file classes/security/authorization/internal/SubmissionAuthorPolicy.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionAuthorPolicy
+ * @ingroup security_authorization_internal
+ *
+ * @brief Class to control access to a submission based on authorship.
+ *
+ * NB: This policy expects a previously authorized submission in the
+ * authorization context.
+ */
+
+import('lib.pkp.classes.security.authorization.AuthorizationPolicy');
+
+class SubmissionAuthorPolicy extends AuthorizationPolicy {
+	/** @var PKPRequest */
+	var $_request;
+
+	/**
+	 * Constructor
+	 * @param $request PKPRequest
+	 */
+	function SubmissionAuthorPolicy(&$request) {
+		parent::AuthorizationPolicy('user.authorization.submissionAuthor');
+		$this->_request =& $request;
+	}
+
+	//
+	// Implement template methods from AuthorizationPolicy
+	//
+	/**
+	 * @see AuthorizationPolicy::effect()
+	 */
+	function effect() {
+		// Get the user
+		$user =& $this->_request->getUser();
+		if (!is_a($user, 'PKPUser')) return AUTHORIZATION_DENY;
+
+		// Get the submission
+		$submission =& $this->getAuthorizedContextObject(ASSOC_TYPE_SUBMISSION);
+		if (!is_a($submission, 'Submission')) return AUTHORIZATION_DENY;
+
+		// Check authorship of the submission.
+		if ($submission->getUserId() === $user->getId()) {
+			return AUTHORIZATION_PERMIT;
+		} else {
+			return AUTHORIZATION_DENY;
+		}
+	}
+}
+
+?>

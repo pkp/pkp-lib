6ada25ea3c43771a9d6fa59a0567689267ecd768
Jason Nugent <jason.nugent@gmail.com>
2013-03-26 18:30:11 -0300

    *8148* port submission file methods to PKP-lib

--- classes/submission/SubmissionFile.inc.php
+++ classes/submission/SubmissionFile.inc.php
@@ -114,6 +114,102 @@ class SubmissionFile extends PKPFile {
 	}
 
 	/**
+	 * Get price of submission file.
+	 * @return numeric
+	 */
+	function getDirectSalesPrice() {
+		return $this->getData('directSalesPrice');
+	}
+
+	/**
+	 * Set direct sales price.
+	 * @param $directSalesPrice numeric
+	 */
+	function setDirectSalesPrice($directSalesPrice) {
+		return $this->setData('directSalesPrice', $directSalesPrice);
+	}
+
+	/**
+	 * Get sales type of submission file.
+	 * @return string
+	 */
+	function getSalesType() {
+		return $this->getData('salesType');
+	}
+
+	/**
+	 * Set sales type.
+	 * @param $salesType string
+	 */
+	function setSalesType($salesType) {
+		return $this->setData('salesType', $salesType);
+	}
+
+	/**
+	 * Set the name of the file
+	 * @param $name string
+	 * @param $locale string
+	 */
+	function setName($name, $locale) {
+		$this->setData('name', $name, $locale);
+	}
+
+	/**
+	 * Get the name of the file
+	 * @param $locale string
+	 * @return string
+	 */
+	function getName($locale) {
+		return $this->getData('name', $locale);
+	}
+
+	/**
+	 * Get the localized name of the file
+	 * @return string
+	 */
+	function getLocalizedName() {
+		return $this->getLocalizedData('name');
+	}
+
+	/**
+	 * Get the file's extension.
+	 * @return string
+	 */
+	function getExtension() {
+		import('lib.pkp.classes.file.FileManager');
+		$fileManager = new FileManager();
+		return strtoupper($fileManager->parseFileExtension($this->getOriginalFileName()));
+	}
+
+	/**
+	 * Get the file's document type (enumerated types)
+	 * @return string
+	 */
+	function getDocumentType() {
+		import('lib.pkp.classes.file.FileManager');
+		$fileManager = new FileManager();
+		return $fileManager->getDocumentType($this->getFileType());
+	}
+
+	/**
+	 * Set the genre id of this file (i.e. referring to Manuscript, Index, etc)
+	 * Foreign key into genres table
+	 * @param $genreId int
+	 */
+	function setGenreId($genreId) {
+		$this->setData('genreId', $genreId);
+	}
+
+	/**
+	 * Get the genre id of this file (i.e. referring to Manuscript, Index, etc)
+	 * Foreign key into genres table
+	 * @return int
+	 */
+	function getGenreId() {
+		return $this->getData('genreId');
+	}
+
+	/**
 	 * Get revision number.
 	 * @return int
 	 */
@@ -246,7 +342,185 @@ class SubmissionFile extends PKPFile {
 		return $this->SetData('viewable', $viewable);
 	}
 
-	// Private helper methods.
+	/**
+	 * Set the uploader's user id.
+	 * @param $uploaderUserId integer
+	 */
+	function setUploaderUserId($uploaderUserId) {
+		$this->setData('uploaderUserId', $uploaderUserId);
+	}
+
+	/**
+	 * Get the uploader's user id.
+	 * @return integer
+	 */
+	function getUploaderUserId() {
+		return $this->getData('uploaderUserId');
+	}
+
+	/**
+	 * Set the uploader's user group id
+	 * @param $userGroupId int
+	 */
+	function setUserGroupId($userGroupId) {
+		$this->setData('userGroupId', $userGroupId);
+	}
+
+	/**
+	 * Get the uploader's user group id
+	 * @return int
+	 */
+	function getUserGroupId() {
+		return $this->getData('userGroupId');
+	}
+
+	/**
+	 * Get type that is associated with this file.
+	 * @return int
+	 */
+	function getAssocType() {
+		return $this->getData('assocType');
+	}
+
+	/**
+	 * Set type that is associated with this file.
+	 * @param $assocType int
+	 */
+	function setAssocType($assocType) {
+		return $this->setData('assocType', $assocType);
+	}
+
+	/**
+	 * Return a context-aware file path.
+	 */
+	function getFilePath() {
+		// Get the context ID
+		$submissionDao = Application::getSubmissionDAO();
+		$submission =& $submissionDao->getById($this->getSubmissionId());
+		if (!$submission) return null;
+		$contextId = $submission->getPressId();
+		unset($submission);
+
+		// Construct the file path
+		import('lib.pkp.classes.file.SubmissionFileManager');
+		$submissionFileManager = new SubmissionFileManager($contextId, $this->getSubmissionId());
+		return $submissionFileManager->getBasePath() . $this->_fileStageToPath($this->getFileStage()) . '/' . $this->getFileName();
+	}
+
+	/**
+	 * Build a file name label.
+	 * @return string
+	 */
+	function getFileLabel($locale = null) {
+		// Retrieve the localized file name as basis for the label.
+		if ($locale) {
+			$fileLabel = $this->getName($locale);
+		} else {
+			$fileLabel = $this->getLocalizedName();
+		}
+
+		// If we have no file name then use a default name.
+		if (empty($fileLabel)) $fileLabel = $this->getOriginalFileName();
+
+		// Add the revision number to the label if we have more than one revision.
+		if ($this->getRevision() > 1) $fileLabel .= ' (' . $this->getRevision() . ')';
+
+		return $fileLabel;
+	}
+
+
+	/**
+	 * Copy the user-facing (editable) metadata from another submission
+	 * file.
+	 * @param $submissionFile SubmissionFile
+	 */
+	function copyEditableMetadataFrom($submissionFile) {
+		assert(is_a($submissionFile, 'SubmissionFile'));
+		$this->setName($submissionFile->getName(null), null);
+	}
+
+	//
+	// Overridden public methods from PKPFile
+	//
+	/**
+	 * @see PKPFile::getFileName()
+	 * Generate the file name from identification data rather than
+	 * retrieving it from the database.
+	 */
+	function getFileName() {
+		return $this->_generateFileName();
+	}
+
+	/**
+	 * @see PKPFile::setFileName()
+	 * Do not allow setting the file name of a submission file
+	 * directly because it is generated from identification data.
+	 */
+	function setFileName($fileName) {
+		// FIXME: Remove this setter from PKPFile, too? See #6446.
+		assert(false);
+	}
+
+	//
+	// Private helper methods
+	//
+
+	/**
+	 * Generate the unique filename for this submission file.
+	 * @return string
+	 */
+	function _generateFileName() {
+		// Remember the ID information we generated the file name
+		// on so that we only have to re-generate the name if the
+		// relevant information changed.
+		static $lastIds = array();
+		static $fileName = null;
+
+		// Retrieve the current id information.
+		$currentIds = array(
+				'genreId' => $this->getGenreId(),
+				'dateUploaded' => $this->getDateUploaded(),
+				'submissionId' => $this->getSubmissionId(),
+				'fileId' => $this->getFileId(),
+				'revision' => $this->getRevision(),
+				'fileStage' => $this->getFileStage(),
+				'extension' => strtolower_codesafe($this->getExtension())
+		);
+
+		// Check whether we need a refresh.
+		$refreshRequired = false;
+		foreach($currentIds as $key => $currentId) {
+			if (!isset($lastIds[$key]) || $lastIds[$key] !== $currentId) {
+				$refreshRequired = true;
+				$lastIds = $currentIds;
+				break;
+			}
+		}
+
+		// Refresh the file name if required.
+		if ($refreshRequired) {
+			// If the file has a file genre set then include
+			// human readable genre information.
+			$genreName = '';
+			if ($currentIds['genreId']) {
+				$primaryLocale = AppLocale::getPrimaryLocale();
+				$genreDao = DAORegistry::getDAO('GenreDAO'); /* @var $genreDao GenreDAO */
+				$genre =& $genreDao->getById($currentIds['genreId']);
+				assert(is_a($genre, 'Genre'));
+				$genreName = $genre->getDesignation($primaryLocale).'_'.$genre->getName($primaryLocale).'-';
+			}
+
+			// Generate a human readable time stamp.
+			$timestamp = date('Ymd', strtotime($currentIds['dateUploaded']));
+
+			// Make the file name unique across all files and file revisions.
+			// Also make sure that files can be ordered sensibly by file name.
+			$fileName = $currentIds['submissionId'].'-'.$genreName.$currentIds['fileId'].'-'.$currentIds['revision'].'-'.$currentIds['fileStage'].'-'.$timestamp.'.'.$currentIds['extension'];
+		}
+
+		return $fileName;
+	}
+
 	/**
 	 * Return path associated with a file stage code.
 	 * @param $fileStage string

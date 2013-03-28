ec2d838044cbf76343bb55de3a81a6f9f82e231d
Alec Smecher <alec@smecher.bc.ca>
2013-03-28 11:43:53 -0700

    Typo

--- js/classes/features/OrderListbuilderItemsFeature.js
+++ js/classes/features/OrderListbuilderItemsFeature.js
@@ -1,5 +1,5 @@
 /**
-  @file js/classes/features/OrderListbuilderItemsFeature.js
+ * @file js/classes/features/OrderListbuilderItemsFeature.js
  *
  * Copyright (c) 2000-2013 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.

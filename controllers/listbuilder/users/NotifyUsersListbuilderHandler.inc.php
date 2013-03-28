9fc8eb3b42e8085f0820a54c954b633c4e606164
Jason Nugent <jason.nugent@gmail.com>
2013-03-28 11:42:09 -0300

    notify users listbuilder handler to PKP-lib

---
+++ controllers/listbuilder/users/NotifyUsersListbuilderHandler.inc.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * @file controllers/listbuilder/users/NotifyUsersListbuilderHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class NotifyUsersListbuilderHandler
+ * @ingroup listbuilder
+ *
+ * @brief Class for adding participants to a stage.
+ */
+
+import('lib.pkp.classes.controllers.listbuilder.ListbuilderHandler');
+
+class NotifyUsersListbuilderHandler extends ListbuilderHandler {
+	/**
+	 * Constructor
+	 */
+	function NotifyUsersListbuilderHandler() {
+		parent::ListbuilderHandler();
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT, ROLE_ID_AUTHOR),
+			array('fetch', 'fetchRow', 'fetchOptions')
+		);
+	}
+
+	//
+	// Getters/Setters
+	//
+	/**
+	 * Get the authorized submission.
+	 * @return Submission
+	 */
+	function getSubmission() {
+		return $this->getAuthorizedContextObject(ASSOC_TYPE_SUBMISSION);
+	}
+
+	function getStageId() {
+		return $this->getAuthorizedContextObject(ASSOC_TYPE_WORKFLOW_STAGE);
+	}
+
+	//
+	// Overridden parent class functions
+	//
+	/**
+	 * @see GridDataProvider::getRequestArgs()
+	 */
+	function getRequestArgs() {
+		$submission = $this->getSubmission();
+		return array(
+			'submissionId' => $submission->getId(),
+			'stageId' => $this->getStageId()
+		);
+	}
+
+	//
+	// Implement protected template methods from PKPHandler
+	//
+	/**
+	 * @see PKPHandler::initialize()
+	 */
+	function initialize(&$request) {
+		parent::initialize($request);
+
+		// Load submission-specific translations
+		AppLocale::requireComponents(LOCALE_COMPONENT_PKP_USER);
+
+		$this->setTitle('email.recipients');
+
+		import('lib.pkp.classes.linkAction.request.NullAction');
+		$this->addAction(
+			new LinkAction(
+				'addItem',
+				new NullAction(),
+				__('grid.action.addUser'),
+				'add_user'
+			)
+		);
+
+		// Basic configuration.
+		$this->setSourceType(LISTBUILDER_SOURCE_TYPE_SELECT);
+		$this->setSaveType(LISTBUILDER_SAVE_TYPE_EXTERNAL);
+		$this->setSaveFieldName('users');
+
+		// Name column
+		$nameColumn = new ListbuilderGridColumn($this, 'name', 'common.name');
+		import('lib.pkp.controllers.listbuilder.users.UserListbuilderGridCellProvider');
+		$cellProvider = new UserListbuilderGridCellProvider();
+		$nameColumn->setCellProvider($cellProvider);
+		$this->addColumn($nameColumn);
+	}
+
+	/**
+	 * @see PKPHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 */
+	function authorize(&$request, &$args, $roleAssignments) {
+		import('classes.security.authorization.SubmissionAccessPolicy');
+		$this->addPolicy(new SubmissionAccessPolicy($request, $args, $roleAssignments, 'submissionId'));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	//
+	// Implement methods from ListbuilderHandler
+	//
+	/**
+	 * @see GridHandler::getRowDataElement
+	 * Get the data element that corresponds to the current request
+	 * Allow for a blank $rowId for when creating a not-yet-persisted row
+	 */
+	function getRowDataElement(&$request, $rowId) {
+		// fallback on the parent if a rowId is found
+		if ( !empty($rowId) ) {
+			return parent::getRowDataElement($request, $rowId);
+		}
+
+		// Otherwise return from the newRowId
+		// FIXME: Validate user ID?
+		$newRowId = $this->getNewRowId($request);
+		$userId = (int) $newRowId['name'];
+		$userDao = DAORegistry::getDAO('UserDAO');
+		$user =& $userDao->getById($userId);
+		return $user;
+	}
+
+	/**
+	 * @see ListbuilderHandler::getOptions
+	 * @params $userGroupId int A user group id to filter by (defaults to URL)
+	 */
+	function getOptions() {
+		// Initialize the object to return
+		$items = array(
+			array()
+		);
+
+		$userStageAssignmentDao = DAORegistry::getDAO('UserStageAssignmentDAO');
+		$submission =& $this->getSubmission();
+
+		// FIXME: add stage id?
+		$users =& $userStageAssignmentDao->getUsersBySubmissionAndStageId($submission->getId());
+
+		while ($user =& $users->next()) {
+			$items[0][$user->getId()] = $user->getFullName() . ' <' . $user->getEmail() . '>';
+			unset($user);
+		}
+		unset($users);
+
+		return $items;
+	}
+
+	/**
+	 * pre-populate the listbuilder if a userId is passed in, possibly via the stage assignments grid.
+	 * @see GridHandler::loadData($request, $filter)
+	 * @params $request PKPRequest
+	 */
+	function loadData(&$request) {
+		$userId = (int) $request->getUserVar('userId');
+		$userStageAssignmentDao = DAORegistry::getDAO('UserStageAssignmentDAO');
+		$submission =& $this->getSubmission();
+
+		$users =& $userStageAssignmentDao->getUsersBySubmissionAndStageId($submission->getId(), null, null, null, $userId);
+		$user =& $users->next();
+
+		if ($user) {
+			$items[0] = $user;
+			unset($user);
+			return $items;
+		}
+	}
+}
+
+?>
---
+++ controllers/listbuilder/users/UserListbuilderGridCellProvider.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file classes/controllers/listbuilder/users/UserListbuilderGridCellProvider.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class UserListbuilderGridCellProvider
+ * @ingroup controllers_grid
+ *
+ * @brief Base class for a cell provider that can retrieve labels from arrays
+ */
+
+import('lib.pkp.classes.controllers.grid.GridCellProvider');
+
+class UserListbuilderGridCellProvider extends GridCellProvider {
+	/**
+	 * Constructor
+	 */
+	function UserListbuilderGridCellProvider() {
+		parent::GridCellProvider();
+	}
+
+	//
+	// Template methods from GridCellProvider
+	//
+	/**
+	 * This implementation assumes a simple data element array that
+	 * has column ids as keys.
+	 * @see GridCellProvider::getTemplateVarsFromRowColumn()
+	 * @param $row GridRow
+	 * @param $column GridColumn
+	 * @return array
+	 */
+	function getTemplateVarsFromRowColumn(&$row, $column) {
+		$user =& $row->getData();
+		$columnId = $column->getId();
+		// Allow for either Users or Authors (both have a getFullName method).
+		assert((is_a($user, 'User') || is_a($user, 'Author')) && !empty($columnId));
+
+		return array('labelKey' => $user->getId(), 'label' => $user->getFullName() . ' <' . $user->getEmail() . '>');
+	}
+}
+
+?>

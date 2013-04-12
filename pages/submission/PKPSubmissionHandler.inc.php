64e79619888c2f416624eb7bad3031e9432432cf
Alec Smecher <alec@smecher.bc.ca>
2013-04-12 15:54:50 -0700

    ONIX stuff is OMP specific

--- pages/submission/PKPSubmissionHandler.inc.php
+++ pages/submission/PKPSubmissionHandler.inc.php
@@ -22,25 +22,6 @@ class PKPSubmissionHandler extends Handler {
 	function PKPSubmissionHandler() {
 		parent::Handler();
 	}
-
-
-	//
-	// Public Handler Methods
-	//
-	/**
-	 * Retrieves a JSON list of available choices for a tagit metadata input field.
-	 * @param $args array
-	 * @param $request Request
-	 */
-	function fetchChoices($args, $request) {
-		$codeList = (int) $request->getUserVar('codeList');
-		$term = $request->getUserVar('term');
-
-		$onixCodelistItemDao = DAORegistry::getDAO('ONIXCodelistItemDAO');
-		$codes = $onixCodelistItemDao->getCodes('List' . $codeList, array(), $term); // $term is escaped in the getCodes method.
-		header('Content-Type: text/json');
-		echo json_encode(array_values($codes));
-	}
 }
 
 ?>

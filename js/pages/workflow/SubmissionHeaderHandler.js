73563200f69d6818f14848309994b3610df6601c
Jason Nugent <jason.nugent@gmail.com>
2013-05-01 13:16:16 -0300

    *8212* bring workflow JS handlers in from OMP

---
+++ js/pages/workflow/SubmissionHeaderHandler.js
@@ -0,0 +1,97 @@
+/**
+ * @file js/pages/workflow/SubmissionHeaderHandler.js
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionHeaderHandler
+ * @ingroup js_pages_workflow
+ *
+ * @brief Handler for the workflow header.
+ *
+ */
+(function($) {
+
+
+	/**
+	 * @constructor
+	 *
+	 * @extends $.pkp.classes.Handler
+	 *
+	 * @param {jQueryObject} $submissionHeader The HTML element encapsulating
+	 *  the header div.
+	 * @param {Object} options Handler options.
+	 */
+	$.pkp.pages.workflow.SubmissionHeaderHandler =
+			function($submissionHeader, options) {
+
+		this.parent($submissionHeader, options);
+
+		// show and hide on click of link
+		$('#participantToggle').click(this.callbackWrapper(
+				this.appendToggleIndicator_));
+
+		this.bind('gridRefreshRequested', this.refreshWorkflowContent_);
+		this.publishEvent('stageParticipantsChanged');
+	};
+	$.pkp.classes.Helper.inherits(
+			$.pkp.pages.workflow.SubmissionHeaderHandler,
+			$.pkp.classes.Handler);
+
+
+	//
+	// Private functions
+	//
+	/**
+	 * Potentially refresh workflow content on contained grid changes.
+	 *
+	 * @param {jQueryObject} callingElement The calling element.
+	 *  that triggered the event.
+	 * @param {Event} event The event.
+	 * @private
+	 */
+	$.pkp.pages.workflow.SubmissionHeaderHandler.prototype.refreshWorkflowContent_ =
+			function(callingElement, event) {
+
+		var $updateSourceElement = $(event.target);
+		if ($updateSourceElement.attr('id').match(/^stageParticipantGridContainer/)) {
+			// If the participants grid was the event source, we
+			// may need to re-draw workflow contents.
+			this.trigger('stageParticipantsChanged');
+		}
+	};
+
+
+	/**
+	 * Append a + or - to the participants grid string based on current visibility
+	 * after toggling the display of the participants grid.
+	 *
+	 * @param {jQueryObject} callingElement The calling element.
+	 *  that triggered the event.
+	 * @param {Event} event The event.
+	 * @private
+	 */
+	$.pkp.pages.workflow.SubmissionHeaderHandler.prototype.appendToggleIndicator_ =
+			function(callingElement, event) {
+
+		var $submissionHeader = this.getHtmlElement(),
+				$participantsPopover = $submissionHeader.find('.participant_popover'),
+				$participantsListElement = $submissionHeader.find('li.participants'),
+				$participantsToggle = $submissionHeader.find('#participantToggle');
+
+		$participantsPopover.toggle();
+		$participantsListElement.toggleClass('expandedIndicator');
+		$participantsToggle.toggleClass('expandedIndicator');
+
+		if ($participantsListElement.hasClass('expandedIndicator')) {
+			this.trigger('callWhenClickOutside', [{
+				container: $participantsPopover.add($participantsListElement),
+				callback: this.callbackWrapper(this.appendToggleIndicator_),
+				skipWhenVisibleModals: true
+			}]);
+		}
+	};
+
+
+/** @param {jQuery} $ jQuery closure. */
+}(jQuery));
---
+++ js/pages/workflow/WorkflowHandler.js
@@ -0,0 +1,91 @@
+/**
+ * @defgroup js_pages_workflow
+ */
+/**
+ * @file js/pages/workflow/WorkflowHandler.js
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class WorkflowHandler
+ * @ingroup js_pages_workflow
+ *
+ * @brief Base handler for the workflow pages.
+ *
+ */
+(function($) {
+
+	/** @type {Object} */
+	$.pkp.pages.workflow = $.pkp.pages.workflow || {};
+
+
+
+	/**
+	 * @constructor
+	 *
+	 * @extends $.pkp.classes.Handler
+	 *
+	 * @param {jQueryObject} $workflowElement The HTML element encapsulating
+	 *  the production page.
+	 * @param {Object} options Handler options.
+	 */
+	$.pkp.pages.workflow.WorkflowHandler =
+			function($workflowElement, options) {
+
+		this.parent($workflowElement, options);
+
+		this.bind('stageParticipantsChanged', this.handleStageParticipantsChanged_);
+		this.bind('dataChanged', this.dataChangedHandler_);
+	};
+	$.pkp.classes.Helper.inherits(
+			$.pkp.pages.workflow.WorkflowHandler,
+			$.pkp.classes.Handler);
+
+
+	//
+	// Private functions
+	//
+	/**
+	 * Potentially refresh workflow content on participant change.
+	 *
+	 * @param {jQueryObject} callingElement The calling element.
+	 *  that triggered the event.
+	 * @param {Event} event The event.
+	 * @private
+	 */
+	$.pkp.pages.workflow.WorkflowHandler.prototype.handleStageParticipantsChanged_ =
+			function(callingElement, event) {
+
+		// Find and reload editor decision action divs.
+		this.getHtmlElement().find('.editorDecisionActions').each(function() {
+			var handler = $.pkp.classes.Handler.getHandler($(this));
+			handler.reload();
+		});
+	};
+
+
+	/**
+	 * Potentially refresh contained grid.
+	 *
+	 * @param {jQueryObject} callingElement The calling element.
+	 *  that triggered the event.
+	 * @param {Event} event The event.
+	 * @param {Object} eventData Event data.
+	 * @private
+	 */
+	$.pkp.pages.workflow.WorkflowHandler.prototype.dataChangedHandler_ =
+			function(callingElement, event, eventData) {
+
+		if ($(event.target, this.getHtmlElement()).children('a').
+				attr('id').match(/submissionEntry/)) {
+			// Refresh the format grid on this page, if any.
+			var $formatsGrid = $('[id^="formatsGridContainer"]',
+					this.getHtmlElement()).children('div');
+			$formatsGrid.trigger('dataChanged', [eventData]);
+			$formatsGrid.trigger('notifyUser', [$formatsGrid]);
+		}
+	};
+
+
+/** @param {jQuery} $ jQuery closure. */
+}(jQuery));

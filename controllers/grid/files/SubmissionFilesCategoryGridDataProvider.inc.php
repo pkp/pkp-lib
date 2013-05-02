3642edb526bc018d38003f10fbb3c9ebf17b5570
Alec Smecher <alec@smecher.bc.ca>
2013-05-02 12:30:59 -0700

    *8212* Review file management to PKP lib

---
+++ controllers/grid/files/SubmissionFilesCategoryGridDataProvider.inc.php
@@ -0,0 +1,203 @@
+<?php
+
+/**
+ * @file controllers/grid/files/review/SubmissionFilesCategoryGridDataProvider.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionFilesCategoryDataProvider
+ * @ingroup controllers_grid_files_review
+ *
+ * @brief Provide access to submission files data for category grids.
+ */
+
+
+import('lib.pkp.classes.controllers.grid.CategoryGridDataProvider');
+
+class SubmissionFilesCategoryGridDataProvider extends CategoryGridDataProvider {
+
+	/** @var $_submissionFiles array */
+	var $_submissionFiles;
+
+
+	/**
+	 * Constructor
+	 * @param $fileStage int The current file stage that the grid is handling
+	 * (others file stages could be shown activating the grid filter, but this
+	 * is the file stage that will be used to bring files from other stages, upload
+	 * new file, etc).
+	 * @param $dataProviderInitParams array Other parameters to initiate the grid
+	 * data provider that this category grid data provider will use to implement
+	 * common behaviours and data.
+	 */
+	function SubmissionFilesCategoryGridDataProvider($fileStage, $dataProviderInitParams = null) {
+		$this->setDataProvider($this->initGridDataProvider($fileStage, $dataProviderInitParams));
+	}
+
+
+	//
+	// Extended method from CategoryGridDataProvider.
+	//
+	/**
+	 * @see CategoryGridDataProvider::setDataProvider()
+	 */
+	function setDataProvider($gridDataProvider) {
+		if (is_a($gridDataProvider, 'SubmissionFilesGridDataProvider')) {
+			parent::setDataProvider($gridDataProvider);
+		} else {
+			assert(false);
+		}
+	}
+
+
+	//
+	// Implement template methods from GridDataProvider
+	//
+	/**
+	 * @see GridDataProvider::getAuthorizationPolicy()
+	 */
+	function getAuthorizationPolicy($request, $args, $roleAssignments) {
+		// Get the submission files grid data provider authorization policy.
+		$dataProvider = $this->getDataProvider();
+		$policy = $dataProvider->getAuthorizationPolicy($request, $args, $roleAssignments);
+
+		return $policy;
+	}
+
+	/**
+	 * @see GridDataProvider::getRequestArgs()
+	 */
+	function getRequestArgs() {
+		$dataProvider = $this->getDataProvider();
+		return $dataProvider->getRequestArgs();
+	}
+
+	/**
+	 * @see GridDataProvider::loadData()
+	 */
+	function loadData() {
+		// Return only the user accessible workflow stages.
+		return array_keys($this->getAuthorizedContextObject(ASSOC_TYPE_ACCESSIBLE_WORKFLOW_STAGES));
+	}
+
+
+	//
+	// Implement template methods from CategoryGridDataProvider
+	//
+	/**
+	 * @see CategoryGridDataProvider::getCategoryData()
+	 */
+	function &getCategoryData($categoryDataElement, $filter = null, $reviewRound = null) {
+		$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO'); /* @var $submissionFileDao SubmissionFileDAO */
+		$dataProvider = $this->getDataProvider();
+		$submission = $dataProvider->getSubmission();
+		$stageId = $categoryDataElement;
+		$fileStage = $this->_getFileStageByStageId($stageId);
+		$stageSubmissionFiles = null;
+
+		// For review stages, get the revisions of the review round that user is currently accessing.
+		if ($stageId == WORKFLOW_STAGE_ID_INTERNAL_REVIEW || $stageId == WORKFLOW_STAGE_ID_EXTERNAL_REVIEW) {
+			if (is_null($reviewRound) || $reviewRound->getStageId() != $stageId) {
+				$reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
+				$reviewRound = $reviewRoundDao->getLastReviewRoundBySubmissionId($submission->getId(), $stageId);
+			}
+			$stageSubmissionFiles = $submissionFileDao->getLatestNewRevisionsByReviewRound($reviewRound, $fileStage);
+		} else {
+			// Filter the passed workflow stage files.
+			if (!$this->_submissionFiles) {
+				$this->_submissionFiles = $submissionFileDao->getLatestRevisions($submission->getId());
+			}
+			$submissionFiles = $this->_submissionFiles;
+			$stageSubmissionFiles = array();
+			foreach ($submissionFiles as $key => $submissionFile) {
+				if ($submissionFile->getFileStage() == $fileStage) {
+					$stageSubmissionFiles[$key] = $submissionFile;
+				}
+			}
+		}
+
+		return $dataProvider->prepareSubmissionFileData($stageSubmissionFiles);
+	}
+
+
+	//
+	// Public methods
+	//
+	/**
+	 * @see SubmissionFilesGridDataProvider::getAddFileAction()
+	 */
+	function getAddFileAction($request) {
+		$dataProvider = $this->getDataProvider();
+		return $dataProvider->getAddFileAction($request);
+	}
+
+	/**
+	 * @see SubmissionFilesGridDataProvider::getFileStage()
+	 */
+	function setStageId($stageId) {
+		$dataProvider = $this->getDataProvider();
+		$dataProvider->setStageId($stageId);
+	}
+
+	/**
+	 * @see SubmissionFilesGridDataProvider::getFileStage()
+	 */
+	function getFileStage() {
+		$dataProvider = $this->getDataProvider();
+		return $dataProvider->getFileStage();
+	}
+
+
+	//
+	// Protected methods.
+	//
+	/**
+	 * Init the grid data provider that this category grid data provider
+	 * will use and return it. Override this to initiate another grid data provider.
+	 * @param $fileStage int
+	 * @param $initParams array (optional) The parameters to initiate the grid data provider.
+	 * @return SubmissionFilesGridDataProvider
+	 */
+	function &initGridDataProvider($fileStage, $initParams = null) {
+		// By default, this category grid data provider use the
+		// SubmissionFilesGridDataProvider.
+		import('lib.pkp.controllers.grid.files.SubmissionFilesGridDataProvider');
+		$gridDataProvider = new SubmissionFilesGridDataProvider($fileStage);
+
+		return $gridDataProvider;
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * Get the file stage using the passed stage id. This will define
+	 * which file stage will be present on each workflow stage category
+	 * of the grid.
+	 * @param $stageId int
+	 * @return int
+	 */
+	function _getFileStageByStageId($stageId) {
+		switch($stageId) {
+			case WORKFLOW_STAGE_ID_SUBMISSION:
+				return SUBMISSION_FILE_SUBMISSION;
+				break;
+			case WORKFLOW_STAGE_ID_INTERNAL_REVIEW:
+			case WORKFLOW_STAGE_ID_EXTERNAL_REVIEW:
+				return SUBMISSION_FILE_REVIEW_FILE;
+				break;
+			case WORKFLOW_STAGE_ID_EDITING:
+				return SUBMISSION_FILE_FINAL;
+				break;
+			case WORKFLOW_STAGE_ID_PRODUCTION:
+				return SUBMISSION_FILE_PRODUCTION_READY;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+?>
---
+++ controllers/grid/files/review/ReviewCategoryGridDataProvider.inc.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file controllers/grid/files/review/ReviewCategoryGridDataProvider.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class ReviewGridCategoryDataProvider
+ * @ingroup controllers_grid_files_review
+ *
+ * @brief Provide access to review file data for category grids.
+ */
+
+
+import('lib.pkp.controllers.grid.files.SubmissionFilesCategoryGridDataProvider');
+
+class ReviewCategoryGridDataProvider extends SubmissionFilesCategoryGridDataProvider {
+
+	/**
+	 * Constructor
+	 * @param $fileStage int
+	 * @param $viewableOnly int Will be passed to the review grid data provider.
+	 * See parameter description there.
+	 */
+	function ReviewCategoryGridDataProvider($fileStage, $viewableOnly = false) {
+		parent::SubmissionFilesCategoryGridDataProvider($fileStage, array('viewableOnly' => $viewableOnly));
+	}
+
+
+	//
+	// Getters and setters.
+	//
+	/**
+	 * @return ReviewRound
+	 */
+	function getReviewRound() {
+		$gridDataProvider = $this->getDataProvider();
+		return $gridDataProvider->getReviewRound();
+	}
+
+
+	//
+	// Overriden public methods from SubmissionFilesCategoryGridDataProvider
+	//
+	/**
+	 * @see SubmissionFilesCategoryGridDataProvider::getCategoryData()
+	 */
+	function &getCategoryData($categoryDataElement, $filter = null) {
+		$reviewRound = $this->getReviewRound();
+		return parent::getCategoryData($categoryDataElement, $filter, $reviewRound);
+	}
+
+	/**
+	 * @see SubmissionFilesCategoryGridDataProvider::initGridDataProvider()
+	 */
+	function &initGridDataProvider($fileStage, $initParams) {
+		// This category grid data provider will use almost all the
+		// same implementation of the ReviewGridDataProvider.
+		import('controllers.grid.files.review.ReviewGridDataProvider');
+		$reviewFilesGridDataProvider = new ReviewGridDataProvider($fileStage);
+		$reviewFilesGridDataProvider->setViewableOnly($initParams['viewableOnly']);
+
+		return $reviewFilesGridDataProvider;
+	}
+
+
+	//
+	// Public methods
+	//
+	/**
+	 * @see ReviewGridDataProvider::getSelectAction()
+	 */
+	function getSelectAction($request) {
+		$gridDataProvider = $this->getDataProvider();
+		return $gridDataProvider->getSelectAction($request);
+	}
+}
+
+?>

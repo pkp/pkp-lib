6164bfb8e18f7b7b53f6820502ac60ad86ebdeca
Alec Smecher <alec@smecher.bc.ca>
2013-05-02 14:50:19 -0700

    *8212* Review components to PKP lib

---
+++ controllers/grid/files/review/SelectableReviewRevisionsGridHandler.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file controllers/grid/files/review/SelectableReviewRevisionsGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SelectableReviewRevisionsGridHandler
+ * @ingroup controllers_grid_files_review
+ *
+ * @brief Display the file revisions authors have uploaded in a selectable grid.
+ *   Used for selecting files to send to external review or editorial stages.
+ */
+
+import('lib.pkp.controllers.grid.files.fileList.SelectableFileListGridHandler');
+
+class SelectableReviewRevisionsGridHandler extends SelectableFileListGridHandler {
+	/**
+	 * Constructor
+	 */
+	function SelectableReviewRevisionsGridHandler() {
+		import('lib.pkp.controllers.grid.files.review.ReviewRevisionsGridDataProvider');
+		// Pass in null stageId to be set in initialize from request var.
+		parent::SelectableFileListGridHandler(
+			new ReviewRevisionsGridDataProvider(),
+			null,
+			FILE_GRID_DELETE|FILE_GRID_VIEW_NOTES
+		);
+
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT),
+			array('fetchGrid', 'fetchRow')
+		);
+
+		// Set the grid information.
+		$this->setTitle('editor.submission.revisions');
+		$this->setInstructions('editor.submission.selectPromoteRevisions');
+	}
+}
+
+?>

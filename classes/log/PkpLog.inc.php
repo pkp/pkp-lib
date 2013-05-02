eabd00c7b44334ca06ab456b6ae2d0760e2bf717
Jason Nugent <jason.nugent@gmail.com>
2013-05-02 14:58:56 -0300

    *8212* refactor logEvent from sub classes

--- classes/log/PkpLog.inc.php
+++ classes/log/PkpLog.inc.php
@@ -17,6 +17,41 @@
  */
 
 class PkpLog {
+
+	/**
+	 * Add a new event log entry with the specified parameters
+	 * @param $request object
+	 * @param $submission object
+	 * @param $eventType int
+	 * @param $messageKey string
+	 * @param $params array optional
+	 * @return object SubmissionLogEntry iff the event was logged
+	 */
+	function logEvent($request, $submission, $eventType, $messageKey, $params = array()) {
+		// Create a new entry object
+		$submissionEventLogDao = DAORegistry::getDAO('SubmissionEventLogDAO');
+		$entry = $submissionEventLogDao->newDataObject();
+
+		// Set implicit parts of the log entry
+		$entry->setDateLogged(Core::getCurrentDate());
+		$entry->setIPAddress($request->getRemoteAddr());
+
+		$user = $request->getUser();
+		if ($user) $entry->setUserId($user->getId());
+
+		$entry->setAssocType(ASSOC_TYPE_SUBMISSION);
+		$entry->setAssocId($submission->getId());
+
+		// Set explicit parts of the log entry
+		$entry->setEventType($eventType);
+		$entry->setMessage($messageKey);
+		$entry->setParams($params);
+		$entry->setIsTranslated(0); // Legacy for other apps. All messages use locale keys.
+
+		// Insert the resulting object
+		$submissionEventLogDao->insertObject($entry);
+		return $entry;
+	}
 }
 
 ?>

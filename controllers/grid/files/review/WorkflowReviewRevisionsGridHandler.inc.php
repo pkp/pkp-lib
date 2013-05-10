8a763421e7383c5d7d603f21e6eb03af0d04b69e
Alec Smecher <alec@smecher.bc.ca>
2013-05-10 10:39:39 -0700

    *8212* Review revisions code to PKP lib

---
+++ controllers/grid/files/review/WorkflowReviewRevisionsGridHandler.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file controllers/grid/files/review/WorkflowReviewRevisionsGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class WorkflowReviewRevisionsGridHandler
+ * @ingroup controllers_grid_files_review
+ *
+ * @brief Display in workflow pages the file revisions that authors have uploaded.
+ */
+
+import('lib.pkp.controllers.grid.files.review.ReviewRevisionsGridHandler');
+
+class WorkflowReviewRevisionsGridHandler extends ReviewRevisionsGridHandler {
+	/**
+	 * Constructor
+	 */
+	function WorkflowReviewRevisionsGridHandler() {
+		$roleAssignments = array(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT),
+			array('fetchGrid', 'fetchRow', 'signOffFile')
+		);
+		parent::ReviewRevisionsGridHandler($roleAssignments);
+
+		$this->setTitle('editor.submission.revisions');
+		$this->setInstructions('editor.submission.revisionsDescription');
+	}
+}
+
+?>

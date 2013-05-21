a1177467f93c423d9a4086c161f79a52e077ed22
Alec Smecher <alec@smecher.bc.ca>
2013-05-21 11:58:42 -0700

    *8212* Add Representation base class

---
+++ classes/submission/Representation.inc.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * @file classes/submission/Representation.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class Representation
+ * @ingroup submission
+ *
+ * @brief A submission's representation (Publication Format, Galley, ...)
+ */
+
+import('lib.pkp.classes.core.DataObject');
+
+class Representation extends DataObject {
+	/**
+	 * Constructor.
+	 */
+	function Representation() {
+		// Switch on meta-data adapter support.
+		$this->setHasLoadableAdapters(true);
+
+		parent::DataObject();
+	}
+
+	/**
+	 * Get sequence of format in format listings for the submission.
+	 * @return float
+	 */
+	function getSeq() {
+		return $this->getData('seq');
+	}
+
+	/**
+	 * Set sequence of format in format listings for the submission.
+	 * @param $sequence float
+	 */
+	function setSeq($seq) {
+		return $this->setData('seq', $seq);
+	}
+
+	/**
+	 * Get "localized" format name (if applicable).
+	 * @return string
+	 */
+	function getLocalizedName() {
+		return $this->getLocalizedData('name');
+	}
+
+	/**
+	 * Get the format name (if applicable).
+	 * @return string
+	 */
+	function getName() {
+		return $this->getData('name');
+	}
+
+	/**
+	 * Set name.
+	 * @param $name string
+	 * @param $locale
+	 */
+	function setName($name) {
+		return $this->setData('name', $name);
+	}
+
+	/**
+	 * Set submission ID.
+	 * @param $submissionId int
+	 */
+	function setSubmissionId($submissionId) {
+		return $this->setData('submissionId', $submissionId);
+	}
+
+	/**
+	 * Get submission id
+	 * @return int
+	 */
+	function getSubmissionId() {
+		return $this->getData('submissionId');
+	}
+
+	/**
+	 * Get stored public ID of the submission.
+	 * @param $pubIdType string One of the NLM pub-id-type values or
+	 * 'other::something' if not part of the official NLM list
+	 * (see <http://dtd.nlm.nih.gov/publishing/tag-library/n-4zh0.html>).
+	 * @return int
+	 */
+	function getStoredPubId($pubIdType) {
+		return $this->getData('pub-id::'.$pubIdType);
+	}
+
+	/**
+	 * Set the stored public ID of the submission.
+	 * @param $pubIdType string One of the NLM pub-id-type values or
+	 * 'other::something' if not part of the official NLM list
+	 * (see <http://dtd.nlm.nih.gov/publishing/tag-library/n-4zh0.html>).
+	 * @param $pubId string
+	 */
+	function setStoredPubId($pubIdType, $pubId) {
+		return $this->setData('pub-id::'.$pubIdType, $pubId);
+	}
+
+	/**
+	 * Get the context id from the submission assigned to this representation.
+	 * @return int
+	 */
+	function getContextId() {
+		$submissionDao = Application::getSubmissionDAO();
+		$submission = $submissionDao->getById($this->getSubmissionId());
+		return $submission->getContextId();
+	}
+}
+
+?>

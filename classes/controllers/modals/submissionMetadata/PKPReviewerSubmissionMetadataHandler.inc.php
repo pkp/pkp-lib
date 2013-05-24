ab8eeedaf23fa696460ee153e2ddd1dcd9b957cb
Alec Smecher <alec@smecher.bc.ca>
2013-05-24 12:41:52 -0700

    *8212* Metadata access into PKP lib

---
+++ classes/controllers/modals/submissionMetadata/PKPReviewerSubmissionMetadataHandler.inc.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file classes/controllers/modals/submissionMetadata/PKPReviewerSubmissionMetadataHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPReviewerSubmissionMetadataHandler
+ * @ingroup controllers_modals_submissionMetadata
+ *
+ * @brief Display submission metadata to reviewers.
+ */
+
+import('lib.pkp.classes.controllers.modals.submissionMetadata.SubmissionMetadataHandler');
+
+// import JSON class for use with all AJAX requests
+import('lib.pkp.classes.core.JSONMessage');
+
+class PKPReviewerSubmissionMetadataHandler extends SubmissionMetadataHandler {
+	/**
+	 * Constructor.
+	 */
+	function PKPReviewerSubmissionMetadataHandler() {
+		parent::SubmissionMetadataHandler();
+		$this->addRoleAssignment(array(ROLE_ID_REVIEWER), array('fetch'));
+	}
+
+	//
+	// Implement template methods from PKPHandler.
+	//
+	/**
+	 * @see PKPHandler::authorize()
+	 * @param $request PKPRequest
+	 * @param $args array
+	 * @param $roleAssignments array
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		import('classes.security.authorization.SubmissionAccessPolicy');
+		$this->addPolicy(new SubmissionAccessPolicy($request, $args, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * @see classes/controllers/modals/submissionMetadata/SubmissionMetadataHandler::fetch()
+	 */
+	function fetch($args, $request) {
+		$press = $request->getPress();
+
+		$reviewAssignment = $this->getAuthorizedContextObject(ASSOC_TYPE_REVIEW_ASSIGNMENT);
+		$reviewMethod = $reviewAssignment->getReviewMethod();
+
+		if ($reviewMethod == SUBMISSION_REVIEW_METHOD_DOUBLEBLIND) {
+			$anonymous = true;
+		} else { /* SUBMISSION_REVIEW_METHOD_BLIND or _OPEN */
+			$anonymous = false;
+		}
+
+		$params = array('readOnly' => true, 'anonymous' => $anonymous, 'hideSubmit' => true);
+
+		return parent::fetch($request, $args, $params);
+	}
+}
+
+?>

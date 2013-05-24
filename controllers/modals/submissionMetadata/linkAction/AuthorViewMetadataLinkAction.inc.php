a635fd22a31663aa745bd45a74bf6f27da0021f7
Alec Smecher <alec@smecher.bc.ca>
2013-05-24 16:11:46 -0700

    *8212* Author dashboard to PKP lib prep

---
+++ controllers/modals/submissionMetadata/linkAction/AuthorViewMetadataLinkAction.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file controllers/modals/submissionMetadata/linkAction/AuthorViewMetadataLinkAction.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorViewMetadataLinkAction
+ * @ingroup controllers_modals_submissionMetadata_linkAction
+ *
+ * @brief An action to open the submission meta-data modal.
+ */
+
+import('lib.pkp.classes.linkAction.LinkAction');
+
+class AuthorViewMetadataLinkAction extends LinkAction {
+
+	/**
+	 * Constructor
+	 * @param $request Request
+	 * @param $submissionId integer The submission to show meta-data for.
+	 */
+	function AuthorViewMetadataLinkAction($request, $submissionId) {
+		$dispatcher = $request->getDispatcher();
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+		parent::LinkAction(
+			'viewMetadata',
+			new AjaxModal(
+				$dispatcher->url($request, ROUTE_COMPONENT, null,
+					'modals.submissionMetadata.AuthorSubmissionMetadataHandler',
+					'fetch', null, array('submissionId' => $submissionId)
+				),
+				__('submission.viewMetadata'),
+				'modal_information'
+			),
+			__('submission.viewMetadata'),
+			'more_info'
+		);
+	}
+}
+
+?>

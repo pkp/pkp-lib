bae694364c0ab1aa6422d6e7a2d303ff244ab3ff
Alec Smecher <alec@smecher.bc.ca>
2013-05-27 15:02:41 -0700

    *8212* Author submission details grid to PKP lib

---
+++ controllers/grid/files/submission/AuthorSubmissionDetailsFilesGridHandler.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file controllers/grid/files/submission/AuthorSubmissionDetailsFilesGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorSubmissionDetailsFilesGridHandler
+ * @ingroup controllers_grid_files_submission
+ *
+ * @brief Handle submission file grid requests on the author's submission details pages.
+ */
+
+// Import the grid layout.
+import('lib.pkp.controllers.grid.files.fileList.FileListGridHandler');
+
+class AuthorSubmissionDetailsFilesGridHandler extends FileListGridHandler {
+	/**
+	 * Constructor
+	 */
+	function AuthorSubmissionDetailsFilesGridHandler() {
+		import('lib.pkp.controllers.grid.files.SubmissionFilesGridDataProvider');
+		$dataProvider = new SubmissionFilesGridDataProvider(SUBMISSION_FILE_SUBMISSION);
+		parent::FileListGridHandler($dataProvider, WORKFLOW_STAGE_ID_SUBMISSION);
+
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT, ROLE_ID_AUTHOR),
+			array('fetchGrid', 'fetchRow')
+		);
+	}
+}
+
+?>

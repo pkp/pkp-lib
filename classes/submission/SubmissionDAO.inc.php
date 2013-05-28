871404b5df78ba43ccdffefb1ab8adf5f2eacb34
Alec Smecher <alec@smecher.bc.ca>
2013-05-28 12:02:42 -0700

    *8212* Abstract SubmissionDAO class

---
+++ classes/submission/SubmissionDAO.inc.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+ * @file classes/submission/SubmissionDAO.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionDAO
+ * @ingroup submission
+ * @see Submission
+ *
+ * @brief Operations for retrieving and modifying Submission objects.
+ */
+
+import('lib.pkp.classes.submission.Submission');
+
+class SubmissionDAO extends DAO {
+	var $authorDao;
+
+	/**
+	 * Constructor.
+	 */
+	function SubmissionDAO() {
+		parent::DAO();
+		$this->authorDao = DAORegistry::getDAO('AuthorDAO');
+	}
+
+	/**
+	 * Get a list of fields for which localized data is supported
+	 * @return array
+	 */
+	function getLocaleFieldNames() {
+		return array(
+			'title', 'cleanTitle', 'abstract', 'prefix', 'subtitle',
+			'discipline', 'subjectClass', 'subject',
+			'coverageGeo', 'coverageChron', 'coverageSample',
+			'type', 'sponsor', 'source', 'rights'
+		);
+	}
+
+	/**
+	 * Get a list of additional fields that do not have
+	 * dedicated accessors.
+	 * @return array
+	 */
+	function getAdditionalFieldNames() {
+		$additionalFields = parent::getAdditionalFieldNames();
+		// FIXME: Move this to a PID plug-in.
+		$additionalFields[] = 'pub-id::publisher-id';
+		return $additionalFields;
+	}
+
+	/**
+	 * Instantiate a new data object.
+	 * @return Submission
+	 */
+	function newDataObject() {
+		return new Submission();
+	}
+
+	/**
+	 * Internal function to return a Submission object from a row.
+	 * @param $row array
+	 * @return Submission
+	 */
+	function _fromRow($row) {
+		$submission = $this->newDataObject();
+		$submission->setLocale($row['locale']);
+		$submission->setUserId($row['user_id']);
+		$submission->setStageId($row['stage_id']);
+		$submission->setStatus($row['status']);
+		$submission->setSubmissionProgress($row['submission_progress']);
+		$submission->setDateSubmitted($this->datetimeFromDB($row['date_submitted']));
+		$submission->setDateStatusModified($this->datetimeFromDB($row['date_status_modified']));
+		$submission->setDatePublished($this->datetimeFromDB($row['date_published']));
+		$submission->setLastModified($this->datetimeFromDB($row['last_modified']));
+		$submission->setLanguage($row['language']);
+		$submission->setCommentsToEditor($row['comments_to_ed']);
+		return $submission;
+	}
+
+	/**
+	 * Delete a submission.
+	 * @param $submission Submission
+	 */
+	function deleteObject($submission) {
+		return $this->deleteById($submission->getId());
+	}
+
+	/**
+	 * Delete a submission by ID.
+	 * @param $submissionId int
+	 */
+	function deleteById($submissionId) {
+		$this->authorDao->deleteAuthorsBySubmission($submissionId);
+
+		$reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
+		$reviewRoundDao->deleteBySubmissionId($submissionId);
+
+		$editDecisionDao = DAORegistry::getDAO('EditDecisionDAO');
+		$editDecisionDao->deleteDecisionsBySubmissionId($submissionId);
+
+		$reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO');
+		$reviewAssignmentDao->deleteBySubmissionId($submissionId);
+
+		// Signoff DAOs
+		$signoffDao = DAORegistry::getDAO('SignoffDAO');
+		$submissionFileSignoffDao = DAORegistry::getDAO('SubmissionFileSignoffDAO');
+
+		// Delete Signoffs associated with a submission file of this submission.
+		$submissionFileSignoffs = $submissionFileSignoffDao->getAllBySubmission($submissionId);
+		while ($signoff = $submissionFileSignoffs->next()) {
+			$signoffDao->deleteObject($signoff);
+		}
+
+		// Delete the Signoffs associated with the submission itself.
+		$submissionSignoffs = $signoffDao->getAllByAssocType(ASSOC_TYPE_SUBMISSION, $submissionId);
+		while ($signoff = $submissionSignoffs->next()) {
+			$signoffDao->deleteObject($signoff);
+		}
+
+		// Delete the stage assignments.
+		$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
+		$stageAssignments = $stageAssignmentDao->getBySubmissionAndStageId($submissionId);
+		while ($stageAssignment = $stageAssignments->next()) {
+			$stageAssignmentDao->deleteObject($stageAssignment);
+		}
+
+		// N.B. Files must be deleted after signoffs to identify submission file signoffs.
+		// Delete submission files.
+		$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO'); /* @var $submissionFileDao SubmissionFileDAO */
+		$submissionFileDao->deleteAllRevisionsBySubmissionId($submissionId);
+
+		$commentDao = DAORegistry::getDAO('CommentDAO');
+		$commentDao->deleteBySubmissionId($submissionId);
+
+		$noteDao = DAORegistry::getDAO('NoteDAO');
+		$noteDao->deleteByAssoc(ASSOC_TYPE_SUBMISSION, $submissionId);
+
+		$submissionCommentDao = DAORegistry::getDAO('SubmissionCommentDAO');
+		$submissionCommentDao->deleteBySubmissionId($submissionId);
+
+		// Delete any outstanding notifications for this submission
+		$notificationDao = DAORegistry::getDAO('NotificationDAO');
+		$notificationDao->deleteByAssoc(ASSOC_TYPE_SUBMISSION, $submissionId);
+
+		$submissionEventLogDao = DAORegistry::getDAO('SubmissionEventLogDAO');
+		$submissionEventLogDao->deleteByAssoc(ASSOC_TYPE_SUBMISSION, $submissionId);
+
+		$submissionEmailLogDao = DAORegistry::getDAO('SubmissionEmailLogDAO');
+		$submissionEmailLogDao->deleteByAssoc(ASSOC_TYPE_SUBMISSION, $submissionId);
+
+		// Delete controlled vocab lists assigned to this submission
+		$submissionKeywordDao = DAORegistry::getDAO('SubmissionKeywordDAO');
+		$submissionKeywordVocab = $submissionKeywordDao->getBySymbolic(CONTROLLED_VOCAB_SUBMISSION_KEYWORD, ASSOC_TYPE_SUBMISSION, $submissionId);
+		if (isset($submissionKeywordVocab)) {
+			$submissionKeywordDao->deleteObject($submissionKeywordVocab);
+		}
+
+		$submissionDisciplineDao = DAORegistry::getDAO('SubmissionDisciplineDAO');
+		$submissionDisciplineVocab = $submissionDisciplineDao->getBySymbolic(CONTROLLED_VOCAB_SUBMISSION_DISCIPLINE, ASSOC_TYPE_SUBMISSION, $submissionId);
+		if (isset($submissionDisciplineVocab)) {
+			$submissionDisciplineDao->deleteObject($submissionDisciplineVocab);
+		}
+
+		$submissionAgencyDao = DAORegistry::getDAO('SubmissionAgencyDAO');
+		$submissionAgencyVocab = $submissionAgencyDao->getBySymbolic(CONTROLLED_VOCAB_SUBMISSION_AGENCY, ASSOC_TYPE_SUBMISSION, $submissionId);
+		if (isset($submissionAgencyVocab)) {
+			$submissionAgencyDao->deleteObject($submissionAgencyVocab);
+		}
+
+		$submissionLanguageDao = DAORegistry::getDAO('SubmissionLanguageDAO');
+		$submissionLanguageVocab = $submissionLanguageDao->getBySymbolic(CONTROLLED_VOCAB_SUBMISSION_LANGUAGE, ASSOC_TYPE_SUBMISSION, $submissionId);
+		if (isset($submissionLanguageVocab)) {
+			$submissionLanguageDao->deleteObject($submissionLanguageVocab);
+		}
+
+		$submissionSubjectDao = DAORegistry::getDAO('SubmissionSubjectDAO');
+		$submissionSubjectVocab = $submissionSubjectDao->getBySymbolic(CONTROLLED_VOCAB_SUBMISSION_SUBJECT, ASSOC_TYPE_SUBMISSION, $submissionId);
+		if (isset($submissionSubjectVocab)) {
+			$submissionSubjectDao->deleteObject($submissionSubjectVocab);
+		}
+
+	}
+
+	/**
+	 * Change the public ID of a submission.
+	 * @param $submissionId int
+	 * @param $pubIdType string One of the NLM pub-id-type values or
+	 * 'other::something' if not part of the official NLM list
+	 * (see <http://dtd.nlm.nih.gov/publishing/tag-library/n-4zh0.html>).
+	 * @param $pubId string
+	 */
+	function changePubId($submissionId, $pubIdType, $pubId) {
+		$this->updateSetting($submissionId, 'pub-id::'.$pubIdType, $pubId, 'string');
+	}
+
+}
+
+?>

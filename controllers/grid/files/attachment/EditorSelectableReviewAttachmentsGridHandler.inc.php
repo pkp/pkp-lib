a4623fa337fb7444509408a4b91f7e29b47571da
Alec Smecher <alec@smecher.bc.ca>
2013-07-09 14:23:54 -0700

    Share review components

---
+++ controllers/grid/files/attachment/EditorSelectableReviewAttachmentsGridHandler.inc.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file controllers/grid/files/attachment/EditorSelectableReviewAttachmentsGridHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class EditorSelectableReviewAttachmentsGridHandler
+ * @ingroup controllers_grid_files_attachments
+ *
+ * @brief Selectable review attachment grid requests (editor's perspective).
+ */
+
+import('lib.pkp.controllers.grid.files.fileList.SelectableFileListGridHandler');
+
+class EditorSelectableReviewAttachmentsGridHandler extends SelectableFileListGridHandler {
+	/**
+	 * Constructor
+	 */
+	function EditorSelectableReviewAttachmentsGridHandler() {
+		import('lib.pkp.controllers.grid.files.review.ReviewGridDataProvider');
+		// Pass in null stageId to be set in initialize from request var.
+		parent::SelectableFileListGridHandler(
+			new ReviewGridDataProvider(SUBMISSION_FILE_REVIEW_ATTACHMENT),
+			null,
+			FILE_GRID_DELETE|FILE_GRID_VIEW_NOTES
+		);
+
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT),
+			array('fetchGrid', 'fetchRow')
+		);
+
+		// Set the grid title.
+		$this->setTitle('grid.reviewAttachments.title');
+
+		$this->setInstructions('editor.submission.selectAttachments');
+	}
+
+	/**
+	 * @see SelectableFileListGridHandler::getSelectName()
+	 */
+	function getSelectName() {
+		return 'selectedAttachments';
+	}
+}
+
+?>

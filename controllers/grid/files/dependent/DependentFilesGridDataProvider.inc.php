25613538633bda0c7d7201d73d251832077acdc3
Jason Nugent <jason.nugent@gmail.com>
2013-07-11 14:55:12 -0300

    *8262* dependent file grid classes

---
+++ controllers/grid/files/dependent/DependentFilesGridDataProvider.inc.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file controllers/grid/files/dependent/DependentFilesGridDataProvider.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class DependentFilesGridDataProvider
+ * @ingroup controllers_grid_files_dependent
+ *
+ * @brief Provide access to dependent file data for grids.
+ */
+
+
+import('lib.pkp.controllers.grid.files.SubmissionFilesGridDataProvider');
+
+class DependentFilesGridDataProvider extends SubmissionFilesGridDataProvider {
+
+	/**
+	 * The submission file id for the parent file.
+	 * @var int
+	 */
+	var $_assocId;
+
+	/**
+	 * Constructor
+	 * @param $fileStage integer One of the SUBMISSION_FILE_* constants.
+	 */
+	function DependentFilesGridDataProvider($assocId) {
+		assert(is_numeric($assocId));
+		$this->_assocId = (int) $assocId;
+		parent::SubmissionFilesGridDataProvider(SUBMISSION_FILE_DEPENDENT);
+
+	}
+
+	/**
+	 * @see GridDataProvider::loadData()
+	 */
+	function loadData() {
+		// Retrieve all dependent files for the given file stage and original submission file id (i.e. the main galley/production file)
+		$submission = $this->getSubmission();
+		$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO'); /* @var $submissionFileDao SubmissionFileDAO */
+		$submissionFiles = $submissionFileDao->getLatestRevisionsByAssocId(ASSOC_TYPE_SUBMISSION_FILE, $this->getAssocId(), $submission->getId(), $this->getFileStage());
+		return $this->prepareSubmissionFileData($submissionFiles, $this->_viewableOnly);
+	}
+
+	/**
+	 * Overridden from SubmissionFilesGridDataProvider - we need to also include the assocType and assocId
+	 * @see FilesGridDataProvider::getAddFileAction()
+	 */
+	function getAddFileAction($request) {
+		import('lib.pkp.controllers.api.file.linkAction.AddFileLinkAction');
+		$submission = $this->getSubmission();
+		return new AddFileLinkAction(
+			$request, $submission->getId(), $this->getStageId(),
+			$this->getUploaderRoles(), $this->getFileStage(),
+			ASSOC_TYPE_SUBMISSION_FILE, $this->getAssocId()
+		);
+	}
+
+	/**
+	 * returns the id of the parent submission file for these dependent files.
+	 * @return int
+	 */
+	function getAssocId() {
+		return $this->_assocId;
+	}
+}
+
+?>

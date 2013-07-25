67c96f2933d2c60d449968eb0a8b5dbe456e9f47
Alec Smecher <alec@smecher.bc.ca>
2013-07-25 10:47:32 -0700

    *7073* Add support for config-specified session_cookie_path (optional)

--- classes/session/SessionManager.inc.php
+++ classes/session/SessionManager.inc.php
@@ -38,7 +38,7 @@ class SessionManager {
 		ini_set('session.use_cookies', 1);
 		ini_set('session.name', Config::getVar('general', 'session_cookie_name')); // Cookie name
 		ini_set('session.cookie_lifetime', 0);
-		ini_set('session.cookie_path', $request->getBasePath() . '/');
+		ini_set('session.cookie_path', Config::getVar('general', 'session_cookie_path', $request->getBasePath() . '/'));
 		ini_set('session.gc_probability', 1);
 		ini_set('session.gc_maxlifetime', 60 * 60);
 		ini_set('session.auto_start', 1);

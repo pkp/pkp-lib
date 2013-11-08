e591939ed461a1fce6befac2b52331a4a39a01d1
Alec Smecher <alec@smecher.bc.ca>
2013-11-08 15:39:18 -0800

    *8472* Add missing JS handler

---
+++ js/pages/authorDashboard/SubmissionEmailHandler.js
@@ -0,0 +1,43 @@
+/**
+ * @defgroup js_pages_authorDashboard
+ */
+
+
+/**
+ * @file js/pages/authorDashboard/SubmissionEmailHandler.js
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionEmailHandler
+ * @ingroup js_pages_authorDashboard
+ *
+ * @brief Handler for reading monograph emails within the author dashboard.
+ */
+(function($) {
+
+
+	/**
+	 * @constructor
+	 *
+	 * @extends $.pkp.controllers.linkAction.LinkActionHandler
+	 *
+	 * @param {jQueryObject} $submissionEmailContainer The container for
+	 *  the monograph email link.
+	 * @param {Object} options Handler options.
+	 */
+	$.pkp.pages.authorDashboard.SubmissionEmailHandler =
+			function($submissionEmailContainer, options) {
+
+		this.parent($submissionEmailContainer, options);
+
+		$submissionEmailContainer.find('a[id^="submissionEmail"]').click(
+				this.callbackWrapper(this.activateAction));
+	};
+	$.pkp.classes.Helper.inherits(
+			$.pkp.pages.authorDashboard.SubmissionEmailHandler,
+			$.pkp.controllers.linkAction.LinkActionHandler);
+
+
+/** @param {jQuery} $ jQuery closure. */
+}(jQuery));

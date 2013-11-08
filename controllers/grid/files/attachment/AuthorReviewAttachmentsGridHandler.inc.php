873f3e5a0b02e32447744532b406205487ceef28
Alec Smecher <alec@smecher.bc.ca>
2013-11-08 15:49:44 -0800

    *8472* Add author review files grids to PKP lib

---
+++ controllers/grid/files/attachment/AuthorReviewAttachmentsGridHandler.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file controllers/grid/files/attachment/AuthorReviewAttachmentsGridHandler.inc.php
+ *
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorReviewAttachmentsGridHandler
+ * @ingroup controllers_grid_files_attachment
+ *
+ * @brief Handle review attachment grid requests (author's perspective)
+ */
+
+import('lib.pkp.classes.controllers.grid.GridRow');
+import('lib.pkp.controllers.grid.files.fileList.FileListGridHandler');
+
+class AuthorReviewAttachmentsGridHandler extends FileListGridHandler {
+	/**
+	 * Constructor
+	 */
+	function AuthorReviewAttachmentsGridHandler() {
+		import('lib.pkp.controllers.grid.files.review.ReviewGridDataProvider');
+		// Pass in null stageId to be set in initialize from request var.
+		parent::FileListGridHandler(
+			new ReviewGridDataProvider(SUBMISSION_FILE_REVIEW_ATTACHMENT, true),
+			null
+		);
+
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT, ROLE_ID_AUTHOR),
+			array('fetchGrid', 'fetchRow')
+		);
+
+		// Set the grid title.
+		$this->setTitle('grid.reviewAttachments.title');
+	}
+}
+
+?>

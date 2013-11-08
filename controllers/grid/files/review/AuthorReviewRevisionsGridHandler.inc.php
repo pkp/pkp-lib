6e6bd515b4f992f877193e16bcf80b7e0f986a95
Alec Smecher <alec@smecher.bc.ca>
2013-11-08 15:49:44 -0800

    *8472* Add author review files grids to PKP lib

---
+++ controllers/grid/files/review/AuthorReviewRevisionsGridHandler.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file controllers/grid/files/review/AuthorReviewRevisionsGridHandler.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AuthorReviewRevisionsGridHandler
+ * @ingroup controllers_grid_files_review
+ *
+ * @brief Display to authors the file revisions that they have uploaded.
+ */
+
+import('lib.pkp.controllers.grid.files.review.ReviewRevisionsGridHandler');
+
+class AuthorReviewRevisionsGridHandler extends ReviewRevisionsGridHandler {
+	/**
+	 * Constructor
+	 */
+	function AuthorReviewRevisionsGridHandler() {
+		$roleAssignments = array(
+			array(ROLE_ID_AUTHOR),
+			array('fetchGrid', 'fetchRow')
+		);
+		parent::ReviewRevisionsGridHandler($roleAssignments);
+
+		$this->setTitle('editor.submission.revisions');
+		$this->setInstructions('editor.submission.revisionsDescription');
+	}
+}
+
+?>

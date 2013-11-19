372c72fdbb25d3f8d9c4d9245ddce455079782a1
Jason Nugent <jason.nugent@gmail.com>
2013-11-19 11:41:03 -0400

    *8445* introduce parent deployment class

---
+++ classes/plugins/importexport/PKPImportExportDeployment.inc.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * @defgroup classes_plugins_importexport import/export deployment
+ */
+
+/**
+ * @file classes/plugins/importexport/PKPImportExportDeployment.inc.php
+ *
+ * Copyright (c) 2000-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPImportExportDeployment
+ * @ingroup plugins_importexport
+ *
+ * @brief Base class configuring the import/export process to an
+ * application's specifics.
+ */
+
+class PKPImportExportDeployment {
+	/** @var Context The current import/export context */
+	var $_context;
+
+	/** @var User The current import/export user */
+	var $_user;
+
+	/** @var Submission The current import/export submission */
+	var $_submission;
+
+	/**
+	 * Constructor
+	 * @param $context Context
+	 * @param $user User
+	 */
+	function PKPImportExportDeployment($context, $user) {
+		$this->setContext($context);
+		$this->setUser($user);
+		$this->setSubmission(null);
+	}
+
+	//
+	// Deployment items for subclasses to override
+	//
+	/**
+	 * Get the submission node name
+	 * @return string
+	 */
+	function getSubmissionNodeName() {
+		assert(false);
+	}
+
+	/**
+	 * Get the submissions node name
+	 * @return string
+	 */
+	function getSubmissionsNodeName() {
+		assert(false);
+	}
+
+	/**
+	 * Get the representation node name
+	 */
+	function getRepresentationNodeName() {
+		assert(false);
+	}
+
+	/**
+	 * Get the namespace URN
+	 * @return string
+	 */
+	function getNamespace() {
+		assert(false);
+	}
+
+	/**
+	 * Get the schema filename.
+	 * @return string
+	 */
+	function getSchemaFilename() {
+		assert(false);
+	}
+
+
+	//
+	// Getter/setters
+	//
+	/**
+	 * Set the import/export context.
+	 * @param $context Context
+	 */
+	function setContext($context) {
+		$this->_context = $context;
+	}
+
+	/**
+	 * Get the import/export context.
+	 * @return Context
+	 */
+	function getContext() {
+		return $this->_context;
+	}
+
+	/**
+	 * Set the import/export submission.
+	 * @param $submission Submission
+	 */
+	function setSubmission($submission) {
+		$this->_submission = $submission;
+	}
+
+	/**
+	 * Get the import/export submission.
+	 * @return Submission
+	 */
+	function getSubmission() {
+		return $this->_submission;
+	}
+
+	/**
+	 * Set the import/export user.
+	 * @param $user User
+	 */
+	function setUser($user) {
+		$this->_user = $user;
+	}
+
+	/**
+	 * Get the import/export user.
+	 * @return User
+	 */
+	function getUser() {
+		return $this->_user;
+	}
+}
+
+?>

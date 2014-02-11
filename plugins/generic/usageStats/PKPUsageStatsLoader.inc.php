0d8a9fe727c356c1d731a7e87c7e5de4cdbc2c9f
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-02-11 18:27:27 -0200

    *8521* Introduce general code for usage stats plugin

---
+++ plugins/generic/usageStats/PKPUsageStatsLoader.inc.php
@@ -0,0 +1,485 @@
+<?php
+
+/**
+ * @file plugins/generic/usageStats/PKPUsageStatsLoader.php
+ *
+ * Copyright (c) 2013 Simon Fraser University Library
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPUsageStatsLoader
+ * @ingroup plugins_generic_usageStats
+ *
+ * @brief Scheduled task to extract transform and load usage statistics data into database.
+ */
+
+import('lib.pkp.classes.task.FileLoader');
+
+/** These are rules defined by the COUNTER project.
+ * See http://www.projectcounter.org/code_practice.htmlcode */
+define('COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS_HTML', 10);
+define('COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS_OTHER', 30);
+
+abstract class PKPUsageStatsLoader extends FileLoader {
+
+	/** @var A GeoLocationTool object instance to provide geo location based on ip. */
+	var $_geoLocationTool;
+
+	/** @var $_plugin Plugin */
+	var $_plugin;
+
+	/** @var $_counterRobotsListFile string */
+	var $_counterRobotsListFile;
+
+	/** @var $_contextsByPath array */
+	var $_contextsByPath;
+
+	/** @var $_baseSystemUrl string */
+	var $_baseSystemUrl;
+
+	/** @var $_baseSystemEscapedPath string */
+	var $_baseSystemEscapedPath;
+
+	/** @var $_autoStage string */
+	var $_autoStage;
+
+	/** @var $_externalLogFiles string */
+	var $_externalLogFiles;
+
+	/**
+	 * Constructor.
+	 * @param $argv array task arguments
+	 */
+	function PKPUsageStatsLoader($args) {
+		$plugin = PluginRegistry::getPlugin('generic', 'usagestatsplugin'); /* @var $plugin UsageStatsPlugin */
+		$this->_plugin = $plugin;
+
+		$arg = current($args);
+
+		switch ($arg) {
+			case 'autoStage':
+				if ($plugin->getSetting(0, 'createLogFiles')) {
+					$this->_autoStage = true;
+				}
+				break;
+			case 'externalLogFiles':
+				$this->_externalLogFiles = true;
+				break;
+		}
+
+
+		// Define the base filesystem path.
+		$args[0] = $plugin->getFilesPath();
+
+		parent::FileLoader($args);
+
+		$this->_baseSystemUrl = Config::getVar('general', 'base_url');
+		$this->_baseSystemEscapedPath = str_replace('/', '\/', parse_url($this->_baseSystemUrl, PHP_URL_PATH));
+
+		// Load the metric type constant.
+		PluginRegistry::loadCategory('reports');
+
+		import('classes.statistics.StatisticsHelper');
+		$statsHelper = new StatisticsHelper();
+		$geoLocationTool = $statsHelper->getGeoLocationTool();
+		$this->_geoLocationTool = $geoLocationTool;
+
+		$plugin->import('UsageStatsTemporaryRecordDAO');
+		$statsDao = new UsageStatsTemporaryRecordDAO();
+		DAORegistry::registerDAO('UsageStatsTemporaryRecordDAO', $statsDao);
+
+		$this->_counterRobotsListFile = $this->_getCounterRobotListFile();
+
+		$contextDao = Application::getContextDAO(); /* @var $contextDao ContextDAO */
+		$contextFactory = $contextDao->getAll(); /* @var $contextFactory DAOResultFactory */
+		$contextsByPath = array();
+		while ($context = $contextFactory->next()) { /* @var $context Context */
+			$contextsByPath[$context->getPath()] = $context;
+		}
+		$this->_contextsByPath = $contextsByPath;
+
+		$this->checkFolderStructure(true);
+
+		if ($this->_autoStage) {
+			// Copy all log files to stage directory, except the current day one.
+			$fileMgr = new FileManager();
+			$logsDirFiles =  glob($plugin->getUsageEventLogsPath() . DIRECTORY_SEPARATOR . '*');
+			$processingDirFiles = glob($this->getProcessingPath() . DIRECTORY_SEPARATOR . '*');
+
+			if (is_array($logsDirFiles) && is_array($processingDirFiles)) {
+				// It's possible that the processing directory have files that
+				// were being processed but the php process was stopped before
+				// finishing the processing. Just copy them to the stage directory too.
+				$dirFiles = array_merge($logsDirFiles, $processingDirFiles);
+				foreach ($dirFiles as $filePath) {
+					// Make sure it's a file.
+					if ($fileMgr->fileExists($filePath)) {
+						// Avoid current day file.
+						$filename = pathinfo($filePath, PATHINFO_BASENAME);
+						$currentDayFilename = $plugin->getUsageEventCurrentDayLogName();
+						if ($filename == $currentDayFilename) continue;
+
+						if ($fileMgr->copyFile($filePath, $this->getStagePath() . DIRECTORY_SEPARATOR . $filename)) {
+							$fileMgr->deleteFile($filePath);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @see FileLoader::processFile()
+	 */
+	protected function processFile($filePath) {
+		$fhandle = fopen($filePath, 'r');
+		$geoTool = $this->_geoLocationTool;
+		if (!$fhandle) {
+			$errorMsg = __('plugins.generic.usageStats.openFileFailed', array('file' => $filePath));
+		}
+
+		$loadId = basename($filePath);
+		$statsDao = DAORegistry::getDAO('UsageStatsTemporaryRecordDAO'); /* @var $statsDao UsageStatsTemporaryRecordDAO */
+
+		// Make sure we don't have any temporary records associated
+		// with the current load id in database.
+		$statsDao->deleteByLoadId($loadId);
+
+		$lastInsertedEntries = array();
+		$lineNumber = 0;
+
+		while(!feof($fhandle)) {
+			$lineNumber++;
+			$line = fgets($fhandle);
+			if ($line == '') continue;
+			$entryData = $this->_getDataFromLogEntry($line);
+			if (!$this->_isLogEntryValid($entryData, $lineNumber)) {
+				$errorMsg = __('plugins.generic.usageStats.invalidLogEntry',
+					array('file' => $filePath, 'lineNumber' => $lineNumber));
+			}
+
+			// Avoid internal apache requests.
+			if ($entryData['url'] == '*') continue;
+
+			// Avoid url with file extension (accept no extension or php only).
+			$fileExtension = pathinfo($entryData['url'], PATHINFO_EXTENSION);
+			if ($fileExtension && substr_count($fileExtension, 'php') == false) continue;
+
+			// Avoid non sucessful requests.
+			$sucessfulReturnCodes = array(200, 304);
+			if (!in_array($entryData['returnCode'], $sucessfulReturnCodes)) continue;
+
+			// Avoid bots.
+			if (Core::isUserAgentBot($entryData['userAgent'], $this->_counterRobotsListFile)) continue;
+
+			list($assocType, $contextPaths, $page, $op, $args) = $this->_getUrlMatches($entryData['url']);
+			if ($assocType && $contextPaths && $page && $op) {
+				list($assocId, $assocType) = $this->getAssoc($assocType, $contextPaths, $page, $op, $args);
+			}
+
+			if(!$assocId || !$assocType) continue;
+
+			list($countryCode, $cityName, $region) = $geoTool->getGeoLocation($entryData['ip']);
+			$day = date('Ymd', $entryData['date']);
+
+			$type = $this->getFileType($assocId, $assocType);
+
+			// Implement double click filtering.
+			$entryHash = $assocType . $assocId . $entryData['ip'];
+
+			// Clean the last inserted entries, removing the entries that have
+			// no importance for the time between requests check.
+			$biggestTimeFilter = COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS_OTHER;
+			foreach($lastInsertedEntries as $hash => $time) {
+				if ($time + $biggestTimeFilter < $entryData['date']) {
+					unset($lastInsertedEntries[$hash]);
+				}
+			}
+
+			// Time between requests check.
+			if (isset($lastInsertedEntries[$entryHash])) {
+				// Decide what time filter to use, depending on object type.
+				if ($type == STATISTICS_FILE_TYPE_PDF || $type == STATISTICS_FILE_TYPE_OTHER) {
+					$timeFilter = COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS_OTHER;
+				} else {
+					$timeFilter = COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS_HTML;
+				}
+
+				$secondsBetweenRequests = $entryData['date'] - $lastInsertedEntries[$entryHash];
+				if ($secondsBetweenRequests < $timeFilter) {
+					// We have to store the last access,
+					// so we delete the most recent one.
+					$statsDao->deleteRecord($assocType, $assocId, $loadId);
+				}
+			}
+
+			$lastInsertedEntries[$entryHash] = $entryData['date'];
+			$statsDao->insert($assocType, $assocId, $day, $countryCode, $region, $cityName, $type, $loadId);
+		}
+
+		fclose($fhandle);
+		$loadResult = $this->_loadData($loadId);
+		$statsDao->deleteByLoadId($loadId);
+
+		if (!$loadResult) {
+			return FILE_LOADER_RETURN_TO_STAGING;
+		} else {
+			return true;
+		}
+	}
+
+
+	//
+	// Abstract protected methods.
+	//
+	/**
+	 * Based on the passed object data, get the file type.
+	 * @param $assocType int
+	 * @param $assocId int
+	 * @return mixed int|null Return one of the file types
+	 * constants STATISTICS_FILE_TYPE... if the
+	 * object is a file, if not, return null.
+	 */
+	abstract protected function getFileType($assocType, $assocId);
+
+	/**
+	 * Get metric type on which this loader process statistics.
+	 */
+	abstract protected function getMetricType();
+
+
+	//
+	// Protected methods.
+	//
+	/**
+	 * Get assoc type and id from the passed page, operation and
+	 * arguments.
+	 * @param unknown_type $page
+	 * @param unknown_type $op
+	 * @param unknown_type $args
+	 */
+	protected function getAssoc($assocType, $contextPaths, $page, $op, $args) {
+		$assocId = null;
+
+		switch ($assocType) {
+			case ASSOC_TYPE_SUBMISSION:
+				if (!isset($args[0])) break;
+				$submissionId = $args[0];
+				$submissionDao = Application::getSubmissionDAO(); /* @var $submissionDao SubmissionDAO */
+				$submission = $submissionDao->getById($submissionId);
+				if ($submission) {
+					$assocId = $submission->getId();
+				}
+				break;
+			case Application::getContextAssocType():
+				$context = $this->getContextByPath($contextPaths);
+				if ($context) {
+					$assocId = $context->getId();
+				}
+				break;
+			case ASSOC_TYPE_SECTION:
+				if (!isset($args[0])) break;
+				$sectionId = $args[0];
+				$sectionDao = Application::getSectionDAO(); /* @var $sectionDao SectionDAO */
+				$section = $sectionDao->getById($sectionId);
+				if ($section) {
+					$assocId = $section->getId();
+				}
+				break;
+		}
+
+		// Object not found, return no assoc info at all.
+		if (!$assocId) $assocType = null;
+
+		return array($assocType, $assocId);
+	}
+
+	/**
+	 * Get the context object based on the context path
+	 * array that's returned by Core::getContextPaths()
+	 * @param $contextPaths array
+	 * @return mixed null|Context
+	 * @see Core::getContextPaths()
+	 */
+	protected function getContextByPath($contextPaths) {
+		$application = Application::getApplication();
+		$deepestContextDepthIndex = $application->getContextDepth() - 1;
+		$contextPath = $contextPaths[$deepestContextDepthIndex];
+
+		$context = null;
+		if (isset($this->_contextsByPath[$contextPath])) {
+			$context = $this->_contextsByPath[$contextPath];
+		}
+
+		return $context;
+	}
+
+	/**
+	* Get the expected page and operation from the stats plugin.
+	* They are grouped by the object type constant that
+	* they give access to.
+	* @return array
+	*/
+	protected function getExpectedPageAndOp() {
+		return array(
+			Application::getContextAssocType() => array(
+				'index/index'
+			)
+		);
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * Validate a access log entry.
+	 * @param $entry array
+	 * @return boolean
+	 */
+	private function _isLogEntryValid($entry, $lineNumber) {
+		if (empty($entry)) {
+			return false;
+		}
+
+		$date = $entry['date'];
+		if (!is_numeric($date) && $date <= 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get data from the passed log entry.
+	 * @param $entry string
+	 * @return mixed array
+	 */
+	private function _getDataFromLogEntry($entry) {
+		$plugin = $this->_plugin; /* @var $plugin Plugin */
+		$createLogFiles = $plugin->getSetting(0, 'createLogFiles');
+		if (!$createLogFiles || $this->_externalLogFiles) {
+			// User wants to process log files that were not created by
+			// the usage stats plugin. Try to get a user defined regex to
+			// parse those external log files then.
+			$parseRegex = $plugin->getSetting(0, 'accessLogFileParseRegex');
+		} else {
+			// Regex to parse this plugin's log access files.
+			$parseRegex = '/^(\S+) \S+ \S+ "(.*?)" (\S+) (\S+) "(.*?)"/';
+		}
+
+		// The default regex will parse only apache log files in combined format.
+		if (!$parseRegex) $parseRegex = '/^(\S+) \S+ \S+ \[(.*?)\] "\S+ (\S+).*?" (\S+) \S+ ".*?" "(.*?)"/';
+
+		$returner = array();
+		if (preg_match($parseRegex, $entry, $m)) {
+			$returner['ip'] = $m[1];
+			$returner['date'] = strtotime($m[2]);
+			$returner['url'] = urldecode($m[3]);
+			$returner['returnCode'] = $m[4];
+			$returner['userAgent'] = $m[5];
+		}
+
+		return $returner;
+	}
+
+	/**
+	 * Get assoc type, page. operation and args from
+	 * the passed url, if it matches anyone that's defined
+	 * in UsageStatsLoader::getExpectedPageAndOp().
+	 * @param $url string
+	 * @return array
+	 * @see UsageStatsLoader::getExpectedPageAndOp()
+	 */
+	private function _getUrlMatches($url) {
+		// Check the passed url.
+		$expectedPageAndOp = $this->getExpectedPageAndOp();
+
+		// Remove base system url from url, if any.
+		$url = str_replace($this->_baseSystemUrl, '', $url);
+
+		// If url don't have the entire protocol and host part,
+		// remove any possible base url path from url.
+		$url = preg_replace('/^' . $this->_baseSystemEscapedPath . '/', '', $url);
+
+		// Remove possible index.php page from url.
+		$url = str_replace('/index.php', '', $url);
+
+		// Check whether it's path info or not.
+		$pathInfo = parse_url($url, PHP_URL_PATH);
+		$isPathInfo = false;
+		if ($pathInfo) {
+			$isPathInfo = true;
+		}
+
+		$contextPaths = Core::getContextPaths($url, $isPathInfo);
+		$page = Core::getPage($url, $isPathInfo);
+		$operation = Core::getOp($url, $isPathInfo);
+		$args = Core::getArgs($url, $isPathInfo);
+
+		if (empty($contextPaths) || !$page || !$operation) return array(false, false);
+
+		$pageAndOperation = $page . '/' . $operation;
+
+		$pageAndOpMatch = false;
+		// It matches the expected ones?
+		foreach ($expectedPageAndOp as $workingAssocType => $workingPageAndOps) {
+			foreach($workingPageAndOps as $workingPageAndOp) {
+				if ($pageAndOperation == $workingPageAndOp) {
+					// Expected url, don't look any futher.
+					$pageAndOpMatch = true;
+					break 2;
+				}
+			}
+		}
+
+		if ($pageAndOpMatch) {
+			return array($workingAssocType, $contextPaths, $page, $operation, $args);
+		} else {
+			return array();
+		}
+	}
+
+	/**
+	 * Load the entries inside the temporary database associated with
+	 * the passed load id to the metrics table.
+	 * @param $loadId string The current load id.
+	 * file path.
+	 * @return boolean Whether or not the process
+	 * was successful.
+	 */
+	private function _loadData($loadId) {
+		$statsDao = DAORegistry::getDAO('UsageStatsTemporaryRecordDAO'); /* @var $statsDao UsageStatsTemporaryRecordDAO */
+		$metricsDao = DAORegistry::getDAO('MetricsDAO'); /* @var $metricsDao PKPMetricsDAO */
+		$metricsDao->purgeLoadBatch($loadId);
+
+		while ($record = $statsDao->getNextByLoadId($loadId)) {
+			$record['metric_type'] = $this->getMetricType();
+			$errorMsg = null;
+			$metricsDao->insertRecord($record, $errorMsg);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get the COUNTER robot list file.
+	 * @return mixed string or false in case of error.
+	 */
+	private function _getCounterRobotListFile() {
+		$file = null;
+		$dir = PKP_LIB_PATH . DIRECTORY_SEPARATOR . $this->_plugin->getPluginPath() . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'counter';
+
+		// We only expect one file inside the directory.
+		$fileCount = 0;
+		foreach (glob($dir . DIRECTORY_SEPARATOR . "*.*") as $file) {
+			$fileCount++;
+		}
+		if (!$file || $fileCount !== 1) {
+			return false;
+		}
+
+		return $file;
+	}
+}
+?>
---
+++ plugins/generic/usageStats/PKPUsageStatsPlugin.inc.php
@@ -0,0 +1,346 @@
+<?php
+
+/**
+ * @file plugins/generic/usageStats/PKPUsageStatsPlugin.inc.php
+ *
+ * Copyright (c) 2013 Simon Fraser University Library
+ * Copyright (c) 2003-2013 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPUsageStatsPlugin
+ * @ingroup plugins_generic_usageStats
+ *
+ * @brief Provide usage statistics to data objects.
+ */
+
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class PKPUsageStatsPlugin extends GenericPlugin {
+
+	/** @var $_currentUsageEvent array */
+	var $_currentUsageEvent;
+
+	/**
+	* Constructor.
+	*/
+	function PKPUsageStatsPlugin() {
+		parent::GenericPlugin();
+
+		// The upgrade and install processes will need access
+		// to constants defined in that report plugin.
+		import('plugins.generic.usageStats.UsageStatsReportPlugin');
+	}
+
+
+	//
+	// Implement methods from PKPPlugin.
+	//
+	/**
+	* @see LazyLoadPlugin::register()
+	*/
+	function register($category, $path) {
+		$success = parent::register($category, $path);
+
+		if ($this->getEnabled() && $success) {
+			// Register callbacks.
+			$app =& PKPApplication::getApplication();
+			$version = $app->getCurrentVersion();
+
+			HookRegistry::register('AcronPlugin::parseCronTab', array($this, 'callbackParseCronTab'));
+			HookRegistry::register('PluginRegistry::loadCategory', array($this, 'callbackLoadCategory'));
+
+			// If the plugin will provide the access logs,
+			// register to the usage event hook provider.
+			if ($this->getSetting(CONTEXT_ID_NONE, 'createLogFiles')) {
+				HookRegistry::register('UsageEventPlugin::getUsageEvent', array(&$this, 'logUsageEvent'));
+			}
+		}
+
+		return $success;
+	}
+
+	/**
+	* @see PKPPlugin::getDisplayName()
+	*/
+	function getDisplayName() {
+		return __('plugins.generic.usageStats.displayName');
+	}
+
+	/**
+	 * @see PKPPlugin::getDescription()
+	 */
+	function getDescription() {
+		return __('plugins.generic.usageStats.description');
+	}
+
+	/**
+	* @see PKPPlugin::isSitePlugin()
+	*/
+	function isSitePlugin() {
+		return true;
+	}
+
+	/**
+	* @see PKPPlugin::getInstallSitePluginSettingsFile()
+	*/
+	function getInstallSitePluginSettingsFile() {
+		return PKP_LIB_PATH . DIRECTORY_SEPARATOR . $this->getPluginPath() . DIRECTORY_SEPARATOR . 'settings.xml';
+	}
+
+	/**
+	 * @see PKPPlugin::getInstallSchemaFile()
+	 */
+	function getInstallSchemaFile() {
+		return PKP_LIB_PATH . DIRECTORY_SEPARATOR . $this->getPluginPath() . DIRECTORY_SEPARATOR . 'schema.xml';
+	}
+
+	/**
+	 * @see Plugin::getTemplatePath($inCore)
+	*/
+	function getTemplatePath($inCore = false) {
+		// This plugin have no application level templates.
+		$inCore = true;
+		return parent::getTemplatePath($inCore) . 'templates' .  DIRECTORY_SEPARATOR;
+	}
+
+	/**
+	* @see PKPPlugin::manage()
+	*/
+	function manage($verb, $args, &$message, &$messageParams, &$pluginModalContent = null) {
+		$returner = parent::manage($verb, $args, $message, $messageParams);
+		if (!$returner) return false;
+
+		$request = $this->getRequest();
+		$this->import('UsageStatsSettingsForm');
+
+		switch($verb) {
+			case 'settings':
+				$templateMgr =& TemplateManager::getManager();
+				$templateMgr->register_function('plugin_url', array(&$this, 'smartyPluginUrl'));
+				$settingsForm = new UsageStatsSettingsForm($this);
+				$settingsForm->initData();
+				$pluginModalContent = $settingsForm->fetch($request);
+				break;
+			case 'save':
+				$settingsForm = new UsageStatsSettingsForm($this);
+				$settingsForm->readInputData();
+				if ($settingsForm->validate()) {
+					$settingsForm->execute();
+					$message = NOTIFICATION_TYPE_SUCCESS;
+					$messageParams = array('contents' => __('plugins.generic.usageStats.settings.saved'));
+					return false;
+				} else {
+					$pluginModalContent = $settingsForm->fetch($request);
+				}
+				break;
+			default:
+				return $returner;
+		}
+		return true;
+	}
+
+
+	//
+	// Implement template methods from GenericPlugin.
+	//
+	/**
+	* @see GenericPlugin::getManagementVerbs()
+	*/
+	function getManagementVerbs() {
+		$verbs = parent::getManagementVerbs();
+		if ($this->getEnabled()) {
+			$verbs[] = array('settings', __('manager.plugins.settings'));
+		}
+		return $verbs;
+	}
+
+	/**
+	 * @see Plugin::getManagementVerbLinkAction()
+	 */
+	function getManagementVerbLinkAction($request, $verb) {
+		$router = $request->getRouter();
+
+		list($verbName, $verbLocalized) = $verb;
+
+		if ($verbName === 'settings') {
+			import('lib.pkp.classes.linkAction.request.AjaxModal');
+			$actionRequest = new AjaxModal(
+				$router->url($request, null, null, 'plugin', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic')),
+				$this->getDisplayName()
+			);
+			return new LinkAction($verbName, $actionRequest, $verbLocalized, null);
+		}
+	}
+
+
+	//
+	// Hook implementations.
+	//
+	/**
+	* @see PluginRegistry::loadCategory()
+	*/
+	function callbackLoadCategory($hookName, $args) {
+		// Instantiate report plugin.
+		$plugin = null;
+		$category = $args[0];
+		if ($category == 'reports') {
+			$this->import('UsageStatsReportPlugin');
+			$plugin = new UsageStatsReportPlugin();
+		}
+
+		// Register report plugin (by reference).
+		if ($plugin) {
+			$seq = $plugin->getSeq();
+			$plugins =& $args[1];
+			if (!isset($plugins[$seq])) $plugins[$seq] = array();
+			$plugins[$seq][$this->getPluginPath()] = $plugin;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @see AcronPlugin::parseCronTab()
+	 */
+	function callbackParseCronTab($hookName, $args) {
+		$taskFilesPath =& $args[0];
+		$taskFilesPath[] = PKP_LIB_PATH . DIRECTORY_SEPARATOR . $this->getPluginPath() . DIRECTORY_SEPARATOR . 'scheduledTasksAutoStage.xml';
+
+		return false;
+	}
+
+	/**
+	 * Log the usage event into a file.
+	 * @param $hookName string
+	 * @param $args array
+	 * @return boolean
+	 */
+	function logUsageEvent($hookName, $args) {
+		$hookName = $args[0];
+		$usageEvent = $args[1];
+
+		if ($hookName == 'FileManager::downloadFileFinished' && !$usageEvent && $this->_currentUsageEvent) {
+			// File download is finished, try to log the current usage event.
+			$downloadSuccess = $args[2];
+			if ($downloadSuccess && !connection_aborted()) {
+				$this->_currentUsageEvent['downloadSuccess'] = true;
+				$usageEvent = $this->_currentUsageEvent;
+			}
+		}
+
+		if ($usageEvent && !$usageEvent['downloadSuccess']) {
+			// Don't log until we get the download finished hook call.
+			$this->_currentUsageEvent = $usageEvent;
+			return false;
+		}
+
+		if ($usageEvent) {
+			$this->_writeUsageEventInLogFile($usageEvent);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get the geolocation tool to process geo localization
+	 * data.
+	 * @return GeoLocationTool
+	 */
+	function &getGeoLocationTool() {
+		/** Geo location tool wrapper class. If changing the geo location tool
+		* is required, change the code inside this class, keeping the public
+		* interface. */
+		$this->import('GeoLocationTool');
+
+		$tool = new GeoLocationTool();
+		return $tool;
+	}
+
+	/**
+	* Get the plugin's files path.
+	* @return string
+	*/
+	function getFilesPath() {
+		import('lib.pkp.classes.file.PrivateFileManager');
+		$fileMgr = new PrivateFileManager();
+
+		return realpath($fileMgr->getBasePath()) . DIRECTORY_SEPARATOR . 'usageStats';
+	}
+
+	/**
+	 * Get the plugin's usage event logs path.
+	 * @return string
+	 */
+	function getUsageEventLogsPath() {
+		return $this->getFilesPath() . DIRECTORY_SEPARATOR . 'usageEventLogs';
+	}
+
+	/**
+	 * Get current day usage event log name.
+	 * @return string
+	 */
+	function getUsageEventCurrentDayLogName() {
+		return 'usage_events_' . date("Ymd") . '.log';
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * @param $usageEvent array
+	 */
+	function _writeUsageEventInLogFile($usageEvent) {
+		$desiredParams = array($usageEvent['ip']);
+
+		if (isset($usageEvent['classification'])) {
+			$desiredParams[] = $usageEvent['classification'];
+		} else {
+			$desiredParams[] = '-';
+		}
+
+		if (isset($usageEvent['user'])) {
+			$desiredParams[] = $usageEvent['user']->getId();
+		} else {
+			$desiredParams[] = '-';
+		}
+
+		$desiredParams = array_merge($desiredParams,
+		array('"' . $usageEvent['time'] . '"', $usageEvent['canonicalUrl'],
+						'200', // The usage event plugin always log requests that returned this code.
+						'"' . $usageEvent['userAgent'] . '"'));
+
+		$usageLogEntry = implode(' ', $desiredParams) . PHP_EOL;
+
+		import('lib.pkp.classes.file.PrivateFileManager');
+		$fileMgr = new PrivateFileManager();
+
+		// Get the current day filename.
+		$filename = $this->getUsageEventCurrentDayLogName();
+
+		// Check the plugin file directory.
+		$usageEventFilesPath = $this->getUsageEventLogsPath();
+		if (!$fileMgr->fileExists($usageEventFilesPath, 'dir')) {
+			$success = $fileMgr->mkdirtree($usageEventFilesPath);
+			if (!$success) {
+				// Files directory wrong configuration?
+				assert(false);
+				return false;
+			}
+		}
+
+		$filePath = $usageEventFilesPath . DIRECTORY_SEPARATOR . $filename;
+		$fp = fopen($filePath, 'ab');
+		if (flock($fp, LOCK_EX)) {
+			fwrite($fp, $usageLogEntry);
+			flock($fp, LOCK_UN);
+		} else {
+			// Couldn't lock the file.
+			assert(false);
+		}
+		fclose($fp);
+	}
+}
+
+?>

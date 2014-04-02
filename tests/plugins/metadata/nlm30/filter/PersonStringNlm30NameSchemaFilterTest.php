271add949bebcb82f166941a520b6637504f03ee
Alec Smecher <alec@smecher.bc.ca>
2014-04-02 10:48:45 -0700

    *8655* Copyright date upates

--- tests/plugins/metadata/nlm30/filter/PersonStringNlm30NameSchemaFilterTest.php
+++ tests/plugins/metadata/nlm30/filter/PersonStringNlm30NameSchemaFilterTest.php
@@ -3,8 +3,8 @@
 /**
  * @file tests/plugins/metadata/nlm30/filter/PersonStringNlm30NameSchemaFilterTest.inc.php
  *
- * Copyright (c) 2013 Simon Fraser University Library
- * Copyright (c) 2000-2013 John Willinsky
+ * Copyright (c) 2013-2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class PersonStringNlm30NameSchemaFilterTest

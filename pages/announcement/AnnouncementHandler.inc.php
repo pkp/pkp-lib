5ba1c4b69f768585ff367f14d9a81c4414cc7c85
Alec Smecher <alec@smecher.bc.ca>
2014-04-10 14:28:45 -0700

    *8693* Remove unnecessary subclassing

---
+++ pages/announcement/AnnouncementHandler.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file pages/announcement/AnnouncementHandler.inc.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPAnnouncementHandler
+ * @ingroup pages_announcement
+ *
+ * @brief Handle requests for public announcement functions.
+ */
+
+import('classes.handler.Handler');
+
+class AnnouncementHandler extends Handler {
+	function AnnouncementHandler() {
+		parent::Handler();
+	}
+
+	//
+	// Implement methods from Handler.
+	//
+	function authorize($request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.ContextRequiredPolicy');
+		$this->addPolicy(new ContextRequiredPolicy($request));
+
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+
+	//
+	// Public handler methods.
+	//
+	/**
+	 * Show public announcements page.
+	 * @param $args array
+	 * @param $request PKPRequest
+	 * @return string
+	 */
+	function index($args, $request) {
+		$this->setupTemplate($request);
+
+		$context = $request->getContext();
+		$announcementsIntro = $context->getLocalizedSetting('announcementsIntroduction');
+
+		$templateMgr = TemplateManager::getManager($request);
+		$templateMgr->assign('announcementsIntroduction', $announcementsIntro);
+
+		$templateMgr->display('announcements/index.tpl');
+	}
+}
+
+?>

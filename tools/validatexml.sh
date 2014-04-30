e754c8b2cfee6c693450f8851423a7864508ecd4
Alec Smecher <alec@smecher.bc.ca>
2014-04-30 13:56:15 -0700

    Oops, move exclusion list into app repo

--- tools/validatexml.sh
+++ tools/validatexml.sh
@@ -9,10 +9,7 @@
 
 set -e # Fail on first error
 
-# Get the script path, in which we'll find the exclusion list
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # Search for all XML files in the current directory
 REPOSITORY_DIR="."
 
-/usr/bin/xmllint --noout --valid `find $REPOSITORY_DIR -name \*.xml | fgrep -v -f $SCRIPT_DIR/xmllint-exclusions.txt`
+/usr/bin/xmllint --noout --valid `find $REPOSITORY_DIR -name \*.xml | fgrep -v -f $REPOSITORY_DIR/tools/xmllint-exclusions.txt`

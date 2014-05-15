cbe66230461d0fa7506d0e52a72f238b9792080b
Alec Smecher <alec@smecher.bc.ca>
2014-05-15 14:10:11 -0700

    Replace app-specific lingo with general

--- controllers/grid/files/fileSignoff/FileSignoffGridHandler.inc.php
+++ controllers/grid/files/fileSignoff/FileSignoffGridHandler.inc.php
@@ -54,7 +54,7 @@ class FileSignoffGridHandler extends SubmissionFilesGridHandler {
 		// Set up the roles we may include as columns
 		$roles = array(
 			ROLE_ID_MANAGER => 'user.role.manager',
-			ROLE_ID_SUB_EDITOR => 'user.role.seriesEditor',
+			ROLE_ID_SUB_EDITOR => 'user.role.subEditor',
 			ROLE_ID_ASSISTANT => 'user.role.assistant'
 		);
 

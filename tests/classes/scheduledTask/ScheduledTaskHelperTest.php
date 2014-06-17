26a62ec0180a38fc7d13029b0d2e74e569077168
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-06-17 07:24:18 -0300

    *8785* Fixed possible task reporting flood to admin, fixed tests

--- tests/classes/scheduledTask/ScheduledTaskHelperTest.php
+++ tests/classes/scheduledTask/ScheduledTaskHelperTest.php
@@ -20,68 +20,111 @@ import('lib.pkp.classes.scheduledTask.ScheduledTask');
 class ScheduledTaskHelperTest extends PKPTestCase {
 
 	/**
-	 * Test ScheduledTaskHelper::notifyExecutionResult() method.
-	 * @dataProvider notifyExecutionResultDataProvider
+	 * Test ScheduledTaskHelper::notifyExecutionResult() method
+	 * when the scheduled task result is false.
+	 * @param $taskId string
+	 * @param $taskName string
+	 * @param $message string
+	 * @dataProvider notifyExecutionResultTestsDataProvider
 	 * @covers notifyExecutionResult
 	 */
-	function testNotifyExecutionResult($result, $message) {
+	function testNotifyExecutionResultError($taskId, $taskName, $message) {
+		$taskResult = false;
+		$expectedSubject = __(SCHEDULED_TASK_MESSAGE_TYPE_ERROR);
+		$this->_setReportErrorOnly('On');
+		$expectedTestResult = null; // Will send email (it's null because we mocked Mail::send()).
+
+		$helper = $this->_getHelper($expectedSubject, $message);
+
+		// Exercise the system.
+		$actualResult = $helper->notifyExecutionResult($taskId, $taskName, $taskResult, $message);
+		$this->assertEquals($expectedTestResult, $actualResult);
+
+		// Now set report error only to off and we should get the same result.
+		$this->_setReportErrorOnly('Off');
+		$actualResult = $helper->notifyExecutionResult($taskId, $taskName, $taskResult, $message);
+		$this->assertEquals($expectedTestResult, $actualResult);
+	}
+
+	/**
+	 * Test ScheduledTaskHelper::notifyExecutionResult() method
+	 * when the scheduled task result is true.
+	 * @param $taskId string
+	 * @param $taskName string
+	 * @param $message string
+	 * @dataProvider notifyExecutionResultTestsDataProvider
+	 * @covers notifyExecutionResult
+	 */
+	function testNotifyExecutionResultSuccess($taskId, $taskName, $message) {
+		$taskResult = true;
+		$expectedSubject = __(SCHEDULED_TASK_MESSAGE_TYPE_COMPLETED);
+		$this->_setReportErrorOnly('On');
+		$expectedTestResult = false; // Will NOT send email.
+
+		$helper = $this->_getHelper($expectedSubject, $message);
+
+		// Exercise the system.
+		$actualResult = $helper->notifyExecutionResult($taskId, $taskName, $taskResult, $message);
+		$this->assertEquals($expectedTestResult, $actualResult);
+
+		// Now change the report setting, so success emails will also be sent.
+		$this->_setReportErrorOnly('Off');
+		$expectedTestResult = null; // Will send email.
+		$actualResult = $helper->notifyExecutionResult($taskId, $taskName, $taskResult, $message);
+		$this->assertEquals($expectedTestResult, $actualResult);
+	}
+
+	/**
+	 * All notifyExecutionResult tests data provider.
+	 * @return array
+	 */
+	function notifyExecutionResultTestsDataProvider() {
+		return array(array('someTaskId', 'TaskName', 'Any message'));
+	}
+
+
+	//
+	// Private helper methods.
+	//
+	/**
+	 * Get helper mock object to exercise the system.
+	 * @param $expectedSubject string
+	 * @param $message string
+	 * @return ScheduledTaskHelper
+	 */
+	private function _getHelper($expectedSubject, $message) {
 		$helperMock = $this->getMock('ScheduledTaskHelper', array('getMail'), array('some@email.com', 'Contact name'));
 
 		// Helper will use the Mail::send() method. Mock it.
 		import('lib.pkp.classes.mail.Mail');
 		$mailMock = $this->getMock('Mail', array('send', 'setBody', 'setSubject'));
 
-		// Expects to be called once.
-		$mailMock->expects($this->once())
-				 ->method('send');
+		$mailMock->expects($this->any())
+			 	 ->method('send');
 
-		// Define what we expect for body.
-		if ($message) {
-			$constraint = 'equalTo';
-			$params = array($message);
-		} else {
-			$constraint = 'anything';
-			$params = array();
-		}
-		$mailMock->expects($this->once())
+		$mailMock->expects($this->any())
 				 ->method('setBody')
-				 ->with(call_user_func_array(array($this, $constraint), $params));
+				 ->with($this->equalTo($message));
 
-		// Define what we expect for subject.
-		if ($result) {
-			$expectedString = __(SCHEDULED_TASK_MESSAGE_TYPE_COMPLETED);
-		} else {
-			$expectedString = __(SCHEDULED_TASK_MESSAGE_TYPE_ERROR);
-		}
-
-		$mailMock->expects($this->once())
+		$mailMock->expects($this->any())
 				 ->method('setSubject')
-				 ->with($this->stringContains($expectedString));
+				 ->with($this->stringContains($expectedSubject));
 
 		// Inject mail dependency.
 		$helperMock->expects($this->any())
 				   ->method('getMail')
 				   ->will($this->returnValue($mailMock));
 
-
-		// Exercise the system. Expect null because it will return the Mail::send() method
-		// return value, which is null in the case of the mailMock object.
-		$taskId = 'someTaskId';
-		$taskName = 'TaskName';
-		$this->assertNull($helperMock->notifyExecutionResult($taskId, $taskName, $result, $message));
+		return $helperMock;
 	}
 
 	/**
-	 * testNotifyExecutionResult data provider.
+	 * Set the scheduled_task_report_error_only setting value.
+	 * @param $state string 'On' or 'Off'
 	 */
-	function notifyExecutionResultDataProvider() {
-		$cases = array();
-		$cases[0] = array(true, '');
-		$cases[1] = array(true, 'A test message');
-		$cases[2] = array(false, '');
-		$cases[3] = array(false, 'A test message');
-
-		return $cases;
+	private function _setReportErrorOnly($state) {
+		$configData =& Config::getData();
+		$configData['general']['scheduled_tasks_report_error_only'] = $state;
 	}
 }
 ?>

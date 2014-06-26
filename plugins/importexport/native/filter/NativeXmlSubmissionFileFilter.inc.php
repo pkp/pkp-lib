3cbf07bd6fde0ff36663d23c85d00461969cbefe
Jason Nugent <jason.nugent@gmail.com>
2014-06-26 14:07:39 -0300

    *8823* implement remote file support

--- plugins/importexport/native/filter/NativeXmlSubmissionFileFilter.inc.php
+++ plugins/importexport/native/filter/NativeXmlSubmissionFileFilter.inc.php
@@ -179,7 +179,12 @@ class NativeXmlSubmissionFileFilter extends NativeImportFilter {
 				$submissionFile->setName($node->textContent, $node->getAttribute('locale'));
 				break;
 			case 'remote':
-				fatalError('UNIMPLEMENTED');
+				$submissionFile->setFileType($node->getAttribute('mime_type'));
+				$src = $node->getAttribute('src');
+				$temporaryFileManager = new TemporaryFileManager();
+				$temporaryFilename = tempnam($temporaryFileManager->getBasePath(), 'remote');
+				$temporaryFileManager->copyFile($src, $temporaryFilename);
+				return $temporaryFilename;
 				break;
 			case 'href':
 				$submissionFile->setFileType($node->getAttribute('mime_type'));

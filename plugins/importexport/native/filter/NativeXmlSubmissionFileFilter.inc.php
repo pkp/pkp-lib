58c641c205d87cbbed9a171c8ce868cffa93dc70
Jason Nugent <jason.nugent@gmail.com>
2014-07-02 13:58:16 -0300

    *8823* remove temporary file after insertion

--- plugins/importexport/native/filter/NativeXmlSubmissionFileFilter.inc.php
+++ plugins/importexport/native/filter/NativeXmlSubmissionFileFilter.inc.php
@@ -163,6 +163,8 @@ class NativeXmlSubmissionFileFilter extends NativeImportFilter {
 		$submissionFile->setFileType($fileType);
 
 		$submissionFileDao->insertObject($submissionFile, $filename, false);
+		$fileManager = new FileManager();
+		$fileManager->deleteFile($filename);
 		return $submissionFile;
 	}
 

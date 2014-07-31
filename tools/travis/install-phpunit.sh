a7ef595e258e95b0ecfb43a427e00b45e2a59793
Alec Smecher <alec@smecher.bc.ca>
2014-07-30 17:10:52 -0700

    *8787* Travis testing tasks broken out into scripts

---
+++ tools/travis/install-phpunit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# @file tools/travis/install-phpunit.sh
+#
+# Copyright (c) 2014 Simon Fraser University Library
+# Copyright (c) 2010-2014 John Willinsky
+# Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+#
+# Script to install PHPUnit.
+#
+
+set -e
+
+# Install phpunit in a phar package with selenium extension already on it.
+sudo rm -R /home/travis/.phpenv/versions/5.3/bin/phpunit
+wget --no-check-certificate https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+sudo mv phpunit.phar /home/travis/.phpenv/versions/5.3/bin/phpunit
---
+++ tools/travis/start-xvfb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# @file tools/travis/start-xvfb.sh
+#
+# Copyright (c) 2014 Simon Fraser University Library
+# Copyright (c) 2010-2014 John Willinsky
+# Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+#
+# Script to install and start xvfb.
+#
+
+set -e
+
+# Xvfb requirements.
+sudo apt-get install -y x11-xkb-utils
+# Add fonts.
+sudo apt-get install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
+sudo apt-get install -y defoma x-ttcidfont-conf cabextract ttf-mscorefonts-installer
+sudo dpkg-reconfigure --default-priority x-ttcidfont-conf
+mkfontdir
+# Start Virtual Framebuffer to imitate a monitor.
+sudo Xvfb :10 -ac > xvfb-output &
+export DISPLAY=:10
+sleep 3 # Give xvfb time to start.

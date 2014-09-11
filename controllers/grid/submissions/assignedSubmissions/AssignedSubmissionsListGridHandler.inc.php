92b7e12d31db4ecf545aa15e146739578682f179
Jason Nugent <jason.nugent@gmail.com>
2014-09-11 14:16:51 -0300

    *8615* show assigned submissions grid to authors

--- controllers/grid/submissions/assignedSubmissions/AssignedSubmissionsListGridHandler.inc.php
+++ controllers/grid/submissions/assignedSubmissions/AssignedSubmissionsListGridHandler.inc.php
@@ -28,7 +28,7 @@ class AssignedSubmissionsListGridHandler extends SubmissionsListGridHandler {
 	function AssignedSubmissionsListGridHandler() {
 		parent::SubmissionsListGridHandler();
 		$this->addRoleAssignment(
-			array(ROLE_ID_SITE_ADMIN, ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_REVIEWER, ROLE_ID_ASSISTANT),
+			array(ROLE_ID_SITE_ADMIN, ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_REVIEWER, ROLE_ID_ASSISTANT, ROLE_ID_AUTHOR),
 			array('fetchGrid', 'fetchRow', 'deleteSubmission')
 		);
 	}

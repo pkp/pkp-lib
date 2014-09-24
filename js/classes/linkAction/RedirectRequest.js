2b8cb8ad0dd0246751253920ddc078bfa5509372
Alec Smecher <alec@smecher.bc.ca>
2014-09-24 11:53:52 -0700

    *8919* Add window target options to RedirectRequest

--- js/classes/linkAction/RedirectRequest.js
+++ js/classes/linkAction/RedirectRequest.js
@@ -43,7 +43,7 @@
 			function(element, event) {
 
 		var options = this.getOptions();
-		window.location = options.url;
+		window.open(options.url, options.name, options.specs);
 
 		return /** @type {boolean} */ this.parent('activate', element, event);
 	};

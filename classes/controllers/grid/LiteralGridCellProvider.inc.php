9e4ca32be74f2daf814e46db52cec767a985ffa0
Alec Smecher <alec@smecher.bc.ca>
2014-10-21 16:34:37 -0700

    *8868* Add generally useful LiteralGridCellProvider

---
+++ classes/controllers/grid/LiteralGridCellProvider.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file classes/controllers/grid/LiteralGridCellProvider.inc.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class LiteralGridCellProvider
+ * @ingroup controllers_grid
+ *
+ * @brief A cell provider that passes literal data through directly.
+ */
+
+import('lib.pkp.classes.controllers.grid.GridCellProvider');
+
+class LiteralGridCellProvider extends GridCellProvider {
+	/**
+	 * Constructor
+	 */
+	function LiteralGridCellProvider() {
+		parent::GridCellProvider();
+	}
+
+	//
+	// Template methods from GridCellProvider
+	//
+	/**
+	 * This implementation assumes a data element that is a literal value.
+	 * If desired, the 'id' column can be used to present the row ID.
+	 * @see GridCellProvider::getTemplateVarsFromRowColumn()
+	 * @param $row GridRow
+	 * @param $column GridColumn
+	 * @return array
+	 */
+	function getTemplateVarsFromRowColumn($row, $column) {
+		switch ($column->getId()) {
+			case 'id':
+				return array('label' => $row->getId());
+			case 'value':
+			default:
+				return array('label' => $row->getData());
+		}
+	}
+}
+
+?>

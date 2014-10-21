b61b8e80c90974c06f949b366b03e1bfbceaa6c9
Alec Smecher <alec@smecher.bc.ca>
2014-10-21 16:27:21 -0700

    *8868* Add EventAction, AddTabAction link action options

---
+++ classes/linkAction/request/AddTabAction.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file classes/linkAction/request/AddTabAction.inc.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class AddTabAction
+ * @ingroup linkAction_request
+ *
+ * @brief This action triggers a containing tabset to add a new tab.
+ */
+
+import('lib.pkp.classes.linkAction.request.EventAction');
+
+class AddTabAction extends EventAction {
+	/**
+	 * Constructor
+	 * @param $targetSelector string Selector for target to receive event.
+	 */
+	function AddTabAction($targetSelector, $url, $title) {
+		parent::EventAction($targetSelector, 'addTab', array(
+			'url' => $url,
+			'title' => $title,
+		));
+	}
+}
+
+?>

46a8657063ac2555e462b62f110096502310e8b6
Alec Smecher <alec@smecher.bc.ca>
2014-10-27 16:54:57 -0700

    *8963* Revert HTTPS in ORCiD URLs

--- tests/classes/validation/ValidatorORCIDTest.php
+++ tests/classes/validation/ValidatorORCIDTest.php
@@ -25,10 +25,8 @@ class ValidatorORCIDTest extends PKPTestCase {
 	 */
 	public function testValidatorORCID() {
 		$validator = new ValidatorORCID();
-		self::assertTrue($validator->isValid('http://orcid.org/0000-0002-1825-0097')); // Valid (http)
-		self::assertTrue($validator->isValid('https://orcid.org/0000-0002-1825-0097')); // Valid (https)
-		self::assertFalse($validator->isValid('ftp://orcid.org/0000-0002-1825-0097')); // Invalid (FTP scheme)
-		self::assertTrue($validator->isValid('http://orcid.org/0000-0002-1694-233X')); // Valid, with an X in the last digit
+		self::assertTrue($validator->isValid('http://orcid.org/0000-0002-1825-0097')); // Valid
+		self::assertTrue($validator->isValid('http://orcid.org/0000-0002-1694-233X')); // Valid, with an X in the last line
 		self::assertFalse($validator->isValid('0000-0002-1694-233X')); // Missing URI component
 		self::assertFalse($validator->isValid('000000021694233X')); // Missing dashes, URI component
 	}

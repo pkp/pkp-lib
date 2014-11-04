1fa5db45742a8cfe5d8d34164b4b989e34763d70
Bruno Beghelli <bruno.beghelli@gmail.com>
2014-11-04 12:24:26 -0200

    *8996* (8958 port) Removed unused variables

--- plugins/generic/usageStats/UsageStatsSettingsForm.inc.php
+++ plugins/generic/usageStats/UsageStatsSettingsForm.inc.php
@@ -39,14 +39,13 @@ class UsageStatsSettingsForm extends Form {
 
 		$this->setData('createLogFiles', $plugin->getSetting(CONTEXT_SITE, 'createLogFiles'));
 		$this->setData('accessLogFileParseRegex', $plugin->getSetting(0, 'accessLogFileParseRegex'));
-		$this->setData('minTimeBetweenRequests', $plugin->getSetting(0, 'minTimeBetweenRequests'));
 	}
 
 	/**
 	 * Assign form data to user-submitted data.
 	 */
 	function readInputData() {
-		$this->readUserVars(array('createLogFiles','accessLogFileParseRegex', 'minTimeBetweenRequests'));
+		$this->readUserVars(array('createLogFiles','accessLogFileParseRegex'));
 	}
 
 	/**
@@ -66,7 +65,6 @@ class UsageStatsSettingsForm extends Form {
 
 		$plugin->updateSetting(0, 'createLogFiles', $this->getData('createLogFiles'));
 		$plugin->updateSetting(0, 'accessLogFileParseRegex', $this->getData('accessLogFileParseRegex'));
-		$plugin->updateSetting(0, 'minTimeBetweenRequests', (int)$this->getData('minTimeBetweenRequests'));
 	}
 
 }

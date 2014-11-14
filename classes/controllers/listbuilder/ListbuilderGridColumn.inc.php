ac70caa4c38acd5075c88e8f4adb67ee3e1ca270
Alec Smecher <alec@smecher.bc.ca>
2014-11-13 21:51:54 -0800

    *8868* Back-port listbuilder changes to omp-stable-1_1

--- classes/controllers/listbuilder/ListbuilderGridColumn.inc.php
+++ classes/controllers/listbuilder/ListbuilderGridColumn.inc.php
@@ -19,6 +19,13 @@ import('lib.pkp.classes.controllers.grid.GridColumn');
 class ListbuilderGridColumn extends GridColumn {
 	/**
 	 * Constructor
+	 * @param $listbuilder ListbuilderHandler The listbuilder handler this column belongs to.
+	 * @param $id string The optional symbolic ID for this column.
+	 * @param $title string The optional title for this column.
+	 * @param $titleTranslated string The optional translated title for this column.
+	 * @param $template string The optional overridden template for this column.
+	 * @param $cellProvider ListbuilderGridCellProvider The optional overridden grid cell provider.
+	 * @param $flags array Optional set of flags for this column's display.
 	 */
 	function ListbuilderGridColumn($listbuilder, $id = '', $title = null, $titleTranslated = null,
 			$template = null, $cellProvider = null, $flags = array()) {

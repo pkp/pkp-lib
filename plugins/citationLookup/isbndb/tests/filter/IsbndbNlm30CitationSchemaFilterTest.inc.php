d202fa41c9e7b844555ee34cf85dfe523f0e2512
Alec Smecher <alec@smecher.bc.ca>
2014-11-18 10:19:09 -0800

    Merge pull request #226 from asmecher/plugin-tests
    *9015* Move plugin tests into plugin directory

---
+++ plugins/citationLookup/isbndb/tests/filter/IsbndbNlm30CitationSchemaFilterTest.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file plugins/citationLookup/isbndb/tests/filter/IsbndbNlm30CitationSchemaFilterTest.inc.php
+ *
+ * Copyright (c) 2014 Simon Fraser University Library
+ * Copyright (c) 2000-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class IsbndbNlm30CitationSchemaFilterTest
+ * @ingroup plugins_citationLookup_isbndb_tests_filter
+ *
+ * @brief Basic configuration for Isbndb tests
+ */
+
+import('lib.pkp.plugins.metadata.nlm30.tests.filter.Nlm30CitationSchemaFilterTestCase');
+
+class IsbndbNlm30CitationSchemaFilterTest extends Nlm30CitationSchemaFilterTestCase {
+	const ISBNDB_TEST_APIKEY = '4B5GQSQ4';
+}
+?>

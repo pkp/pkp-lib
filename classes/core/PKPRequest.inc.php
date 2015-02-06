df2a88d8d462870947bbbb7177040ae216c404a6
Alec Smecher <alec@smecher.bc.ca>
2015-02-06 12:18:07 -0800

    pkp/pkp-lib#276 Fix missing end quote

--- classes/core/PKPRequest.inc.php
+++ classes/core/PKPRequest.inc.php
@@ -137,7 +137,7 @@ class PKPRequest {
 
 		// Note that we are intentionally skipping PKP processing of REQUEST_URI and QUERY_STRING for a protocol redirect
 		// This processing is deferred to the redirected (target) URI
-		$url = 'http://' . $_this->getServerHost() . $_SERVER['REQUEST_URI];
+		$url = 'http://' . $_this->getServerHost() . $_SERVER['REQUEST_URI'];
 		$queryString = $_SERVER['QUERY_STRING'];
 		if (!empty($queryString)) $url .= "?$queryString";
 		$_this->redirectUrl($url);

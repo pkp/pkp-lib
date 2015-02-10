082a27482e1729ebd8821782bb92660cbee7adfe
Alec Smecher <alec@smecher.bc.ca>
2015-02-09 17:15:38 -0800

    Merge pull request #326 from ulsdevteam/add-uuid-string-creation
    Add a UUID generator (version 4) to the String library.

--- classes/core/String.inc.php
+++ classes/core/String.inc.php
@@ -954,6 +954,22 @@ class String {
 	static function enumerateAlphabetically($steps) {
 		return chr(ord('A') + $steps);
 	}
+
+	/**
+	 * Create a new UUID (version 4)
+	 * @return string
+	 */
+	function generateUUID() {
+		mt_srand((double)microtime()*10000);
+		$charid = strtoupper(md5(uniqid(rand(), true)));
+		$hyphen = '-';
+		$uuid = substr($charid, 0, 8).$hyphen
+				.substr($charid, 8, 4).$hyphen
+				.'4'.substr($charid,13, 3).$hyphen
+				.strtoupper(dechex(hexdec(ord(substr($charid,16,1))) % 4 + 8)).substr($charid,17, 3).$hyphen
+				.substr($charid,20,12);
+		return $uuid;
+	}
 }
 
 ?>

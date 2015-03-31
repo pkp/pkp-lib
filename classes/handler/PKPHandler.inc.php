6f2705054806d0b2a4249b87f3a28cb5aa6b6442
Alec Smecher <alec@smecher.bc.ca>
2015-03-31 11:44:19 -0700

    *5868* Remove PKPHandler::getLoginExemptions

--- classes/handler/PKPHandler.inc.php
+++ classes/handler/PKPHandler.inc.php
@@ -450,16 +450,6 @@ class PKPHandler {
 	}
 
 	/**
-	 * Get a list of pages that don't require login, even if the system does
-	 * FIXME: Delete this method when authorization re-factoring is complete.
-	 * @return array
-	 */
-	function getLoginExemptions() {
-		import('lib.pkp.classes.security.authorization.RestrictedSiteAccessPolicy');
-		return RestrictedSiteAccessPolicy::_getLoginExemptions();
-	}
-
-	/**
 	 * Get the iterator of working contexts.
 	 * @param $request PKPRequest
 	 * @return ItemIterator

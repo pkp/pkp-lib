afc1cb0f27e28f07f2110061c8c9cdc126c64bed
Alec Smecher <alec@smecher.bc.ca>
2015-04-17 10:57:13 -0700

    pkp/pkp-lib#474 Make ISBNDB API key secure

--- tests/plugins/citationLookup/isbndb/filter/IsbndbNlm30CitationSchemaFilterTest.inc.php
+++ tests/plugins/citationLookup/isbndb/filter/IsbndbNlm30CitationSchemaFilterTest.inc.php
@@ -17,6 +17,11 @@
 import('lib.pkp.tests.plugins.metadata.nlm30.filter.Nlm30CitationSchemaFilterTestCase');
 
 class IsbndbNlm30CitationSchemaFilterTest extends Nlm30CitationSchemaFilterTestCase {
-	const ISBNDB_TEST_APIKEY = 'K6WLK8YW';
+	/**
+	 * Get the ISBNDB API key
+	 */
+	protected function _getIsbndbApiKey() {
+		return getenv('ISBNDB_TEST_APIKEY');
+	}
 }
 ?>

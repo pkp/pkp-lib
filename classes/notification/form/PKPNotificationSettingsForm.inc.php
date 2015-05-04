958663d9317abd2c5ec318ff82d2e92548cb9e0b
Bruno Beghelli <bruno.beghelli@gmail.com>
2015-05-04 14:19:13 -0300

    *8891* Generalized notification settings code, moved to library

--- classes/notification/form/PKPNotificationSettingsForm.inc.php
+++ classes/notification/form/PKPNotificationSettingsForm.inc.php
@@ -31,6 +31,51 @@ class PKPNotificationSettingsForm extends Form {
 	}
 
 	/**
+	 * Assign form data to user-submitted data.
+	 */
+	function readInputData() {
+		$userVars = array();
+		foreach($this->getNotificationSettingsMap() as $notificationSetting) {
+			$userVars[] = $notificationSetting['settingName'];
+			$userVars[] = $notificationSetting['emailSettingName'];
+		}
+
+		$this->readUserVars($userVars);
+	}
+
+	/**
+	 * Get all notification settings form names and their setting type values
+	 * @return array
+	 */
+	protected function getNotificationSettingsMap() {
+		return array(
+			NOTIFICATION_TYPE_SUBMISSION_SUBMITTED => array('settingName' => 'notificationSubmissionSubmitted',
+				'emailSettingName' => 'emailNotificationSubmissionSubmitted',
+				'settingKey' => 'notification.type.submissionSubmitted'),
+			NOTIFICATION_TYPE_METADATA_MODIFIED => array('settingName' => 'notificationMetadataModified',
+				'emailSettingName' => 'emailNotificationMetadataModified',
+				'settingKey' => 'notification.type.metadataModified'),
+			NOTIFICATION_TYPE_REVIEWER_COMMENT => array('settingName' => 'notificationReviewerComment',
+				'emailSettingName' => 'emailNotificationReviewerComment',
+				'settingKey' => 'notification.type.reviewerComment')
+		);
+	}
+
+	/**
+	 * Get a list of notification category names (to display as headers)
+	 *  and the notification types under each category
+	 * @return array
+	 */
+	protected function getNotificationSettingCategories() {
+		return array(
+			array('categoryKey' => 'notification.type.submissions',
+				'settings' => array(NOTIFICATION_TYPE_SUBMISSION_SUBMITTED, NOTIFICATION_TYPE_METADATA_MODIFIED)),
+			array('categoryKey' => 'notification.type.reviewing',
+				'settings' => array(NOTIFICATION_TYPE_REVIEWER_COMMENT))
+		);
+	}
+
+	/**
 	 * @copydoc
 	 */
 	function fetch($request) {
@@ -47,7 +92,33 @@ class PKPNotificationSettingsForm extends Form {
 		$templateMgr->assign('emailSettings', $emailSettings);
 		$templateMgr->assign('titleVar', __('common.title'));
 		$templateMgr->assign('userVar', __('common.user'));
-		return parent::fetch();
+		$templateMgr->assign('notificationSettingCategories', $this->getNotificationSettingCategories());
+		$templateMgr->assign('notificationSettings',  $this->getNotificationSettingsMap());
+		return parent::fetch($request);
+	}
+
+	/**
+	 * @copydoc
+	 */
+	function execute($request) {
+		$user = $request->getUser();
+		$userId = $user->getId();
+		$context = $request->getContext();
+
+		$blockedNotifications = array();
+		$emailSettings = array();
+		foreach($this->getNotificationSettingsMap() as $settingId => $notificationSetting) {
+			// Get notifications that the user wants blocked
+			if(!$this->getData($notificationSetting['settingName'])) $blockedNotifications[] = $settingId;
+			// Get notifications that the user wants to be notified of by email
+			if($this->getData($notificationSetting['emailSettingName'])) $emailSettings[] = $settingId;
+		}
+
+		$notificationSubscriptionSettingsDao = DAORegistry::getDAO('NotificationSubscriptionSettingsDAO');
+		$notificationSubscriptionSettingsDao->updateNotificationSubscriptionSettings('blocked_notification', $blockedNotifications, $userId, $context->getId());
+		$notificationSubscriptionSettingsDao->updateNotificationSubscriptionSettings('blocked_emailed_notification', $emailSettings, $userId, $context->getId());
+
+		return true;
 	}
 }
 

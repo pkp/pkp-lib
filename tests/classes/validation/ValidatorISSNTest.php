bc3ec028c7134ff15ed5e092fef838a97be7fd09
Clinton Graham <ctgraham@pitt.edu>
2015-05-20 09:31:32 -0400

    pkp/pkp-lib#529 Fix ISSN Validator to properly calculate checksum for 0 and X cases.

--- tests/classes/validation/ValidatorISSNTest.php
+++ tests/classes/validation/ValidatorISSNTest.php
@@ -28,6 +28,8 @@ class ValidatorISSNTest extends PKPTestCase {
 		self::assertTrue($validator->isValid('0378-5955')); // Valid
 		self::assertFalse($validator->isValid('03785955')); // Missing dash
 		self::assertFalse($validator->isValid('1234-5678')); // Wrong check digit
+		self::assertTrue($validator->isValid('0031-790X')); // Check digit is X
+		self::assertTrue($validator->isValid('1945-2020')); // Check digit is 0
 	}
 }
 

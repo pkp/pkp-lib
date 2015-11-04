d32654912e4d3fad478686033b47f0b9a2c8eb84
Jason Nugent <jason.nugent@gmail.com>
2015-11-04 10:59:17 -0400

    pkp/pkp-lib#862 including logging for webservice requests to error log

--- classes/webservice/XmlWebService.inc.php
+++ classes/webservice/XmlWebService.inc.php
@@ -63,7 +63,9 @@ class XmlWebService extends WebService {
 	function &call(&$webServiceRequest) {
 		// Call the web service
 		$xmlResult = parent::call($webServiceRequest);
-
+		if (Config::getVar('debug', 'log_web_service_info')) {
+			error_log('Time: ' . date('c') . "\nRequest: " . print_r($webServiceRequest, true) . "\nResponse: " . print__r($xmlResult, true) . "\nLast response status: " . $this->_lastResponseStatus . "\n", 0);
+		}
 		// Catch web service errors
 		if (is_null($xmlResult)) return $xmlResult;
 

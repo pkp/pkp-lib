a99e7f580fa180bf91a8c7a0f72f1108c74cffac
bozana <bozana.bokan@posteo.net>
2015-11-13 23:12:47 +0100

    use setStoredPubId function

--- classes/submission/SubmissionDAO.inc.php
+++ classes/submission/SubmissionDAO.inc.php
@@ -240,7 +240,7 @@ abstract class SubmissionDAO extends DAO {
 	 */
 	function changePubId($submissionId, $pubIdType, $pubId) {
 		$submission = $this->getById($submissionId);
-		$submission->setData('pub-id::'.$pubIdType, $pubId);
+		$submission->setStoredPubId($pubIdType, $pubId);
 		$this->updateObject($submission);
 	}
 

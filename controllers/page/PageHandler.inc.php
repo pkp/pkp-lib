b482eb6ad9994f021f1429a350e77d5968f2fe62
Nate Wright <natew@notthisway.com>
2015-12-08 15:48:18 +0000

    pkp/pkp-lib#910 Switch journals links now send the user to the dashboard

--- controllers/page/PageHandler.inc.php
+++ controllers/page/PageHandler.inc.php
@@ -164,7 +164,7 @@ class PageHandler extends Handler {
 			$dispatcher = $request->getDispatcher();
 			$contextsNameAndUrl = array();
 			while ($workingContext = $workingContexts->next()) {
-				$contextUrl = $dispatcher->url($request, ROUTE_PAGE, $workingContext->getPath());
+				$contextUrl = $dispatcher->url($request, ROUTE_PAGE, $workingContext->getPath(), 'dashboard');
 				$contextsNameAndUrl[$contextUrl] = $workingContext->getLocalizedName();
 			}
 

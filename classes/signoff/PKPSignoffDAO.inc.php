d65ea0371242e42c0bff56d3399bc3230d81219e
Lepidus Tecnologia <lepidus@lepidus.com.br>
2015-12-15 11:45:05 -0200

    [pkp/pkp-lib#1036] Change the signoffs to the new user on merging

--- classes/signoff/PKPSignoffDAO.inc.php
+++ classes/signoff/PKPSignoffDAO.inc.php
@@ -390,8 +390,8 @@ class PKPSignoffDAO extends DAO {
 			SET	user_id = ?
 			WHERE	user_id = ?',
 			array(
-				(int) $oldUserId,
-				(int) $newUserId
+				(int) $newUserId,
+				(int) $oldUserId
 			)
 		);
 	}

3f15d6fd2d1c1334f4de167146a611ef1ef546d8
Bruno Beghelli <bruno.beghelli@gmail.com>
2015-12-16 11:38:00 -0200

    Use phpunit from library to avoid duplicated loading of phpunit classes

--- tools/runAllTests.sh
+++ tools/runAllTests.sh
@@ -158,47 +158,47 @@ while getopts "bCPcpfdH" opt; do
 			;;
 	esac
 done
-
+phpunit='php lib/pkp/lib/vendor/phpunit/phpunit/phpunit'
 REPORT_SWITCH=''
 REPORT_TMP="$TESTS_DIR/results/coverage-tmp"
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_DATA" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_DATA.php"
 	fi
-	phpunit $DEBUG $TEST_CONF1 --debug -v --stop-on-failure --stop-on-skipped $REPORT_SWITCH tests/data
+	$phpunit $DEBUG $TEST_CONF1 --debug -v --stop-on-failure --stop-on-skipped $REPORT_SWITCH tests/data
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_PKP_CLASSES" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-PKP_CLASSES.php"
 	fi
-	phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH lib/pkp/tests/classes
+	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH lib/pkp/tests/classes
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_PKP_PLUGINS" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-PKP_PLUGINS.php"
 	fi
-	phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH lib/pkp/plugins
+	$phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH lib/pkp/plugins
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_CLASSES" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_CLASSES.php"
 	fi
-	phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/classes
+	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/classes
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_PLUGINS" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_PLUGINS.php"
 	fi
-	phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH plugins
+	$phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH plugins
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_FUNCTIONAL" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_FUNCTIONAL.php"
 	fi
-	phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/functional
+	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/functional
 fi

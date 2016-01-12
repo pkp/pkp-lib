9c87ee3310f83277a159499ef83c2d44eb05422b
Alec Smecher <alec@smecher.bc.ca>
2016-01-12 11:49:07 -0800

    pkp/pkp-lib#1039 Fix 4ba8a22 - Show announcements at the published date

--- classes/announcement/AnnouncementDAO.inc.php
+++ classes/announcement/AnnouncementDAO.inc.php
@@ -280,7 +280,8 @@ class AnnouncementDAO extends DAO {
 			FROM announcements
 			WHERE assoc_type = ?
 				AND assoc_id = ?
-				AND (date_expire IS NULL OR date_expire >= CURRENT_DATE)
+				AND (date_expire IS NULL OR DATE(date_expire) > CURRENT_DATE)
+				AND (DATE(date_posted) <= CURRENT_DATE)
 			ORDER BY announcement_id DESC',
 			array((int) $assocType, (int) $assocId),
 			$rangeInfo
@@ -303,7 +304,8 @@ class AnnouncementDAO extends DAO {
 			FROM announcements
 			WHERE assoc_type = ?
 				AND assoc_id = ?
-				AND (date_expire IS NULL OR date_expire >= CURRENT_DATE)
+				AND (date_expire IS NULL OR DATE(date_expire) > CURRENT_DATE)
+				AND (DATE(date_posted) <= CURRENT_DATE)
 			ORDER BY announcement_id DESC LIMIT ?',
 			array((int) $assocType, (int) $assocId, (int) $numAnnouncements),
 			$rangeInfo

09abd92cbaca3bbc7ce9806086cd5742cb7d8bd5
Nate Wright <natew@notthisway.com>
2016-03-07 12:35:02 +0000

    pkp/pkp-lib#1208 Remove signoff file grid and replace with basic file list grid

--- controllers/grid/files/review/WorkflowReviewRevisionsGridHandler.inc.php
+++ controllers/grid/files/review/WorkflowReviewRevisionsGridHandler.inc.php
@@ -13,18 +13,24 @@
  * @brief Display in workflow pages the file revisions that authors have uploaded.
  */
 
-import('lib.pkp.controllers.grid.files.review.ReviewRevisionsGridHandler');
+import('lib.pkp.controllers.grid.files.fileList.FileListGridHandler');
 
-class WorkflowReviewRevisionsGridHandler extends ReviewRevisionsGridHandler {
+class WorkflowReviewRevisionsGridHandler extends FileListGridHandler {
 	/**
 	 * Constructor
 	 */
 	function WorkflowReviewRevisionsGridHandler() {
-		$roleAssignments = array(
-			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT),
-			array('fetchGrid', 'fetchRow', 'signOffFile')
+		import('lib.pkp.controllers.grid.files.review.ReviewGridDataProvider');
+		parent::FileListGridHandler(
+			new ReviewGridDataProvider(SUBMISSION_FILE_REVIEW_REVISION),
+			null,
+			FILE_GRID_ADD|FILE_GRID_EDIT|FILE_GRID_VIEW_NOTES|FILE_GRID_DELETE
 		);
-		parent::ReviewRevisionsGridHandler($roleAssignments);
+
+		$this->addRoleAssignment(
+			array(ROLE_ID_MANAGER, ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT),
+			array('fetchGrid', 'fetchRow', 'addFile')
+		);
 
 		$this->setTitle('editor.submission.revisions');
 	}

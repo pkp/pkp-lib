09260a19c087059b9228a2c3a73c937c10a84e3d
Alec Smecher <alec@smecher.bc.ca>
2016-05-27 11:00:01 +0200

    Verbose output on tests

--- tools/runAllTests.sh
+++ tools/runAllTests.sh
@@ -172,33 +172,33 @@ if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_PKP_CLASSES" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-PKP_CLASSES.php"
 	fi
-	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH lib/pkp/tests/classes
+	$phpunit $DEBUG $TEST_CONF1 --debug -v $REPORT_SWITCH lib/pkp/tests/classes
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_PKP_PLUGINS" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-PKP_PLUGINS.php"
 	fi
-	$phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH lib/pkp/plugins
+	$phpunit $DEBUG $TEST_CONF2 --debug -v $REPORT_SWITCH lib/pkp/plugins
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_CLASSES" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_CLASSES.php"
 	fi
-	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/classes
+	$phpunit $DEBUG $TEST_CONF1 --debug -v $REPORT_SWITCH tests/classes
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_PLUGINS" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_PLUGINS.php"
 	fi
-	$phpunit $DEBUG $TEST_CONF2 $REPORT_SWITCH plugins
+	$phpunit $DEBUG $TEST_CONF2 --debug -v $REPORT_SWITCH plugins
 fi
 
 if [ \( "$DO_ALL" -eq 1 \) -o \( "$DO_APP_FUNCTIONAL" -eq 1 \) ]; then
 	if [ \( "$DO_COVERAGE" -eq 1 \) ]; then
 		REPORT_SWITCH="--coverage-php $REPORT_TMP/coverage-APP_FUNCTIONAL.php"
 	fi
-	$phpunit $DEBUG $TEST_CONF1 $REPORT_SWITCH tests/functional
+	$phpunit $DEBUG $TEST_CONF1 --debug -v $REPORT_SWITCH tests/functional
 fi

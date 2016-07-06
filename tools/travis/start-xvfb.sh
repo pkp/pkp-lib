c25eb6aa09d7f89a59c452b37af1102db9eb4ec3
Alec Smecher <alec@smecher.bc.ca>
2016-07-06 16:08:35 -0700

    Try a specific resolution

--- tools/travis/start-xvfb.sh
+++ tools/travis/start-xvfb.sh
@@ -15,13 +15,15 @@ export DISPLAY=":99.0" # Travis init script for xvfb specifies this
 
 # Xvfb requirements.
 sudo apt-get install -y x11-xkb-utils
+
 # Add fonts.
 sudo apt-get install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
 sudo apt-get install -y defoma x-ttcidfont-conf cabextract ttf-mscorefonts-installer
 sudo dpkg-reconfigure --default-priority x-ttcidfont-conf
 mkfontdir
+
 # Start Virtual Framebuffer to imitate a monitor.
-sh -e /etc/init.d/xvfb start
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 
 # Wait for xvfb to start.
 until xprop -root; do sleep 1; done

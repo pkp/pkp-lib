12c7be362bfb76c18302116b5f5a4cee86156794
Alec Smecher <alec@smecher.bc.ca>
2016-08-27 07:43:22 -0700

    (Temporarily) fix moved download of Google Java tools

--- tools/travis/install-linter.sh
+++ tools/travis/install-linter.sh
@@ -12,11 +12,13 @@
 set -xe
 
 # Install python, linter, closure compiler stuff
-sudo easy_install "http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz"
-wget -O compiler.zip "https://closure-compiler.googlecode.com/files/compiler-20120917.zip"
-unzip compiler.zip compiler.jar
+sudo apt-get install python-pip
+sudo  pip install https://github.com/google/closure-linter/zipball/master
+# wget -O compiler.zip "http://dl.google.com/closure-compiler/compiler-20130603.zip"
+# unzip compiler.zip compiler.jar
 mkdir ~/bin
-mv compiler.jar ~/bin
-wget "http://jslint4java.googlecode.com/files/jslint4java-2.0.2-dist.zip"
+# mv compiler.jar ~/bin/compiler.jar
+cp lib/pkp/tools/travis/compiler.jar ~/bin
+wget "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jslint4java/jslint4java-2.0.2-dist.zip"
 unzip jslint4java-2.0.2-dist.zip
 mv jslint4java-2.0.2/jslint4java-2.0.2.jar ~/bin/jslint4java.jar

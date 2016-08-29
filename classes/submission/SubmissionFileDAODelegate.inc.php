4dd86f617029b9501982387731d427504b752c12
Bozana Bokan <bozana.bokan@posteo.net>
2016-08-29 20:32:03 +0200

    pkp/pkp-lib#1663 anonymous default file names in review stage

--- classes/submission/SubmissionFileDAODelegate.inc.php
+++ classes/submission/SubmissionFileDAODelegate.inc.php
@@ -89,8 +89,19 @@ class SubmissionFileDAODelegate extends DAO {
 			$submissionFile->setFileId($this->_getInsertId('submission_files', 'file_id'));
 		}
 
+		$reviewStage = $submissionFile->getFileStage() == SUBMISSION_FILE_REVIEW_FILE ||
+			$submissionFile->getFileStage() == SUBMISSION_FILE_REVIEW_ATTACHMENT ||
+			$submissionFile->getFileStage() == SUBMISSION_FILE_REVIEW_REVISION;
 		if (!$submissionFile->getName(AppLocale::getPrimaryLocale())) {
-			$submissionFile->setName($submissionFile->_generateName(), AppLocale::getPrimaryLocale());
+			if ($reviewStage) {
+				$submissionFile->setName($submissionFile->_generateName(true), AppLocale::getPrimaryLocale());
+			} else {
+				$submissionFile->setName($submissionFile->_generateName(), AppLocale::getPrimaryLocale());
+			}
+		} else {
+			if ($reviewStage &&	$submissionFile->getName(AppLocale::getPrimaryLocale()) == $submissionFile->_generateName()) {
+				$submissionFile->setName($submissionFile->_generateName(true), AppLocale::getPrimaryLocale());
+			}
 		}
 
 		$this->updateLocaleFields($submissionFile);

3a2c003d37d0bc391c894628b35f5b40bfd66f3b
Alec Smecher <alec@smecher.bc.ca>
2016-10-17 15:08:30 -0700

    pkp/pkp-lib#1893 Force CSS for text/plain or text/x-c

--- classes/file/FileManager.inc.php
+++ classes/file/FileManager.inc.php
@@ -93,6 +93,10 @@ class FileManager {
 	function getUploadedFileType($fileName) {
 		if (isset($_FILES[$fileName])) {
 			$type = PKPString::mime_content_type($_FILES[$fileName]['tmp_name']);
+
+			// pkp/pkp-lib#1893 Force text/css for .css files
+			if (in_array($type, array('text/plain', 'text/x-c')) && preg_match('/\.css$/i', $_FILES[$fileName]['name'])) return 'text/css';
+
 			if (!empty($type)) return $type;
 			return $_FILES[$fileName]['type'];
 		}

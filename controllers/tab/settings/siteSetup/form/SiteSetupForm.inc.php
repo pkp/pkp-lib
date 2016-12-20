pkp/pkp-lib

commit 3d68ac9b717a25c2df9c9a6360988f53dca64e7e

Syntax cleanup

controllers/tab/settings/siteSetup/form/SiteSetupForm.inc.php
@@ -59,18 +59,19 @@ class SiteSetupForm extends PKPSiteSettingsForm {
 		$cssView = $this->renderFileView($cssSettingName, $request);
 		$imageView = $this->renderFileView($imageSettingName, $request);
 
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('locale', AppLocale::getLocale());
-		$templateMgr->assign('siteStyleFileExists', file_exists($siteStyleFilename));
-		$templateMgr->assign('uploadCssLinkAction', $uploadCssLinkAction);
-		$templateMgr->assign('uploadImageLinkAction', $uploadImageLinkAction);
-		$templateMgr->assign('cssView', $cssView);
-		$templateMgr->assign('imageView', $imageView);
-		$templateMgr->assign('redirectOptions', $contexts);
-		$templateMgr->assign('pageHeaderTitleImage', $site->getSetting($imageSettingName));
-
 		$application = Application::getApplication();
-		$templateMgr->assign('availableMetricTypes', $application->getMetricTypes(true));
+		$templateMgr = TemplateManager::getManager($request);
+		$templateMgr->assign(array(
+			'locale' => AppLocale::getLocale(),
+			'siteStyleFileExists' => file_exists($siteStyleFilename),
+			'uploadCssLinkAction' => $uploadCssLinkAction,
+			'uploadImageLinkAction' => $uploadImageLinkAction,
+			'cssView' => $cssView,
+			'imageView' => $imageView,
+			'redirectOptions' => $contexts,
+			'pageHeaderTitleImage' => $site->getSetting($imageSettingName),
+			'availableMetricTypes' => $application->getMetricTypes(true),
+		));
 
 		$themePlugins = PluginRegistry::getPlugins('themes');
 		$enabledThemes = array();
@@ -208,12 +209,12 @@ class SiteSetupForm extends PKPSiteSettingsForm {
 			}
 
 			$publicFileManager = $publicFileManager = new PublicFileManager();
-
-			$templateMgr->assign('publicFilesDir', $request->getBasePath() . '/' . $publicFileManager->getSiteFilesPath());
-			$templateMgr->assign('file', $file);
-			$templateMgr->assign('deleteLinkAction', $deleteLinkAction);
-			$templateMgr->assign('fileSettingName', $fileSettingName);
-
+			$templateMgr->assign(array(
+				'publicFilesDir' => $request->getBasePath() . '/' . $publicFileManager->getSiteFilesPath())			'file', $file,
+				'file' => $file,
+				'deleteLinkAction' => $deleteLinkAction,
+				'fileSettingName' => $fileSettingName,
+			));
 			return $templateMgr->fetch($template);
 		} else {
 			return null;

classes/admin/form/PKPSiteSettingsForm.inc.php
@@ -49,15 +49,17 @@ class PKPSiteSettingsForm extends Form {
 		$publicFileManager = new PublicFileManager();
 		$siteStyleFilename = $publicFileManager->getSiteFilesPath() . '/' . $site->getSiteStyleFilename();
 		$templateMgr = TemplateManager::getManager();
-		$templateMgr->assign('showThumbnail', $site->getSetting('showThumbnail'));
-		$templateMgr->assign('showTitle', $site->getSetting('showTitle'));
-		$templateMgr->assign('showDescription', $site->getSetting('showDescription'));
-		$templateMgr->assign('originalStyleFilename', $site->getOriginalStyleFilename());
-		$templateMgr->assign('pageHeaderTitleImage', $site->getSetting('pageHeaderTitleImage'));
-		$templateMgr->assign('styleFilename', $site->getSiteStyleFilename());
-		$templateMgr->assign('publicFilesDir', Request::getBasePath() . '/' . $publicFileManager->getSiteFilesPath());
-		$templateMgr->assign('dateStyleFileUploaded', file_exists($siteStyleFilename)?filemtime($siteStyleFilename):null);
-		$templateMgr->assign('siteStyleFileExists', file_exists($siteStyleFilename));
+		$templateMgr->assign(array(
+			'showThumbnail' => $site->getSetting('showThumbnail'),
+			'showTitle' => $site->getSetting('showTitle'),
+			'showDescription' => $site->getSetting('showDescription'),
+			'originalStyleFilename' => $site->getOriginalStyleFilename(),
+			'pageHeaderTitleImage' => $site->getSetting('pageHeaderTitleImage'),
+			'styleFilename' => $site->getSiteStyleFilename(),
+			'publicFilesDir' => Request::getBasePath() . '/' . $publicFileManager->getSiteFilesPath(),
+			'dateStyleFileUploaded' => file_exists($siteStyleFilename)?filemtime($siteStyleFilename):null,
+			'siteStyleFileExists' => file_exists($siteStyleFilename),
+		));
 		return parent::display();
 	}
 

classes/controllers/grid/GridCellProvider.inc.php
@@ -50,11 +50,13 @@ class GridCellProvider {
 		foreach ($templateVars as $varName => $varValue) {
 			$templateMgr->assign($varName, $varValue);
 		}
-		$templateMgr->assign('id', $cellId);
-		$templateMgr->assign('column', $column);
-		$templateMgr->assign('actions', $this->getCellActions($request, $row, $column));
-		$templateMgr->assign('flags', $column->getFlags());
-		$templateMgr->assign('formLocales', AppLocale::getSupportedFormLocales());
+		$templateMgr->assign(array(
+			'id' => $cellId,
+			'column' => $column,
+			'actions' => $this->getCellActions($request, $row, $column),
+			'flags' => $column->getFlags(),
+			'formLocales' => AppLocale::getSupportedFormLocales(),
+		));
 		$template = $column->getTemplate();
 		assert(!empty($template));
 		return $templateMgr->fetch($template);

classes/controllers/grid/GridHandler.inc.php
@@ -1011,10 +1011,12 @@ class GridHandler extends PKPHandler {
 
 		// Pass control to the view to render the row
 		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('grid', $this);
-		$templateMgr->assign('columns', $columns);
-		$templateMgr->assign('cells', $renderedCells);
-		$templateMgr->assign('row', $row);
+		$templateMgr->assign(array(
+			'grid' => $this,
+			'columns' => $columns,
+			'cells' => $renderedCells,
+			'row' => $row,
+		));
 		return $templateMgr->fetch($row->getTemplate());
 	}
 

classes/controllers/grid/feature/InfiniteScrollingFeature.inc.php
@@ -42,7 +42,6 @@ class InfiniteScrollingFeature extends GeneralPagingFeature {
 	 * @copydoc GridFeature::fetchUIElements()
 	 */
 	function fetchUIElements($request, $grid) {
-		$iterator = $this->getItemIterator();
 		$options = $this->getOptions();
 
 		$shown = $options['currentItemsPerPage'] * $options['currentPage'];
@@ -60,10 +59,12 @@ class InfiniteScrollingFeature extends GeneralPagingFeature {
 		}
 
 		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('iterator', $iterator);
-		$templateMgr->assign('shown', $shown);
-		$templateMgr->assign('grid', $grid);
-		$templateMgr->assign('moreItemsLinkAction', $moreItemsLinkAction);
+		$templateMgr->assign(array(
+			'iterator' => $this->getItemIterator(),
+			'shown' => $shown,
+			'grid' => $grid,
+			'moreItemsLinkAction' => $moreItemsLinkAction,
+		));
 
 		return array(
 			'pagingMarkup' => $templateMgr->fetch('controllers/grid/feature/infiniteScrolling.tpl'),

classes/controllers/grid/feature/PagingFeature.inc.php
@@ -43,14 +43,13 @@ class PagingFeature extends GeneralPagingFeature {
 	 * @copydoc GridFeature::fetchUIElements()
 	 */
 	function fetchUIElements($request, $grid) {
-		$iterator = $this->getItemIterator();
 		$options = $this->getOptions();
-
 		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('iterator', $iterator);
-		$templateMgr->assign('currentItemsPerPage', $options['currentItemsPerPage']);
-		$templateMgr->assign('grid', $grid);
-
+		$templateMgr->assign(array(
+			'iterator' => $this->getItemIterator(),
+			'currentItemsPerPage' => $options['currentItemsPerPage'],
+			'grid' => $grid,
+		));
 		return array('pagingMarkup' => $templateMgr->fetch('controllers/grid/feature/gridPaging.tpl'));
 	}
 

classes/form/FormBuilderVocabulary.inc.php
@@ -114,13 +114,14 @@ class FormBuilderVocabulary {
 	 */
 	function smartyFBVFormArea($params, $content, &$smarty, &$repeat) {
 		assert(isset($params['id']));
-
 		if (!$repeat) {
-			$smarty->assign('FBV_class', isset($params['class']) ? $params['class'] : null);
-			$smarty->assign('FBV_id', $params['id']);
-			$smarty->assign('FBV_content', isset($content) ? $content : null);
-			$smarty->assign('FBV_translate', isset($params['translate']) ? $params['translate'] : true);
-			$smarty->assign('FBV_title', isset($params['title']) ? $params['title'] : null);
+			$smarty->assign(array(
+				'FBV_class' => isset($params['class']) ? $params['class'] : null,
+				'FBV_id' => $params['id'],
+				'FBV_content' => isset($content) ? $content : null,
+				'FBV_translate' => isset($params['translate']) ? $params['translate'] : true,
+				'FBV_title' => isset($params['title']) ? $params['title'] : null,
+			));
 			return $smarty->fetch('form/formArea.tpl');
 		}
 		return '';
@@ -152,13 +153,14 @@ class FormBuilderVocabulary {
 			} else {
 				$smarty->assign('FBV_labelFor', null);
 			}
-			$smarty->assign('FBV_title', isset($params['title']) ? $params['title'] : null);
-			$smarty->assign('FBV_label', isset($params['label']) ? $params['label'] : null);
-			$smarty->assign('FBV_layoutInfo', $this->_getLayoutInfo($params));
-			$smarty->assign('FBV_description', isset($params['description']) ? $params['description'] : null);
-			$smarty->assign('FBV_content', isset($content) ? $content: null);
-			// default is to perform translation:
-			$smarty->assign('FBV_translate', isset($params['translate']) ? $params['translate'] : true);
+			$smarty->assign(array(
+				'FBV_title' => isset($params['title']) ? $params['title'] : null,
+				'FBV_label' => isset($params['label']) ? $params['label'] : null,
+				'FBV_layoutInfo' => $this->_getLayoutInfo($params),
+				'FBV_description' => isset($params['description']) ? $params['description'] : null,
+				'FBV_content' => isset($content) ? $content: null,
+				'FBV_translate' => isset($params['translate']) ? $params['translate'] : true,
+			));
 
 			$class = $params['class'];
 
@@ -202,20 +204,17 @@ class FormBuilderVocabulary {
 	 * @param $repeat
 	 */
 	function smartyFBVFormButtons($params, &$smarty) {
-		// Submit button options.
-		$smarty->assign('FBV_submitText', isset($params['submitText']) ? $params['submitText'] : 'common.ok');
-		$smarty->assign('FBV_submitDisabled', isset($params['submitDisabled']) ? (boolean)$params['submitDisabled'] : false);
-		$smarty->assign('FBV_confirmSubmit', isset($params['confirmSubmit']) ? $params['confirmSubmit'] : null);
-
-		// Cancel button options.
-		$smarty->assign('FBV_cancelText', isset($params['cancelText']) ? $params['cancelText'] : 'common.cancel');
-		$smarty->assign('FBV_hideCancel', isset($params['hideCancel']) ? (boolean)$params['hideCancel'] : false);
-		$smarty->assign('FBV_cancelAction', isset($params['cancelAction']) ? $params['cancelAction'] : null);
-		$smarty->assign('FBV_cancelUrl', isset($params['cancelUrl']) ? $params['cancelUrl'] : null);
-		$smarty->assign('FBV_cancelUrlTarget', isset($params['cancelUrlTarget']) ? $params['cancelUrlTarget'] : '');
-
-		$smarty->assign('FBV_translate', isset($params['translate']) ? $params['translate'] : true);
-
+		$smarty->assign(array(
+			'FBV_submitText' => isset($params['submitText']) ? $params['submitText'] : 'common.ok',
+			'FBV_submitDisabled' => isset($params['submitDisabled']) ? (boolean)$params['submitDisabled'] : false,
+			'FBV_confirmSubmit' => isset($params['confirmSubmit']) ? $params['confirmSubmit'] : null,
+			'FBV_cancelText' => isset($params['cancelText']) ? $params['cancelText'] : 'common.cancel',
+			'FBV_hideCancel' => isset($params['hideCancel']) ? (boolean)$params['hideCancel'] : false,
+			'FBV_cancelAction' => isset($params['cancelAction']) ? $params['cancelAction'] : null,
+			'FBV_cancelUrl' => isset($params['cancelUrl']) ? $params['cancelUrl'] : null,
+			'FBV_cancelUrlTarget' => isset($params['cancelUrlTarget']) ? $params['cancelUrlTarget'] : '',
+			'FBV_translate' => isset($params['translate']) ? $params['translate'] : true,
+		));
 		return $smarty->fetch('form/formButtons.tpl');
 	}
 
@@ -229,15 +228,17 @@ class FormBuilderVocabulary {
 		if (!isset($params['id'])) $smarty->trigger_error('FBV: Element ID not set');
 
 		// Set up the element template
-		$smarty->assign('FBV_id', $params['id']);
-		$smarty->assign('FBV_class', isset($params['class']) ? $params['class'] : null);
-		$smarty->assign('FBV_required', isset($params['required']) ? $params['required'] : false);
-		$smarty->assign('FBV_layoutInfo', $this->_getLayoutInfo($params));
-		$smarty->assign('FBV_label', isset($params['label']) ? $params['label'] : null);
-		$smarty->assign('FBV_for', isset($params['for']) ? $params['for'] : null);
-		$smarty->assign('FBV_tabIndex', isset($params['tabIndex']) ? $params['tabIndex'] : null);
-		$smarty->assign('FBV_translate', isset($params['translate']) ? $params['translate'] : true);
-		$smarty->assign('FBV_keepLabelHtml', isset($params['keepLabelHtml']) ? $params['keepLabelHtml'] : false);
+		$smarty->assign(array(
+			'FBV_id' => $params['id'],
+			'FBV_class' => isset($params['class']) ? $params['class'] : null,
+			'FBV_required' => isset($params['required']) ? $params['required'] : false,
+			'FBV_layoutInfo' => $this->_getLayoutInfo($params),
+			'FBV_label' => isset($params['label']) ? $params['label'] : null,
+			'FBV_for' => isset($params['for']) ? $params['for'] : null,
+			'FBV_tabIndex' => isset($params['tabIndex']) ? $params['tabIndex'] : null,
+			'FBV_translate' => isset($params['translate']) ? $params['translate'] : true,
+			'FBV_keepLabelHtml' => isset($params['keepLabelHtml']) ? $params['keepLabelHtml'] : false,
+		));
 
 		// Unset these parameters so they don't get assigned twice
 		unset($params['class']);
@@ -390,18 +391,15 @@ class FormBuilderVocabulary {
 	function _smartyFBVRangeSlider($params, &$smarty) {
 		// Make sure our required fields are included
 		assert(isset($params['min']) && isset($params['max']));
-
-		// Assign the min and max values to the handler
-		$smarty->assign('FBV_min', $params['min']);
-		$smarty->assign('FBV_max', $params['max']);
-
-		$smarty->assign('FBV_value_min', isset($params['valueMin']) ? $params['valueMin'] : $params['min']);
-		$smarty->assign('FBV_value_max', isset($params['valueMax']) ? $params['valueMax'] : $params['max']);
-
-		$smarty->assign('FBV_toggleable', isset($params['toggleable']) ? $params['toggleable'] : false);
-		$smarty->assign('FBV_toggleable_label', isset($params['toggleable_label']) ? $params['toggleable_label'] : '');
-		$smarty->assign('FBV_enabled', isset($params['enabled']) ? $params['enabled'] : false);
-
+		$smarty->assign(array(
+			'FBV_min' => $params['min'],
+			'FBV_max' => $params['max'],
+			'FBV_value_min' => isset($params['valueMin']) ? $params['valueMin'] : $params['min'],
+			'FBV_value_max' => isset($params['valueMax']) ? $params['valueMax'] : $params['max'],
+			'FBV_toggleable' => isset($params['toggleable']) ? $params['toggleable'] : false,
+			'FBV_toggleable_label' => isset($params['toggleable_label']) ? $params['toggleable_label'] : '',
+			'FBV_enabled' => isset($params['enabled']) ? $params['enabled'] : false,
+		));
 		return $smarty->fetch('form/rangeSlider.tpl');
 	}
 

classes/notification/PKPNotificationOperationManager.inc.php
@@ -413,19 +413,21 @@ abstract class PKPNotificationOperationManager implements INotificationInfoProvi
 			$notification->setDateRead($dateRead);
 		}
 
-		$templateMgr->assign('notificationDateCreated', $notification->getDateCreated());
-		$templateMgr->assign('notificationId', $notification->getId());
-		$templateMgr->assign('notificationContents',$this->getNotificationContents($request, $notification));
-		$templateMgr->assign('notificationTitle',$this->getNotificationTitle($notification));
-		$templateMgr->assign('notificationStyleClass', $this->getStyleClass($notification));
-		$templateMgr->assign('notificationIconClass', $this->getIconClass($notification));
-		$templateMgr->assign('notificationDateRead', $notification->getDateRead());
+		$user = $request->getUser();
+		$templateMgr->assign(array(
+			'isUserLoggedIn' => $user,
+			'notificationDateCreated' => $notification->getDateCreated(),
+			'notificationId' => $notification->getId(),
+			'notificationContents' => $this->getNotificationContents($request, $notification),
+			'notificationTitle' => $this->getNotificationTitle($notification),
+			'notificationStyleClass' => $this->getStyleClass($notification),
+			'notificationIconClass' => $this->getIconClass($notification),
+			'notificationDateRead' => $notification->getDateRead(),
+		));
+
 		if($notification->getLevel() != NOTIFICATION_LEVEL_TRIVIAL) {
 			$templateMgr->assign('notificationUrl', $this->getNotificationUrl($request, $notification));
 		}
-
-		$user = $request->getUser();
-		$templateMgr->assign('isUserLoggedIn', $user);
 
 		return $templateMgr->fetch($notificationTemplate);
 	}

commit 48d44f4c8dd21cba789f66fe5b4e57bcb78ed034

pkp/pkp-lib#1859 Fix syntax error introduced in 3d68ac9b7

controllers/tab/settings/siteSetup/form/SiteSetupForm.inc.php
@@ -210,7 +210,7 @@ class SiteSetupForm extends PKPSiteSettingsForm {
 
 			$publicFileManager = $publicFileManager = new PublicFileManager();
 			$templateMgr->assign(array(
-				'publicFilesDir' => $request->getBasePath() . '/' . $publicFileManager->getSiteFilesPath())			'file', $file,
+				'publicFilesDir' => $request->getBasePath() . '/' . $publicFileManager->getSiteFilesPath(),
 				'file' => $file,
 				'deleteLinkAction' => $deleteLinkAction,
 				'fileSettingName' => $fileSettingName,
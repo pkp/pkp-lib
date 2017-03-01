6f9c8bfe9a7d3c72dbeff288a89764cd63a71f64
Nate Wright <natew@notthisway.com>
2017-03-01 12:24:02 +0000

    pkp/pkp-lib#2163 Implement footer and loading more results

--- js/controllers/list/ListPanelCount.vue
+++ js/controllers/list/ListPanelCount.vue
@@ -1,12 +1,21 @@
 <template>
 	<div class="pkpListPanelCount">
-		{{ __('itemCount', { count: this.count }) }}
+		{{ countString }}
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'ListPanelCount',
-	props: ['count', 'i18n'],
+	props: ['count', 'total', 'i18n'],
+	computed: {
+		countString: function() {
+			if (this.total) {
+				return this.__('itemsOfTotal', { count: this.count, total: this.total });
+			} else {
+				return this.__('itemCount', { count: this.count });
+			}
+		}
+	}
 }
 </script>

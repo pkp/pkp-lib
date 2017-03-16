8a03743be11ceeae51e53752a6396c2c52764136
Bozana Bokan <bozana.bokan@posteo.net>
2017-03-16 11:05:07 +0100

    Resolve PHP7 complaints

--- classes/core/PKPRequest.inc.php
+++ classes/core/PKPRequest.inc.php
@@ -169,10 +169,10 @@ class PKPRequest {
 			# Strip the PHP filename off of the script's executed path
 			# We expect the SCRIPT_NAME to look like /path/to/file.php
 			# If the SCRIPT_NAME ends in /, assume this is the directory and the script's actual name is masked as the DirectoryIndex
-			# If the SCRIPT_NAME ends in neither / or .php, assume the the script's actual name is masked and we need to avoid stripping the terminal directory 
+			# If the SCRIPT_NAME ends in neither / or .php, assume the the script's actual name is masked and we need to avoid stripping the terminal directory
 			$path = preg_replace('#/[^/]*$#', '', $_SERVER['SCRIPT_NAME'].(substr($_SERVER['SCRIPT_NAME'], -1) == '/' || preg_match('#.php$#i', $_SERVER['SCRIPT_NAME']) ? '' : '/'));
 
-			// Encode charcters which need to be encoded in a URL.
+			// Encode characters which need to be encoded in a URL.
 			// Simply using rawurlencode() doesn't work because it
 			// also encodes characters which are valid in a URL (i.e. @, $).
 			$parts = explode('/', $path);
@@ -310,7 +310,7 @@ class PKPRequest {
 			if ($_this->isRestfulUrlsEnabled()) {
 				$_this->_requestPath = $_this->getBasePath();
 			} else {
-				$_this->_requestPath = $_SERVER['SCRIPT_NAME'];
+				$_this->_requestPath = isset($_SERVER['SCRIPT_NAME'])?$_SERVER['SCRIPT_NAME']:'';
 			}
 
 			if ($_this->isPathInfoEnabled()) {

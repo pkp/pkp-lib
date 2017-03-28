8620e850d9781a257ee27358b844207a2d6bfb12
Nate Wright <natew@notthisway.com>
2017-03-28 16:28:46 +0100

    pkp/pkp-lib#2163 Add API base URL to JS data package loaded with each template

--- classes/template/PKPTemplateManager.inc.php
+++ classes/template/PKPTemplateManager.inc.php
@@ -635,6 +635,7 @@ class PKPTemplateManager extends Smarty {
 
 		$application = PKPApplication::getApplication();
 		$context = $this->_request->getContext();
+		$contextPath = isset($context) ? '/' . $context->getPath() : '';
 
 		// Instantiate the namespace
 		$output = '$.pkp = $.pkp || {};';
@@ -643,7 +644,7 @@ class PKPTemplateManager extends Smarty {
 		import('lib.pkp.classes.security.Role');
 		$app_data = array(
 			'baseUrl' => $this->_request->getBaseUrl(),
-			'apiBaseUrl' => $this->_request->getBaseUrl() . '/index.php/' . $context->getPath() . '/api/v1',
+			'apiBaseUrl' => $this->_request->getBaseUrl() . '/index.php' . $contextPath . '/api/v1',
 			'accessRoles' => array(
 				'manager' => ROLE_ID_MANAGER,
 				'siteAdmin' => ROLE_ID_SITE_ADMIN,

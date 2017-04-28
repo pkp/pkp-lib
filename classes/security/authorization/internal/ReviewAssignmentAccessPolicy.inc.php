9817128d4d376bf043bb290be2a9a0158dcd3d8a
Alec Smecher <alec@smecher.bc.ca>
2017-04-28 11:31:26 -0700

    pkp/pkp-lib#1828 Fix back-end access to declined review assignment

--- classes/security/authorization/internal/ReviewAssignmentAccessPolicy.inc.php
+++ classes/security/authorization/internal/ReviewAssignmentAccessPolicy.inc.php
@@ -21,13 +21,18 @@ class ReviewAssignmentAccessPolicy extends AuthorizationPolicy {
 	/** @var PKPRequest */
 	var $_request;
 
+	/** @var bool */
+	var $_permitDeclined;
+
 	/**
 	 * Constructor
 	 * @param $request PKPRequest
+	 * @param $permitDeclined bool True if declined reviews are acceptable.
 	 */
-	function __construct($request) {
+	function __construct($request, $permitDeclined = false) {
 		parent::__construct('user.authorization.submissionReviewer');
 		$this->_request = $request;
+		$this->_permitDeclined = $permitDeclined;
 	}
 
 	//
@@ -49,13 +54,15 @@ class ReviewAssignmentAccessPolicy extends AuthorizationPolicy {
 		$reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO'); /* @var $reviewAssignmentDao ReviewAssignmentDAO */
 		$reviewAssignment = $reviewAssignmentDao->getLastReviewRoundReviewAssignmentByReviewer($submission->getId(), $user->getId());
 
-		if (is_a($reviewAssignment, 'ReviewAssignment')) {
-			// Save the review assignment to the authorization context.
-			$this->addAuthorizedContextObject(ASSOC_TYPE_REVIEW_ASSIGNMENT, $reviewAssignment);
-			return AUTHORIZATION_PERMIT;
-		} else {
-			return AUTHORIZATION_DENY;
-		}
+		// Ensure a valid review assignment was fetched from the database
+		if (!is_a($reviewAssignment, 'ReviewAssignment')) return AUTHORIZATION_DENY;
+
+		// Ensure that the assignment isn't declined, unless that's permitted
+		if (!$this->_permitDeclined && $reviewAssignment->getDeclined()) return AUTHORIZATION_DENY;
+
+		// Save the review assignment to the authorization context.
+		$this->addAuthorizedContextObject(ASSOC_TYPE_REVIEW_ASSIGNMENT, $reviewAssignment);
+		return AUTHORIZATION_PERMIT;
 	}
 }
 

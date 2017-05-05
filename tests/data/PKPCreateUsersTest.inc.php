2c367a42dc9fde1c865795267313ff39d0abfd4c
Alec Smecher <alec@smecher.bc.ca>
2017-05-05 14:16:47 -0700

    pkp/pkp-lib#2495 Allow create user function to handle multiple users at a go

--- tests/data/PKPCreateUsersTest.inc.php
+++ tests/data/PKPCreateUsersTest.inc.php
@@ -17,51 +17,54 @@ import('lib.pkp.tests.WebTestCase');
 
 class PKPCreateUsersTest extends WebTestCase {
 	/**
-	 * Create a user account.
-	 * @param $data array
+	 * Creat user accounts.
+	 * @param $users array
 	 */
-	protected function createUser($data) {
-		// Come up with sensible defaults for data not supplied
-		$username = $data['username'];
-		$data = array_merge(array(
-			'email' => $username . '@mailinator.com',
-			'password' => $username . $username,
-			'password2' => $username . $username,
-			'roles' => array()
-		), $data);
-
+	protected function createUsers($users) {
 		$this->open(self::$baseUrl);
 		$this->waitForElementPresent($selector='css=li.profile a:contains(\'Dashboard\')');
 		$this->clickAndWait($selector);
 		$this->waitForElementPresent($selector='link=Users & Roles');
 		$this->click($selector);
-		$this->waitForElementPresent($selector='css=[id^=component-grid-settings-user-usergrid-addUser-button-]');
-		$this->click($selector);
-		$this->waitForElementPresent('css=[id^=firstName-]');
 
-		// Fill in user data
-		$this->type('css=[id^=firstName-]', $data['firstName']);
-		$this->type('css=[id^=lastName-]', $data['lastName']);
-		$this->type('css=[id^=username-]', $username);
-		$this->type('css=[id^=email-]', $data['email']);
-		$this->type('css=[id^=password-]', $data['password']);
-		$this->type('css=[id^=password2-]', $data['password2']);
-		if (isset($data['country'])) $this->select('id=country', $data['country']);
-		if (isset($data['affiliation'])) $this->type('css=[id^=affiliation-]', $data['affiliation']);
-		$this->click('css=[id=mustChangePassword]'); // Uncheck the reset password requirement
-		$this->click('//button[text()=\'OK\']');
-		$this->waitJQuery();
+		foreach ($users as $data) {
+			// Come up with sensible defaults for data not supplied
+			$username = $data['username'];
+			$data = array_merge(array(
+				'email' => $username . '@mailinator.com',
+				'password' => $username . $username,
+				'password2' => $username . $username,
+				'roles' => array()
+			), $data);
 
-		// Roles
-		foreach ($data['roles'] as $role) {
-			$this->waitForElementPresent('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]');
-			$this->clickAt('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]', '10,10');
-			$this->waitForElementPresent('//select[@name=\'newRowId[name]\']//option[text()=\'' . $role . '\']');
-			$this->select('name=newRowId[name]', $role);
+			$this->waitForElementPresent($selector='css=[id^=component-grid-settings-user-usergrid-addUser-button-]');
+			$this->click($selector);
+			$this->waitForElementPresent('css=[id^=firstName-]');
+
+			// Fill in user data
+			$this->type('css=[id^=firstName-]', $data['firstName']);
+			$this->type('css=[id^=lastName-]', $data['lastName']);
+			$this->type('css=[id^=username-]', $username);
+			$this->type('css=[id^=email-]', $data['email']);
+			$this->type('css=[id^=password-]', $data['password']);
+			$this->type('css=[id^=password2-]', $data['password2']);
+			if (isset($data['country'])) $this->select('id=country', $data['country']);
+			if (isset($data['affiliation'])) $this->type('css=[id^=affiliation-]', $data['affiliation']);
+			$this->click('css=[id=mustChangePassword]'); // Uncheck the reset password requirement
+			$this->click('//button[text()=\'OK\']');
 			$this->waitJQuery();
-		}
 
-		$this->click('//button[text()=\'Save\']');
-		$this->waitForElementNotPresent('css=div.pkp_modal_panel');
+			// Roles
+			foreach ($data['roles'] as $role) {
+				$this->waitForElementPresent('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]');
+				$this->clickAt('css=[id^=component-listbuilder-users-userusergrouplistbuilder-addItem-button-]', '10,10');
+				$this->waitForElementPresent('//select[@name=\'newRowId[name]\']//option[text()=\'' . $role . '\']');
+				$this->select('name=newRowId[name]', $role);
+				$this->waitJQuery();
+			}
+
+			$this->click('//button[text()=\'Save\']');
+			$this->waitForElementNotPresent('css=div.pkp_modal_panel');
+		}
 	}
 }

a817f311bd95597e7c4a74e9c0c892bd36062939
Nate Wright <natew@notthisway.com>
2017-05-25 16:55:41 +0100

    pkp/pkp-lib#2163 Initial work on ordering feature for ListPanel components.
    This commit implements a pattern for drag-and-drop ordering of lists. It also
    scaffolds keyboard-based ordering, but that's not yet implemented.

---
+++ js/controllers/list/ListPanelItem.vue
@@ -0,0 +1,30 @@
+<template>
+	<li class="pkpListPanelItem" :class="{'--isFeatured': isFeatured}">
+		<div class="pkpListPanelItem--submission__item"></div>
+	</li>
+</template>
+
+<script>
+export default {
+	name: 'ListPanelItem',
+	methods: {
+		/**
+		 * Pass an itemOrderUp event up to the list panel
+		 *
+		 * This event emerges from a ListPanelItemOrderer component.
+		 */
+		itemOrderUp: function() {
+			this.$emit('itemOrderUp', this.submission);
+		},
+
+		/**
+		 * Pass an itemOrderDown event up to the list panel
+		 *
+		 * This event emerges from a ListPanelItemOrderer component.
+		 */
+		itemOrderDown: function() {
+			this.$emit('itemOrderDown', this.submission);
+		},
+	},
+};
+</script>
---
+++ js/controllers/list/ListPanelItemOrderer.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="pkpListPanelItem__orderer">
+		<button tabindex="-1" class="pkpListPanelItem__ordererDragDrop">
+			<span class="fa fa-sort"></span>
+		</button>
+		<button class="pkpListPanelItem__ordererUp" @click.prevent="orderUp">
+			<span class="fa fa-chevron-up"></span>
+			<span class="pkp_screen_reader">Move {{ itemTitle }} up.</span>
+		</button>
+		<button class="pkpListPanelItem__ordererDown" @click.prevent="orderDown">
+			<span class="fa fa-chevron-down"></span>
+			<span class="pkp_screen_reader">Move {{ itemTitle }} down.</span>
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ListPanelItemOrderer',
+	props: ['i18n', 'itemTitle'],
+	methods: {
+		/**
+		 * Emit an event to move this item up
+		 */
+		orderUp: function() {
+			this.$emit('itemOrderUp');
+		},
+
+		/**
+		 * Emit an event to move this item down
+		 */
+		orderDown: function() {
+			this.$emit('itemOrderDown');
+		}
+	}
+}
+</script>

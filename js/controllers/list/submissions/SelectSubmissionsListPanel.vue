ff71f151340cfb013a78917295419d5c0d21ff0c
Nate Wright <natew@notthisway.com>
2017-05-29 10:59:04 +0100

    pkp/pkp-lib#2163 Add aria-live to ListPanel items

--- js/controllers/list/submissions/SelectSubmissionsListPanel.vue
+++ js/controllers/list/submissions/SelectSubmissionsListPanel.vue
@@ -9,7 +9,7 @@
 				:i18n="i18n"
 			/>
 		</div>
-		<ul class="pkpListPanel__items">
+		<ul class="pkpListPanel__items" aria-live="polite">
 			<select-submissions-list-item
 				v-for="item in collection.items"
 				:submission="item"

fa7c3fdb5774eaa26298d4979cd0a09ba0291a44
Nate Wright <natew@notthisway.com>
2017-06-05 17:52:00 +0100

    pkp/pkp-lib#2163 More work addressing JS linting errors

--- js/classes/Handler.js
+++ js/classes/Handler.js
@@ -10,7 +10,7 @@
  *
  * @brief Base class for handlers bound to a DOM HTML element.
  */
-(function($) {
+(function($, _, pkp) {
 
 
 	/**
@@ -869,4 +869,4 @@
 
 
 /** @param {jQuery} $ jQuery closure. */
-}(jQuery));
+}(jQuery, _, pkp));

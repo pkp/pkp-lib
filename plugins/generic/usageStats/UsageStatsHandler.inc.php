5c97960fc4a4c5103d84c65a91d091d893f22fdd
Bozana Bokan <bozana.bokan@posteo.net>
2017-06-20 15:06:44 +0200

    pkp/pkp-lib#2599 fix privacy option in usage stats

--- plugins/generic/usageStats/UsageStatsHandler.inc.php
+++ plugins/generic/usageStats/UsageStatsHandler.inc.php
@@ -43,7 +43,7 @@ class UsageStatsHandler extends Handler {
 		}
 
 		$router = $request->getRouter(); /* @var $router PageRouter */
-		$privacyStatementUrl = $router->url($request, null, 'about', 'submissions', null, null, 'privacyStatement');
+		$privacyStatementUrl = $router->url($request, null, 'about', 'submissions');
 
 		// Display the privacy info page.
 		$this->setupTemplate($request);
@@ -53,7 +53,7 @@ class UsageStatsHandler extends Handler {
 		$templateMgr->assign('usageStatsDisplayPrivacyInfo', true);
 		$templateMgr->assign('hasOptedOut', ($request->getCookieVar('usageStats-opt-out') ? true : false));
 		$templateMgr->assign('privacyStatementUrl', $privacyStatementUrl);
-		$templateMgr->display($plugin->getTemplatePath().'privacyInformation.tpl');
+		$templateMgr->display($plugin->getTemplatePath(true).'privacyInformation.tpl');
 	}
 
 	//

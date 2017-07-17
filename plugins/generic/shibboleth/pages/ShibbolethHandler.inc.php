c3c0d68de0dce35a36508400390303793d3f09ca
Christopher R. Maden <crism@maden.org>
2017-07-17 11:45:13 -0500

    pkp/pkp-lib#1963: working on actual login stuff

--- plugins/generic/shibboleth/pages/ShibbolethHandler.inc.php
+++ plugins/generic/shibboleth/pages/ShibbolethHandler.inc.php
@@ -24,20 +24,48 @@ class ShibbolethHandler extends Handler {
 	function shibLogin($args, $request) {
 		$plugin = $this->_getPlugin();
 		$uin_header = $plugin->getSetting($contextId, 'shibbolethHeaderUin');
+		$email_header = $plugin->getSetting($contextId, 'shibbolethHeaderEmail');
 
-		// We rely on this header being present.
+		// We rely on these headers being present.
 		if (!isset($_SERVER[$uin_header])) {
 			syslog(LOG_ERR, "Shibboleth plugin enabled, but not properly configured; failed to find $uin_header.");
 			Validation::logout();
 			Validation::redirectLogin();
 		}
+		if (!isset($_SERVER[$email_header])) {
+			syslog(LOG_ERR, "Shibboleth plugin enabled, but not properly configured; failed to find $email_header.");
+			Validation::logout();
+			Validation::redirectLogin();
+		}
 
-		// ... and being set.
+		// The UIN must be set; otherwise login failed.
 		$uin = $_SERVER[$uin_header];
 		if ($uin == null) {
 			Validation::logout();
 			Validation::redirectLogin();
 		}
+
+        // Try to locate the user by UIN.
+		$userDao =& DAORegistry::getDAO('UserDAO');
+		$user =& $userDao->getUserByAuthStr($uin, true);
+        if (isset($user)) {
+            syslog(LOG_INFO, "Shibboleth located returning user $uin.");
+            // @@@ TODO do the login
+        }
+
+		// We use the e-mail as a key.
+		$user_email = $_SERVER[$email_header];
+        if (!isset($user)) {
+            $user =& $userDao=>getUserByEmail($email);
+
+            if (isset($user)) {
+                syslog(LOG_INFO, "Shibboleth located returning email $email.");
+            // @@@ TODO do the login
+            }
+        }
+
+        // @@@ TODO admin stuff
+        return true;
 	}
 
 	//

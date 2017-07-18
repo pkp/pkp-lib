6520ed1a20ba9a9b0f5cf30b896c9a09216e9e16
Christopher R. Maden <crism@maden.org>
2017-07-18 12:36:42 -0500

    pkp/pkp-lib#1963: whitespace cleanup

--- plugins/generic/shibboleth/pages/ShibbolethHandler.inc.php
+++ plugins/generic/shibboleth/pages/ShibbolethHandler.inc.php
@@ -74,26 +74,26 @@ class ShibbolethHandler extends Handler {
 			// We use the e-mail as a key.
 			$user =& $userDao->getUserByEmail($user_email);
 
-            if (isset($user)) {
-                syslog(LOG_INFO, "Shibboleth located returning email $user_email");
+			if (isset($user)) {
+				syslog(LOG_INFO, "Shibboleth located returning email $user_email");
 
-                if ($user->getAuthStr() != "") {
-                    syslog(
-                        LOG_ERR,
-                        "Shibboleth user with email $user_email already has UID"
-                    );
-                    Validation::logout();
-                    Validation::redirectLogin();
-                    return false;
-                } else {
-                    $user->setAuthStr($uin);
-                    $userDao->updateObject($user);
-                }
-            } else {
-                // @@@ TODO register a new user
-                return false;
-            }
-        }
+				if ($user->getAuthStr() != "") {
+					syslog(
+						LOG_ERR,
+						"Shibboleth user with email $user_email already has UID"
+					);
+					Validation::logout();
+					Validation::redirectLogin();
+					return false;
+				} else {
+					$user->setAuthStr($uin);
+					$userDao->updateObject($user);
+				}
+			} else {
+				// @@@ TODO register a new user
+				return false;
+			}
+		}
 
 		if (isset($user)) {
 			$this->_checkAdminStatus($user);

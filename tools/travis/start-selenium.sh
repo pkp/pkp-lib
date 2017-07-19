d7283da546c61dc796f14ec04bb890fc5bb22ffb
Alec Smecher <alec@smecher.bc.ca>
2017-07-19 15:17:58 -0700

    Configure Selenium log

--- tools/travis/start-selenium.sh
+++ tools/travis/start-selenium.sh
@@ -17,7 +17,7 @@ mkdir screenshots
 
 # Start Selenium server.
 wget -O selenium.jar http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
-nohup java -jar selenium.jar -forcedBrowserMode *firefox -browserSessionReuse 2>&1 >> selenium-output &
+nohup java -jar selenium.jar -forcedBrowserMode *firefox -browserSessionReuse -log selenium-output &
 
 # Wait for Selenium to start
 until wget -O - -q "http://localhost:4444/selenium-server/driver/?cmd=testComplete" | grep -e "^OK$"; do sleep 1; done

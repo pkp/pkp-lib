a4f9bc2044f4295bd9277b8d84be1f76b7c62470
Alec Smecher <alec@smecher.bc.ca>
2017-07-21 13:21:34 -0700

    Create webserver access log

--- tools/travis/prepare-webserver.sh
+++ tools/travis/prepare-webserver.sh
@@ -17,4 +17,4 @@ if [[ ${TRAVIS_PHP_VERSION:0:2} == "5." ]]; then sudo apt-get install php5-curl 
 phpenv config-add lib/pkp/tools/travis/php.ini
 
 sudo socat TCP-LISTEN:80,fork,reuseaddr TCP:localhost:8080 &
-php -S 127.0.0.1:8080 -t . >& /dev/null &
+php -S 127.0.0.1:8080 -t . >& access.log &

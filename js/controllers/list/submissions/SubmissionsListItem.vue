b0071c6774f80758f64d31b8a94b30b5e3c75d3b
Nate Wright <natew@notthisway.com>
2017-08-22 12:27:16 +0100

    pkp/pkp-lib#2708 Use stage ID constants in SubmissionsListItem

--- js/controllers/list/submissions/SubmissionsListItem.vue
+++ js/controllers/list/submissions/SubmissionsListItem.vue
@@ -260,7 +260,7 @@ export default {
 		 * @return bool
 		 */
 		isReviewStage: function() {
-			return this.activeStage.id === 2 || this.activeStage.id === 3;
+			return this.activeStage.id === pkp.const.WORKFLOW_STAGE_ID_INTERNAL_REVIEW || this.activeStage.id === pkp.const.WORKFLOW_STAGE_ID_EXTERNAL_REVIEW;
 		},
 
 		/**

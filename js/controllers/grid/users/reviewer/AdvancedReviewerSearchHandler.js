3c29eaec8e8f7813d638f6bae78344a53a43405c
Dimitris Efstathiou <defstat@gmail.com>
2017-09-11 17:11:04 +0300

    pkp/pkp-lib#2180 Reviewer Name for email template editor changed on select reviewer form

--- js/controllers/grid/users/reviewer/AdvancedReviewerSearchHandler.js
+++ js/controllers/grid/users/reviewer/AdvancedReviewerSearchHandler.js
@@ -79,6 +79,16 @@
 			// Hide the grid now
 			$('#searchGridAndButton').hide();
 			$('#regularReviewerForm').show();
+
+			// Change the NAME placeholder in the mail editor
+			$('[name^="personalMessage"]').val().replace('{$reviewerName}', reviewerName);
+			$("iframe[id^='personalMessage']")
+				.contents()
+				.find('[data-symbolic="reviewerName"]')
+				.each(function () {
+					$(this).html(reviewerName);
+					$(this).attr('class', '');
+				});
 		}
 	};
 

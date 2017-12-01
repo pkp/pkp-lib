5b5df4912ba0f946c38fae43987d07a6ef2bc774
Dimitris Efstathiou <defstat@gmail.com>
2017-12-01 20:33:59 +0200

    pkp/pkp-lib#2998 Archiving Page Changes

--- controllers/tab/settings/archiving/form/ArchivingForm.inc.php
+++ controllers/tab/settings/archiving/form/ArchivingForm.inc.php
@@ -24,11 +24,55 @@ class ArchivingForm extends ContextSettingsForm {
 		$settings = array(
 			'enableLockss' => 'bool',
 			'enableClockss' => 'bool',
+			'enablePln' => 'bool',
+			'enablePortico' => 'bool'
 		);
+
+		$this->addCheck(new FormValidatorCSRF($this));
 
 		parent::__construct($settings, 'controllers/tab/settings/archiving/form/archivingForm.tpl', $wizardMode);
 	}
 
+	/**
+	 * @copydoc Form::fetch()
+	 */
+	function fetch($request, $params = null) {
+		$isPLNPluginInstalled = false;
+		$isPLNPluginEnabled = false;
+		$isPorticoPluginInstalled = false;
+
+		$context = $request->getContext();
+
+		// Get if PLNPlugin is installed
+		$versionDao = DAORegistry::getDAO('VersionDAO');
+		$currentPLNVersion = $versionDao->getCurrentVersion("plugins.generic", "pln", true);
+		if (isset($currentPLNVersion)) {
+			$isPLNPluginInstalled = true;
+		}
+
+		// Get if PLNPlugin is enabled
+		$pluginSettingsDao = DAORegistry::getDAO('PluginSettingsDAO');
+		if ($pluginSettingsDao->settingExists($context->getId(), 'plnplugin', 'enabled')) {
+			$isPLNPluginEnabled = $pluginSettingsDao->getSetting($context->getId(), 'plnplugin', 'enabled');
+		}
+
+		// Get if Portico is installed
+		$currentPorticoVersion = $versionDao->getCurrentVersion("plugins.importexport", "portico", true);
+		if (isset($currentPorticoVersion)) {
+			$isPorticoPluginInstalled = true;
+		}
+
+		$plnSettingsShowAction = $this->_getPLNPluginSettingsLinkAction($request);
+
+		$params = array(
+			'isPLNPluginInstalled' => $isPLNPluginInstalled,
+			'isPLNPluginEnabled' => $isPLNPluginEnabled,
+			'isPorticoPluginInstalled' => $isPorticoPluginInstalled,
+			'plnSettingsShowAction' => $plnSettingsShowAction,
+		);
+
+		return parent::fetch($request, $params);
+	}
 
 	//
 	// Implement template methods from Form.
@@ -38,6 +82,86 @@ class ArchivingForm extends ContextSettingsForm {
 	 */
 	function getLocaleFieldNames() {
 		return array('lockssLicense', 'clockssLicense');
+	}
+
+	/**
+	 * @see Form::execute()
+	 * @param $request PKPRequest
+	 */
+	function execute($request) {
+		parent::execute($request);
+
+		$this->enablePlugin($request, 'plugins.generic', 'generic', 'pln', 'enablePln');
+		$this->enablePlugin($request, 'plugins.importexport', 'importexport', 'portico', 'enablePortico');
+
+		return new JSONMessage(true, $this->fetch($request));
+	}
+
+	/**
+	 * Get a link action for PLN Plugin Settings.
+	 * @param $request Request
+	 * @return LinkAction
+	 */
+	function _getPLNPluginSettingsLinkAction($request) {
+		$router = $request->getRouter();
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+
+		$ajaxModal = new AjaxModal(
+			$router->url($request, null, 'grid.settings.plugins.SettingsPluginGridHandler', 'manage', null, array('verb' => 'settings', 'plugin' => 'plnplugin', 'category' => 'generic')),
+			'PLN Plugin'
+		);
+
+		import('lib.pkp.classes.linkAction.LinkAction');
+		$linkAction = new LinkAction(
+			'pln-settings',
+			$ajaxModal,
+			__('manager.plugins.settings'),
+			null
+		);
+
+		return $linkAction;
+	}
+
+	/**
+	 * Enable plugin by using check box.
+	 * @param $request Request
+	 * @param $pluginType string
+	 * @param $pluginCategory string
+	 * @param $pluginName string
+	 * @param $parameterName string
+	 *
+	 */
+	function enablePlugin($request, $pluginType, $pluginCategory, $pluginName, $parameterName) {
+		$versionDao = DAORegistry::getDAO('VersionDAO');
+		$product = $versionDao->getCurrentVersion($pluginType, $pluginName, true);
+		if (isset($product)) {
+			$plugin = PluginRegistry::loadPlugin($pluginCategory, $pluginName);
+			if ($plugin && is_object($plugin)) {
+				$notification = null;
+
+				if (isset($this->_data[$parameterName])) {
+					if (!$plugin->getEnabled()) {
+						if ($plugin->getCanEnable()) {
+							$plugin->setEnabled(true);
+							$notification = NOTIFICATION_TYPE_PLUGIN_ENABLED;
+						}
+					}
+				} else {
+					if ($plugin->getEnabled()) {
+						if ($plugin->getCanDisable()) {
+							$plugin->setEnabled(false);
+							$notification = NOTIFICATION_TYPE_PLUGIN_DISABLED;
+						}
+					}
+				}
+
+				if (isset($notification)) {
+					$user = $request->getUser();
+					$notificationManager = new NotificationManager();
+					$notificationManager->createTrivialNotification($user->getId(), $notification, array('pluginName' => $plugin->getDisplayName()));
+				}
+			}
+		}
 	}
 }
 

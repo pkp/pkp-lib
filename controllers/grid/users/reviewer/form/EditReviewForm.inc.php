05525a44661c09add9d296e714ee9558e3941764
ajnyga <ajnyga@gmail.com>
2018-01-17 16:31:20 +0200

    notify also authors when reviewassignment details change

--- controllers/grid/users/reviewer/form/EditReviewForm.inc.php
+++ controllers/grid/users/reviewer/form/EditReviewForm.inc.php
@@ -112,22 +112,35 @@ class EditReviewForm extends Form {
 		$reviewAssignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO');
 		$reviewAssignment = $reviewAssignmentDao->getReviewAssignment($this->_reviewRound->getId(), $this->_reviewAssignment->getReviewerId(), $this->_reviewRound->getRound(), $this->_reviewRound->getStageId());
 		
-		// Send notification to reviewer if details have changed
+		// Send notification to reviewer if details have changed.
 		if ($reviewAssignment->getDateDue != $this->getData('reviewDueDate') || $reviewAssignment->getDateResponseDue != $this->getData('responseDueDate') || $reviewAssignment->getReviewMethod != $this->getData('reviewMethod')){
 			$notificationManager = new NotificationManager();		
 			$request = Application::getRequest();
 			$context = $request->getContext();
-			$reviewerId = $reviewAssignment->getReviewerId();
+			$userIds[] = $reviewAssignment->getReviewerId();
+
+			// Also notify the author, if review assignment already accepted
+			if ($reviewAssignment->getDateConfirmed()){
+				$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDao');
+				$submitterAssignments = $stageAssignmentDao->getBySubmissionAndRoleId($reviewAssignment->getSubmissionId(), ROLE_ID_AUTHOR);
+				while ($assignment = $submitterAssignments->next()) {
+					$userIds[] = $assignment->getUserId();
+				}
+			}
 
 			$notificationManager->createNotification(
 				$request,
-				$reviewerId,
+				$userIds,
 				NOTIFICATION_TYPE_REVIEW_ASSIGNMENT_UPDATED,
 				$context->getId(),
 				ASSOC_TYPE_REVIEW_ASSIGNMENT,
 				$reviewAssignment->getId(),
 				NOTIFICATION_LEVEL_TASK
 			);
+			
+			
+			
+			
 		}
 		
 		$reviewAssignment->setDateDue($this->getData('reviewDueDate'));

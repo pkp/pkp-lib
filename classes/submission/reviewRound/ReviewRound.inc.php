5d980888a69def70c0569a482cfcb3b2a87300cf
simon <31130607+forgive38@users.noreply.github.com>
2018-01-22 13:06:42 +0100

    introduce -Submit my Revisions- for author and new status for reviewRound : REVIEW_ROUND_STATUS_SUBMISSION_RESUBMITTED

--- classes/submission/reviewRound/ReviewRound.inc.php
+++ classes/submission/reviewRound/ReviewRound.inc.php
@@ -1,8 +1,8 @@
 <?php
+
 /**
  * @defgroup submission_reviewRound Review Round
  */
-
 /**
  * @file classes/submission/reviewRound/ReviewRound.inc.php
  *
@@ -16,7 +16,6 @@
  *
  * @brief Basic class describing a review round.
  */
-
 // The first four statuses are set explicitly by EditorDecisions, which override
 // the current status.
 define('REVIEW_ROUND_STATUS_REVISIONS_REQUESTED', 1);
@@ -32,9 +31,7 @@ define('REVIEW_ROUND_STATUS_PENDING_REVIEWS', 7); // Waiting for reviews to be s
 define('REVIEW_ROUND_STATUS_REVIEWS_READY', 8); // One or more reviews is ready for an editor to view
 define('REVIEW_ROUND_STATUS_REVIEWS_COMPLETED', 9); // All assigned reviews have been confirmed by an editor
 define('REVIEW_ROUND_STATUS_REVIEWS_OVERDUE', 10); // One or more reviews is overdue
-
-// The following status is calculated when the round is in revisions and at
-// at least one revision file has been uploaded.
+// The following status is set by controllers/grid/files/review/AuthorReviewRevisionsGridHandler/sendRevisions
 define('REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED', 11);
 
 // The following statuses are calculated based on the statuses of recommendOnly EditorAssignments
@@ -42,12 +39,13 @@ define('REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED', 11);
 define('REVIEW_ROUND_STATUS_PENDING_RECOMMENDATIONS', 12); // Waiting for recommendations to be submitted by recommendOnly editors
 define('REVIEW_ROUND_STATUS_RECOMMENDATIONS_READY', 13); // One or more recommendations are ready for an editor to view
 define('REVIEW_ROUND_STATUS_RECOMMENDATIONS_COMPLETED', 14); // All assigned recommendOnly editors have made a recommendation
-
+// The following status is set by controllers/grid/files/review/AuthorReviewRevisionsGridHandler/sendRevisions
+define('REVIEW_ROUND_STATUS_SUBMISSION_RESUBMITTED', 15);
 
 class ReviewRound extends DataObject {
-
 	//
 	// Get/set methods
+
 	//
 
 	/**
@@ -126,31 +124,19 @@ class ReviewRound extends DataObject {
 	 */
 	public function determineStatus() {
 		import('lib.pkp.classes.submission.SubmissionFile'); // Submission file constants
-
 		// Check if revisions requested or received, if this is latest review round and then check files
 		$roundStatus = $this->getStatus();
 
-		// If revisions have been requested, check to see if any have been
-		// submitted
-		if ($this->getStatus() == REVIEW_ROUND_STATUS_REVISIONS_REQUESTED || $this->getStatus() == REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED) {
-			$submissionFileDao = DAORegistry::getDAO('SubmissionFileDAO');
-			$submissionFiles = $submissionFileDao->getRevisionsByReviewRound($this, SUBMISSION_FILE_REVIEW_REVISION);
-			if (empty($submissionFiles)) {
-				return REVIEW_ROUND_STATUS_REVISIONS_REQUESTED;
-			}
-			return REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED;
-		}
-
-		// Leave the status alone if it is set to one of the EditorDecisions
-		// which have advanced the submission beyond this round
 		$statusFinished = in_array(
-			$this->getStatus(),
-			array(
-				REVIEW_ROUND_STATUS_RESUBMITTED,
-				REVIEW_ROUND_STATUS_SENT_TO_EXTERNAL,
-				REVIEW_ROUND_STATUS_ACCEPTED,
-				REVIEW_ROUND_STATUS_DECLINED
-			)
+				$this->getStatus(), array(
+			REVIEW_ROUND_STATUS_REVISIONS_REQUESTED,
+			REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED,
+			REVIEW_ROUND_STATUS_RESUBMITTED,
+			REVIEW_ROUND_STATUS_SUBMISSION_RESUBMITTED,
+			REVIEW_ROUND_STATUS_SENT_TO_EXTERNAL,
+			REVIEW_ROUND_STATUS_ACCEPTED,
+			REVIEW_ROUND_STATUS_DECLINED
+				)
 		);
 		if ($statusFinished) {
 			return $this->getStatus();
@@ -245,7 +231,7 @@ class ReviewRound extends DataObject {
 			case REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED:
 				return 'editor.submission.roundStatus.revisionsSubmitted';
 			case REVIEW_ROUND_STATUS_RESUBMITTED:
-				return 'editor.submission.roundStatus.resubmitted';
+				return 'editor.submission.roundStatus.submissionRequested';
 			case REVIEW_ROUND_STATUS_SENT_TO_EXTERNAL:
 				return 'editor.submission.roundStatus.sentToExternal';
 			case REVIEW_ROUND_STATUS_ACCEPTED:
@@ -257,7 +243,7 @@ class ReviewRound extends DataObject {
 			case REVIEW_ROUND_STATUS_PENDING_REVIEWS:
 				return 'editor.submission.roundStatus.pendingReviews';
 			case REVIEW_ROUND_STATUS_REVIEWS_READY:
-				return $isAuthor?'author.submission.roundStatus.reviewsReady':'editor.submission.roundStatus.reviewsReady';
+				return $isAuthor ? 'author.submission.roundStatus.reviewsReady' : 'editor.submission.roundStatus.reviewsReady';
 			case REVIEW_ROUND_STATUS_REVIEWS_COMPLETED:
 				return 'editor.submission.roundStatus.reviewsCompleted';
 			case REVIEW_ROUND_STATUS_REVIEWS_OVERDUE:
@@ -268,9 +254,13 @@ class ReviewRound extends DataObject {
 				return 'editor.submission.roundStatus.recommendationsReady';
 			case REVIEW_ROUND_STATUS_RECOMMENDATIONS_COMPLETED:
 				return 'editor.submission.roundStatus.recommendationsCompleted';
+			case REVIEW_ROUND_STATUS_SUBMISSION_RESUBMITTED:
+				return 'editor.submission.roundStatus.submissionResubmitted';
+
 			default: return null;
 		}
 	}
+
 }
 
 ?>

33262ea899349d2d2f4ccf6b572c45e2daf296d9
Dimitris Efstathiou <defstat@gmail.com>
2018-02-04 20:08:15 +0200

    pkp/pkp-lib#3287 Remove trailing comma from NMIs title list

--- controllers/grid/navigationMenus/NavigationMenusGridCellProvider.inc.php
+++ controllers/grid/navigationMenus/NavigationMenusGridCellProvider.inc.php
@@ -69,6 +69,9 @@ class NavigationMenusGridCellProvider extends GridCellProvider {
 						->transformNavMenuItemTitle($templateMgr, $item);
 					$navigationMenusTitles = $navigationMenusTitles.$item->getLocalizedTitle().', ';
 				}
+
+				$navigationMenusTitles = trim($navigationMenusTitles, ', ');
+
 				return array('label' => $navigationMenusTitles);
 			default:
 				break;

6deab6e944111df58bafb75c8635e1272491bae4
Bozana Bokan <bozana.bokan@posteo.net>
2018-02-13 09:25:13 +0100

    pkp/pkp-lib#3371 return only distinct interests

--- classes/user/InterestEntryDAO.inc.php
+++ classes/user/InterestEntryDAO.inc.php
@@ -64,6 +64,7 @@ class InterestEntryDAO extends ControlledVocabEntryDAO {
 				' . ($filter?'JOIN controlled_vocab_entry_settings cves ON (cves.controlled_vocab_entry_id = cve.controlled_vocab_entry_id)':'') . '
 			WHERE cve.controlled_vocab_id = ?
 			' . ($filter?'AND cves.setting_name=? AND cves.setting_value LIKE ?':'') . '
+			GROUP BY cve.controlled_vocab_entry_id
 			ORDER BY seq',
 			$params,
 			$rangeInfo

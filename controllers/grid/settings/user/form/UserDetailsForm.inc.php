e3d8002dee053609f6b9e6bfeb46a0a9a6d0644b
Nate Wright <natew@notthisway.com>
2018-02-14 15:13:56 +0000

    Revert "pkp/pkp-lib#2894 Remove locale from Identity::getBiography() method."
    This reverts commit e4b38a7888f2d3bbc06e0ec7dd8ad114776f7436.

--- controllers/grid/settings/user/form/UserDetailsForm.inc.php
+++ controllers/grid/settings/user/form/UserDetailsForm.inc.php
@@ -103,7 +103,7 @@ class UserDetailsForm extends UserForm {
 				'orcid' => $user->getOrcid(),
 				'mailingAddress' => $user->getMailingAddress(),
 				'country' => $user->getCountry(),
-				'biography' => $user->getBiography(), // Localized
+				'biography' => $user->getBiography(null), // Localized
 				'interests' => $interestManager->getInterestsForUser($user),
 				'userLocales' => $user->getLocales(),
 			);
@@ -126,7 +126,7 @@ class UserDetailsForm extends UserForm {
 				'userUrl' => $author->getUrl(),
 				'orcid' => $author->getOrcid(),
 				'country' => $author->getCountry(),
-				'biography' => $author->getBiography(), // Localized
+				'biography' => $author->getBiography(null), // Localized
 			);
 		} else {
 			$data = array(

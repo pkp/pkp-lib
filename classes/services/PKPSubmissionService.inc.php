610c494e5a3bab96683405775a2bbfd29433e643
Nate Wright <natew@notthisway.com>
2018-03-06 11:06:43 +0000

    pkp/pkp-lib#3448 Add submission locale to backend submissions API response

--- classes/services/PKPSubmissionService.inc.php
+++ classes/services/PKPSubmissionService.inc.php
@@ -565,7 +565,7 @@ abstract class PKPSubmissionService extends PKPBaseEntityPropertyService {
 
 		$props = array (
 			'id','fullTitle','status','submissionProgress','stages','reviewRounds','reviewAssignments',
-			'urlWorkflow','urlPublished','_href',
+			'locale', 'urlWorkflow','urlPublished','_href',
 		);
 
 		if ($this->canUserViewAuthor($currentUser, $submission)) {

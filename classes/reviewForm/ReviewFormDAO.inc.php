efd69bdd5d8fbc85df1a027a09a479c074d5d6b0
Nate Wright <natew@notthisway.com>
2018-05-14 15:56:40 +0100

    pkp/pkp-ilb#3590 Don't include declined reviews in active count.
    This commit also adds a count of declined reviews to the API response.

--- classes/reviewForm/ReviewFormDAO.inc.php
+++ classes/reviewForm/ReviewFormDAO.inc.php
@@ -38,7 +38,7 @@ class ReviewFormDAO extends DAO {
 				SUM(CASE WHEN ra.date_completed IS NOT NULL THEN 1 ELSE 0 END) AS complete_count,
 				SUM(CASE WHEN ra.review_id IS NOT NULL AND ra.date_completed IS NULL THEN 1 ELSE 0 END) AS incomplete_count
 			FROM	review_forms rf
-				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id)
+				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id AND ra.declined<>1)
 			WHERE	rf.review_form_id = ? AND rf.assoc_type = ? AND rf.assoc_id = ?
 			GROUP BY rf.review_form_id',
 			$params
@@ -216,7 +216,7 @@ class ReviewFormDAO extends DAO {
 				SUM(CASE WHEN ra.date_completed IS NOT NULL THEN 1 ELSE 0 END) AS complete_count,
 				SUM(CASE WHEN ra.review_id IS NOT NULL AND ra.date_completed IS NULL THEN 1 ELSE 0 END) AS incomplete_count
 			FROM	review_forms rf
-				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id)
+				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id AND ra.declined<>1)
 			WHERE   rf.assoc_type = ? AND rf.assoc_id = ?
 			GROUP BY rf.review_form_id
 			ORDER BY rf.seq',
@@ -239,7 +239,7 @@ class ReviewFormDAO extends DAO {
 				SUM(CASE WHEN ra.date_completed IS NOT NULL THEN 1 ELSE 0 END) AS complete_count,
 				SUM(CASE WHEN ra.review_id IS NOT NULL AND ra.date_completed IS NULL THEN 1 ELSE 0 END) AS incomplete_count
 			FROM	review_forms rf
-				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id)
+				LEFT JOIN review_assignments ra ON (ra.review_form_id = rf.review_form_id AND ra.declined<>1)
 			WHERE	rf.assoc_type = ? AND rf.assoc_id = ? AND rf.is_active = 1
 			GROUP BY rf.review_form_id
 			ORDER BY rf.seq',

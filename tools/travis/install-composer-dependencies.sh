dc9143168e1231d656a8a9eecdbee9bc5ab2fcbd
Alec Smecher <alec@smecher.bc.ca>
2018-09-26 11:38:31 -0700

    Move composer dependency installation to script

---
+++ tools/travis/install-composer-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# @file tools/travis/install-composer-dependencies.sh
+#
+# Copyright (c) 2014-2018 Simon Fraser University
+# Copyright (c) 2010-2018 John Willinsky
+# Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+#
+# Script to install the Composer dependencies.
+#
+
+set -xe
+
+find . -name composer.json -exec bash -c 'composer -d="`dirname {}`" install --no-dev' ";"

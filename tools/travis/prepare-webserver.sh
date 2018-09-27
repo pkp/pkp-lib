6d7bc3593fdb933864758a460a4b5ba49961e035
Alec Smecher <alec@smecher.bc.ca>
2018-09-26 21:10:31 -0700

    Get rid of PHP5 testing stuff

--- tools/travis/prepare-webserver.sh
+++ tools/travis/prepare-webserver.sh
@@ -11,9 +11,6 @@
 
 set -xe
 
-# Start apache and configure a virtual host.
-if [[ ${TRAVIS_PHP_VERSION:0:2} == "5." ]]; then sudo apt-get install -q php5-curl php5-mysql php5-pgsql php5-intl php5-xsl; fi
-
 phpenv config-add lib/pkp/tools/travis/php.ini
 
 sudo socat TCP-LISTEN:80,fork,reuseaddr TCP:localhost:8080 &

2b19d72733abc1b5f3ae5e8a29a8126a825a022a
Alec Smecher <alec@smecher.bc.ca>
2018-10-01 16:52:31 -0700

    Fix callback

--- classes/plugins/ImportExportPlugin.inc.php
+++ classes/plugins/ImportExportPlugin.inc.php
@@ -62,10 +62,7 @@ abstract class ImportExportPlugin extends Plugin {
 	 */
 	function display($args, $request) {
 		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->register_function(
-			'plugin_url',
-			array($this, 'pluginUrl')
-		);
+		$templateMgr->registerPlugin('function', 'plugin_url', array($this, 'smartyPluginUrl'));
 		$this->_request = $request; // Store this for use by the pluginUrl function
 	}
 

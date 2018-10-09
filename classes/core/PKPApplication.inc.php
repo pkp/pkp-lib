aeccaee8ac8e021aed6cb173776062b3ea2cdb01
Alec Smecher <alec@smecher.bc.ca>
2018-10-09 12:28:01 -0700

    Add locale load to CC badge resolution

--- classes/core/PKPApplication.inc.php
+++ classes/core/PKPApplication.inc.php
@@ -678,6 +678,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider {
 		);
 
 		if (isset($licenseKeyMap[$ccLicenseURL])) {
+			PKPLocale::requireComponents(LOCALE_COMPONENT_PKP_SUBMISSION);
 			return __($licenseKeyMap[$ccLicenseURL]);
 		}
 		return null;

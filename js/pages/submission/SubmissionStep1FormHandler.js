21b274acc7224713f7147bc9b2182aebfd49db52
Nate Wright <natew@notthisway.com>
2018-10-12 13:35:46 +0100

    pkp/pkp-lib#2638 Use JS handler for section policy display during submission

---
+++ js/pages/submission/SubmissionStep1FormHandler.js
@@ -0,0 +1,58 @@
+/**
+ * @defgroup js_pages_submission
+ */
+// Create the namespace.
+jQuery.pkp.pages.submission = jQuery.pkp.pages.submission || { };
+
+/**
+ * @file js/pages/submission/SubmissionStep1FormHandler.js
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2000-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SubmissionStep1FormHandler
+ * @ingroup js_pages_submission
+ *
+ * @brief Handle the submission step 1 form.
+ */
+(function($) {
+
+
+	/**
+	 * @constructor
+	 *
+	 * @extends $.pkp.controllers.form.AjaxFormHandler
+	 *
+	 * @param {jQueryObject} $form the wrapped HTML form element.
+	 * @param {Object} options form options.
+	 */
+	$.pkp.pages.submission.SubmissionStep1FormHandler =
+			function($form, options) {
+
+		this.parent($form, options);
+
+		this.showSectionPolicy();
+		$('#sectionId').change(this.callbackWrapper(this.showSectionPolicy));
+	};
+	$.pkp.classes.Helper.inherits(
+			$.pkp.pages.submission.SubmissionStep1FormHandler,
+			$.pkp.controllers.form.AjaxFormHandler);
+
+
+	//
+	// Public methods.
+	//
+	/**
+	 * Show the correct section policy whenever the selected section is changed
+	 * @param {Event} event The triggering event.
+	 */
+	$.pkp.pages.submission.SubmissionStep1FormHandler.
+			prototype.showSectionPolicy = function(event) {
+		$('.section-policy').hide();
+		$('.section-policy.section-id-' + $('#sectionId').val()).fadeIn();
+	};
+
+
+/** @param {jQuery} $ jQuery closure. */
+}(jQuery));

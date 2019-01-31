dcc5074fb0694aaced3936094be79485e7e34628
Alec Smecher <alec@smecher.bc.ca>
2019-01-31 13:21:09 -0800

    PKPIndexHandler / Context cleanup

---
+++ pages/index/PKPIndexHandler.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file pages/index/IndexHandler.inc.php
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2003-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class IndexHandler
+ * @ingroup pages_index
+ *
+ * @brief Handle site index requests.
+ */
+
+import('classes.handler.Handler');
+
+class PKPIndexHandler extends Handler {
+	/**
+	 * Set up templates with announcement data.
+	 * @protected
+	 * @param $context Context
+	 * @param $templateMgr PKPTemplateManager
+	 */
+	protected function _setupAnnouncements($context, $templateMgr) {
+		$enableAnnouncements = $context->getData('enableAnnouncements');
+		$numAnnouncementsHomepage = $context->getData('numAnnouncementsHomepage');
+		if ($enableAnnouncements && $numAnnouncementsHomepage) {
+			$announcementDao = DAORegistry::getDAO('AnnouncementDAO');
+			$announcements =& $announcementDao->getNumAnnouncementsNotExpiredByAssocId($context->getAssocType(), $context->getId(), $numAnnouncementsHomepage);
+			$templateMgr->assign(array(
+				'announcements' => $announcements->toArray(),
+				'numAnnouncementsHomepage' => $numAnnouncementsHomepage,
+			));
+		}
+
+	}
+}
+
+

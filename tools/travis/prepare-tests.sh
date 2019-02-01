ff5ba04103b2cb49a4d7ce7a88072c45a1d8621f
Alec Smecher <alec@smecher.bc.ca>
2019-02-01 12:24:38 -0800

    Extend tests with more options

--- tools/travis/prepare-tests.sh
+++ tools/travis/prepare-tests.sh
@@ -47,6 +47,15 @@ fi
 # Use the template configuration file.
 cp config.TEMPLATE.inc.php config.inc.php
 
+# Use ENABLE_CDN = 1 to prevent default disabling of the CDN for test purposes.
+if [[ "$ENABLE_CDN" != "1" ]]; then
+	sed -i -e "s/enable_cdn = On/enable_cdn = Off/" config.inc.php
+fi
+# Use DISABLE_PATH_INFO = 1 to turn on disable_path_info mode in config.inc.php.
+if [[ "$DISABLE_PATH_INFO" == "1" ]]; then
+	sed -i -e "s/disable_path_info = Off/disable_path_info = On/" config.inc.php
+fi
+
 # Disable CDN usage.
 sed -i -e "s/enable_cdn = On/enable_cdn = Off/" config.inc.php
 

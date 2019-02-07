4a615df50c86fb58367b857edea0d6069d17f350
Nate Wright <natew@notthisway.com>
2019-02-07 14:10:52 +0000

    pkp/pkp-lib#3340 Fix whitespace

--- classes/template/PKPTemplateManager.inc.php
+++ classes/template/PKPTemplateManager.inc.php
@@ -1505,8 +1505,8 @@ class PKPTemplateManager extends Smarty {
 			$params['context'] = 'frontend';
 		}
 
-        $versionDao = DAORegistry::getDAO('VersionDAO');	
-		$appVersion = defined('SESSION_DISABLE_INIT')?null:$versionDao->getCurrentVersion()->getVersionString();
+		$versionDao = DAORegistry::getDAO('VersionDAO');
+		$appVersion = defined('SESSION_DISABLE_INIT') ? null : $versionDao->getCurrentVersion()->getVersionString();
 
 		$stylesheets = $this->getResourcesByContext($this->_styleSheets, $params['context']);
 
@@ -1586,7 +1586,7 @@ class PKPTemplateManager extends Smarty {
 		}
 
 		$versionDao = DAORegistry::getDAO('VersionDAO');
-		$appVersion = defined('SESSION_DISABLE_INIT')?null:$versionDao->getCurrentVersion()->getVersionString();	
+		$appVersion = defined('SESSION_DISABLE_INIT') ? null : $versionDao->getCurrentVersion()->getVersionString();
 
 		$scripts = $this->getResourcesByContext($this->_javaScripts, $params['context']);
 

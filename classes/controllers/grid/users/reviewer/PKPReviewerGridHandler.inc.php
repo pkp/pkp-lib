e49e1b592011b64793699ba5140a4c87a169a5ae
Patrick Bernaud <patrick.bernaud@univ-grenoble-alpes.fr>
2019-03-12 14:08:45 +0100

    pkp/pkp-lib#4590 Replace params on template fetch.

--- classes/controllers/grid/users/reviewer/PKPReviewerGridHandler.inc.php
+++ classes/controllers/grid/users/reviewer/PKPReviewerGridHandler.inc.php
@@ -815,6 +815,7 @@ class PKPReviewerGridHandler extends GridHandler {
 			'editorialContactSignature' => $user->getContactSignature(),
 			'signatureFullName' => $user->getFullname(),
 		));
+		$template->replaceParams();
 
 		return new JSONMessage(true, $template->getBody());
 	}

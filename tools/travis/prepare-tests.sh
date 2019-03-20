a6a1cd01f1eaefd84f0e49f66f737bc5c7f9061f
Alec Smecher <alec@smecher.bc.ca>
2019-03-19 17:22:23 -0700

    pkp/pkp-lib#4545 Set +e after setup is complete

--- tools/travis/prepare-tests.sh
+++ tools/travis/prepare-tests.sh
@@ -61,3 +61,5 @@ sed -i -e "s/enable_cdn = On/enable_cdn = Off/" config.inc.php
 
 # Make the files directory (this will be files_dir in config.inc.php after installation).
 mkdir --parents ${FILESDIR}
+
+set +e

pkp/pkp-lib

commit 27eeff555bdcfa6da9f787d259184b1cfacff025

Standardize return value for PluginRegistry::getPlugins

classes/plugins/PluginRegistry.inc.php
@@ -26,10 +26,14 @@ class PluginRegistry {
 	 * category is not specified, all plugins in an associative array of
 	 * arrays by category.
 	 * @param $category String the name of the category to retrieve
+	 * @return array
 	 */
 	static function &getPlugins($category = null) {
 		$plugins =& Registry::get('plugins', true, array());
-		if ($category !== null) return $plugins[$category];
+		if ($category !== null) {
+			if (!isset($plugins[$category])) $plugins[$category] = array();
+			return $plugins[$category];
+		}
 		return $plugins;
 	}
 

commit 9cf0bf9d6f1b1bcfc07342492e1dc299399c11d8

Fix fatal error from missing themes
Fixes regression introduced in 27eeff555bdcfa6da9f787d259184b1cfacff025 that
prevented the themes from loading and adjusts similar checks to now expect
an array from PluginRegistry::getPlugins().

classes/context/Context.inc.php
@@ -339,7 +339,7 @@ abstract class Context extends DataObject {
 	function getMetricTypes($withDisplayNames = false) {
 		// Retrieve report plugins enabled for this journal.
 		$reportPlugins = PluginRegistry::loadCategory('reports', true, $this->getId());
-		if (!is_array($reportPlugins)) return array();
+		if (empty($reportPlugins)) return array();
 
 		// Run through all report plugins and retrieve all supported metrics.
 		$metricTypes = array();

classes/core/PKPApplication.inc.php
@@ -489,7 +489,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider {
 	public function getMetricTypes($withDisplayNames = false) {
 		// Retrieve site-level report plugins.
 		$reportPlugins = PluginRegistry::loadCategory('reports', true, CONTEXT_SITE);
-		if (!is_array($reportPlugins)) return array();
+		if (empty($reportPlugins)) return array();
 
 		// Run through all report plugins and retrieve all supported metrics.
 		$metricTypes = array();
@@ -594,7 +594,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider {
 			$contextId = CONTEXT_SITE;
 		}
 		$reportPlugins = PluginRegistry::loadCategory('reports', true, $contextId);
-		if (!is_array($reportPlugins)) return null;
+		if (empty($reportPlugins)) return null;
 
 		// Run through all report plugins and try to retrieve the requested metrics.
 		$report = array();

classes/install/Installer.inc.php
@@ -721,7 +721,7 @@ class Installer {
 		foreach ($categories as $category) {
 			PluginRegistry::loadCategory($category);
 			$plugins = PluginRegistry::getPlugins($category);
-			if (is_array($plugins)) {
+			if (!empty($plugins)) {
 				foreach ($plugins as $plugin) {
 					$versionFile = $plugin->getPluginPath() . '/version.xml';
 

classes/plugins/PKPPubIdPluginHelper.inc.php
@@ -25,7 +25,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function validate($contextId, $form, $pubObject) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				$fieldNames = $pubIdPlugin->getFormFieldNames();
 				foreach ($fieldNames as $fieldName) {
@@ -48,7 +48,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function setLinkActions($contextId, $form, $pubObject) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				$linkActions = $pubIdPlugin->getLinkActions($pubObject);
 				foreach ($linkActions as $linkActionName => $linkAction) {
@@ -67,7 +67,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function addJavaScripts($contextId, $request, $templateMgr) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				$pubIdPlugin->addJavaScript($request, $templateMgr);
 			}
@@ -84,7 +84,7 @@ class PKPPubIdPluginHelper {
 	function init($contextId, $form, $pubObject) {
 		if (isset($pubObject)) {
 			$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-			if (is_array($pubIdPlugins)) {
+			if (!empty($pubIdPlugins)) {
 				foreach ($pubIdPlugins as $pubIdPlugin) {
 					$fieldNames = $pubIdPlugin->getFormFieldNames();
 					foreach ($fieldNames as $fieldName) {
@@ -102,7 +102,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function readInputData($contextId, $form) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				$form->readUserVars($pubIdPlugin->getFormFieldNames());
 				$form->readUserVars(array($pubIdPlugin->getAssignFormFieldName()));
@@ -120,7 +120,7 @@ class PKPPubIdPluginHelper {
 		$request = Application::get()->getRequest();
 		$context = $request->getContext();
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $context->getId());
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				$form->readUserVars(array($pubIdPlugin->getAssignFormFieldName()));
 			}
@@ -136,7 +136,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function execute($contextId, $form, $pubObject) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				// Public ID data can only be changed as long
 				// as no ID has been generated.
@@ -166,7 +166,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function assignPubId($contextId, $form, $pubObject, $save = false) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				if ($form->getData($pubIdPlugin->getAssignFormFieldName())) {
 					$pubId = $pubIdPlugin->getPubId($pubObject);
@@ -189,7 +189,7 @@ class PKPPubIdPluginHelper {
 	 */
 	function clearPubId($contextId, $pubIdPlugInClassName, $pubObject) {
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $contextId);
-		if (is_array($pubIdPlugins)) {
+		if (!empty($pubIdPlugins)) {
 			foreach ($pubIdPlugins as $pubIdPlugin) {
 				if (get_class($pubIdPlugin) == $pubIdPlugInClassName) {
 					// clear the pubId:

classes/plugins/PluginRegistry.inc.php
@@ -43,7 +43,7 @@ class PluginRegistry {
 	static function &getAllPlugins() {
 		$plugins =& PluginRegistry::getPlugins();
 		$allPlugins = array();
-		if (is_array($plugins)) foreach ($plugins as $list) {
+		if (!empty($plugins)) foreach ($plugins as $list) {
 			if (is_array($list)) $allPlugins += $list;
 		}
 		return $allPlugins;

classes/statistics/PKPStatisticsHelper.inc.php
@@ -149,7 +149,7 @@ abstract class PKPStatisticsHelper {
 
 		// Retrieve site-level report plugins.
 		$reportPlugins = PluginRegistry::loadCategory('reports', true, CONTEXT_SITE);
-		if (!is_array($reportPlugins) || empty($metricType)) {
+		if (empty($reportPlugins) || empty($metricType)) {
 			return $returner;
 		}
 
@@ -179,7 +179,7 @@ abstract class PKPStatisticsHelper {
 	function getAllMetricTypeStrings() {
 		$allMetricTypes = array();
 		$reportPlugins = PluginRegistry::loadCategory('reports', true, CONTEXT_SITE);
-		if (is_array($reportPlugins)) {
+		if (!empty($reportPlugins)) {
 			foreach ($reportPlugins as $reportPlugin) {
 				/* @var $reportPlugin ReportPlugin */
 				$reportMetricTypes = $reportPlugin->getMetricTypes();

classes/template/PKPTemplateManager.inc.php
@@ -106,13 +106,12 @@ class PKPTemplateManager extends Smarty {
 		$currentContext = $request->getContext();
 
 		$activeTheme = null;
-		if ($currentContext) {
-			$allThemes = PluginRegistry::getPlugins('themes');
-			foreach ($allThemes as $theme) {
-				if ($currentContext->getData('themePluginPath') === $theme->getDirName()) {
-					$activeTheme = $theme;
-					break;
-				}
+		$contextOrSite = $currentContext ? $currentContext : $request->getSite();
+		$allThemes = PluginRegistry::getPlugins('themes');
+		foreach ($allThemes as $theme) {
+			if ($contextOrSite->getData('themePluginPath') === $theme->getDirName()) {
+				$activeTheme = $theme;
+				break;
 			}
 		}
 
@@ -957,7 +956,7 @@ class PKPTemplateManager extends Smarty {
 		if ($instance === null) {
 			$instance = new TemplateManager();
 			$themes = PluginRegistry::getPlugins('themes');
-			if (is_null($themes)) {
+			if (empty($themes)) {
 				$themes = PluginRegistry::loadCategory('themes', true);
 			}
 			$instance->initialize($request);
@@ -1708,7 +1707,7 @@ class PKPTemplateManager extends Smarty {
 
 		// Don't load menus for an area that's not registered by the active theme
 		$themePlugins = PluginRegistry::getPlugins('themes');
-		if (is_null($themePlugins)) {
+		if (empty($themePlugins)) {
 			$themePlugins = PluginRegistry::loadCategory('themes', true);
 		}
 		$activeThemeNavigationAreas = array();

controllers/grid/navigationMenus/form/NavigationMenuForm.inc.php
@@ -56,7 +56,7 @@ class NavigationMenuForm extends Form {
 		$templateMgr = TemplateManager::getManager($request);
 
 		$themePlugins = PluginRegistry::getPlugins('themes');
-		if (is_null($themePlugins)) {
+		if (empty($themePlugins)) {
 			$themePlugins = PluginRegistry::loadCategory('themes', true);
 		}
 

plugins/generic/usageEvent/PKPUsageEventPlugin.inc.php
@@ -228,7 +228,7 @@ abstract class PKPUsageEventPlugin extends GenericPlugin {
 		// 2) Standardized public identifiers, e.g. DOI, URN, etc.
 		if ($this->isPubIdObjectType($pubObject)) {
 			$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $context->getId());
-			if (is_array($pubIdPlugins)) {
+			if (!empty($pubIdPlugins)) {
 				foreach ($pubIdPlugins as $pubIdPlugin) {
 					if (!$pubIdPlugin->getEnabled()) continue;
 					$pubId = $pubObject->getStoredPubId($pubIdPlugin->getPubIdType());

plugins/importexport/native/filter/RepresentationNativeXmlFilter.inc.php
@@ -123,7 +123,7 @@ class RepresentationNativeXmlFilter extends NativeExportFilter {
 
 		// Add pub IDs by plugin
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $deployment->getContext()->getId());
-		foreach ((array) $pubIdPlugins as $pubIdPlugin) {
+		foreach ($pubIdPlugins as $pubIdPlugin) {
 			$this->addPubIdentifier($doc, $representationNode, $representation, $pubIdPlugin);
 		}
 	}

plugins/importexport/native/filter/SubmissionFileNativeXmlFilter.inc.php
@@ -150,7 +150,7 @@ class SubmissionFileNativeXmlFilter extends NativeExportFilter {
 
 		// Add pub IDs by plugin
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $deployment->getContext()->getId());
-		foreach ((array) $pubIdPlugins as $pubIdPlugin) {
+		foreach ($pubIdPlugins as $pubIdPlugin) {
 			$this->addPubIdentifier($doc, $revisionNode, $submissionFile, $pubIdPlugin);
 		}
 	}

plugins/importexport/native/filter/SubmissionNativeXmlFilter.inc.php
@@ -133,7 +133,7 @@ class SubmissionNativeXmlFilter extends NativeExportFilter {
 
 		// Add pub IDs by plugin
 		$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $deployment->getContext()->getId());
-		foreach ((array) $pubIdPlugins as $pubIdPlugin) {
+		foreach ($pubIdPlugins as $pubIdPlugin) {
 			$this->addPubIdentifier($doc, $submissionNode, $submission, $pubIdPlugin);
 		}
 	}
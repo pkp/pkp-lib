bfcdb573144bbc167911c9aac0acbbaedfd0f53f
Nate Wright <natew@notthisway.com>
2019-10-14 12:12:33 +0100

    pkp/pkp-lib#4905 Restore method used for exporting pub ids

--- classes/submission/PKPSubmissionDAO.inc.php
+++ classes/submission/PKPSubmissionDAO.inc.php
@@ -327,4 +327,72 @@ abstract class PKPSubmissionDAO extends SchemaDAO {
 		list($sortBy, $sortDir) = explode("-", $sortOption);
 		return $sortDir;
 	}
+
+	/**
+	 * Get all published submissions (eventually with a pubId assigned and) matching the specified settings.
+	 * @param $contextId integer optional
+	 * @param $pubIdType string
+	 * @param $title string optional
+	 * @param $author string optional
+	 * @param $issueId integer optional
+	 * @param $pubIdSettingName string optional
+	 * (e.g. crossref::status or crossref::registeredDoi)
+	 * @param $pubIdSettingValue string optional
+	 * @param $rangeInfo DBResultRange optional
+	 * @return DAOResultFactory
+	 */
+	function getExportable($contextId, $pubIdType = null, $title = null, $author = null, $issueId = null, $pubIdSettingName = null, $pubIdSettingValue = null, $rangeInfo = null) {
+		if ($pubIdSettingName) {
+			$params[] = $pubIdSettingValue;
+		}
+		$params[] = STATUS_PUBLISHED;
+		$params[] = $contextId;
+		if ($pubIdType) {
+			$params[] = 'pub-id::'.$pubIdType;
+		}
+		if ($title) {
+			$params[] = 'title';
+			$params[] = '%' . $title . '%';
+		}
+		if ($author) {
+			$params[] = $author;
+			$params[] = $author;
+		}
+		if ($issueId) {
+			$params[] = $issueId;
+		}
+		if ($pubIdSettingName && $pubIdSettingValue && $pubIdSettingValue != EXPORT_STATUS_NOT_DEPOSITED) {
+			$params[] = $pubIdSettingValue;
+		}
+
+		$result = $this->retrieveRange(
+			'SELECT	s.*
+			FROM	submissions s
+				LEFT JOIN publications p ON s.current_publication_id = p.publication_id
+				LEFT JOIN publication_settings ps ON p.publication_id = ps.publication_id'
+				. ($issueId ? ' LEFT JOIN publication_settings psi ON p.publication_id = ps.publication_id AND ps.setting_name = "issueId"' : '')
+				. ($pubIdType != null?' LEFT JOIN publication_settings pspidt ON (p.publication_id = pspidt.publication_id)':'')
+				. ($title != null?' LEFT JOIN publication_settings pst ON (p.publication_id = pst.publication_id)':'')
+				. ($author != null?' LEFT JOIN authors au ON (p.publication_id = au.publication_id)
+						LEFT JOIN author_settings asgs ON (asgs.author_id = au.author_id AND asgs.setting_name = \''.IDENTITY_SETTING_GIVENNAME.'\')
+						LEFT JOIN author_settings asfs ON (asfs.author_id = au.author_id AND asfs.setting_name = \''.IDENTITY_SETTING_FAMILYNAME.'\')
+					':'')
+				. ($pubIdSettingName != null?' LEFT JOIN publication_settings pss ON (p.publication_id = pss.publication_id AND pss.setting_name = ?)':'')
+			. ' WHERE	s.status = ?
+				AND s.context_id = ?'
+				. ($pubIdType != null?' AND pspidt.setting_name = ? AND pspidt.setting_value IS NOT NULL':'')
+				. ($title != null?' AND (pst.setting_name = ? AND pst.setting_value LIKE ?)':'')
+				. ($author != null?' AND (asgs.setting_value LIKE ? OR asfs.setting_value LIKE ?)':'')
+				. ($issueId != null?' AND psi.setting_value = ?':'')
+				. (($pubIdSettingName != null && $pubIdSettingValue != null && $pubIdSettingValue == EXPORT_STATUS_NOT_DEPOSITED)?' AND pss.setting_value IS NULL':'')
+				. (($pubIdSettingName != null && $pubIdSettingValue != null && $pubIdSettingValue != EXPORT_STATUS_NOT_DEPOSITED)?' AND pss.setting_value = ?':'')
+				. (($pubIdSettingName != null && is_null($pubIdSettingValue))?' AND (pss.setting_value IS NULL OR pss.setting_value = \'\')':'')
+			. ' GROUP BY s.submission_id
+			ORDER BY p.date_published DESC, s.submission_id DESC',
+			$params,
+			$rangeInfo
+		);
+
+		return new DAOResultFactory($result, $this, '_fromRow');
+	}
 }

c4dee85eb2c688b86b068023c915129d13d60da1
Alec Smecher <alec@smecher.bc.ca>
2019-10-16 17:39:52 -0700

    Fix filenaming typo

--- pages/catalog/PKPCatalogHandler.inc.php
+++ pages/catalog/PKPCatalogHandler.inc.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * @file pages/catalog/CatalogHandler.inc.php
+ * @file pages/catalog/PKPCatalogHandler.inc.php
  *
  * Copyright (c) 2014-2019 Simon Fraser University
  * Copyright (c) 2003-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @class CatalogHandler
+ * @class PKPCatalogHandler
  * @ingroup pages_catalog
  *
  * @brief Handle requests for the public-facing catalog.

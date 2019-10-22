77c4f0ab23e108035c3ca0aa17a7c8865c54723f
Alec Smecher <alec@smecher.bc.ca>
2019-10-22 13:49:23 -0700

    pkp/pkp-lib#5201 Rewrite Slim routing using PATH_INFO

--- classes/handler/APIHandler.inc.php
+++ classes/handler/APIHandler.inc.php
@@ -114,6 +114,15 @@ class APIHandler extends PKPHandler {
 					$handler->_slimRequest = $request->withUri($uri);
 					return $app->process($handler->_slimRequest, $response);
 				}
+			} elseif ($pathInfoEnabled) {
+				// pkp/pkp-lib#4919: PKP software routes with PATH_INFO (unaffected by
+				// mod_rewrite) but Slim relies on REQUEST_URI. Inject PATH_INFO into
+				// Slim for consistent behavior in URL rewriting scenarios.
+				$newUri = $uri->withPath($_SERVER['PATH_INFO']);
+				if ($uri != $newUri) {
+					$handler->_slimRequest = $request->withUri($newUri);
+					return $app->process($handler->_slimRequest, $response);
+				}
 			}
 			return $next($request, $response);
 		});

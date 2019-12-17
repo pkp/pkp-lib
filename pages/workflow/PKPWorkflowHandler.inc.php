aaaaa7294815225881a15094968d812962f8bf14
Nate Wright <natew@notthisway.com>
2019-12-17 11:04:35 +0000

    pkp/pkp-lib#5355 Add confirm prompt before creating version

--- pages/workflow/PKPWorkflowHandler.inc.php
+++ pages/workflow/PKPWorkflowHandler.inc.php
@@ -297,6 +297,7 @@ abstract class PKPWorkflowHandler extends Handler {
 				'activityLog' => __('submission.list.infoCenter'),
 				'cancel' => __('common.cancel'),
 				'ok' => __('common.ok'),
+				'no' => __('common.no'),
 				'preview' => __('common.preview'),
 				'publicationTabsLabel' => __('publication.version.details'),
 				'publish' => __('publication.publish'),
@@ -308,6 +309,8 @@ abstract class PKPWorkflowHandler extends Handler {
 				'unscheduleConfirm' => __('publication.unschedule.confirm'),
 				'view' => __('common.view'),
 				'version' => __('semicolon', ['label' => __('admin.version')]),
+				'versionConfirm' => __('publication.version.confirm'),
+				'yes' => __('common.yes'),
 			],
 		];
 

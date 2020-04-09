79acdafc6231f26f1c3d4255ad9c8b8428569e45
Alec Smecher <alec@smecher.bc.ca>
2020-04-08 17:38:00 -0700

    Remove unused (deprecated) function

--- classes/search/ArticleSearchIndex.inc.php
+++ classes/search/ArticleSearchIndex.inc.php
@@ -60,14 +60,6 @@ class ArticleSearchIndex extends SubmissionSearchIndex {
 	}
 
 	/**
-	 * @copydoc SubmissionSearchIndex::submissionMetadataChanged()
-	 */
-	public function articleMetadataChanged($article) {
-		if (Config::getVar('debug', 'deprecation_warnings')) trigger_error('Deprecated call to articleMetadataChanged. Use submissionMetadataChanged instead.');
-		$this->submissionMetadataChanged($article);
-	}
-
-	/**
 	 * Delete keywords from the search index.
 	 * @param $articleId int
 	 * @param $type int optional

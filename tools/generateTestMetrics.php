e93150015ee5ee9ee38e6fecc0431121e9042b29
Alec Smecher <alec@smecher.bc.ca>
2020-05-13 16:50:55 -0700

    Update license versions

--- tools/generateTestMetrics.php
+++ tools/generateTestMetrics.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2014-2020 Simon Fraser University
  * Copyright (c) 2003-2020 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class generateTestMetrics
  * @ingroup tools

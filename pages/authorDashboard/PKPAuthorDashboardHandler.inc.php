049cfdc7ada60d3b3a20792513850d085c7d5691
JhonathanMiranda <jhon@lepidus.com.br>
2020-07-02 11:51:30 -0400

    pkp/pkp-lib#6056 Modifies the author dashboard handler so it sends the publication version number
    Signed-off-by: JhonathanMiranda <jhon@lepidus.com.br>

--- pages/authorDashboard/PKPAuthorDashboardHandler.inc.php
+++ pages/authorDashboard/PKPAuthorDashboardHandler.inc.php
@@ -246,7 +246,7 @@ abstract class PKPAuthorDashboardHandler extends Handler {
 		foreach ($submission->getData('publications') as $publication) {
 			$publicationList[] = Services::get('publication')->getProperties(
 				$publication,
-				['id', 'datePublished', 'status'],
+				['id', 'datePublished', 'status', 'version'],
 				[
 					'context' => $submissionContext,
 					'submission' => $submission,

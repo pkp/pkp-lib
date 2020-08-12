548143471dbbd957531d33e8451a78e6a6a17d99
Alec Smecher <alec@smecher.bc.ca>
2020-08-12 16:42:34 -0700

    Conventions/locale/arrays cleanup

--- classes/i18n/PKPLocale.inc.php
+++ classes/i18n/PKPLocale.inc.php
@@ -74,6 +74,154 @@ class PKPLocale {
 	static $request;
 
 	/**
+	 * Get all supported UI locales for the current context.
+	 * @return array
+	 */
+	static function getSupportedLocales() {
+		static $supportedLocales;
+		if (!isset($supportedLocales)) {
+			if (defined('SESSION_DISABLE_INIT')) {
+				$supportedLocales = AppLocale::getAllLocales();
+			} elseif (($context = self::$request->getContext())) {
+				$supportedLocales = $context->getSupportedLocaleNames();
+			} else {
+				$site = self::$request->getSite();
+				$supportedLocales = $site->getSupportedLocaleNames();
+			}
+		}
+		return $supportedLocales;
+	}
+
+	/**
+	 * Get all supported form locales for the current context.
+	 * @return array
+	 */
+	static function getSupportedFormLocales() {
+		static $supportedFormLocales;
+		if (!isset($supportedFormLocales)) {
+			if (defined('SESSION_DISABLE_INIT')) {
+				$supportedFormLocales = AppLocale::getAllLocales();
+			} elseif (($context = self::$request->getContext())) {
+				$supportedFormLocales = $context->getSupportedFormLocaleNames();
+			} else {
+				$site = self::$request->getSite();
+				$supportedFormLocales = $site->getSupportedLocaleNames();
+			}
+		}
+		return $supportedFormLocales;
+	}
+
+	/**
+	 * Return the key name of the user's currently selected locale (default
+	 * is "en_US" for U.S. English).
+	 * @return string
+	 */
+	static function getLocale() {
+		static $currentLocale;
+		if (!isset($currentLocale)) {
+			if (defined('SESSION_DISABLE_INIT')) {
+				// If the locale is specified in the URL, allow
+				// it to override. (Necessary when locale is
+				// being set, as cookie will not yet be re-set)
+				$locale = self::$request->getUserVar('setLocale');
+				if (empty($locale) || !in_array($locale, array_keys(AppLocale::getSupportedLocales()))) $locale = self::$request->getCookieVar('currentLocale');
+			} else {
+				$sessionManager = SessionManager::getManager();
+				$session = $sessionManager->getUserSession();
+				$locale = self::$request->getUserVar('uiLocale');
+
+				$context = self::$request->getContext();
+				$site = self::$request->getSite();
+
+				if (!isset($locale)) {
+					$locale = $session->getSessionVar('currentLocale');
+				}
+
+				if (!isset($locale)) {
+					$locale = self::$request->getCookieVar('currentLocale');
+				}
+
+				if (isset($locale)) {
+					// Check if user-specified locale is supported
+					if ($context != null) {
+						$locales = $context->getSupportedLocaleNames();
+					} else {
+						$locales = $site->getSupportedLocaleNames();
+					}
+
+					if (!in_array($locale, array_keys($locales))) {
+						unset($locale);
+					}
+				}
+
+				if (!isset($locale)) {
+					// Use context/site default
+					if ($context != null) {
+						$locale = $context->getPrimaryLocale();
+					}
+
+					if (!isset($locale)) {
+						$locale = $site->getPrimaryLocale();
+					}
+				}
+			}
+
+			if (!AppLocale::isLocaleValid($locale)) {
+				$locale = LOCALE_DEFAULT;
+			}
+
+			$currentLocale = $locale;
+		}
+		return $currentLocale;
+	}
+
+	/**
+	 * Get the stack of "important" locales, most important first.
+	 * @return array
+	 */
+	static function getLocalePrecedence() {
+		static $localePrecedence;
+		if (!isset($localePrecedence)) {
+			$localePrecedence = array(AppLocale::getLocale());
+
+			$context = self::$request->getContext();
+			if ($context && !in_array($context->getPrimaryLocale(), $localePrecedence)) $localePrecedence[] = $context->getPrimaryLocale();
+
+			$site = self::$request->getSite();
+			if ($site && !in_array($site->getPrimaryLocale(), $localePrecedence)) $localePrecedence[] = $site->getPrimaryLocale();
+		}
+		return $localePrecedence;
+	}
+
+	/**
+	 * Retrieve the primary locale of the current context.
+	 * @return string
+	 */
+	static function getPrimaryLocale() {
+		static $locale;
+		if ($locale) return $locale;
+
+		if (defined('SESSION_DISABLE_INIT')) return $locale = LOCALE_DEFAULT;
+
+		$context = self::$request->getContext();
+
+		if (isset($context)) {
+			$locale = $context->getPrimaryLocale();
+		}
+
+		if (!isset($locale)) {
+			$site = self::$request->getSite();
+			$locale = $site->getPrimaryLocale();
+		}
+
+		if (!isset($locale) || !AppLocale::isLocaleValid($locale)) {
+			$locale = LOCALE_DEFAULT;
+		}
+
+		return $locale;
+	}
+
+	/**
 	 * Get a list of locale files currently registered, either in all
 	 * locales (in an array for each locale), or for a specific locale.
 	 * @param $locale string Locale identifier (optional)

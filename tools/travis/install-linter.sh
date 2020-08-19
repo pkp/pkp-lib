eaca503c9aa720fac57f92f12d138c6b4228f1d9
Alec Smecher <alec@smecher.bc.ca>
2020-08-19 15:30:20 -0700

    pkp/pkp-lib#6196 Fix validation test process

--- tools/travis/install-linter.sh
+++ tools/travis/install-linter.sh
@@ -15,11 +15,6 @@ set -xe
 sudo pip install six
 sudo pip install https://github.com/google/closure-linter/zipball/master
 
-wget -O compiler.zip "https://dl.google.com/closure-compiler/compiler-latest.zip"
-JARNAME=`unzip -Z1 compiler.zip "*.jar"`
-unzip compiler.zip ${JARNAME}
-mv ${JARNAME} ~/bin/compiler.jar
-
 # Install jslint4java
 wget "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jslint4java/jslint4java-2.0.2-dist.zip"
 unzip jslint4java-2.0.2-dist.zip

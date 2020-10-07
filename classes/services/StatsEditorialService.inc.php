e6d9746c2f9246a0513265a8480b20ebcbb4a4e3
ajnyga <ajnyga@gmail.com>
2020-10-07 16:39:16 +0300

    pkp/pkp-lib#6171 Remove editorial stats not used in OPS

--- classes/services/StatsEditorialService.inc.php
+++ classes/services/StatsEditorialService.inc.php
@@ -15,6 +15,43 @@
 namespace APP\Services;
 
 class StatsEditorialService extends \PKP\Services\PKPStatsEditorialService {
+
+	/**
+	 * Initialize hooks for extending PKPStatsEditorialService
+	 */
+	public function __construct() {
+		\HookRegistry::register('EditorialStats::overview', array($this, 'modifyOverview'));
+	}
+
+	/**
+	 * Collect and sanitize request params for submissions API endpoint
+	 *
+	 * @param $hookName string
+	 * @param $args array [
+	 *		@option array $overview
+	 *		@option array args
+	 * ]
+	 *
+	 * @return array
+	 */
+	public function modifyOverview($hookName, $args) {
+		$overview =& $args[0];
+
+		// Remove statistics not used with OPS
+		$removeKeys = [];
+		foreach ($overview as $key => $item) {
+			if (in_array($item['key'], ['submissionsAccepted','submissionsDeclinedPostReview', 'submissionsDeclinedDeskReject', 'daysToDecision', 'daysToAccept', 'daysToReject', 'declinedDeskRate', 'declinedReviewRate'])) {
+				$removeKeys[] = $key;
+			}
+		}
+
+		foreach ($removeKeys as $key) {
+			unset($overview[$key]);
+		}
+		// Reset keys
+		$overview = array_values($overview);
+	}
+
 	/**
 	 * Process the sectionIds param when getting the query builder
 	 *

4d106e3b14458a8a3e3446378fa1a3a1e81e98c2
Alec Smecher <alec@smecher.bc.ca>
2021-01-18 12:23:35 -0800

    pkp/pkp-lib#6621 Fix remote galleys

--- controllers/grid/articleGalleys/form/ArticleGalleyForm.inc.php
+++ controllers/grid/articleGalleys/form/ArticleGalleyForm.inc.php
@@ -67,13 +67,13 @@ class ArticleGalleyForm extends Form {
 		$templateMgr = TemplateManager::getManager($request);
 		if ($this->_articleGalley) {
 			$articleGalleyFile = $this->_articleGalley->getFile();
-			$filepath = $articleGalleyFile->getData('path');
-			$templateMgr->assign(array(
+			$filepath = $articleGalleyFile ? $articleGalleyFile->getData('path') : null;
+			$templateMgr->assign([
 				'representationId' => $this->_articleGalley->getId(),
 				'articleGalley' => $this->_articleGalley,
 				'articleGalleyFile' => $articleGalleyFile,
-				'supportsDependentFiles' => Services::get('submissionFile')->supportsDependentFiles($articleGalleyFile, $filepath),
-			));
+				'supportsDependentFiles' => $articleGalleyFile ? Services::get('submissionFile')->supportsDependentFiles($articleGalleyFile, $filepath) : null,
+			]);
 		}
 		$context = $request->getContext();
 		$templateMgr->assign(array(

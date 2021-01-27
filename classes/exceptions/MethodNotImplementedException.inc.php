cb22adf4deea86b2064471bf854c1ad0ebf97c91
defstat <defstat@gmail.com>
2021-01-27 20:01:57 +0200

    pkp/pkp-lib#6490 MethodNotImplementedException removed

--- classes/exceptions/MethodNotImplementedException.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class MethodNotImplementedException extends BadMethodCallException {
-	function __construct() {
-		$message = 'No implementation found for the method';
-		parent::__construct($message);
-	}
-}

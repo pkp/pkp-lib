d3f975a44b1a01bdbc3487be57cb1b01f46a537b
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2021-02-01 14:51:00 +0200

    pkp/pkp-lib#6663 Shorten extension retrival code

--- classes/services/PKPFileService.inc.php
+++ classes/services/PKPFileService.inc.php
@@ -88,9 +88,7 @@ class PKPFileService {
 		$mimetype = $this->fs->getMimetype($to);
 
 		// Check and override ambiguous mime types based on file extension
-		$pathinfo = pathinfo($to);
-		if (array_key_exists('extension', $pathinfo)) {
-			$extension = $pathinfo['extension'];
+		if ($extension = pathinfo($to, PATHINFO_EXTENSION)) {
 			$checkAmbiguous = strtolower($extension . ':' . $mimetype);
 			if (array_key_exists($checkAmbiguous, $extensionsMap = \PKPString::getAmbiguousExtensionsMap())) {
 				$mimetype = $extensionsMap[$checkAmbiguous];

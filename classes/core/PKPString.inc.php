67fdd8b21cdd5c54fd12f0702f60e07b9c352afd
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2021-02-11 17:57:50 +0200

    pkp/pkp-lib#6752 Remove redundant mbstring setting

--- classes/core/PKPString.inc.php
+++ classes/core/PKPString.inc.php
@@ -59,7 +59,6 @@ class PKPString {
 			// Set up required ini settings for mbstring
 			// FIXME Do any other mbstring settings need to be set?
 			mb_internal_encoding($clientCharset);
-			mb_substitute_character('63');		// question mark
 		}
 
 		// Define modifier to be used in regexp_* routines

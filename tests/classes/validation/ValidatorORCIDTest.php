5435f5ccbbe085fde48007d73eed899d1c11e002
Alec Smecher <alec@smecher.bc.ca>
2021-02-11 17:26:09 -0800

    pkp/pkp-lib#6753 HTTP ORCID URLs are no longer valid

--- tests/classes/validation/ValidatorORCIDTest.php
+++ tests/classes/validation/ValidatorORCIDTest.php
@@ -28,7 +28,7 @@ class ValidatorORCIDTest extends PKPTestCase {
 		self::assertFalse($validator->isValid('http://orcid.org/0000-0002-1825-0097')); // Invalid (http)
 		self::assertTrue($validator->isValid('https://orcid.org/0000-0002-1825-0097')); // Valid (https)
 		self::assertFalse($validator->isValid('ftp://orcid.org/0000-0002-1825-0097')); // Invalid (FTP scheme)
-		self::assertTrue($validator->isValid('http://orcid.org/0000-0002-1694-233X')); // Valid, with an X in the last digit
+		self::assertTrue($validator->isValid('https://orcid.org/0000-0002-1694-233X')); // Valid, with an X in the last digit
 		self::assertFalse($validator->isValid('0000-0002-1694-233X')); // Missing URI component
 		self::assertFalse($validator->isValid('000000021694233X')); // Missing dashes, URI component
 	}

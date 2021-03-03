7c411f82b780a881ead2ea76f0139649bdbd16f0
defstat <defstat@gmail.com>
2021-03-03 10:13:38 +0200

    pkp/pkp-lib#5048 Review Changes

--- classes/notification/form/PKPNotificationsUnsubscribeForm.inc.php
+++ classes/notification/form/PKPNotificationsUnsubscribeForm.inc.php
@@ -6,9 +6,9 @@
 /**
  * @file classes/notification/form/PKPNotificationsUnsubscribeForm.inc.php
  *
- * Copyright (c) 2014-2019 Simon Fraser University
- * Copyright (c) 2000-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class PKPNotificationsUnsubscribeForm
  * @ingroup notification_form
@@ -20,11 +20,17 @@
 import('lib.pkp.classes.form.Form');
 
 class PKPNotificationsUnsubscribeForm extends Form {
+	/** @var Notification The notification that triggered the unsubscribe event */
 	var $_notification;
+
+	/** @var string The unsubscribe validation token */
 	var $_validationToken;
 
 	/**
 	 * Constructor.
+	 *
+	 * @param $notification Notification The notification that triggered the unsubscribe event
+	 * @param $validationToken string $name The unsubscribe validation token
 	 */
 	function __construct($notification, $validationToken) {
 		parent::__construct('notification/unsubscribeNotificationsForm.tpl');
@@ -40,7 +46,7 @@ class PKPNotificationsUnsubscribeForm extends Form {
 	/**
 	 * Assign form data to user-submitted data.
 	 */
-	function readInputData() {
+	public function readInputData() {
 		$userVars = array();
 		foreach($this->getNotificationSettingsMap() as $notificationSetting) {
 			$userVars[] = $notificationSetting['emailSettingName'];
@@ -61,17 +67,21 @@ class PKPNotificationsUnsubscribeForm extends Form {
 	/**
 	 * @copydoc Form::fetch
 	 */
-	function fetch($request, $template = null, $display = false) {
+	public function fetch($request, $template = null, $display = false) {
 		$userId = $this->_notification->getUserId();
 		$contextId = $this->_notification->getContextId();
 
+		if ($contextId != $request->getContext()->getId()) {
+			$dispatcher = $request->getDispatcher();
+			$dispatcher->handle404();
+		}
+
 		$emailSettings = $this->getNotificationSettingsMap();
 
-		$contextDao = Application::getContextDAO();
 		$userDao = DAORegistry::getDAO('UserDAO');
 
 		$user = $userDao->getById($userId);
-		$context = $contextDao->getById($contextId);
+		$context = $request->getContext();
 
 		$templateMgr = TemplateManager::getManager($request);
 
@@ -89,7 +99,7 @@ class PKPNotificationsUnsubscribeForm extends Form {
 	/**
 	 * @copydoc Form::execute
 	 */
-	function execute(...$functionArgs) {
+	public function execute(...$functionArgs) {
 		$emailSettings = array();
 		foreach($this->getNotificationSettingsMap() as $settingId => $notificationSetting) {
 			// Get notifications that the user wants to be notified of by email

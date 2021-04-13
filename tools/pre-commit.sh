b2b45d77413f62d091714aa0c48521c8dbe1ee1c
Nate Wright <natew@notthisway.com>
2021-04-13 11:37:15 +0100

    pkp/pkp-lib#5678 Add PHP-CS formatter and pre-commit hook to format committed changes

---
+++ tools/pre-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# @file tools/pre-commit.sh
+#
+# Copyright (c) 2014-2021 Simon Fraser University
+# Copyright (c) 2010-2021 John Willinsky
+# Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+#
+# A pre-commit hook to run php-cs-fixer on committed changes
+#
+
+# Run php-cs-fixer on all committed changes
+git diff --name-only --cached | xargs ./lib/vendor/bin/php-cs-fixer fix --allow-risky=yes --path-mode=intersection --config=.php_cs -q
+
+# Run php-cs-fixer again with --dry-run to throw an error if any files could not be automatically formatted
+git diff --name-only --cached | xargs ./lib/vendor/bin/php-cs-fixer fix --allow-risky=yes --dry-run --path-mode=intersection --config=.php_cs
+
+if [ $? -eq 0 ]
+then
+	echo -e "\n\e[32m✔ Files formatted successfully.\e[0m\n"
+	git diff --name-only --cached | xargs git add -u
+else
+	echo -e "\n\e[31m✘ Commit aborted. Files could not be formatted.\e[0m\n"
+	exit 1
+fi
+

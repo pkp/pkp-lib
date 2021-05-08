490bb83c035e927abb97cb5492879397e883779a
Alec Smecher <alec@smecher.bc.ca>
2021-05-07 18:23:20 -0700

    pkp/pkp-lib#6099 Remove check that breaks context creation Cypress test

--- cypress/tests/data/20-CreateContext.spec.js
+++ cypress/tests/data/20-CreateContext.spec.js
@@ -23,7 +23,6 @@ describe('Data suite tests', function() {
 		cy.get('div[id=context-acronym-error-en_US]').find('span').contains('This field is required.');
 		cy.get('div[id=context-urlPath-error]').find('span').contains('This field is required.');
 		cy.get('div[id=context-primaryLocale-error]').find('span').contains('This field is required.');
-		cy.get('div[id=context-country-error]').find('span').contains('This field is required.');
 		cy.get('input[name="name-en_US"]').type(Cypress.env('contextTitles')['en_US'], {delay: 0});
 		cy.get('input[name=acronym-en_US]').type('JPK', {delay: 0});
 		cy.get('span').contains('Enable this preprint server').siblings('input').check();

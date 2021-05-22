0a81a1bb6bce2dc90350e1b30bda923421d12728
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-05-21 23:30:14 -0300

    [4622] Implementing new events for submission deleting

---
+++ Jobs/Submissions/DeletedSubmissionSearchJob.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @file Jobs/Submissions/DeletedSubmissionSearchJob.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class DeletedSubmissionSearchJob
+ * @ingroup jobs
+ *
+ * @brief Classe to handle the Deleted Submission data update as a Job
+ */
+
+namespace PKP\Jobs\Submissions;
+
+use APP\core\Application;
+use APP\core\Services;
+use PKP\config\Config;
+use PKP\Domains\Jobs\Exceptions\JobException;
+
+use PKP\Support\Jobs\BaseJob;
+
+class DeletedSubmissionSearchJob extends BaseJob
+{
+    /**
+     * The name of the connection the job should be sent to.
+     *
+     * @var string|null
+     */
+    public $connection;
+
+    /**
+     * @var int The submission ID
+     */
+    protected $submissionId;
+
+    /**
+     * Create a new job instance.
+     *
+     */
+    public function __construct(int $submissionId)
+    {
+        $this->connection = Config::getVar('queues', 'default_connection', 'sync');
+        $this->queue = Config::getVar('queues', 'default_queue', null);
+
+        $this->submissionId = $submissionId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     */
+    public function handle(): void
+    {
+        $submission = Services::get('submission')->get($this->submissionId);
+
+        if (!$submission) {
+            $this->failed(new JobException(JobException::INVALID_PAYLOAD));
+
+            return;
+        }
+
+        $articleSearchIndex = Application::getSubmissionSearchIndex();
+        $articleSearchIndex->articleDeleted($submission->getId());
+        $articleSearchIndex->submissionChangesFinished();
+    }
+}
---
+++ classes/observers/events/DeletedSubmission.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file classes/observers/events/DeletedSubmission.inc.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class DeletedSubmission
+ * @ingroup core
+ *
+ * @brief Event fired when submission's deleted
+ */
+
+namespace PKP\observers\events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+
+use PKP\Jobs\Metadata\DeletedSubmissionSearchJob;
+use PKP\submission\PKPSubmission;
+
+class DeletedSubmission
+{
+    use Dispatchable;
+
+    /** @var PKPSubmission $submission Submission associated */
+    public $submission;
+
+    public function __construct(PKPSubmission $submission)
+    {
+        $this->submission = $submission;
+
+        dispatch(new DeletedSubmissionSearchJob($submission->getId()));
+    }
+}

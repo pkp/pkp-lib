pkp/pkp-lib

commit a1cf06976223d91b5ecef4ec79a951f872f658db

Clean up file to stage check; remove unnecessary constructor

classes/services/PKPSubmissionFileService.inc.php
@@ -795,10 +795,28 @@ class PKPSubmissionFileService implements EntityPropertyInterface, EntityReadInt
                 $reviewRound = $reviewRoundDao->getBySubmissionFileId($submissionFile->getId());
                 return $reviewRound->getStageId();
             case SUBMISSION_FILE_QUERY:
+                // This file should be associated with a note. If not, fail.
+                if ($submissionFile->getData('assocType') != ASSOC_TYPE_NOTE) {
+                    return null;
+                }
+
+                // Get the associated note.
                 $noteDao = DAORegistry::getDAO('NoteDAO'); /** @var NoteDAO $noteDao */
                 $note = $noteDao->getById($submissionFile->getData('assocId'));
+                if (!$note) {
+                    return null;
+                }
+
+                // The note should be associated with a query. If not, fail.
+                if ($query->getAssocType() != ASSOC_TYPE_QUERY) {
+                    return null;
+                }
+
+                // Get the associated query.
                 $queryDao = DAORegistry::getDAO('QueryDAO'); /** @var QueryDAO $queryDao */
                 $query = $queryDao->getById($note->getAssocId());
+
+                // The query will have an associated file stage.
                 return $query ? $query->getStageId() : null;
         }
         throw new \Exception('Could not determine the workflow stage id from submission file ' . $submissionFile->getId() . ' with file stage ' . $submissionFile->getData('fileStage'));

plugins/importexport/native/PKPNativeImportExportDeployment.inc.php
@@ -19,17 +19,6 @@ use PKP\submission\SubmissionFile;
 
 class PKPNativeImportExportDeployment extends PKPImportExportDeployment
 {
-    /**
-     * Constructor
-     *
-     * @param $context Context
-     * @param $user User
-     */
-    public function __construct($context, $user)
-    {
-        parent::__construct($context, $user);
-    }
-
     //
     // Deployment items for subclasses to override
     //

commit fc03d9e9fb291c64820ce7f424b2e2f97a73d858

Fix typo introduced in a1cf06976223d91b5ecef4ec79a951f872f658db

classes/services/PKPSubmissionFileService.inc.php
@@ -808,7 +808,7 @@ class PKPSubmissionFileService implements EntityPropertyInterface, EntityReadInt
                 }
 
                 // The note should be associated with a query. If not, fail.
-                if ($query->getAssocType() != ASSOC_TYPE_QUERY) {
+                if ($note->getAssocType() != ASSOC_TYPE_QUERY) {
                     return null;
                 }
 
5165c6c3b3e0e2444194fd321b82e55e16f17782
Alec Smecher <alec@smecher.bc.ca>
2021-08-19 16:46:20 -0700

    pkp/pkp-lib#7127 Fix self-doc

--- classes/user/maps/Schema.inc.php
+++ classes/user/maps/Schema.inc.php
@@ -2,13 +2,13 @@
 /**
  * @file classes/user/maps/Schema.inc.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @class publication
+ * @class Schema
  *
- * @brief Map publications to the properties defined in the publication schema
+ * @brief Map users to the properties defined in the user schema
  */
 
 namespace PKP\user\maps;

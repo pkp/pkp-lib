891557df330b624558d2bda7fbf766064a69f901
Alec Smecher <alec@smecher.bc.ca>
2021-08-18 17:39:54 -0700

    pkp/pkp-lib#7127 Add explicit int cast

--- controllers/grid/settings/user/UserGridRow.inc.php
+++ controllers/grid/settings/user/UserGridRow.inc.php
@@ -71,7 +71,7 @@ class UserGridRow extends GridRow
                 $actionArgs['newUserId'] = $rowId;
 
                 // Verify that the old user exists
-                $oldUser = Repo::user()->get($this->getOldUserId());
+                $oldUser = Repo::user()->get((int) $this->getOldUserId());
 
                 // Don't merge a user in itself
                 if ($oldUser && $actionArgs['oldUserId'] != $actionArgs['newUserId']) {

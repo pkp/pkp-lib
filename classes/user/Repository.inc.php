96afd094ee0907d0b368ba859e5bd7bef21fa9b2
Alec Smecher <alec@smecher.bc.ca>
2021-08-19 16:30:37 -0700

    pkp/pkp-lib#7127 Fix self-doc

--- classes/user/Repository.inc.php
+++ classes/user/Repository.inc.php
@@ -2,13 +2,13 @@
 /**
  * @file classes/user/Repository.inc.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @class announcement
+ * @class Repository
  *
- * @brief A repository to find and manage announcements.
+ * @brief A repository to find and manage users.
  */
 
 namespace PKP\user;

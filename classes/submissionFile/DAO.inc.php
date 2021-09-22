82c17e3950b3ede73d749504713bb26f0efc8742
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-09-22 19:01:42 -0300

    pkp/pkp-lib#7125 Removing groupBy from submissionFile/DAO get and getMany methods. Also changing join to leftJoin on getMany

--- classes/submissionFile/DAO.inc.php
+++ classes/submissionFile/DAO.inc.php
@@ -82,9 +82,7 @@ class DAO extends EntityDAO implements PKPPubIdPluginDAO
             ->leftJoin('files as f', 'f.file_id', '=', 'sf.file_id')
             ->where($this->primaryKeyColumn, '=', $id)
             ->select(['sf.*', 'f.*', 's.locale as locale'])
-            ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale'])
             ->first();
-        // Testing groupBy
         return $row ? $this->fromRow($row) : null;
     }
 
@@ -116,10 +114,9 @@ class DAO extends EntityDAO implements PKPPubIdPluginDAO
     {
         $rows = $query
             ->getQueryBuilder()
-            ->join('submissions as s', 's.submission_id', '=', 'sf.submission_id')
-            ->join('files as f', 'f.file_id', '=', 'sf.file_id')
+            ->leftJoin('submissions as s', 's.submission_id', '=', 'sf.submission_id')
+            ->leftJoin('files as f', 'f.file_id', '=', 'sf.file_id')
             ->select(['sf.*', 'f.*', 's.locale as locale'])
-            ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale'])
             ->get();
 
         return LazyCollection::make(function () use ($rows) {

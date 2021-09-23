06ddfe1151c67ff223fb23829ebdbc3b807f6e45
Alec Smecher <alec@smecher.bc.ca>
2021-09-23 12:32:56 -0700

    pkp/pkp-lib#6093 Use installer log rather than error_log

--- classes/migration/upgrade/v3_4_0/PreflightCheckMigration.inc.php
+++ classes/migration/upgrade/v3_4_0/PreflightCheckMigration.inc.php
@@ -30,25 +30,25 @@ class PreflightCheckMigration extends \PKP\migration\Migration
             $contextDao = \APP\core\Application::getContextDAO();
             $orphanedIds = DB::table('announcement_types AS at')->leftJoin($contextDao->tableName . ' AS c', 'at.assoc_id', '=', 'c.' . $contextDao->primaryKeyColumn)->whereNull('c.' . $contextDao->primaryKeyColumn)->orWhere('at.assoc_type', '<>', \Application::get()->getContextAssocType())->distinct()->pluck('at.type_id');
             foreach ($orphanedIds as $typeId) {
-                error_log("Removing orphaned announcement type ID ${typeId} with no matching context ID.");
+                $this->_installer->log("Removing orphaned announcement type ID ${typeId} with no matching context ID.");
                 DB::table('announcement_types')->where('type_id', '=', $typeId)->delete();
             }
 
             // Clean orphaned announcement_type_setting entries
             $orphanedIds = DB::table('announcement_type_settings AS ats')->leftJoin('announcement_types AS at', 'ats.type_id', '=', 'at.type_id')->whereNull('at.type_id')->distinct()->pluck('ats.type_id');
             foreach ($orphanedIds as $typeId) {
-                error_log("Removing orphaned settings for missing announcement type ID ${typeId}");
+                $this->_installer->log("Removing orphaned settings for missing announcement type ID ${typeId}");
                 DB::table('announcement_type_settings')->where('type_id', '=', $typeId)->delete();
             }
 
             if ($count = DB::table('announcements AS a')->leftJoin('announcement_types AS at', 'a.type_id', '=', 'at.type_id')->whereNull('at.type_id')->whereNotNull('a.type_id')->update(['a.type_id' => null])) {
-                error_log("Reset ${count} announcements with orphaned (non-null) announcement types to no announcement type.");
+                $this->_installer->log("Reset ${count} announcements with orphaned (non-null) announcement types to no announcement type.");
             }
 
             // Clean orphaned announcement_setting entries
             $orphanedIds = DB::table('announcement_settings AS a_s')->leftJoin('announcements AS a', 'a_s.announcement_id', '=', 'a.announcement_id')->whereNull('a.announcement_id')->distinct()->pluck('a_s.announcement_id');
             foreach ($orphanedIds as $announcementId) {
-                error_log("Removing orphaned settings for missing announcement ID ${announcementId}");
+                $this->_installer->log("Removing orphaned settings for missing announcement ID ${announcementId}");
                 DB::table('announcement_settings')->where('announcement_id', '=', $announcementId)->delete();
             }
         } catch (\Exception $e) {

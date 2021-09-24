4f01d252836b4470eca577b6ff01d75b5ef44132
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-09-24 16:23:25 -0300

    pkp/pkp-lib#7125 Adding sf.created_at at SubmissionFile DAO::getMany groupBy clause

--- classes/submissionFile/DAO.inc.php
+++ classes/submissionFile/DAO.inc.php
@@ -117,7 +117,7 @@ class DAO extends EntityDAO implements PKPPubIdPluginDAO
             ->leftJoin('submissions as s', 's.submission_id', '=', 'sf.submission_id')
             ->leftJoin('files as f', 'f.file_id', '=', 'sf.file_id')
             ->select(['sf.*', 'f.*', 's.locale as locale'])
-            ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale'])
+            ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale', 'sf.created_at'])
             ->get();
         // GroupBy and LeftJoin
 

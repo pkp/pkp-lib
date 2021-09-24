8998a85489024dc6f301b2014c330906c36fbce3
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-09-24 17:41:11 -0300

    pkp/pkp-lib#7125 Adding 'sf.submission_file_id' at DAO::getMany statement groupBy clause

--- classes/submissionFile/DAO.inc.php
+++ classes/submissionFile/DAO.inc.php
@@ -117,7 +117,7 @@ class DAO extends EntityDAO implements PKPPubIdPluginDAO
             ->leftJoin('submissions as s', 's.submission_id', '=', 'sf.submission_id')
             ->leftJoin('files as f', 'f.file_id', '=', 'sf.file_id')
             ->addSelect(['f.*', 's.locale as locale'])
-            ->groupBy(['f.file_id', 's.locale', 'sf.created_at'])
+            ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale', 'sf.created_at'])
             ->get();
 
         return LazyCollection::make(function () use ($rows) {

2f583cab2b814168e5350f9a339514e311390bc5
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-09-24 12:16:29 -0300

    pkp/pkp-lib#7125 Adding comment at SubmissionFile DAO::getMany

--- classes/submissionFile/DAO.inc.php
+++ classes/submissionFile/DAO.inc.php
@@ -119,6 +119,7 @@ class DAO extends EntityDAO implements PKPPubIdPluginDAO
             ->select(['sf.*', 'f.*', 's.locale as locale'])
             ->groupBy(['sf.submission_file_id', 'f.file_id', 's.locale'])
             ->get();
+        // GroupBy and LeftJoin
 
         return LazyCollection::make(function () use ($rows) {
             foreach ($rows as $row) {

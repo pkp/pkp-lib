f03ef014b53a24ebdd207ed09c0e8466ed137495
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-10-02 13:43:16 -0300

    pkp/pkp-lib#7125 Deleting SubmissionFileService, updating submissionFile Repository with custom methods for OPS

--- classes/submissionFile/Repository.inc.php
+++ classes/submissionFile/Repository.inc.php
@@ -13,8 +13,12 @@
 
 namespace APP\submissionFile;
 
+use APP\core\Application;
 use APP\core\Request;
-
+use PKP\db\DAORegistry;
+use PKP\plugins\HookRegistry;
+use PKP\search\SubmissionSearch;
+use PKP\security\Role;
 use PKP\services\PKPSchemaService;
 use PKP\submissionFile\Repository as BaseRepository;
 
@@ -24,12 +28,55 @@ class Repository extends BaseRepository
     public $dao;
 
     public function __construct(
-        DAO $dao,
-        Request $request,
-        PKPSchemaService $schemaService
+        Request $request
     ) {
-        parent::__construct($dao, $request, $schemaService);
+        parent::__construct($request);
 
-        $this->dao = $dao;
+        $this->dao = new DAO(new PKPSchemaService());
+
+        HookRegistry::register('SubmissionFile::delete::before', [$this, 'deleteSubmissionFile']);
+        HookRegistry::register('SubmissionFile::assignedFileStages', [$this, 'modifyAssignedFileStages']);
+    }
+
+    /**
+     * Delete related objects when a submission file is deleted
+     *
+     * @param array $args [
+     *      @option SubmissionFile
+     * ]
+     */
+    public function deleteSubmissionFile(array $args): void
+    {
+        $submissionFile = $args[0];
+
+        // Remove galley associations and update search index
+        if ($submissionFile->getData('assocType') == ASSOC_TYPE_REPRESENTATION) {
+            $galleyDao = DAORegistry::getDAO('PreprintGalleyDAO'); /* @var $galleyDao PreprintGalleyDAO */
+            $galley = $galleyDao->getById($submissionFile->getData('assocId'));
+            if ($galley && $galley->getData('submissionFileId') == $submissionFile->getId()) {
+                $galley->_data['submissionFileId'] = null; // Work around pkp/pkp-lib#5740
+                $galleyDao->updateObject($galley);
+            }
+            $preprintSearchIndex = Application::getSubmissionSearchIndex();
+            $preprintSearchIndex->deleteTextIndex($submissionFile->getData('submissionId'), SubmissionSearch::SUBMISSION_SEARCH_GALLEY_FILE, $submissionFile->getId());
+        }
+    }
+
+    /**
+     * Allow authors to upload to galley file stages
+     *
+     * @param string $hookName
+     * @param array $args [
+     * ]
+     */
+    public function modifyAssignedFileStages($hookName, $args)
+    {
+        $allowedFileStages = & $args[0];
+        $stageAssignments = $args[1];
+
+        if (array_key_exists(WORKFLOW_STAGE_ID_PRODUCTION, $stageAssignments)
+                && in_array(Role::ROLE_ID_AUTHOR, $stageAssignments[WORKFLOW_STAGE_ID_PRODUCTION])) {
+            $allowedFileStages[] = SubmissionFile::SUBMISSION_FILE_PROOF;
+        }
     }
 }

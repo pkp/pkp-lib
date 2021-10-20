d0cd6035a0b842081065a59de943380fd83bf8e0
Dimitris Efstathiou <defstat@gmail.com>
2021-10-20 13:00:22 +0300

    pkp/pkp-lib#7388 Add author email recipients selection to pkp-lib. Check if contributor list exists for submission.

--- classes/submission/form/SubmissionSubmitStep4Form.inc.php
+++ classes/submission/form/SubmissionSubmitStep4Form.inc.php
@@ -49,11 +49,6 @@ class SubmissionSubmitStep4Form extends PKPSubmissionSubmitStep4Form
             $authorMail->setFrom($this->context->getData('contactEmail'), $this->context->getData('contactName'));
 
             $user = $request->getUser();
-            $primaryAuthor = $submission->getPrimaryAuthor();
-            if (!isset($primaryAuthor)) {
-                $authors = Repo::author()->getSubmissionAuthors($submission);
-                $primaryAuthor = $authors->first();
-            }
             $mail->addRecipient($user->getEmail(), $user->getFullName());
 
             // Add primary contact and e-mail addresses as specified in the preprint server settings
@@ -72,20 +67,6 @@ class SubmissionSubmitStep4Form extends PKPSubmissionSubmitStep4Form
                 }
             }
 
-            if ($user->getEmail() != $primaryAuthor->getEmail()) {
-                $authorMail->addRecipient($primaryAuthor->getEmail(), $primaryAuthor->getFullName());
-            }
-
-            $assignedAuthors = Repo::author()->getSubmissionAuthors($submission);
-
-            foreach ($assignedAuthors as $author) {
-                $authorEmail = $author->getEmail();
-                // only add the author email if they have not already been added as the primary author
-                // or user creating the submission.
-                if ($authorEmail != $primaryAuthor->getEmail() && $authorEmail != $user->getEmail()) {
-                    $authorMail->addRecipient($author->getEmail(), $author->getFullName());
-                }
-            }
             $mail->bccAssignedSubEditors($submission->getId(), WORKFLOW_STAGE_ID_PRODUCTION);
 
             // OPS: Check if author can publish and let her know in the email
@@ -118,14 +99,18 @@ class SubmissionSubmitStep4Form extends PKPSubmissionSubmitStep4Form
                 'submissionUrl' => $router->url($request, null, 'authorDashboard', 'submission', $submission->getId()),
             ]);
 
+            if (!$mail->send($request)) {
+                $notificationMgr = new NotificationManager();
+                $notificationMgr->createTrivialNotification($request->getUser()->getId(), NOTIFICATION_TYPE_ERROR, ['contents' => __('email.compose.error')]);
+            }
+
             $authorMail->assignParams([
                 'submitterName' => $user->getFullName(),
                 'editorialContactSignature' => $context->getData('contactName'),
             ]);
 
-            if (!$mail->send($request)) {
-                $notificationMgr = new NotificationManager();
-                $notificationMgr->createTrivialNotification($request->getUser()->getId(), NOTIFICATION_TYPE_ERROR, ['contents' => __('email.compose.error')]);
+            foreach ($this->emailRecipients as $authorEmailRecipient) {
+                $authorMail->addRecipient($authorEmailRecipient->getEmail(), $authorEmailRecipient->getFullName());
             }
 
             $recipients = $authorMail->getRecipients();

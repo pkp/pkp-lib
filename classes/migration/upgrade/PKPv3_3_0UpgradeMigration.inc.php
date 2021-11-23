1a138ad62b4724d8c20711c13d84c284174523fb
Jonas Raoni Soares da Silva <jonas@raoni.org>
2021-11-23 21:07:41 +0300

    pkp/pkp-lib#7167 Reverted setting the user_settings assoc_id/assoc_type fields to null

--- classes/migration/upgrade/PKPv3_3_0UpgradeMigration.inc.php
+++ classes/migration/upgrade/PKPv3_3_0UpgradeMigration.inc.php
@@ -82,10 +82,6 @@ class PKPv3_3_0UpgradeMigration extends Migration {
 			});
 		}
 
-		// Use nulls instead of 0 for assoc_type/id in user_settings
-		Capsule::table('user_settings')->where('assoc_type', 0)->update(['assoc_type' => null]);
-		Capsule::table('user_settings')->where('assoc_id', 0)->update(['assoc_id' => null]);
-
 		// pkp/pkp-lib#6093 Don't allow nulls (previously an upgrade workaround)
 		Capsule::schema()->table('announcement_types', function (Blueprint $table) {
 			$table->bigInteger('assoc_type')->nullable(false)->change();

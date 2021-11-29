056b45ad0cd2821fa42cf8775d447a864bae6d34
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-11-28 21:05:58 -0300

    pkp/pkp-lib#7125 Removing unused select from getCount at DAO

--- classes/submission/DAO.inc.php
+++ classes/submission/DAO.inc.php
@@ -90,8 +90,6 @@ class DAO extends EntityDAO
     {
         return $query
             ->getQueryBuilder()
-            ->select('s.' . $this->primaryKeyColumn)
-            ->get()
             ->count();
     }
 

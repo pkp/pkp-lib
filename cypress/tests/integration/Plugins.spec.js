21295d80eb374b45e44f706d34733301576602c1
Henrique Ramos <henrique@henriqueramos.eti.br>
2021-12-09 09:23:16 -0300

    pkp/pkp-lib#5998 Adding test for Plugins.xml caching inside lib/pkp

---
+++ cypress/tests/integration/Plugins.spec.js
@@ -0,0 +1,26 @@
+/**
+ * @file cypress/tests/integration/Plugins.spec.js
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ */
+
+describe('Plugins tests', function() {
+    it('Check if Plugins.xml file was cached', function() {
+        cy.server();
+        cy.route('GET', '**/grid/plugins/plugin-gallery-grid/fetch-grid*').as('getPluginList');
+        cy.login('dbarnes', null, 'publicknowledge');
+
+        cy.get('a:contains("Website")').click();
+        cy.waitJQuery();
+        cy.get('button#plugins-button').click();
+        cy.get('button#pluginGallery-button').click();
+        cy.wait('@getPluginList', {
+            responseTimeout: 30000
+        }).then((evt) => {
+            cy.readFile('cache/fc-loadPluginsXML-0.php');
+        });
+    });
+})

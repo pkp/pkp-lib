18d4c7a1e3e4c6c555b949dcf7b634999011d07b
Alec Smecher <alec@smecher.bc.ca>
2022-01-13 17:38:17 -0800

    Composer classmap should not be authoritative

--- tools/travis/install-composer-dependencies.sh
+++ tools/travis/install-composer-dependencies.sh
@@ -12,4 +12,4 @@
 set -e
 
 # Search for composer.json files, and run Composer to install the dependencies.
-find . -maxdepth 4 -name composer.json -exec bash -c 'composer --no-ansi --working-dir="`dirname {}`" install --optimize-autoloader --classmap-authoritative' ";"
+find . -maxdepth 4 -name composer.json -exec bash -c 'composer --no-ansi --working-dir="`dirname {}`" install --optimize-autoloader' ";"

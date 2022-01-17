2f08f4918c8f862024c9807274941e55adad9b1d
Bartek Kawula <b.kawula@gmail.com>
2022-01-17 10:33:43 -0500

    pkp/pkp-lib#9665 Fix for full text indexing not working

--- classes/search/SubmissionSearchIndex.php
+++ classes/search/SubmissionSearchIndex.php
@@ -45,6 +45,9 @@ abstract class SubmissionSearchIndex
             $text = join("\n", $text);
         }
 
+        // Make sure all text is UTF-8
+        $text = mb_convert_encoding($text, 'UTF-8');
+
         // Removes all control (C) characters, marks (M), punctuations (P), symbols (S) and separators (Z) except "*" (which is addressed below)
         $text = PKPString::regexp_replace('/(?!\*)[\\p{C}\\p{M}\\p{P}\\p{S}\\p{Z}]+/', ' ', $text);
         $text = PKPString::regexp_replace('/[\*]/', $allowWildcards ? '%' : ' ', $text);

43dbbb3729b3afe7ecc883154efc1b7de1f86ea4
Bozana Bokan <bozana.bokan@posteo.net>
2022-03-04 17:02:59 +0100

    StatisticsHelper

--- classes/statistics/PKPStatisticsHelper.inc.php
+++ classes/statistics/PKPStatisticsHelper.inc.php
@@ -16,36 +16,33 @@
 
 namespace PKP\statistics;
 
-use APP\core\Application;
-use PKP\core\PKPApplication;
-
-use PKP\plugins\PluginRegistry;
+use PKP\file\PrivateFileManager;
 
 abstract class PKPStatisticsHelper
 {
     // Dimensions:
     // 1) publication object dimension:
     public const STATISTICS_DIMENSION_CONTEXT_ID = 'context_id';
-    public const STATISTICS_DIMENSION_PKP_SECTION_ID = 'pkp_section_id';
-    public const STATISTICS_DIMENSION_ASSOC_OBJECT_TYPE = 'assoc_object_type';
-    public const STATISTICS_DIMENSION_ASSOC_OBJECT_ID = 'assoc_object_id';
     public const STATISTICS_DIMENSION_SUBMISSION_ID = 'submission_id';
-    public const STATISTICS_DIMENSION_REPRESENTATION_ID = 'representation_id';
     public const STATISTICS_DIMENSION_ASSOC_TYPE = 'assoc_type';
-    public const STATISTICS_DIMENSION_ASSOC_ID = 'assoc_id';
     public const STATISTICS_DIMENSION_FILE_TYPE = 'file_type';
+    public const STATISTICS_DIMENSION_SUBMISSION_FILE_ID = 'submission_file_id';
+    public const STATISTICS_DIMENSION_REPRESENTATION_ID = 'representation_id';
+
     // 2) time dimension:
+    public const STATISTICS_DIMENSION_YEAR = 'year';
     public const STATISTICS_DIMENSION_MONTH = 'month';
-    public const STATISTICS_DIMENSION_DAY = 'day';
+    public const STATISTICS_DIMENSION_DAY = 'day'; // used as API parameter for timelines
+    public const STATISTICS_DIMENSION_DATE = 'date';
+
     // 3) geography dimension:
-    public const STATISTICS_DIMENSION_COUNTRY = 'country_id';
+    public const STATISTICS_DIMENSION_COUNTRY = 'country';
     public const STATISTICS_DIMENSION_REGION = 'region';
     public const STATISTICS_DIMENSION_CITY = 'city';
-    // 4) metric type dimension (non-additive!):
-    public const STATISTICS_DIMENSION_METRIC_TYPE = 'metric_type';
 
     // Metrics:
     public const STATISTICS_METRIC = 'metric';
+    public const STATISTICS_METRIC_UNIQUE = 'metric_unique';
 
     // Ordering:
     public const STATISTICS_ORDER_ASC = 'ASC';
@@ -57,366 +54,49 @@ abstract class PKPStatisticsHelper
     public const STATISTICS_FILE_TYPE_OTHER = 3;
     public const STATISTICS_FILE_TYPE_DOC = 4;
 
-    // Geography.
-    public const STATISTICS_UNKNOWN_COUNTRY_ID = 'ZZ';
-
-    // Constants used to filter time dimension to current time.
-    public const STATISTICS_YESTERDAY = 'yesterday';
-    public const STATISTICS_CURRENT_MONTH = 'currentMonth';
-
     // Set the earliest date used
-    public const STATISTICS_EARLIEST_DATE = '20010101';
+    public const STATISTICS_EARLIEST_DATE = '2001-01-01';
 
-    public function __construct()
-    {
-    }
-
-    /**
-    * Check whether the filter filters on a context
-    * and if so: retrieve it.
-    *
-    * NB: We do not check filters below the context level as this would
-    * be unnecessarily complex. We'd have to check whether the given
-    * publication objects are actually from the same context. This again
-    * would require us to retrieve all context objects for the filtered
-    * objects, etc.
-    *
-    * @param array $filter
-    *
-    * @return null|Context
-    */
-    public function &getContext($filter)
-    {
-        // Check whether the report is on context level.
-        $context = null;
-        if (isset($filter[self::STATISTICS_DIMENSION_CONTEXT_ID])) {
-            $contextFilter = $filter[self::STATISTICS_DIMENSION_CONTEXT_ID];
-            if (is_scalar($contextFilter)) {
-                // Retrieve the context object.
-                $contextDao = Application::getContextDAO(); /** @var ContextDAO $contextDao */
-                $context = $contextDao->getById($contextFilter);
-            }
-        }
-        return $context;
-    }
-
-    /**
-    * Identify and canonicalize the filtered metric type.
-    *
-    * @param string|array $metricType A wildcard can be used to
-    * identify all metric types.
-    * @param null|Context $context
-    * @param string $defaultSiteMetricType
-    * @param array $siteMetricTypes
-    *
-    * @return null|array The canonicalized metric type array. Null if an error
-    *  occurred.
-    */
-    public function canonicalizeMetricTypes($metricType, $context, $defaultSiteMetricType, $siteMetricTypes)
-    {
-        // Metric type is null: Return the default metric for
-        // the filtered context.
-        if (is_null($metricType)) {
-            if ($context instanceof \PKP\context\Context) {
-                $metricType = $context->getDefaultMetricType();
-            } else {
-                $metricType = $defaultSiteMetricType;
-            }
-        }
-
-        // Canonicalize the metric type to an array of metric types.
-        if (!is_null($metricType)) {
-            if (is_scalar($metricType) && $metricType !== '*') {
-                // Metric type is a scalar value: Select a single metric.
-                $metricType = [$metricType];
-            } elseif ($metricType === '*') {
-                // Metric type is '*': Select all available metrics.
-                if ($context instanceof \PKP\context\Context) {
-                    $metricType = $context->getMetricTypes();
-                } else {
-                    $metricType = $siteMetricTypes;
-                }
-            } else {
-                // Only arrays are otherwise supported as metric type
-                // specification.
-                if (!is_array($metricType)) {
-                    $metricType = null;
-                }
-
-                // Metric type is an array: Select multiple metrics. This is the
-                // canonical format so no change is required.
-            }
-        }
-
-        return $metricType;
-    }
-
-    /**
-     * Get the report plugin that implements
-     * the passed metric type.
-     *
-     * @param string $metricType
-     *
-     * @return mixed ReportPlugin or null
+    /** These are rules defined by the COUNTER project.
+     * See https://www.projectcounter.org/code-of-practice-five-sections/7-processing-rules-underlying-counter-reporting-data/#doubleclick
      */
-    public function &getReportPluginByMetricType($metricType)
-    {
-        $returner = null;
-
-        // Retrieve site-level report plugins.
-        $reportPlugins = PluginRegistry::loadCategory('reports', true, \PKP\core\PKPApplication::CONTEXT_SITE);
-        if (empty($reportPlugins) || empty($metricType)) {
-            return $returner;
-        }
-
-        if (is_scalar($metricType)) {
-            $metricType = [$metricType];
-        }
-
-        foreach ($reportPlugins as $reportPlugin) {
-            /** @var ReportPlugin $reportPlugin */
-            $pluginMetricTypes = $reportPlugin->getMetricTypes();
-            $metricTypeMatches = array_intersect($pluginMetricTypes, $metricType);
-            if (!empty($metricTypeMatches)) {
-                $returner = $reportPlugin;
-                break;
-            }
-        }
-
-        return $returner;
-    }
+    public const COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS = 30;
 
     /**
-     * Get metric type display strings implemented by all
-     * available report plugins.
-     *
-     * @return array Metric type as index and the display string
-     * as values.
+     * Get the usage stats directory path.
      */
-    public function getAllMetricTypeStrings()
+    public static function getUsageStatsDirPath(): string
     {
-        $allMetricTypes = [];
-        $reportPlugins = PluginRegistry::loadCategory('reports', true, \PKP\core\PKPApplication::CONTEXT_SITE);
-        if (!empty($reportPlugins)) {
-            foreach ($reportPlugins as $reportPlugin) {
-                /** @var ReportPlugin $reportPlugin */
-                $reportMetricTypes = $reportPlugin->getMetricTypes();
-                foreach ($reportMetricTypes as $metricType) {
-                    $allMetricTypes[$metricType] = $reportPlugin->getMetricDisplayType($metricType);
-                }
-            }
-        }
-
-        return $allMetricTypes;
+        $fileMgr = new PrivateFileManager();
+        return realpath($fileMgr->getBasePath()) . '/usageStats';
     }
-
-    /**
-    * Get report column name.
-    *
-    * @param string $column (optional)
-    *
-    * @return array|string|null
-    */
-    public function getColumnNames($column = null)
-    {
-        $columns = $this->getReportColumnsArray();
-
-        if ($column) {
-            if (isset($columns[$column])) {
-                return $columns[$column];
-            } else {
-                return null;
-            }
-        } else {
-            return $columns;
-        }
-    }
-
-    /**
-    * Get object type string.
-    *
-    * @param mixed $assocType int or null (optional)
-    *
-    * @return mixed string or array
-    */
-    public function getObjectTypeString($assocType = null)
-    {
-        $objectTypes = $this->getReportObjectTypesArray();
-
-        if (is_null($assocType)) {
-            return $objectTypes;
-        } else {
-            if (isset($objectTypes[$assocType])) {
-                return $objectTypes[$assocType];
-            } else {
-                assert(false);
-            }
-        }
-    }
-
-    /**
-     * Get file type string.
-     *
-     * @param mixed $fileType int or null (optional)
-     *
-     * @return mixed string or array
-     */
-    public function getFileTypeString($fileType = null)
-    {
-        $fileTypeArray = $this->getFileTypesArray();
-
-        if (is_null($fileType)) {
-            return $fileTypeArray;
-        } else {
-            if (isset($fileTypeArray[$fileType])) {
-                return $fileTypeArray[$fileType];
-            } else {
-                assert(false);
-            }
-        }
-    }
-
-    /**
-     * Get an url that requests a statiscs report,
-     * using the passed parameters as request arguments.
-     *
-     * @param PKPRequest $request
-     * @param string $metricType Report metric type.
-     * @param array $columns Report columns
-     * @param array $filter Report filters.
-     * @param array $orderBy (optional) Report order by values.
-     *
-     * @return string
-     */
-    public function getReportUrl($request, $metricType, $columns, $filter, $orderBy = [])
-    {
-        $dispatcher = $request->getDispatcher(); /** @var Dispatcher $dispatcher */
-        $args = [
-            'metricType' => $metricType,
-            'columns' => $columns,
-            'filters' => json_encode($filter)
-        ];
-
-        if (!empty($orderBy)) {
-            $args['orderBy'] = json_encode($orderBy);
-        }
-
-        return $dispatcher->url($request, PKPApplication::ROUTE_PAGE, null, 'stats', 'reports', 'generateReport', $args);
-    }
-
-
-    /**
-    * Get the geo location tool.
-    *
-    * @return mixed GeoLocationTool object or null
-    */
-    public function &getGeoLocationTool()
-    {
-        $geoLocationTool = null;
-        $plugin = PluginRegistry::getPlugin('generic', 'usagestatsplugin'); /** @var UsageStatsPlugin $plugin */
-        if ($plugin) {
-            $geoLocationTool = $plugin->getGeoLocationTool();
-        }
-        return $geoLocationTool;
-    }
-
-
-    //
-    // Protected methods.
-    //
-    /**
-     * Get all statistics report columns, with their respective
-     * names as array values.
-     *
-     * @return array
-     */
-    protected function getReportColumnsArray()
-    {
-        return [
-            self::STATISTICS_DIMENSION_ASSOC_ID => __('common.id'),
-            self::STATISTICS_DIMENSION_ASSOC_TYPE => __('common.type'),
-            self::STATISTICS_DIMENSION_FILE_TYPE => __('common.fileType'),
-            self::STATISTICS_DIMENSION_SUBMISSION_ID => $this->getAppColumnTitle(self::STATISTICS_DIMENSION_SUBMISSION_ID),
-            self::STATISTICS_DIMENSION_CONTEXT_ID => $this->getAppColumnTitle(self::STATISTICS_DIMENSION_CONTEXT_ID),
-            self::STATISTICS_DIMENSION_PKP_SECTION_ID => $this->getAppColumnTitle(self::STATISTICS_DIMENSION_PKP_SECTION_ID),
-            self::STATISTICS_DIMENSION_CITY => __('manager.statistics.city'),
-            self::STATISTICS_DIMENSION_REGION => __('manager.statistics.region'),
-            self::STATISTICS_DIMENSION_COUNTRY => __('common.country'),
-            self::STATISTICS_DIMENSION_DAY => __('common.day'),
-            self::STATISTICS_DIMENSION_MONTH => __('common.month'),
-            self::STATISTICS_DIMENSION_METRIC_TYPE => __('common.metric'),
-            self::STATISTICS_METRIC => __('common.count')
-        ];
-    }
-
-    /**
-     * Get all statistics report public objects, with their
-     * respective names as array values.
-     *
-     * @return array
-     */
-    protected function getReportObjectTypesArray()
-    {
-        return [
-            ASSOC_TYPE_SUBMISSION_FILE => __('submission.submit.submissionFiles')
-        ];
-    }
-
-    /**
-     * Get all file types that have statistics, with
-     * their respective names as array values.
-     *
-     * @return array
-     */
-    public function getFileTypesArray()
-    {
-        return [
-            self::STATISTICS_FILE_TYPE_PDF => 'PDF',
-            self::STATISTICS_FILE_TYPE_HTML => 'HTML',
-            self::STATISTICS_FILE_TYPE_OTHER => __('common.other'),
-            self::STATISTICS_FILE_TYPE_DOC => 'DOC',
-        ];
-    }
-
-    /**
-     * Get an application specific column name.
-     *
-     * @param string $column One of the statistics column constant.
-     *
-     * @return string A localized text.
-     */
-    abstract protected function getAppColumnTitle($column);
 }
 
 if (!PKP_STRICT_MODE) {
     class_alias('\PKP\statistics\PKPStatisticsHelper', '\PKPStatisticsHelper');
     foreach ([
         'STATISTICS_DIMENSION_CONTEXT_ID',
-        'STATISTICS_DIMENSION_PKP_SECTION_ID',
-        'STATISTICS_DIMENSION_ASSOC_OBJECT_TYPE',
-        'STATISTICS_DIMENSION_ASSOC_OBJECT_ID',
         'STATISTICS_DIMENSION_SUBMISSION_ID',
         'STATISTICS_DIMENSION_REPRESENTATION_ID',
         'STATISTICS_DIMENSION_ASSOC_TYPE',
-        'STATISTICS_DIMENSION_ASSOC_ID',
         'STATISTICS_DIMENSION_FILE_TYPE',
+        'STATISTICS_DIMENSION_YEAR',
         'STATISTICS_DIMENSION_MONTH',
         'STATISTICS_DIMENSION_DAY',
+        'STATISTICS_DIMENSION_DATE',
         'STATISTICS_DIMENSION_COUNTRY',
         'STATISTICS_DIMENSION_REGION',
         'STATISTICS_DIMENSION_CITY',
-        'STATISTICS_DIMENSION_METRIC_TYPE',
         'STATISTICS_METRIC',
+        'STATISTICS_METRIC_UNIQUE',
         'STATISTICS_ORDER_ASC',
         'STATISTICS_ORDER_DESC',
         'STATISTICS_FILE_TYPE_HTML',
         'STATISTICS_FILE_TYPE_PDF',
         'STATISTICS_FILE_TYPE_OTHER',
         'STATISTICS_FILE_TYPE_DOC',
-        'STATISTICS_UNKNOWN_COUNTRY_ID',
-        'STATISTICS_YESTERDAY',
-        'STATISTICS_CURRENT_MONTH',
         'STATISTICS_EARLIEST_DATE',
+        'COUNTER_DOUBLE_CLICK_TIME_FILTER_SECONDS',
     ] as $constantName) {
         define($constantName, constant('\PKPStatisticsHelper::' . $constantName));
     }

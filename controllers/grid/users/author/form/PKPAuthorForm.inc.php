d87fcfe446fd5e21d683433c09ec69fcffbc5003
Tyler Lendon <tyl13@pitt.edu>
2022-03-14 10:51:06 -0400

    Uses array_map to trim whitespace from givenName

--- controllers/grid/users/author/form/PKPAuthorForm.inc.php
+++ controllers/grid/users/author/form/PKPAuthorForm.inc.php
@@ -189,7 +189,7 @@ class PKPAuthorForm extends Form {
 			if ($publication->getId() !== $author->getData('publicationId')) fatalError('Invalid author!');
 		}
 
-		$author->setGivenName($this->getData('givenName'), null);
+		$author->setGivenName(array_map('trim',$this->getData('givenName')), null);
 		$author->setFamilyName($this->getData('familyName'), null);
 		$author->setPreferredPublicName($this->getData('preferredPublicName'), null);
 		$author->setAffiliation($this->getData('affiliation'), null); // localized

0067912b50b0ac0ff26ff8e73c62b94b07f8a509
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2022-03-23 19:57:25 +0200

    pkp/pkp-lib#7787 Fix User::getContactSignature() return type

--- classes/user/User.inc.php
+++ classes/user/User.inc.php
@@ -415,11 +415,17 @@ class User extends Identity
         $this->setData('inlineHelp', $inlineHelp);
     }
 
-    public function getContactSignature(?string $locale = null)
+    /**
+     * Get localized user signature, compose of contact details of not set
+     */
+    public function getContactSignature(?string $locale = null): string
     {
-        if ($this->getSignature($locale)) {
-            return $this->getSignature($locale);
+        $signature = $locale ? $this->getSignature($locale) : $this->getLocalizedSignature();
+
+        if (!empty($signature)) {
+            return $signature;
         }
+
         $signature = htmlspecialchars($this->getFullName());
         if ($a = $this->getLocalizedAffiliation()) {
             $signature .= '<br/>' . htmlspecialchars($a);

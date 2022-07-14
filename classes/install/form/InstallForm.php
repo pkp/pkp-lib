015d7cc14d7936e6e4576faa17bd924104902e45
Jonas Raoni Soares da Silva <github@raoni.org>
2022-07-14 13:27:43 +0300

    pkp/pkp-lib#8040 Restored previous driver name

--- classes/install/form/InstallForm.php
+++ classes/install/form/InstallForm.php
@@ -43,7 +43,7 @@ class InstallForm extends MaintenanceForm
     public $supportedDatabaseDrivers = [
         // <driver> => array(<php-module>, <name>)
         'mysqli' => ['mysqli', 'MySQLi'],
-        'postgres' => ['pgsql', 'PostgreSQL'],
+        'postgres9' => ['pgsql', 'PostgreSQL'],
         'mysql' => ['mysql', 'MySQL']
     ];
 

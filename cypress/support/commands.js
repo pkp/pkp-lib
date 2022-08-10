6b322c56d1e5dd35f28e36c70ea36e3fd4354ba1
Jonas Raoni Soares da Silva <github@raoni.org>
2022-08-10 23:53:24 +0300

    pkp/pkp-lib#8160 Fixed flaky test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -135,6 +135,13 @@ Cypress.Commands.add('createSubmission', (data, context) => {
 
 	// === Submission Step 2 ===
 
+	// Setup upload listener
+	cy.server();
+	cy.route({
+		method: "POST",
+		url: /submissions\/\d+\/files$/
+	}).as('fileUploaded');
+
 	// OPS uses the galley grid
 	if (Cypress.env('contextTitles').en_US == 'Public Knowledge Preprint Server') {
 		data.files.forEach(file => {
@@ -153,6 +160,7 @@ Cypress.Commands.add('createSubmission', (data, context) => {
 				cy.get('input[type=file]').attachFile(
 					{fileContent, 'filePath': file.fileName, 'mimeType': 'application/pdf', 'encoding': 'base64'}
 				);
+				cy.wait('@fileUploaded').its('status').should('eq', 200);
 			});
 			cy.get('button').contains('Continue').click();
 			cy.wait(2000);
@@ -189,20 +197,30 @@ Cypress.Commands.add('createSubmission', (data, context) => {
 				cy.get('input[type=file]').attachFile(
 					{fileContent, 'filePath': file.fileName, 'mimeType': 'application/pdf', 'encoding': 'base64'}
 				);
-				var $row = cy.get('a:contains("' + file.fileName + '")').parents('.listPanel__item');
+				cy.wait('@fileUploaded').its('status').should('eq', 200);
+				const $row = cy.get('a:contains("' + file.fileName + '")').parents('.listPanel__item');
+				cy.wait(1000);
 				if (primaryFileGenres.includes(file.genre)) {
+					cy.route({
+						method: "PUT",
+						url: /submissions\/\d+\/files\/\d+/
+					}).as('genreDefined');
 					// For some reason this is locating two references to the button,
 					// so just click the last one, which should be the most recently
 					// uploaded file.
 					$row.get('button:contains("' + file.genre + '")').last().click();
-					$row.get('span:contains("' + file.genre + '")');
 				} else {
 					$row.get('button:contains("Other")').last().click();
 					cy.get('#submission-files-container .modal__panel label:contains("' + file.genre + '")').click();
+					cy.route({
+						method: "POST",
+						url: /submissions\/\d+\/files\/\d+/
+					}).as('genreDefined');
 					cy.get('#submission-files-container .modal__panel button:contains("Save")').click();
 				}
-				// Make sure the genre selection is complete before moving to the
-				// next file.
+				cy.wait('@genreDefined').its('status').should('eq', 200);
+				$row.get('span:contains("' + file.genre + '")');
+				// Make sure the genre selection is complete before moving to the next file.
 				$row.get('button:contains("What kind of file is this?")').should('not.exist');
 			});
 		});

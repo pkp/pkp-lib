2250be511523f589de147a56442ad1814f46379f
Alec Smecher <alec@smecher.bc.ca>
2022-08-12 20:04:35 -0700

    Suppress warnings on installation

--- classes/emailTemplate/DAO.php
+++ classes/emailTemplate/DAO.php
@@ -278,8 +278,8 @@ class DAO extends EntityDAO
 
             DB::table('email_templates_default')->insert([
                 'email_key' => $attrs['key'],
-                'can_disable' => isset($attrs['can_disable']) ? ((int) $attrs['can_disable']) : null,
-                'can_edit' => isset($attrs['can_edit']) ? ((int) $attrs['can_edit']) : null,
+                'can_disable' => isset($attrs['can_disable']) ? ((int) $attrs['can_disable']) : 0,
+                'can_edit' => isset($attrs['can_edit']) ? ((int) $attrs['can_edit']) : 0,
                 'from_role_id' => isset($attrs['from_role_id']) ? ((int) $attrs['from_role_id']) : null,
                 'to_role_id' => isset($attrs['to_role_id']) ? ((int) $attrs['to_role_id']) : null,
                 'stage_id' => isset($attrs['stage_id']) ? ((int) $attrs['stage_id']) : null,

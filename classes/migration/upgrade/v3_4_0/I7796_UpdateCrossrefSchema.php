4ba39c6d7e5496637baee625884cd7008219ffce
Bozana Bokan <bozana.bokan@posteo.net>
2022-09-02 13:10:25 +0200

    pkp/pkp-lib#7796 update crossref schema to 5.3.1

---
+++ classes/migration/upgrade/v3_4_0/I7796_UpdateCrossrefSchema.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file classes/migration/upgrade/v3_4_0/I7796_UpdateCrossrefSchema.php
+ *
+ * Copyright (c) 2022 Simon Fraser University
+ * Copyright (c) 2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class I7796_UpdateCrossrefSchema
+ * @brief Upgrade Crossref schema in filter_groups.
+ */
+
+namespace PKP\migration\upgrade\v3_4_0;
+
+use Illuminate\Support\Facades\DB;
+use PKP\install\DowngradeNotSupportedException;
+
+class I7796_UpdateCrossrefSchema extends \PKP\migration\Migration
+{
+    /**
+     * Run the migration.
+     */
+    public function up(): void
+    {
+        DB::statement('UPDATE filter_groups SET output_type=? WHERE output_type = ?', ['xml::schema(https://www.crossref.org/schemas/crossref5.3.1.xsd)', 'xml::schema(https://www.crossref.org/schemas/crossref4.3.6.xsd)']);
+    }
+
+    /**
+     * Reverse the downgrades
+     *
+     * @throws DowngradeNotSupportedException
+     */
+    public function down(): void
+    {
+        throw new DowngradeNotSupportedException();
+    }
+}

846ef191562983fc6b0083837fd1c6f9311a6774
Jonas Raoni Soares da Silva <github@raoni.org>
2022-09-16 15:06:57 +0300

    Dropped suffix from class file

--- classes/migration/install/ServersMigration.php
+++ classes/migration/install/ServersMigration.php
@@ -14,7 +14,6 @@
 namespace APP\migration\install;
 
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class ServersMigration extends \PKP\migration\Migration

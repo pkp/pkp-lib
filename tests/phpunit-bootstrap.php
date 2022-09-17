9fc4a0968e3fe20d1e663a5cd9675114b97d9e8d
Alec Smecher <alec@smecher.bc.ca>
2022-09-17 08:40:17 -0700

    Don't try to configure assertions at runtime

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -39,7 +39,6 @@ ini_set('assert.quiet_eval', '0');
 ini_set('memory_limit', '-1');
 ini_set('error_reporting', '-1');
 ini_set('log_errors_max_len', '0');
-ini_set('zend.assertions', '1');
 ini_set('assert.exception', '1');
 ini_set('xdebug.show_exception_trace', '0');
 

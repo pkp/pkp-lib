a709e60edac8bcdff736acb285d1da0514d99d3a
Nate Wright <natew@notthisway.com>
2022-10-18 17:25:05 +0100

    pkp/pkp-lib#7191 Update submission wizard tests

---
+++ cypress/support/api.js
@@ -0,0 +1,37 @@
+/**
+ * @file cypress/support/api.js
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2000-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ */
+
+class Api {
+
+    constructor(baseUrl) {
+        this.url = baseUrl;
+    }
+
+    submissions(id) {
+        return this.url + '/submissions' + (id ? '/' + id : '');
+    }
+
+    submit(id) {
+        return this.submissions(id) + '/submit';
+    }
+
+    publications(submissionId, publicationId) {
+        return this.submissions(submissionId) + '/publications' + (publicationId ? '/' + publicationId : '');
+    }
+
+    contributors(submissionId, publicationId, contributorId) {
+        return this.publications(submissionId, publicationId) + '/contributors' + (contributorId ? '/' + publicationId : '');
+    }
+
+    submissionFiles(submissionId, submissionFileId) {
+        return this.submissions(submissionId) + '/files' + (submissionFileId ? '/' + submissionFileId : '');
+    }
+}
+
+export default Api;

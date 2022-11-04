b5dd120601e17b60a57b4c968562bcb4017ca1f8
iudi <iudi@lepidus.com.br>
2022-11-04 18:13:13 -0300

    Remove spec specific to OJS from file
    as it should be in `ojs/cypress/tests/integration/DataAvailabilityStatements.spec.js`
    
    Signed-off-by: iudi <iudi@lepidus.com.br>
    Signed-off-by: yves <yves@lepidus.com.br>

--- cypress/tests/integration/DataAvailabilityStatements.spec.js
+++ cypress/tests/integration/DataAvailabilityStatements.spec.js
@@ -4,11 +4,7 @@
 
 describe('DataAvailabilityStatements', function () {
 	var statement = 'This is an example of a data availability statement';
-	var submission = {
-		title: 'Towards Designing an Intercultural Curriculum',
-		author_family_name: 'Daniel'
-	}
-	var reviewer = { name: 'Paul Hudson', login: 'phudson' }
+	var author_family_name = 'Daniel';
 
 	it('Enables Data Availability Statements as submission metadata', function () {
 		cy.login('dbarnes');
@@ -19,31 +15,12 @@ describe('DataAvailabilityStatements', function () {
 	});
 
 	it('Adds a statement to a submission', function () {
-		cy.findSubmissionAsEditor('dbarnes', null, submission.author_family_name);
+		cy.findSubmissionAsEditor('dbarnes', null, author_family_name);
 		cy.get('#publication-button').click();
 		cy.get('#metadata-button').click();
 		cy.get('#metadata-dataAvailability-control-en_US').clear();
 		cy.get('#metadata-dataAvailability-control-en_US').type(statement);
 		cy.get('#metadata button').contains('Save').click();
 		cy.get('#metadata [role="status"]').contains('Saved');
-	});
-
-	it('Sends submission for review with "disclosed author" method and check Statement as reviewer', function () {
-		cy.findSubmissionAsEditor('dbarnes', null, submission.author_family_name);
-		cy.clickDecision('Send for Review');
-		cy.contains('Skip this email').click();
-		cy.get('.pkpButton--isPrimary').contains('Record Decision').click();
-		cy.findSubmissionAsEditor('dbarnes', null, submission.author_family_name);
-		cy.contains('Add Reviewer').click();
-		cy.contains(reviewer.name).parentsUntil('.listPanel__item').find('.pkpButton').click();
-		cy.get('#skipEmail').check();
-		cy.get('#reviewMethod-1').check();
-		cy.get('#advancedSearchReviewerForm').contains('Add Reviewer').click();
-		cy.logout();
-		cy.login(reviewer.login);
-		cy.contains('View ' + submission.title).click();
-		cy.contains('View All Submission Details').click();
-		cy.contains('Data Availability Statement');
-		cy.contains(statement);
 	});
 });

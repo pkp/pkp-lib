bdc497e1497afaceb6fa2bea9bea187ead963d3b
Nate Wright <natew@notthisway.com>
2022-11-30 15:31:10 +0000

    pkp/pkp-lib#5716 Separate email template installation from migrations

---
+++ classes/migration/upgrade/v3_4_0/InstallEmailTemplates.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file classes/migration/upgrade/v3_4_0/InstallEmailTemplates.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2000-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class InstallEmailTemplates
+ * @brief Install new email templates for 3.4
+ */
+
+namespace APP\migration\upgrade\v3_4_0;
+
+class InstallEmailTemplates extends \PKP\migration\upgrade\v3_4_0\InstallEmailTemplates
+{
+    protected function getEmailTemplateKeys(): array
+    {
+        return [
+            'EDITOR_DECISION_NOTIFY_OTHER_AUTHORS',
+            'EDITOR_DECISION_REVERT_INITIAL_DECLINE',
+            'DISCUSSION_NOTIFICATION_PRODUCTION',
+        ];
+    }
+
+    protected function getAppVariableNames(): array
+    {
+        return [
+            'contextName' => 'serverName',
+            'contextUrl' => 'serverUrl',
+            'contextSignature' => 'serverSignature',
+        ];
+    }
+}

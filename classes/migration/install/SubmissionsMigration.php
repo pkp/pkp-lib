1c3051766fea5ab0d8c9035662cbc3381f56c2fe
Alec Smecher <alec@smecher.bc.ca>
2022-12-09 12:37:51 -0800

    pkp/pkp-lib#7191 TEXT columns not allowed to have defaults

--- classes/migration/install/SubmissionsMigration.php
+++ classes/migration/install/SubmissionsMigration.php
@@ -48,7 +48,7 @@ class SubmissionsMigration extends \PKP\migration\Migration
 
             $table->smallInteger('status')->default(PKPSubmission::STATUS_QUEUED);
 
-            $table->text('submission_progress')->default('files');
+            $table->string('submission_progress', 50)->default('files');
             //  Used in OMP only; should not be null there
             $table->smallInteger('work_type')->default(0)->nullable();
         });

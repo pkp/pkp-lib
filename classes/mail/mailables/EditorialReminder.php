fea411f468aaa0d81fa1483d4e63b6a93a152816
Alec Smecher <alec@smecher.bc.ca>
2023-02-20 15:28:49 -0800

    Correct double translation

--- classes/mail/mailables/EditorialReminder.php
+++ classes/mail/mailables/EditorialReminder.php
@@ -117,7 +117,7 @@ class EditorialReminder extends Mailable
      */
     protected function addFooter(string $locale): Mailable
     {
-        $this->setupUnsubscribeFooter($locale, $this->context, __('emails.footer.unsubscribe.automated'));
+        $this->setupUnsubscribeFooter($locale, $this->context, 'emails.footer.unsubscribe.automated');
         return $this;
     }
 }

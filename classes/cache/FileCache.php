c472078434c3d161e424eb0db89798d0ba47c7d3
Jonas Raoni Soares da Silva <github@raoni.org>
2023-03-07 21:11:35 +0300

    pkp/pkp-lib#8733 Addressed warning at the include, which could possibly lead to a bad cache value

--- classes/cache/FileCache.php
+++ classes/cache/FileCache.php
@@ -20,6 +20,7 @@
 
 namespace PKP\cache;
 
+use Exception;
 use PKP\config\Config;
 use PKP\file\FileManager;
 
@@ -42,15 +43,23 @@ class FileCache extends GenericCache
     {
         parent::__construct($context, $cacheId, $fallback);
 
-        $this->filename = "{$path}/fc-${context}-" . str_replace('/', '.', $cacheId) . '.php';
+        $this->filename = "{$path}/fc-{$context}-" . str_replace('/', '.', $cacheId) . '.php';
 
-        // Load the cache data if it exists.
-        if (($fp = @fopen($this->filename, 'r')) !== false) {
-            flock($fp, LOCK_SH);
-            $this->cache = include($this->filename);
-            flock($fp, LOCK_UN);
-        } else {
+        // If the file couldn't be opened or if a lock couldn't be acquired, quit
+        if (!($fp = @fopen($this->filename, 'r')) || !flock($fp, LOCK_SH)) {
+            return $this->cache = null;
+        }
+
+        // Reasoning: When the include below fails, it returns "false" and we have no way to determine if it's an error or a valid cache value
+        $previousHandler = set_error_handler(fn () => throw new Exception('Failed to include file'));
+        try {
+            $this->cache = include $this->filename;
+        } catch (Exception) {
             $this->cache = null;
+        } finally {
+            set_error_handler($previousHandler);
+            flock($fp, LOCK_UN);
+            fclose($fp);
         }
     }
 

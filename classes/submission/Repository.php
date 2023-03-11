5f6f084b16688cdfc8e39d4e3be2af0c50ca09b5
Jonas Raoni Soares da Silva <github@raoni.org>
2023-03-11 11:08:26 +0300

    pkp/pkp-lib#8789 Fixed invalid method call

--- classes/submission/Repository.php
+++ classes/submission/Repository.php
@@ -666,7 +666,7 @@ abstract class Repository
     public function resetPermissions(int $contextId)
     {
         $submissions = Repo::submission()->getCollector()->filterByContextIds([$contextId])->getMany();
-        while ($submission = $submissions->next()) {
+        foreach ($submissions as $submission) {
             $publications = $submission->getData('publications');
             if (empty($publications)) {
                 continue;

06382fff8863f96bebcb4137cb92ae014f06c124
Alec Smecher <alec@smecher.bc.ca>
2023-03-29 16:38:07 -0700

    Add explicit Smarty modifiers

--- classes/template/PKPTemplateManager.inc.php
+++ classes/template/PKPTemplateManager.inc.php
@@ -256,6 +256,7 @@ class PKPTemplateManager extends Smarty {
 		$this->registerPlugin('modifier', 'json_encode', 'json_encode');
 		$this->registerPlugin('modifier', 'uniqid', 'uniqid');
 		$this->registerPlugin('modifier', 'substr', 'substr');
+		$this->registerPlugin('modifier', 'strstr', 'strstr');
 		$this->registerPlugin('modifier', 'translate', 'AppLocale::translate');
 		$this->registerPlugin('modifier','strip_unsafe_html', 'PKPString::stripUnsafeHtml');
 		$this->registerPlugin('modifier','String_substr', 'PKPString::substr');

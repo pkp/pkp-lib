df246bee3d9045838ae092385b466da8a2b4d526
Jonas Raoni Soares da Silva <github@raoni.org>
2023-04-16 15:53:18 +0300

    Added missing headers

--- classes/migration/upgrade/v3_4_0/I7014_DoiMigration.php
+++ classes/migration/upgrade/v3_4_0/I7014_DoiMigration.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * @file classes/migration/upgrade/v3_4_0/I7014_DoiMigration.php
+ *
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2000-2023 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class I7014_DoiMigration
+ *
+ * @brief Describe upgrade/downgrade operations for DB table DOIs.
+ */
+
 namespace APP\migration\upgrade\v3_4_0;
 
 use Illuminate\Database\Query\Builder;

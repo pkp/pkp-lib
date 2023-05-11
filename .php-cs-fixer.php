f533c05cda2de03b1262fbb8d86951befc0035c5
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-11 15:15:57 +0300

    pkp/pkp-lib#8989 Updated the Finder setup for the PHP code linter

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,24 +2,16 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->notPath([
-        '.github',
-        'cypress',
-        'dtd',
-        'js',
-        'lib',
-        'locale',
-        'registry',
-        'schemas',
-        'styles',
-        'templates',
-        'xml',
-    ])
     ->name('*.php')
-    ->name('_ide_helper')
-    ->notName('*.blade.php')
+    // The next two rules are enabled by default, kept for clarity
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreVCS(true)
+    // The pattern is matched against each found filename, thus:
+    // - The "/" is needed to avoid having "vendor" match "Newsvendor.php"
+    // - The presence of "node_modules" here doesn't prevent the Finder from recursing on it, so we merge these paths below at the "exclude()"
+    ->notPath($ignoredDirectories = ['cypress/', 'js/', 'locale/', 'node_modules/', 'styles/', 'templates/', 'vendor/'])
+    // Ignore root based directories
+    ->exclude(array_merge($ignoredDirectories, ['dtd', 'lib', 'registry', 'schemas', 'xml']));
 
 $rules = include '.php_cs_rules';
 

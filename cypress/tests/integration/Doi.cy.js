5dacbcf39744d03bc70a36fb42ea1126fef29a14
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-13 08:57:17 +0300

    pkp/pkp-lib#8951 Fixed path on header

--- cypress/tests/integration/Doi.cy.js
+++ cypress/tests/integration/Doi.cy.js
@@ -1,5 +1,5 @@
 /**
- * @file cypress/tests/integration/Doi.spec.js
+ * @file cypress/tests/integration/Doi.cy.js
  *
  * Copyright (c) 2014-2022 Simon Fraser University
  * Copyright (c) 2000-2022 John Willinsky

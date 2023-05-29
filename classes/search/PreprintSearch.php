97de9f262e6c97c7bf36410010ed0cf54db6d15a
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-29 10:53:15 +0300

    Fixed nonexistent method and invalid array key

--- classes/search/PreprintSearch.php
+++ classes/search/PreprintSearch.php
@@ -207,7 +207,7 @@ class PreprintSearch extends SubmissionSearch
                 while ($context = $contexts->next()) {
                     if (in_array(
                         $request->getUserVar('serverTitle'),
-                        (array) $context->getTitle(null)
+                        (array) $context->getName(null)
                     )) {
                         break;
                     }
@@ -234,7 +234,7 @@ class PreprintSearch extends SubmissionSearch
         $indexFieldMap[SubmissionSearch::SUBMISSION_SEARCH_INDEX_TERMS] = 'indexTerms';
         $keywords = [];
         if (isset($searchFilters['query'])) {
-            $keywords[null] = $searchFilters['query'];
+            $keywords[''] = $searchFilters['query'];
         }
         foreach ($indexFieldMap as $bitmap => $searchField) {
             if (isset($searchFilters[$searchField]) && !empty($searchFilters[$searchField])) {

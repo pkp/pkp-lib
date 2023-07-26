14471a94177b3303b24a45d473c97ed3110b7b2e
Alec Smecher <alec@smecher.bc.ca>
2023-07-26 16:27:39 -0700

    Allow callables for validation callbacks

--- tests/classes/form/validation/FormValidatorArrayCustomTest.php
+++ tests/classes/form/validation/FormValidatorArrayCustomTest.php
@@ -27,8 +27,8 @@ class FormValidatorArrayCustomTest extends PKPTestCase
 {
     private array $checkedValues = [];
     private Form $form;
-    private array $subfieldValidation;
-    private array $localeFieldValidation;
+    private $subfieldValidation;
+    private $localeFieldValidation;
 
     protected function setUp(): void
     {

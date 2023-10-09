260c796f33a6e6483bcdec71d1a1469c048e68f1
Nate Wright <natew@notthisway.com>
2023-10-09 12:20:44 +0100

    pkp/pkp-lib#9262 Fix typo in StoreTemporaryFileException

--- classes/core/exceptions/StoreTemporaryFileException.php
+++ classes/core/exceptions/StoreTemporaryFileException.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
 
 /**
- * @file classes/core/exceptions/StoryTemporaryFileException.php
+ * @file classes/core/exceptions/StoreTemporaryFileException.php
  *
  * Copyright (c) 2014-2023 Simon Fraser University
  * Copyright (c) 2000-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @class StoryTemporaryFileException
+ * @class StoreTemporaryFileException
  *
  * @brief Use this exception when an error is encountered while moving
  *   a temporary file to a permanent place.
@@ -22,7 +22,7 @@ use PKP\core\DataObject;
 use PKP\file\TemporaryFile;
 use PKP\user\User;
 
-class StoryTemporaryFileException extends Exception
+class StoreTemporaryFileException extends Exception
 {
     public function __construct(public TemporaryFile $temporaryFile, public string $targetPath, public ?User $user, public ?DataObject $dataObject)
     {

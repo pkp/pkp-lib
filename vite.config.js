08166076dd86b1a39e7742fb81e111e36bd9261b
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2023-10-10 11:24:47 +0200

    pkp/pkp-lib#9371 use vite locale key extractor, instead of manual list

--- vite.config.js
+++ vite.config.js
@@ -1,66 +1,77 @@
-import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import path from "path";
-import copy from "rollup-plugin-copy";
+import {defineConfig} from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import path from 'path';
+import copy from 'rollup-plugin-copy';
+import i18nExtractKeys from './lib/pkp/tools/i18nExtractKeys.vite.js';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
 	// its very unclear how the plugin-vue is handling inProduction option
 	// in any case its still heavily relying on NODE_ENV, thats why its being set
 	// so for example the devtools support is enabled in development mode
 	process.env.NODE_ENV = mode;
 	return {
 		plugins: [
+			i18nExtractKeys({
+				// existing in tpl files, to be replaced in future
+				extraKeys: [
+					'common.view',
+					'common.close',
+					'common.editItem',
+					'stats.descriptionForStat',
+					'common.commaListSeparator',
+				],
+			}),
 			Vue({
-				isProduction: mode === "production",
+				isProduction: mode === 'production',
 				template: {
 					compilerOptions: {
 						// to keep vue2 behaviour where spaces between html tags are preserved
-						whitespace: "preserve",
+						whitespace: 'preserve',
 					},
 				},
 			}),
 			copy({
 				targets: [
 					{
-						src: "lib/ui-library/public/styles/tinymce/*",
-						dest: "lib/pkp/styles/tinymce",
+						src: 'lib/ui-library/public/styles/tinymce/*',
+						dest: 'lib/pkp/styles/tinymce',
 					},
 				],
 				// run the copy task after writing the bundle
-				hook: "writeBundle",
+				hook: 'writeBundle',
 			}),
 		],
 		publicDir: false,
 		resolve: {
 			alias: {
-				"@": path.resolve(__dirname, "lib/ui-library/src"),
+				'@': path.resolve(__dirname, 'lib/ui-library/src'),
 				// use vue version with template compiler
-				vue: "vue/dist/vue.esm-bundler.js",
+				vue: 'vue/dist/vue.esm-bundler.js',
 			},
 		},
 		build: {
-			sourcemap: mode === "development" ? "inline" : false,
-			target: ["chrome64", "edge79", "firefox67", "safari12"],
+			sourcemap: mode === 'development' ? 'inline' : false,
+			target: ['chrome64', 'edge79', 'firefox67', 'safari12'],
 			emptyOutDir: false,
 			rollupOptions: {
 				input: {
-					build: "./js/load.js",
+					build: './js/load.js',
 				},
 				output: {
-					format: "iife", // Set the format to IIFE
-					entryFileNames: "js/build.js",
+					format: 'iife', // Set the format to IIFE
+					entryFileNames: 'js/build.js',
 					assetFileNames: (assetInfo) => {
-						const info = assetInfo.name.split(".");
+						const info = assetInfo.name.split('.');
 						const extType = info[info.length - 1];
 						if (/\.(css)$/.test(assetInfo.name)) {
-							return "styles/build.css";
+							return 'styles/build.css';
 						}
 						return `[name].${extType}`;
 					},
 					// Provide global variables to use in the UMD build
 					// for externalized deps
 					globals: {
-						vue: "pkp.Vue",
+						vue: 'pkp.Vue',
 					},
 				},
 			},

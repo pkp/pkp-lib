ad781a0c26159c03116745711262fb561672f1a0
Touhidur Rahman <abir.ruet@gmail.com>
2023-10-23 16:25:24 +0600

    pkp/pkp-lib#9434 Allow plugin level API call

--- classes/core/APIRouter.php
+++ classes/core/APIRouter.php
@@ -3,8 +3,8 @@
 /**
  * @file classes/core/APIRouter.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2000-2021 John Willinsky
+ * Copyright (c) 2023 Simon Fraser University
+ * Copyright (c) 2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class APIRouter
@@ -23,12 +23,20 @@ namespace PKP\core;
 use APP\core\Application;
 use Exception;
 use Illuminate\Http\Response;
+use PKP\core\PKPBaseController;
+use PKP\core\PKPRequest;
+use PKP\handler\APIHandler;
+use PKP\session\SessionManager;
 
 class APIRouter extends PKPRouter
 {
+    /** 
+     * Define if the api call for a plugin implemented endpoint 
+     */
+    protected bool $_pluginApi = false;
+
     /**
      * Determines path info parts
-     *
      */
     protected function getPathInfoParts(): array
     {
@@ -36,18 +44,69 @@ class APIRouter extends PKPRouter
     }
 
     /**
+     * Get the API controller file path
+     */
+    protected function getSourceFilePath(): string
+    {
+        if ($this->_pluginApi) {
+            return sprintf(
+                '%s/%s/%s/api/%s/%s/index.php',
+                $this->getPluginApiPathPrefix(),
+                $this->getPluginCategory(),
+                $this->getPluginName(),
+                $this->getVersion(), 
+                $this->getEntity()
+            );
+        }
+
+        return sprintf('api/%s/%s/index.php', $this->getVersion(), $this->getEntity());
+    }
+
+    /**
+     * Get the starting api url segment for plugin implemented API endpoint
+     * 
+     * @example Considering an API endpoint such as 
+     *          http://BASE_URL/index.php/CONTEXT_PATH/plugins/PLUGIN_CATEGORY/PLUGIN_NAME/api/VERSION/ENTITY
+     *          the plugin api uri prefix is `plugins` which start right after the CONTEXT_PATH
+     */
+    public function getPluginApiPathPrefix(): string
+    {
+        return 'plugins';
+    }
+
+    /**
+     * Define if the target reqeust is for plugin implemented API routes
+     */
+    public function isPluginApi(): bool
+    {
+        return $this->_pluginApi;
+    }
+
+    /**
      * Determines whether this router can route the given request.
-     *
-     * @param PKPRequest $request
      *
      * @return bool true, if the router supports this request, otherwise false
      */
-    public function supports($request): bool
+    public function supports(PKPRequest $request): bool
     {
         $pathInfoParts = $this->getPathInfoParts();
 
-        if (!is_null($pathInfoParts) && count($pathInfoParts) >= 2 && $pathInfoParts[1] == 'api') {
-            // Context-specific API requests: [index.php]/{contextPath}/api
+        if (is_null($pathInfoParts)) {
+            return false;
+        }
+
+        if (count($pathInfoParts) < 2) {
+            return false;
+        }
+
+        // Context-specific API requests: [index.php]/{contextPath}/api
+        if ($pathInfoParts[1] == 'api') {
+            return true;
+        }
+
+        // plugin specific API request [index.php]/{contextPath}/plugins/{category}/{pluginName}/api
+        if ($pathInfoParts[1] == $this->getPluginApiPathPrefix() && $pathInfoParts[4] == 'api') {
+            $this->_pluginApi = true;
             return true;
         }
 
@@ -60,6 +119,11 @@ class APIRouter extends PKPRouter
     public function getVersion(): string
     {
         $pathInfoParts = $this->getPathInfoParts();
+
+        if ($this->isPluginApi()) {
+            return Core::cleanFileVar($pathInfoParts[5] ?? '');
+        }
+
         return Core::cleanFileVar($pathInfoParts[2] ?? '');
     }
 
@@ -69,18 +133,51 @@ class APIRouter extends PKPRouter
     public function getEntity(): string
     {
         $pathInfoParts = $this->getPathInfoParts();
+
+        if ($this->isPluginApi()) {
+            return Core::cleanFileVar($pathInfoParts[6] ?? '');
+        }
+
         return Core::cleanFileVar($pathInfoParts[3] ?? '');
+    }
+
+    /** 
+     * Get the plugin name if the api endpoint is implemented at plugin level
+     */
+    public function getPluginName(): string
+    {
+        if (!$this->isPluginApi()) {
+            return '';
+        }
+
+        $pathInfoParts = $this->getPathInfoParts();
+
+        return Core::cleanFileVar($pathInfoParts[3]);
+    }
+
+    /** 
+     * Get the plugin category if the api endpoint is implemented at plugin level
+     */
+    public function getPluginCategory(): string
+    {
+        if (!$this->isPluginApi()) {
+            return '';
+        }
+
+        $pathInfoParts = $this->getPathInfoParts();
+
+        return Core::cleanFileVar($pathInfoParts[2]);
     }
 
     //
     // Implement template methods from PKPRouter
     //
     /**
-     * @copydoc PKPRouter::route()
+     * @copydoc \PKP\core\PKPRouter::route()
      */
     public function route(PKPRequest $request): void
     {
-        $sourceFile = sprintf('api/%s/%s/index.php', $this->getVersion(), $this->getEntity());
+        $sourceFile = $this->getSourceFilePath();
 
         if (!file_exists($sourceFile)) {
             response()->json([
@@ -90,14 +187,26 @@ class APIRouter extends PKPRouter
             exit;
         }
 
-        $handler = require('./' . $sourceFile);
+        if (!SessionManager::isDisabled()) {
+            SessionManager::getManager(); // Initialize session
+        }
+
+        $handler = require('./' . $sourceFile); /** @var \PKP\handler\APIHandler|\PKP\core\PKPBaseController $handler */
+
+        if ($handler instanceof PKPBaseController) {
+            $handler = new APIHandler($handler); /** @var \PKP\handler\APIHandler $handler */
+        }
+
         $this->setHandler($handler);
+        $handler->runRoutes();
     }
 
     /**
      * Get the requested operation
+     *
+     * @return string
      */
-    public function getRequestedOp(PKPRequest $request): string
+    public function getRequestedOp(PKPRequest $request)
     {
         if ($routeActionName = PKPBaseController::getRouteActionName()) {
             return $routeActionName;

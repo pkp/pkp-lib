0921547c5fb072749ddd8b949cca2336e938d884
Touhidur Rahman <abir.ruet@gmail.com>
2023-10-23 16:25:24 +0600

    pkp/pkp-lib#9434 Allow plugin level API call

--- classes/core/PKPBaseController.php
+++ classes/core/PKPBaseController.php
@@ -76,7 +76,6 @@ abstract class PKPBaseController extends Controller
      * The unique endpoint string for the APIs that will be served through controller.
      *
      * This is equivalent to property \PKP\handler\APIHandler::_handlerPath
-     * which will be passed through \PKP\handler\APIHandler\PKPApiRoutingHandler
      */
     abstract public function getHandlerPath(): string;
 
@@ -148,7 +147,6 @@ abstract class PKPBaseController extends Controller
      * The endpoint pattern for the APIs that will be served through controller.
      *
      * This is equivalent to property \PKP\handler\APIHandler::_pathPattern
-     * which will be passed through \PKP\handler\APIHandler\PKPApiRoutingHandler
      */
     public function getPathPattern(): ?string
     {
@@ -159,7 +157,6 @@ abstract class PKPBaseController extends Controller
      * Define if all the path building for admin api use rather than at context level
      *
      * This is equivalent to property \PKP\handler\APIHandler::_apiForAdmin
-     * which will be passed through \PKP\handler\APIHandler\PKPApiRoutingHandler
      */
     public function isSiteWide(): bool
     {

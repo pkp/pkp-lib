681c9c42a56d50ccbd35853854238e950d27f360
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-01-25 15:11:38 +0100

    pkp/pkp-lib#9527 Update vite dependency to address css stylesheet order issue

--- js/load.js
+++ js/load.js
@@ -8,6 +8,10 @@
  * @brief Compiler entry point for building the JavaScript package. File imports
  *  using the `@` symbol are aliased to `lib/ui-library/src`.
  */
+
+// styles
+import '@/styles/_global.less';
+
 import PkpLoad from '../lib/pkp/js/load.js';
 
 // Import controllers used by OPS
@@ -32,9 +36,6 @@ import JobsPage from '@/components/Container/JobsPage.vue';
 import FailedJobsPage from '@/components/Container/FailedJobsPage.vue';
 import FailedJobDetailsPage from '@/components/Container/FailedJobDetailsPage.vue';
 
-// styles
-import '@/styles/_global.less';
-
 // Expose Vue, the registry and controllers in a global var
 window.pkp = Object.assign(PkpLoad, window.pkp || {}, {
 	controllers: {
@@ -57,6 +58,6 @@ window.pkp = Object.assign(PkpLoad, window.pkp || {}, {
 		StatsContextPage,
 		StatsUsersPage,
 		SubmissionWizardPage,
-		WorkflowPage
+		WorkflowPage,
 	},
 });

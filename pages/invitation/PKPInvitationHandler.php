2413f55d1df260ae859a53a4cd0777fe69779605
ipula <ipularanasinghe007@gmail.com>
2024-02-02 18:38:46 +0100

    user create

--- pages/invitation/PKPInvitationHandler.php
+++ pages/invitation/PKPInvitationHandler.php
@@ -16,9 +16,13 @@
 
 namespace PKP\pages\invitation;
 
+use APP\core\Application;
 use APP\core\Request;
 use APP\facades\Repo;
 use APP\handler\Handler;
+use APP\template\TemplateManager;
+use PKP\components\forms\createUser\UserDetailsForm;
+use PKP\core\PKPApplication;
 use PKP\invitation\invitations\BaseInvitation;
 
 class PKPInvitationHandler extends Handler
@@ -27,13 +31,44 @@ class PKPInvitationHandler extends Handler
     public const REPLY_OP_ACCEPT = 'accept';
     public const REPLY_OP_DECLINE = 'decline';
 
+    /** @copydoc PKPHandler::_isBackendPage */
+    public $_isBackendPage = true;
+
+    //
+    // Overridden methods from Handler
+    //
+    /**
+     * @see PKPHandler::initialize()
+     */
+    public function initialize($request)
+    {
+        parent::initialize($request);
+
+        $templateMgr = TemplateManager::getManager($request);
+        $templateMgr->assign('pageComponent', 'SettingsPage');
+    }
     /**
      * Accept invitation handler
      */
     public function accept($args, $request): void
     {
         $invitation = $this->getInvitationByKey($request);
-        $invitation->acceptHandle();
+        $templateMgr = TemplateManager::getManager($request);
+        $this->setupTemplate($request);
+        $context = $request->getContext();
+        $steps = $this->getSteps($request);
+        $templateMgr->setState([
+            'steps' => $steps,
+            'createAccountApiUrl'=>$this->getCreateUserApiUrl($request),
+            'primaryLocale'=> $context->getData('primaryLocale'),
+            'pageTitle' => __('invitation.wizard.pageTitle'),
+            'pageTitleDescription' => __('invitation.wizard.pageTitleDescription'),
+        ]);
+        $templateMgr->assign([
+            'pageComponent' => 'PageOJS',
+        ]);
+        $templateMgr->display('invitation/createUser.tpl');
+//        $invitation->acceptHandle();
     }
 
     /**
@@ -58,5 +93,125 @@ class PKPInvitationHandler extends Handler
         }
 
         return $invitation;
+    }
+
+    /**
+     * get user account create steps
+     */
+    protected function getSteps(Request $request): array
+    {
+        $apiUrl = $this->getCreateUserApiUrl($request);
+
+        $steps = [];
+        $steps[] = $this->verifyOrcid();
+        $steps[] = $this->userCreate();
+        $steps[] = $this->getUserDetailsForm($request,$apiUrl);
+        $steps[] = $this->userCreateReview();
+
+        return $steps;
+    }
+
+    /**
+     * Get the state for the user orcid verification
+     */
+    protected function verifyOrcid(): array
+    {
+        return [
+            'id' => 'verifyOrcid',
+            'name' => __('invitation.verifyOrcid'),
+            'reviewName' => __('invitation.verifyOrcidStep'),
+            'type' => 'popup',
+            'description' => __('invitation.verifyOrcidDescription'),
+            'sections' => [],
+        ];
+    }
+
+    /**
+     * create username and password for ojs account
+     */
+    protected function userCreate(): array
+    {
+        return [
+            'id' => 'userCreate',
+            'name' => __('invitation.userCreate'),
+            'reviewName' => __('invitation.userCreateStep'),
+            'type' => 'form',
+            'description' => __('invitation.userCreateDescription'),
+            'sections' => [],
+        ];
+    }
+
+    protected function getUserDetailsForm(Request $request,string $apiUrl): array
+    {
+        $localeNames = $request->getContext()->getSupportedFormLocaleNames();
+        $locales = [];
+        foreach ($localeNames as $key => $name) {
+            $locales[] = [
+                'key' => $key,
+                'label' => $name,
+            ];
+        }
+        $contactForm = new UserDetailsForm($apiUrl, $locales);
+
+        $sections = [
+            [
+                'id' => 'userCreateDetailsForm',
+                'type'=>'form',
+                'description' => $request->getContext()->getLocalizedData('detailsHelp'),
+                'form' => $contactForm->getConfig(),
+            ],
+            [
+                'id' => 'userCreateRoles',
+                'type'=>'table',
+                'description' => '',
+                'rows' => [
+                    [
+                        'date_start'=>'2024-03-01',
+                        'date_end'=>'2025-01-01',
+                        'user_group_id'=>3,
+                        'setting_value'=>'test',
+                    ]
+                ]
+            ],
+        ];
+
+        return [
+            'id' => 'userDetails',
+            'name' => __('invitation.userCreateDetails'),
+            'reviewName' => __('invitation.userCreateDetailStep'),
+            'type' => 'form',
+            'description' => __('invitation.userCreateDetailsDescription'),
+            'sections' => $sections,
+        ];
+    }
+
+    /**
+     * create review all steps for create ojs account
+     */
+    protected function userCreateReview(): array
+    {
+        return [
+            'id' => 'userCreateReview',
+            'name' => __('invitation.userCreateReview'),
+            'reviewName' => __('invitation.userCreateReviewStep'),
+            'type' => 'review',
+            'description' => __('invitation.userCreateReviewDescription'),
+            'sections' => [],
+        ];
+    }
+
+    /**
+     * Get the url to the create user API endpoint
+     */
+    protected function getCreateUserApiUrl(Request $request): string
+    {
+        return $request
+            ->getDispatcher()
+            ->url(
+                $request,
+                PKPApplication::ROUTE_API,
+                $request->getContext()->getPath(),
+                'users'
+            );
     }
 }

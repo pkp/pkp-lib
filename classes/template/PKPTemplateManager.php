42de372906da3b05d8078885b2581ad87cbef1ac
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-02-08 21:13:13 +0100

    pkp/pkp-lib#9650 Prevent template injection (#9711)

--- classes/template/PKPTemplateManager.php
+++ classes/template/PKPTemplateManager.php
@@ -118,6 +118,11 @@ class PKPTemplateManager extends Smarty
     /** @var string[] */
     private array $headers = [];
 
+
+    /** @var bool Track whether its backend page */
+    private bool $isBackendPage = false;
+
+
     /**
      * Constructor.
      * Initialize template engine and assign basic template variables.
@@ -800,6 +805,7 @@ class PKPTemplateManager extends Smarty
      */
     public function setupBackendPage()
     {
+        $this->isBackendPage = true;
         $request = Application::get()->getRequest();
         $dispatcher = $request->getDispatcher();
         /** @var PageRouter */
@@ -1261,6 +1267,37 @@ class PKPTemplateManager extends Smarty
      */
     public function display($template = null, $cache_id = null, $compile_id = null, $parent = null)
     {
+
+        if($this->isBackendPage) {
+            $this->unregisterPlugin('modifier', 'escape');
+
+            /** prevent {{ JS }} injection  */
+            $this->registerPlugin('modifier', 'escape', function ($string, $esc_type = 'html', $char_set = 'ISO-8859-1') {
+                $result = $string;
+                if($esc_type === 'html') {
+                    $result = $this->smartyEscape($result, $esc_type, $char_set);
+                    $result = str_replace('{{', '<span v-pre>{{</span>', $result);
+                    $result = str_replace('}}', '<span v-pre>}}</span>', $result);
+                    return $result;
+                }
+
+
+                return $this->smartyEscape($result, $esc_type, $char_set);
+                
+            });
+
+            $this->unregisterPlugin('modifier', 'strip_unsafe_html');
+
+            /** prevent {{ JS }} injection  */
+            $this->registerPlugin('modifier', 'strip_unsafe_html', function ($input, $configKey = 'allowed_html') {
+                $result = \PKP\core\PKPString::stripUnsafeHtml($input, $configKey);
+                $result = str_replace('{{', '<span v-pre>{{</span>', $result);
+                $result = str_replace('}}', '<span v-pre>}}</span>', $result);
+                return $result;                
+            });
+
+
+        }
         // Output global constants and locale keys used in new component library
         $output = '';
         if (!empty($this->_constants)) {

199ee9a0a9d0b84ecf033dc8f4258e5d9a0d4776
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2024-03-26 10:43:54 +0200

    pkp/pkp-lib#9825 Submission data in review assignment schema

--- classes/submission/reviewAssignment/maps/Schema.php
+++ classes/submission/reviewAssignment/maps/Schema.php
@@ -1,19 +1,20 @@
 <?php
 /**
- * @file classes/announcement/maps/Schema.php
+ * @file classes/reviewAssignment/maps/Schema.php
  *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2000-2023 John Willinsky
+ * Copyright (c) 2014-2024 Simon Fraser University
+ * Copyright (c) 2000-2024 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class Schema
  *
- * @brief Map announcements to the properties defined in the announcement schema
+ * @brief Map review assignments to the properties defined in the review assignment schema
  */
 
 namespace PKP\submission\reviewAssignment\maps;
 
 use APP\facades\Repo;
+use APP\submission\Submission;
 use Illuminate\Support\Enumerable;
 use PKP\services\PKPSchemaService;
 use PKP\submission\reviewAssignment\ReviewAssignment;
@@ -25,23 +26,23 @@ class Schema extends \PKP\core\maps\Schema
     public string $schema = PKPSchemaService::SCHEMA_REVIEW_ASSIGNMENT;
 
     /**
-     * Map an announcement
+     * Map the Review Assignment
      *
      * Includes all properties in the review assignment schema.
      */
-    public function map(ReviewAssignment $item): array
+    public function map(ReviewAssignment $item, Submission $submission): array
     {
-        return $this->mapByProperties($this->getProps(), $item);
+        return $this->mapByProperties($this->getProps(), $item, $submission);
     }
 
     /**
-     * Summarize an announcement
+     * Summarize the Review Assignment
      *
      * Includes properties with the apiSummary flag in the review assignment schema.
      */
-    public function summarize(ReviewAssignment $item): array
+    public function summarize(ReviewAssignment $item, Submission $submission): array
     {
-        return $this->mapByProperties($this->getSummaryProps(), $item);
+        return $this->mapByProperties($this->getSummaryProps(), $item, $submission);
     }
 
     /**
@@ -52,9 +53,21 @@ class Schema extends \PKP\core\maps\Schema
     public function mapMany(Enumerable $collection): Enumerable
     {
         $this->collection = $collection;
-        return $collection->map(function ($item) {
-            return $this->map($item);
-        });
+        $submissions = Repo::submission()->getCollector()
+            ->filterByContextIds([$this->context->getId()])
+            ->filterByReviewIds($collection->keys()->toArray())
+            ->getMany()
+            ->remember();
+
+        $associatedSubmissions = $this->collection->map(
+            fn (ReviewAssignment $reviewAssignment) =>
+            $reviewAssignment->getData('submissionId')
+        );
+
+        return $collection->map(
+            fn ($item) =>
+            $this->map($item, $submissions->get($associatedSubmissions->get($item->getId())))
+        );
     }
 
     /**
@@ -65,19 +78,34 @@ class Schema extends \PKP\core\maps\Schema
     public function summarizeMany(Enumerable $collection): Enumerable
     {
         $this->collection = $collection;
-        return $collection->map(function ($item) {
-            return $this->summarize($item);
-        });
+        $submissions = Repo::submission()->getCollector()
+            ->filterByContextIds([$this->context->getId()])
+            ->filterByReviewIds($collection->keys()->toArray())
+            ->getMany()
+            ->remember();
+
+        $associatedSubmissions = $this->collection->map(
+            fn (ReviewAssignment $reviewAssignment) =>
+                $reviewAssignment->getData('submissionId')
+        );
+
+        return $collection->map(
+            fn ($item) =>
+            $this->summarize($item, $submissions->get($associatedSubmissions->get($item->getId())))
+        );
     }
 
     /**
-     * Map schema properties of an Announcement to an assoc array
+     * Map schema properties of the Review Assignment to an assoc array
      */
-    protected function mapByProperties(array $props, ReviewAssignment $item): array
+    protected function mapByProperties(array $props, ReviewAssignment $item, Submission $submission): array
     {
         $output = [];
         foreach ($props as $prop) {
             switch ($prop) {
+                case 'publicationTitle':
+                    $output[$prop] = $submission->getCurrentPublication()->getFullTitles('html');
+                    break;
                 case '_href':
                     $output[$prop] = $this->getApiUrl('_submissions/reviewAssignments/' . $item->getId());
                     break;

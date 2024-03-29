224509ce0a0722e4934ead7826e8402b21be2d81
Jonas Raoni Soares da Silva <github@raoni.org>
2024-03-29 18:31:39 +0300

    pkp/pkp-lib#6186 If the field is multilingual, but doesn't have a default value, it still need to be decoded

--- classes/plugins/ThemePlugin.inc.php
+++ classes/plugins/ThemePlugin.inc.php
@@ -571,6 +571,16 @@ abstract class ThemePlugin extends LazyLoadPlugin {
 						break;
 				}
 			}
+			// If the value isn't null and it's a multilingual field, then we must ensure it's an array
+			if ($optionConfig->isMultilingual && $value !== null && !is_array($value)) {
+				try {
+					$value =  json_decode((string) $value, true, 512, JSON_THROW_ON_ERROR);
+				} catch (Exception $e) {
+					// FIXME: pkp/pkp-lib#6250 Remove after 3.3.x upgrade code is removed (see also pkp/pkp-lib#5772)
+					$value = unserialize($value);
+				}
+				$value = is_array($value) ? $value : [];
+			}
 			$return[$optionName] = $value;
 		}
 

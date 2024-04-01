ba8c0c6b185fdbb208483886f54a9f80e43933a2
Jonas Raoni Soares da Silva <github@raoni.org>
2024-04-01 17:32:50 +0300

    pkp/pkp-lib#9851 Added pre-flight check to avoid deleting section editor assignments

--- classes/migration/upgrade/v3_4_0/PreflightCheckMigration.php
+++ classes/migration/upgrade/v3_4_0/PreflightCheckMigration.php
@@ -44,6 +44,7 @@ abstract class PreflightCheckMigration extends \PKP\migration\Migration
     {
         try {
             $this->checkUsageStatsLogs();
+            $this->checkRequiredUserEditor();
             // It's needed to clear the duplicated settings before looking for duplicated localized data to avoid false positives
             $this->clearDuplicatedUserSettings();
             $this->checkLocaleConflicts();
@@ -218,6 +219,32 @@ abstract class PreflightCheckMigration extends \PKP\migration\Migration
             count(glob($usageStatsDir . '/stage/*')) !== 0
         ) {
             throw new Exception("There are one or more log files that were unable to finish processing. This happens when the scheduled task to process usage stats logs encounters a failure of some kind. These logs must be repaired and reprocessed or removed before the upgrade can continue. The logs can be found in the folders reject, processing and stage in {$usageStatsDir}.");
+        }
+    }
+
+    /**
+     * Ensures that contexts with section editor assignments have a section editor role
+     * @see classes\migration\upgrade\v3_4_0\I7191_EditorAssignments.php
+     *
+     * @throws Exception
+     */
+    protected function checkRequiredUserEditor(): void
+    {
+        $contextId = "c.{$this->getContextKeyField()}";
+        // Look for contexts that have section editor assignments, but no section editor role
+        $contextsWithoutSubEditor = DB::table($this->getContextTable(), 'c')
+            ->whereNotExists(fn (Builder $q) => $q->from('user_groups', 'ug')
+                ->whereColumn('ug.context_id', '=', $contextId)
+                ->where('ug.role_id', '=', 17) // Role::ROLE_ID_SUB_EDITOR
+                ->selectRaw('0')
+            )
+            ->whereExists(fn (Builder $q) => $q->from('subeditor_submission_group', 'ssg')
+                ->whereColumn('ssg.context_id', '=', $contextId)
+                ->selectRaw('0')
+            )
+            ->pluck('c.path');
+        if ($contextsWithoutSubEditor->count()) {
+            throw new Exception("The following contexts have section editor assignments, but no \"section editor\" role. Please, create the \"section editor\" role before proceeding with the upgrade: {$contextsWithoutSubEditor->join(', ')}");
         }
     }
 

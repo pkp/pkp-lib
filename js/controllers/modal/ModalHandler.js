756c73202d66d2a4d6d681878b559c5377df0371
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2024-04-08 15:04:58 +0200

    wip

--- js/controllers/modal/ModalHandler.js
+++ js/controllers/modal/ModalHandler.js
@@ -16,9 +16,9 @@
  *
  *  A modal that has only one button and expects a simple message string.
  */
-(function ($) {
+(function($){
 	/** @type {Object} */
-	$.pkp.controllers.modal = $.pkp.controllers.modal || {};
+	$.pkp.controllers.modal = $.pkp.controllers.modal || { };
 
 	/**
 	 * @constructor

82771f74c6e831a74c327358fb0294230ebebee6
Hafsa-Naeem <hafsanaeem321@hotmail.com>
2024-06-15 07:51:39 +0500

    pkp/pkp-lib#10051 Fixed HTML entity code &amp; in OJS email subject

--- classes/mail/Mailable.php
+++ classes/mail/Mailable.php
@@ -359,6 +359,9 @@ class Mailable extends IlluminateMailable
             ->toArray();
 
         $subject = app('mailer')->compileParams($this->subject, $withoutTagViewData);
+        // decode HTML entities for the subject
+        $subject = htmlspecialchars_decode($subject, ENT_QUOTES | ENT_HTML5);
+
         if (empty($subject)) {
             trigger_error(
                 'You are sending ' . static::getName() ?? static::class . ' email with empty subject',

338ac3eb7c04b5fea7cbb39336dc33c988e4bf71
Hafsa-Naeem <hafsanaeem321@hotmail.com>
2024-06-17 23:22:38 +0500

    pkp/pkp-lib#10051 Ensure empty subject triggers a warning

--- classes/mail/Mailable.php
+++ classes/mail/Mailable.php
@@ -353,7 +353,9 @@ class Mailable extends IlluminateMailable
      */
     protected function buildSubject($message): self
     {
-        $this->subject ??= ''; // Allow email with empty subject if not set
+        if ($this->subject === null) {
+            $this->subject = ''; // Allow email with empty subject if not set
+        }
         $withoutTagViewData = collect($this->viewData)
             ->map(fn (mixed $viewableData) => is_string($viewableData) ? strip_tags($viewableData) : $viewableData)
             ->toArray();

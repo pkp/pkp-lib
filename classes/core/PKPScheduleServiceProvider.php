19047a2549370ee1ef505cdcb0aa4aaa5837a76b
Touhidur Rahman <abir.ruet@gmail.com>
2024-07-04 16:49:31 +0600

    pkp/pkp-lib#9678 rearrange the process of task runner for web based request

--- classes/core/PKPScheduleServiceProvider.php
+++ classes/core/PKPScheduleServiceProvider.php
@@ -41,44 +41,48 @@ class PKPScheduleServiceProvider extends ServiceProvider implements DeferrablePr
             if (!Application::get()->isUnderMaintenance()) {
                 $scheduler->registerSchedules();
             }
-
-            if (Config::getVar('schedule', 'task_runner', true)) {
-                $taskRunnerInterval = Config::getVar('schedule', 'task_runner_interval', 60);
-                $lastRunTimestamp   = Cache::get('schedule::taskRunner::lastRunAt') ?? 0;
-                $curerntTimestamp   = Carbon::now()->timestamp;
-                
-                if ($curerntTimestamp - $lastRunTimestamp > $taskRunnerInterval) {
-                    
-                    Cache::forget('schedule::taskRunner::lastRunAt');
-                    Cache::put('schedule::taskRunner::lastRunAt', $curerntTimestamp, 3600);
-
-                    register_shutdown_function(function () {
-                        // As this runs at the current request's end but the 'register_shutdown_function' registered
-                        // at the service provider's registration time at application initial bootstrapping,
-                        // need to check the maintenance status within the 'register_shutdown_function'
-                        if (Application::get()->isUnderMaintenance()) {
-                            return;
-                        }
-        
-                        // Application is set to sandbox mode and will not run any schedule tasks
-                        if (Config::getVar('general', 'sandbox', false)) {
-                            error_log('Application is set to sandbox mode and will not run any schedule tasks');
-                            return;
-                        }
-        
-                        // We only want to web based task runner for the web request life cycle
-                        // not in any CLI based request life cycle
-                        if ($this->app->runningInConsole()) {
-                            return;
-                        }
-                        
-                        $scheduler = $this->app->get(Scheduler::class); /** @var \APP\scheduler\Scheduler $scheduler */
-                        $scheduler->registerPluginSchedules();
-                        $scheduler->runWebBasedScheduleTaskRunner();
-                    });
-                }
-            }
         });
+
+        if (Config::getVar('schedule', 'task_runner', true)) {
+            $taskRunnerInterval = Config::getVar('schedule', 'task_runner_interval', 60);
+            $lastRunTimestamp   = Cache::get('schedule::taskRunner::lastRunAt') ?? 0;
+            $curerntTimestamp   = Carbon::now()->timestamp;
+            
+            if ($curerntTimestamp - $lastRunTimestamp > $taskRunnerInterval) {
+                
+                if (!$this->app->runningInConsole()) {
+                    $this->booted(fn () => $this->app->make(Schedule::class));
+                }
+
+                Cache::forget('schedule::taskRunner::lastRunAt');
+                Cache::put('schedule::taskRunner::lastRunAt', $curerntTimestamp, 3600);
+
+                register_shutdown_function(function () {
+                    // As this runs at the current request's end but the 'register_shutdown_function' registered
+                    // at the service provider's registration time at application initial bootstrapping,
+                    // need to check the maintenance status within the 'register_shutdown_function'
+                    if (Application::get()->isUnderMaintenance()) {
+                        return;
+                    }
+    
+                    // Application is set to sandbox mode and will not run any schedule tasks
+                    if (Config::getVar('general', 'sandbox', false)) {
+                        error_log('Application is set to sandbox mode and will not run any schedule tasks');
+                        return;
+                    }
+    
+                    // We only want to web based task runner for the web request life cycle
+                    // not in any CLI based request life cycle
+                    if ($this->app->runningInConsole()) {
+                        return;
+                    }
+                    
+                    $scheduler = $this->app->get(Scheduler::class); /** @var \APP\scheduler\Scheduler $scheduler */
+                    $scheduler->registerPluginSchedules();
+                    $scheduler->runWebBasedScheduleTaskRunner();
+                });
+            }
+        }
     }
 
     /**
@@ -104,10 +108,6 @@ class PKPScheduleServiceProvider extends ServiceProvider implements DeferrablePr
                 return new Scheduler($app->get(Schedule::class));
             }
         );
-
-        if (!$this->app->runningInConsole()) {
-            $this->booted(fn () => $this->app->make(Schedule::class));
-        }
     }
 
     /**

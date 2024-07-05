56e8ece08eb2c368bba45a51bebef41cbf57a5cf
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-07-05 19:41:19 +0800

    pkp/pkp-lib#9753 Upgrade jquery validation from v1.11.1 to v1.19.5

--- js/lib/jquery/plugins/validate/localization/methods_es_CL.js
+++ js/lib/jquery/plugins/validate/localization/methods_es_CL.js
@@ -10,11 +10,14 @@
 
 /*
  * Localized default methods for the jQuery validation plugin.
- * Locale: PT_BR
+ * Locale: ES_CL
  */
 $.extend( $.validator.methods, {
 	date: function( value, element ) {
-		return this.optional( element ) || /^\d\d?\/\d\d?\/\d\d\d?\d?$/.test( value );
+		return this.optional( element ) || /^\d\d?\-\d\d?\-\d\d\d?\d?$/.test( value );
+	},
+	number: function( value, element ) {
+		return this.optional( element ) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test( value );
 	}
 } );
 return $;
--- js/lib/jquery/plugins/validate/localization/methods_it.js
+++ js/lib/jquery/plugins/validate/localization/methods_it.js
@@ -10,11 +10,14 @@
 
 /*
  * Localized default methods for the jQuery validation plugin.
- * Locale: PT_BR
+ * Locale: IT
  */
 $.extend( $.validator.methods, {
 	date: function( value, element ) {
-		return this.optional( element ) || /^\d\d?\/\d\d?\/\d\d\d?\d?$/.test( value );
+		return this.optional( element ) || /^\d\d?\-\d\d?\-\d\d\d?\d?$/.test( value );
+	},
+	number: function( value, element ) {
+		return this.optional( element ) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test( value );
 	}
 } );
 return $;

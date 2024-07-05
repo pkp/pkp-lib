7c101fb1e18eec85e1810b0b3ba80bfafef06ad4
Alec Smecher <alec@smecher.bc.ca>
2024-07-05 12:57:43 -0700

    Proof of concept: Allow OJS to send .ical invites with RSVP capabilities for review invites

--- classes/mail/mailables/ReviewRequest.php
+++ classes/mail/mailables/ReviewRequest.php
@@ -16,7 +16,18 @@
 
 namespace PKP\mail\mailables;
 
+use APP\facades\Repo;
+use Illuminate\Mail\Mailables\Attachment;
+use Jsvrcek\ICS\CalendarExport;
+use Jsvrcek\ICS\CalendarStream;
+use Jsvrcek\ICS\Model\Calendar;
+use Jsvrcek\ICS\Model\CalendarEvent;
+use Jsvrcek\ICS\Model\Relationship\Attendee;
+use Jsvrcek\ICS\Model\Relationship\Organizer;
+use Jsvrcek\ICS\Utility\Formatter;
+use PKP\config\Config;
 use PKP\context\Context;
+use PKP\core\PKPApplication;
 use PKP\mail\Mailable;
 use PKP\mail\traits\Configurable;
 use PKP\mail\traits\Recipient;
@@ -40,8 +51,50 @@ class ReviewRequest extends Mailable
     protected static array $fromRoleIds = [Role::ROLE_ID_SUB_EDITOR];
     protected static array $toRoleIds = [Role::ROLE_ID_REVIEWER];
 
+    protected ReviewAssignment $reviewAssignment;
+
     public function __construct(Context $context, PKPSubmission $submission, ReviewAssignment $reviewAssignment)
     {
+        $this->reviewAssignment = $reviewAssignment;
         parent::__construct(func_get_args());
+    }
+
+    public function attachments(): array
+    {
+        // If a machine reply-to email address was not specified, do not attach an ical invite.
+        if (!Config::getVar('email', 'reply_to_address')) {
+            return [];
+        }
+
+        $event = new CalendarEvent();
+        $event->setStart(new \DateTime())
+            ->setSummary(__(static::$name))
+            ->setUid(uniqid('reviewAssignment'));
+
+        $reviewerUser = Repo::user()->get($this->reviewAssignment->getReviewerId());
+        $attendee = new Attendee(new Formatter());
+        $attendee->setValue($reviewerUser->getEmail())
+            ->setName($reviewerUser->getFullName())
+            ->setRsvp('TRUE');
+        $event->addAttendee($attendee);
+
+        $request = PKPApplication::get()->getRequest();
+        $user = $request->getUser();
+        $organizer = new Organizer(new Formatter());
+        $organizer->setValue(Config::getVar('email', 'reply_to_address'))
+            ->setName($user->getFullName());
+        $event->setOrganizer($organizer);
+
+        $calendar = new Calendar();
+        $calendar->setProdId('-//PKP//Public Knowledge Project//EN')
+            ->addEvent($event);
+
+        $calendarExport = new CalendarExport(new CalendarStream(), new Formatter());
+        $calendarExport->addCalendar($calendar);
+
+        return [
+            Attachment::fromData(fn () => $calendarExport->getStream(), 'invite.ics')
+                ->withMime('text/calendar; charset="utf-8"; method=REQUEST')
+        ];
     }
 }

add96c21d416e0922978d5cc86d897acc88e5d9e
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-07-12 01:33:35 +0800

    pkp/pkp-lib#9753 [main] Update paths when importing chart.js, jquery, jquery-ui and jquery-validation

--- classes/template/PKPTemplateManager.php
+++ classes/template/PKPTemplateManager.php
@@ -664,10 +664,10 @@ class PKPTemplateManager extends Smarty
         // properly by our build script
         $this->addJavaScript(
             'jqueryValidate',
-            $baseUrl . '/lib/pkp/lib/vendor/jquery/validation/dist/jquery.validate.min.js',
+            $baseUrl . '/js/build/jquery-validation/jquery.validate.min.js',
             $args
         );
-        $jqvLocalePath = 'lib/pkp/lib/vendor/jquery/validation/dist/localization/messages_';
+        $jqvLocalePath = 'js/build/jquery-validation/localization/messages_';
         foreach ($localeChecks as $localeCheck) {
             if (file_exists($jqvLocalePath . $localeCheck . '.js')) {
                 $this->addJavaScript('jqueryValidateLocale', $baseUrl . '/' . $jqvLocalePath . $localeCheck . '.js', $args);
@@ -868,7 +868,7 @@ class PKPTemplateManager extends Smarty
         $min = Config::getVar('general', 'enable_minified') ? '.min' : '';
         $this->addJavaScript(
             'jquery',
-            $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jquery/jquery' . $min . '.js',
+            $request->getBaseUrl() . '/js/build/jquery/jquery' . $min . '.js',
             [
                 'priority' => self::STYLE_SEQUENCE_CORE,
                 'contexts' => 'backend',
@@ -876,7 +876,7 @@ class PKPTemplateManager extends Smarty
         );
         $this->addJavaScript(
             'jqueryUI',
-            $request->getBaseUrl() . '/lib/pkp/lib/vendor/jquery/ui/dist/jquery-ui' . $min . '.js',
+            $request->getBaseUrl() . '/js/build/jquery-ui/jquery-ui' . $min . '.js',
             [
                 'priority' => self::STYLE_SEQUENCE_CORE,
                 'contexts' => 'backend',

d09a88d24b81870fff873a1633cf8fe6f995336a
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-07-18 21:38:46 +0800

    pkp/pkp-lib#9753 [stable-3_4_0] Add script to copy jquery and jqueryui when running composer install

--- classes/dev/ComposerScript.php
+++ classes/dev/ComposerScript.php
@@ -32,4 +32,48 @@ class ComposerScript
             throw new Exception("The ISO639-2b file {$iso6392bFile} does not exist.");
         }
     }
+
+    
+    /**
+     * A post-install-cmd custom composer script that
+     * copies node_module dependencies
+     */
+    public static function copyNodeModuleDeps(): void
+    {   
+        function copyDir($src, $dst) {
+            $dir = opendir($src);
+            @mkdir($dst, 0755, true);
+            while (false !== ($file = readdir($dir))) {
+                if ($file != '.' && $file != '..') {
+                    if (is_dir($src . '/' . $file)) {
+                        copyDir($src . '/' . $file, $dst . '/' . $file);
+                    } else {
+                        copy($src . '/' . $file, $dst . '/' . $file);
+                    }
+                }
+            }
+            closedir($dir);
+        }
+
+        try {
+            $baseDir = __DIR__ . '/../../../../node_modules';
+            $jqueryDist = $baseDir . '/jquery/dist';
+            $jqueryUiDist = $baseDir . '/jquery-ui/dist';
+            $vendorComponents = __DIR__ . '/../../lib/vendor/components';
+
+            if (!file_exists($vendorComponents . '/jquery')) {
+                mkdir($vendorComponents . '/jquery', 0755, true);
+            }
+            if (!file_exists($vendorComponents . '/jqueryui')) {
+                mkdir($vendorComponents . '/jqueryui', 0755, true);
+            }
+
+            copyDir($jqueryDist, $vendorComponents . '/jquery');
+            copy($jqueryUiDist . '/jquery-ui.js', $vendorComponents . '/jqueryui/jquery-ui.js');
+            copy($jqueryUiDist . '/jquery-ui.min.js', $vendorComponents . '/jqueryui/jquery-ui.min.js');
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
 }

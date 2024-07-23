9b5740514b6bd94934ab89fa368adefe29729af3
Kaitlin Newson <kaitlin.newson@gmail.com>
2024-07-23 17:10:26 -0300

    pkp/pkp-lib#9812 enable plugin gallery in admin site settings

--- controllers/grid/plugins/PluginGalleryGridHandler.inc.php
+++ controllers/grid/plugins/PluginGalleryGridHandler.inc.php
@@ -256,15 +256,12 @@ class PluginGalleryGridHandler extends GridHandler {
 	 * @param $isUpgrade boolean
 	 */
 	function installPlugin($args, $request, $isUpgrade = false) {
-		if ($request->getContext() == '') {
-			$handler = 'admin';
-			$path = null;
+		if ($request->getContext()) {
+			$redirectUrl = $request->getDispatcher()->url($request, ROUTE_PAGE, null, 'management', 'settings', ['website'], array('r' => uniqid()), 'plugins');
 		} else {
-			$handler = 'management';
-			$path = array('website');
+			$redirectUrl = $request->getDispatcher()->url($request, ROUTE_PAGE, null, 'admin', 'settings', null, array('r' => uniqid()), 'plugins');
 		}
 
-		$redirectUrl = $request->getDispatcher()->url($request, ROUTE_PAGE, null, $handler, 'settings', $path, array('r' => uniqid()), 'plugins');
 		if (!$request->checkCSRF()) return $request->redirectUrlJson($redirectUrl);
 		$notificationMgr = new NotificationManager();
 		$user = $request->getUser();

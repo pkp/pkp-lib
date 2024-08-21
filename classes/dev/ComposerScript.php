6270fa6c74326106f64804af3f1afda02b499c46
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-08-22 02:55:57 +0800

    pkp/pkp-lib#9753 Update ComposerScript to use native functions to copy files

--- classes/dev/ComposerScript.php
+++ classes/dev/ComposerScript.php
@@ -15,11 +15,48 @@ namespace PKP\dev;
 
 use Exception;
 
-require_once __DIR__ . '/../../../../tools/bootstrap.inc.php';
-import('lib.pkp.classes.file.FileManager');
-
 class ComposerScript
 {
+	/**
+	 * Recursively copies the contents of a directory from source to destination.
+	 *
+	 * @param string $src The source directory.
+	 * @param string $dst The destination directory.
+	 * @throws Exception If a directory cannot be opened or a file cannot be copied.
+	 */
+	private static function copyDir(string $src, string $dst): void
+	{
+		if (!is_dir($src)) {
+			throw new Exception("Source directory does not exist: $src");
+		}
+
+		$dir = @opendir($src);
+		if (!$dir) {
+			throw new Exception("Failed to open directory: $src");
+		}
+
+		if (!@mkdir($dst, 0755, true) && !is_dir($dst)) {
+			throw new Exception("Failed to create destination directory: $dst");
+		}
+
+		while (false !== ($file = readdir($dir))) {
+			if ($file != '.' && $file != '..') {
+				$srcFile = $src . '/' . $file;
+				$dstFile = $dst . '/' . $file;
+
+				if (is_dir($srcFile)) {
+					self::copyDir($srcFile, $dstFile);
+				} else {
+					if (!@copy($srcFile, $dstFile)) {
+						throw new Exception("Failed to copy file: $srcFile to $dstFile");
+					}
+				}
+			}
+		}
+
+		closedir($dir);
+	}
+
 	/**
 	 * A post-install-cmd custom composer script that
 	 * copies composer installs from repositories
@@ -28,7 +65,6 @@ class ComposerScript
 	 */
 	public static function copyVendorAssets(): void
 	{
-		$fileManager = new \FileManager();
 		$vendorBaseDir = __DIR__ . '/../../lib/vendor';
 		$jsPluginsDir = __DIR__ . '/../../js/lib';
 
@@ -44,17 +80,33 @@ class ComposerScript
 			'jquery-validate' => $jsPluginsDir . '/jquery/plugins/validate'
 		];
 
-		// jQuery UI
-		if (!$fileManager->copyFile($source['jquery-ui.js'], $dest['jquery-ui.js'])) {
-			throw new Exception('Failed to copy jquery-ui.js to destination folder');
-		}
-		if (!$fileManager->copyFile($source['jquery-ui.min.js'], $dest['jquery-ui.min.js'])) {
-			throw new Exception('Failed to copy jquery-ui.min.js to destination folder');
-		}
+		try {
+			// jQuery UI
+			if (!file_exists($vendorBaseDir . '/components/jqueryui')) {
+				if (!mkdir($vendorBaseDir . '/components/jqueryui', 0755, true)) {
+					throw new Exception("Failed to create directory: {$vendorBaseDir}/jqueryui");
+				}
+			}
 
-		// jQuery Validation
-		if (!$fileManager->copyDir($source['jquery-validate'], $dest['jquery-validate'])) {
-			throw new Exception('Failed to copy jquery-validate to destination folder');
+			if (!copy($source['jquery-ui.js'], $dest['jquery-ui.js'])) {
+				throw new Exception('Failed to copy jquery-ui.js to destination folder');
+			}
+
+			if (!copy($source['jquery-ui.min.js'], $dest['jquery-ui.min.js'])) {
+				throw new Exception('Failed to copy jquery-ui.min.js to destination folder');
+			}
+			
+
+			// jQuery Validation
+			if (!file_exists($dest['jquery-validate'])) {
+				if (!mkdir($dest['jquery-validate'], 0755, true)) {
+					throw new Exception("Failed to create directory: {$dest['jquery-validate']}");
+				}
+			}
+
+			self::copyDir($source['jquery-validate'], $dest['jquery-validate']);
+		} catch (Exception $e) {
+			throw $e;
 		}
 	}
 }

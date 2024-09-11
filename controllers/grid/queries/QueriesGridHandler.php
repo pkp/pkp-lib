14e1f63e18b07f0269c3bf1bc08293a8f014298e
simon <31130607+forgive38@users.noreply.github.com>
2024-09-11 16:58:22 +0200

    resolve new discussion error refs https://github.com/pkp/pkp-lib/issues/10385 (#10386)
    Co-authored-by: simon <chevancs@ujf-grenoble.fr>

--- controllers/grid/queries/QueriesGridHandler.php
+++ controllers/grid/queries/QueriesGridHandler.php
@@ -672,7 +672,7 @@ class QueriesGridHandler extends GridHandler
                     $user->getId(),
                     $request->getContext()->getId()
                 );
-                if (in_array(Notification::NOTIFICATION_TYPE_NEW_QUERY, $notificationSubscriptionSettings)) {
+                if (!$notification || in_array(Notification::NOTIFICATION_TYPE_NEW_QUERY, $notificationSubscriptionSettings)) {
                     continue;
                 }
 

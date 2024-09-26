63b00c7cc87f3788efba7e5386b9c6929e1a0807
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-09-27 06:25:30 +0800

    pkp/pkp-lib#9753 [stable-3_4_0] Update jquery, jquery-ui and chart.js to address security vulnerability reports (#10166)
    * pkp/pkp-lib#9753 Update jquery version from 3.6.0 to 3.7.1
    
    * pkp/pkp-lib#9753 Update chartjs version from 2.0.1 to 2.9.4
    
    * pkp/pkp-lib#9753 Update jquery-validate version from 1.11.1 to 1.19.5
    
    * pkp/pkp-lib#9753 Remove components/jqueryui as a composer dependency
    
    * pkp/pkp-lib#9753 Manually add jquery-ui v1.13.3 to jquery plugins
    
    * pkp/pkp-lib#9753 Move import of jqueryui from vendors to js plugins
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Remove jquery-ui local files
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Add jquery-ui dependency to composer from npm
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Remove jquery-validation local files
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Add query-validation to composer from npm
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Adjust tooltips configuration for chart.js
    
    * pkp/pkp-lib#9753 Remove jquery and jquery-ui as composer dependencies
    
    * pkp/pkp-lib#9753 Undo updates to jquery, jquery-ui and jquery-validation paths
    
    * pkp/pkp-lib#9753 Add js/lib/jquery/plugins/validate/ path to gitignore
    
    * pkp/pkp-lib#9753 Remove local copies of Chart.js
    
    * pkp/pkp-lib#9753 Add js/lib/Chart.js to gitignore
    
    * pkp/pkp-lib#9753 [stable-3_4_0] Add script to copy jquery and jqueryui when running composer install
    
    * pkp/pkp-lib#9753 Add composer custom repositories for jquery-ui, jquery-validate and chartjs from npm registry
    
    * pkp/pkp-lib#9753 Update composer script when copying jqueryui, validation and chartjs
    
    * pkp/pkp-lib#9753 Update ComposerScript to use FileManager to copy files and dir
    
    * pkp/pkp-lib#9753 Update spacing used for ComposerScript file
    
    * pkp/pkp-lib#9753 Resolve unexpected throw statement on ComposerScript file
    
    * pkp/pkp-lib#9753 Update path when requiring tools/bootstrap.php
    
    * pkp/pkp-lib#9754 Update ComposerScript to use native functions to copy files
    
    * pkp/pkp-lib#9753 Update message log if creating dir fails in ComposerScript

--- classes/dev/ComposerScript.php
+++ classes/dev/ComposerScript.php
@@ -32,4 +32,111 @@ class ComposerScript
             throw new Exception("The ISO639-2b file {$iso6392bFile} does not exist.");
         }
     }
+
+    /**
+     * Recursively copies the contents of a directory from source to destination.
+     *
+     * @param string $src The source directory.
+     * @param string $dst The destination directory.
+     * @throws Exception If a directory cannot be opened or a file cannot be copied.
+     */
+    private static function copyDir(string $src, string $dst): void
+    {
+        if (!is_dir($src)) {
+            throw new Exception("Source directory does not exist: $src");
+        }
+
+        $dir = @opendir($src);
+        if (!$dir) {
+            throw new Exception("Failed to open directory: $src");
+        }
+
+        if (!@mkdir($dst, 0755, true) && !is_dir($dst)) {
+            throw new Exception("Failed to create destination directory: $dst");
+        }
+
+        while (false !== ($file = readdir($dir))) {
+            if ($file != '.' && $file != '..') {
+                $srcFile = $src . '/' . $file;
+                $dstFile = $dst . '/' . $file;
+
+                if (is_dir($srcFile)) {
+                    self::copyDir($srcFile, $dstFile);
+                } else {
+                    if (!@copy($srcFile, $dstFile)) {
+                        throw new Exception("Failed to copy file: $srcFile to $dstFile");
+                    }
+                }
+            }
+        }
+
+        closedir($dir);
+    }
+
+    /**
+     * A post-install-cmd custom composer script that
+     * copies composer installs from repositories
+     * to the correct/existing directories of the following dependencies:
+     * jquery-ui, jquery validation and chartjs
+     */
+    public static function copyVendorAssets(): void
+    {
+        $vendorBaseDir = __DIR__ . '/../../lib/vendor';
+        $jsPluginsDir = __DIR__ . '/../../js/lib';
+
+        $source = [
+            'jquery-ui.js' => $vendorBaseDir . '/jquery/ui/dist/jquery-ui.js',
+            'jquery-ui.min.js' => $vendorBaseDir . '/jquery/ui/dist/jquery-ui.min.js',
+            'jquery-validate' => $vendorBaseDir . '/jquery/validation/dist',
+            'Chart.js' => $vendorBaseDir . '/chart/js/dist/Chart.js',
+            'Chart.min.js' => $vendorBaseDir . '/chart/js/dist/Chart.min.js'
+        ];
+
+        $dest = [
+            'jquery-ui.js' => $vendorBaseDir . '/components/jqueryui/jquery-ui.js',
+            'jquery-ui.min.js' => $vendorBaseDir . '/components/jqueryui/jquery-ui.min.js',
+            'jquery-validate' => $jsPluginsDir . '/jquery/plugins/validate',
+            'Chart.js' => $jsPluginsDir . '/Chart.js',
+            'Chart.min.js' => $jsPluginsDir . '/Chart.min.js'
+        ];
+
+        try {
+            // jQuery UI
+            if (!file_exists($vendorBaseDir . '/components/jqueryui')) {
+                if (!mkdir($vendorBaseDir . '/components/jqueryui', 0755, true)) {
+                    throw new Exception("Failed to create directory: {$vendorBaseDir}/components/jqueryui");
+                }
+            }
+
+            if (!copy($source['jquery-ui.js'], $dest['jquery-ui.js'])) {
+                throw new Exception('Failed to copy jquery-ui.js to destination folder');
+            }
+
+            if (!copy($source['jquery-ui.min.js'], $dest['jquery-ui.min.js'])) {
+                throw new Exception('Failed to copy jquery-ui.min.js to destination folder');
+            }
+            
+
+            // jQuery Validation
+            if (!file_exists($dest['jquery-validate'])) {
+                if (!mkdir($dest['jquery-validate'], 0755, true)) {
+                    throw new Exception("Failed to create directory: {$dest['jquery-validate']}");
+                }
+            }
+
+            self::copyDir($source['jquery-validate'], $dest['jquery-validate']);
+
+
+            // Chart.js
+            if (!copy($source['Chart.js'], $dest['Chart.js'])) {
+                throw new Exception('Failed to copy Chart.js to destination folder');
+            }
+
+            if (!copy($source['Chart.min.js'], $dest['Chart.min.js'])) {
+                throw new Exception('Failed to copy Chart.min.js to destination folder');
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
 }

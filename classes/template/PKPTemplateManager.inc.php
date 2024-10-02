f6d9d78459aadc66afe1f06d46721c0225b4cc01
Nate Wright <natew@notthisway.com>
2024-10-02 10:18:35 +0100

    pkp/pkp-lib#10492 Add support for module script tags to TemplateManager

--- classes/template/PKPTemplateManager.inc.php
+++ classes/template/PKPTemplateManager.inc.php
@@ -512,6 +512,7 @@ class PKPTemplateManager extends Smarty {
 				'priority' => STYLE_SEQUENCE_NORMAL,
 				'contexts' => ['frontend'],
 				'inline'   => false,
+				'type' => 'text/javascript',
 			],
 			$args
 		);
@@ -521,6 +522,7 @@ class PKPTemplateManager extends Smarty {
 			$this->_javaScripts[$context][$args['priority']][$name] = [
 				'script' => $script,
 				'inline' => $args['inline'],
+				'type' => $args['type'],
 			];
 		}
 	}
@@ -2027,12 +2029,12 @@ class PKPTemplateManager extends Smarty {
 		foreach($scripts as $priorityList) {
 			foreach($priorityList as $name => $data) {
 				if ($data['inline']) {
-					$output .= '<script type="text/javascript">' . $data['script'] . '</script>';
+					$output .= '<script type="' . $data['type'] . '">' . $data['script'] . '</script>';
 				} else {
 					if ($appVersion && strpos($data['script'], '?') === false) {
 						$data['script'] .= '?v=' . $appVersion;
 					}
-					$output .= '<script src="' . $data['script'] . '" type="text/javascript"></script>';
+					$output .= '<script src="' . $data['script'] . '" type="' . $data['type'] . '"></script>';
 				}
 			}
 		}

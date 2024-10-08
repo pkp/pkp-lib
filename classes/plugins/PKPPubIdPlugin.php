05f3a9535d460012030251fdc5702b064f382b4c
Blesilda Ramirez <ramirezblesilda@gmail.com>
2024-10-09 01:53:56 +0800

    pkp/pkp-lib#10444 Add modalStyle when using AjaxModal modal

--- classes/plugins/PKPPubIdPlugin.php
+++ classes/plugins/PKPPubIdPlugin.php
@@ -77,7 +77,8 @@ abstract class PKPPubIdPlugin extends LazyLoadPlugin
                     'settings',
                     new AjaxModal(
                         $router->url($request, null, null, 'manage', null, $actionArgs),
-                        $this->getDisplayName()
+                        $this->getDisplayName(),
+                        'side-modal'
                     ),
                     __('manager.plugins.settings'),
                     null

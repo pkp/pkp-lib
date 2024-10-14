5d155f998595bb0bb52682a2b68ba39323640069
Touhidur Rahman <abir.ruet@gmail.com>
2024-10-14 18:06:11 +0600

    pkp/pkp-lib#4787 Reviewer suggestions api update

--- classes/submission/reviewer/suggestion/ReviewerSuggestion.php
+++ classes/submission/reviewer/suggestion/ReviewerSuggestion.php
@@ -14,42 +14,88 @@
 
 namespace PKP\submission\reviewer\suggestion;
 
-use Illuminate\Database\Eloquent\Model;
 use APP\facades\Repo;
 use Carbon\Carbon;
-use Eloquence\Behaviours\HasCamelCasing;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-
+use PKP\core\traits\ModelWithSettings;
 
 class ReviewerSuggestion extends Model
 {
-    use HasCamelCasing;
+    use ModelWithSettings;
 
     protected $table = 'reviewer_suggestions';
     protected $primaryKey = 'reviewer_suggestion_id';
 
-    protected $fillable = [
-        
-    ];
+    protected $guarded = [];
 
     protected function casts(): array
     {
         return [
-            'userId'        => 'int',
-            'submissionId'  => 'int',
-            'email'         => 'string',
-            'orcidId'       => 'string',
-            'approvedAt'    => 'datetime',
-            'stageId'       => 'int',
-            'approverId'    => 'int',
-            'reviewerId'    => 'int',
+            'suggestingUserId'  => 'int',
+            'submissionId'      => 'int',
+            'email'             => 'string',
+            'orcidId'           => 'string',
+            'approvedAt'        => 'datetime',
+            'stageId'           => 'int',
+            'approverId'        => 'int',
+            'reviewerId'        => 'int',
+        ];
+    }
+
+    public function getSettingsTable(): string
+    {
+        return 'reviewer_suggestion_settings';
+    }
+
+    public static function getSchemaName(): ?string
+    {
+        return null;
+    }
+
+    // TODO Add instution details as setting 
+    public function getSettings(): array
+    {
+        return [
+            'familyName',
+            'givenName',
+            'affiliation',
+            'suggestionReason',
+        ];
+    }
+
+    // TODO should the instution details be a multigingual prop
+    public function getMultilingualProps(): array
+    {
+        return [
+            'fullName',
+            'familyName',
+            'givenName',
+            'affiliation',
+            'suggestionReason',
         ];
     }
 
     public function hasApproved(): ?Carbon
     {
         return $this->approvedAt;
+    }
+
+    /**
+     * Get the full name
+     */
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: function (mixed $value, array $attributes) {
+                $familyName = $this->familyName;
+                return collect($this->givenName)
+                    ->map(fn ($givenName, $locale) => $givenName. ' ' . $familyName[$locale])
+                    ->toArray();
+            }
+        );
     }
 
     /**
@@ -60,74 +106,61 @@ class ReviewerSuggestion extends Model
         return Attribute::make(
             get: fn($value, $attributes) => $attributes[$this->primaryKey] ?? null,
             set: fn($value) => [$this->primaryKey => $value],
-        );
+        )->shouldCache();
     }
 
-    /**
-     * Accessor for submission.
-     * Should replace with relationship once Submission is converted to an Eloquent Model.
-     */
     protected function submission(): Attribute
     {
         return Attribute::make(
             get: fn () => Repo::submission()->get($this->submissionId, true),
-        );
+        )->shouldCache();
     }
 
-    /**
-     * Accessor for user.
-     * Should replace with relationship once User is converted to an Eloquent Model.
-     */
-    protected function user(): Attribute
+    protected function suggestingUser(): Attribute
     {
         return Attribute::make(
-            get: fn () => Repo::user()->get($this->userId, true),
-        );
+            get: fn () => $this->suggestingUserId
+                ? Repo::user()->get($this->suggestingUserId, true)
+                : null
+        )->shouldCache();
     }
 
-    /**
-     * Accessor for user.
-     * Should replace with relationship once User is converted to an Eloquent Model.
-     */
-    protected function approver(): ?Attribute
+    protected function approver(): Attribute
     {
         return Attribute::make(
             get: fn () => $this->approverId ? Repo::user()->get($this->approverId, true) : null,
-        );
+        )->shouldCache();
     }
 
-    /**
-     * Accessor for user.
-     * Should replace with relationship once User is converted to an Eloquent Model.
-     */
-    protected function reviewer(): ?Attribute
+    protected function reviewer(): Attribute
     {
         return Attribute::make(
             get: fn () => $this->reviewerId ? Repo::user()->get($this->reviewerId, true) : null,
-        );
+        )->shouldCache();
     }
 
     public function scopeWithContextId(Builder $query, int $contextId): Builder
     {
-        return $query->whereIn("submission_id", fn (Builder $query) => $query
-            ->select("submission_id")
-            ->from("submissions")
-            ->where("context_id", $contextId)
-        );
+        return $query
+            ->whereIn('submission_id', fn (Builder $query) => $query
+                ->select('submission_id')
+                ->from('submissions')
+                ->where('context_id', $contextId)
+            );
     }
 
-    public function scopeWithSubmissionId(Builder $query, int $submissionId): Builder
+    public function scopeWithSubmissionIds(Builder $query, int|array $submissionId): Builder
     {
-        return $query->where("submission_id", $submissionId);
+        return $query->whereIn('submission_id', Arr::wrap($submissionId));
     }
 
-    public function scopeWithUserId(Builder $query, int $userId): Builder
+    public function scopeWithSuggestingUserIds(Builder $query, int|array $userId): Builder
     {
-        return $query->where("user_id", $userId);
+        return $query->whereIn('suggesting_user_id', Arr::wrap($userId));
     }
 
-    public function scopeWithStageId(Builder $query, int $stageId): Builder
+    public function scopeWithStageIds(Builder $query, int|array $stageId): Builder
     {
-        return $query->where("stage_id", $stageId);
+        return $query->whereIn('stage_id', Arr::wrap($stageId));
     }
 }

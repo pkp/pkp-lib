b50008026cbde5afecf01ddac4e5df419da29a98
Touhidur Rahman <abir.ruet@gmail.com>
2024-11-25 13:49:37 +0600

    pkp/pkp-lib#10214 added status REVIEW_ROUND_STATUS_PENDING_REVIEWS and REVIEW_ROUND_STATUS_REVIEWS_READY to consider reminder
    pkp/pkp-lib#10214 added missing checkings to handle mail sending

--- jobs/email/EditorialReminder.php
+++ jobs/email/EditorialReminder.php
@@ -33,6 +33,7 @@ use PKP\notification\NotificationSubscriptionSettingsDAO;
 use PKP\notification\PKPNotification;
 use PKP\submission\reviewRound\ReviewRound;
 use PKP\submission\reviewRound\ReviewRoundDAO;
+use PKP\security\Role;
 use PKP\user\User;
 use PKP\workflow\WorkflowStageDAO;
 
@@ -54,13 +55,23 @@ class EditorialReminder extends BaseJob
      */
     public function handle(): void
     {
-        if (!$this->isSubscribed()) {
-            return;
-        }
-
         /** @var Context $context */
         $context = Services::get('context')->get($this->contextId);
         $editor = Repo::user()->get($this->editorId);
+
+        // Context or user was removed since job was created, or the user was disabled
+        if (!$context || !$editor) {
+            return;
+        }
+
+        // If the user has been removed form manager or editor role since the job was created
+        if (!$editor->hasRole([Role::ROLE_ID_MANAGER, Role::ROLE_ID_SUB_EDITOR], $context->getId())) {
+            return;
+        }
+
+        if (!$this->isSubscribed()) {
+            return;
+        }
 
         // Don't use the request locale because this job is
         // run during a scheduled task
@@ -89,26 +100,36 @@ class EditorialReminder extends BaseJob
             }
 
             if (in_array($submission->getData('stageId'), [WORKFLOW_STAGE_ID_INTERNAL_REVIEW, WORKFLOW_STAGE_ID_EXTERNAL_REVIEW])) {
-                /** @var ReviewRoundDAO $reviewRoundDao */
-                $reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
+                $reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO'); /** @var ReviewRoundDAO $reviewRoundDao */
                 $reviewRound = $reviewRoundDao->getLastReviewRoundBySubmissionId($submission->getId(), $submission->getData('stageId'));
+                $status = $reviewRound->determineStatus();
 
-                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWERS) {
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWERS) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.pendingReviewers');
                     continue;
                 }
 
-                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_COMPLETED) {
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWS) {
+                    $outstanding[$submissionId] = __('editor.submission.roundStatus.pendingReviews');
+                    continue;
+                }
+
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_READY) {
+                    $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewsReady');
+                    continue;
+                }
+
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_COMPLETED) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewsCompleted');
                     continue;
                 }
 
-                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_OVERDUE) {
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_OVERDUE) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewOverdue');
                     continue;
                 }
 
-                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED) {
+                if ($status === ReviewRound::REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.revisionsSubmitted');
                     continue;
                 }
@@ -139,11 +160,6 @@ class EditorialReminder extends BaseJob
             return;
         }
 
-        // Context or user was removed since job was created, or the user was disabled
-        if (!$context || !$editor) {
-            return;
-        }
-
         $notificationManager = new NotificationManager();
         $notification = $notificationManager->createNotification(
             Application::get()->getRequest(),
@@ -160,11 +176,8 @@ class EditorialReminder extends BaseJob
             ->from($context->getContactEmail(), $context->getLocalizedName(Locale::getLocale()))
             ->recipients([$editor])
             ->subject($emailTemplate->getLocalizedData('subject'))
-            ->body($emailTemplate->getLocalizedData('body'));
-        
-        if ($notification) {
-            $mailable->allowUnsubscribe($notification);
-        }
+            ->body($emailTemplate->getLocalizedData('body'))
+            ->allowUnsubscribe($notification);
 
         Mail::send($mailable);
 
@@ -173,13 +186,18 @@ class EditorialReminder extends BaseJob
     }
 
     /**
-     * Is this editor subscribed to this email?
+     * Is this editor subscribed to this email notification type?
      */
     protected function isSubscribed(): bool
     {
         /** @var NotificationSubscriptionSettingsDAO $notificationSubscriptionSettingsDao  */
         $notificationSubscriptionSettingsDao = DAORegistry::getDAO('NotificationSubscriptionSettingsDAO');
-        $blockedEmails = $notificationSubscriptionSettingsDao->getNotificationSubscriptionSettings('blocked_emailed_notification', $this->editorId, $this->contextId);
+        $blockedEmails = $notificationSubscriptionSettingsDao->getNotificationSubscriptionSettings(
+            NotificationSubscriptionSettingsDAO::BLOCKED_EMAIL_NOTIFICATION_KEY,
+            $this->editorId,
+            $this->contextId
+        );
+
         return !in_array(Notification::NOTIFICATION_TYPE_EDITORIAL_REMINDER, $blockedEmails);
     }
 

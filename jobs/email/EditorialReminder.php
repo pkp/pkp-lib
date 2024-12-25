77b924814133856ce48507d814fe756df57490d9
Touhidur Rahman <abir.ruet@gmail.com>
2024-12-25 15:14:36 +0600

    pkp/pkp-lib#10214 Editorial reminder issue update

--- jobs/email/EditorialReminder.php
+++ jobs/email/EditorialReminder.php
@@ -16,10 +16,10 @@
 
 namespace PKP\jobs\email;
 
-use APP\core\Application;
 use APP\facades\Repo;
 use APP\notification\NotificationManager;
 use APP\submission\Submission;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 use PKP\context\Context;
 use PKP\db\DAORegistry;
@@ -90,30 +90,41 @@ class EditorialReminder extends BaseJob
                 $reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
                 $reviewRound = $reviewRoundDao->getLastReviewRoundBySubmissionId($submission->getId(), $submission->getData('stageId'));
 
-                if ($reviewRound->getStatus() === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWERS) {
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWERS) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.pendingReviewers');
                     continue;
                 }
 
-                if ($reviewRound->getStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_COMPLETED) {
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_PENDING_REVIEWS) {
+                    $outstanding[$submissionId] = __('editor.submission.roundStatus.pendingReviews');
+                    continue;
+                }
+
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_READY) {
+                    $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewsReady');
+                    continue;
+                }
+
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_COMPLETED) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewsCompleted');
                     continue;
                 }
 
-                if ($reviewRound->getStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_OVERDUE) {
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVIEWS_OVERDUE) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.reviewOverdue');
                     continue;
                 }
 
-                if ($reviewRound->getStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED) {
+                if ($reviewRound->determineStatus() === ReviewRound::REVIEW_ROUND_STATUS_REVISIONS_SUBMITTED) {
                     $outstanding[$submissionId] = __('editor.submission.roundStatus.revisionsSubmitted');
                     continue;
                 }
             }
 
             if (in_array($submission->getData('stageId'), [WORKFLOW_STAGE_ID_EDITING, WORKFLOW_STAGE_ID_PRODUCTION])) {
-                $lastActivityTimestamp = strtotime($submission->getData('dateLastActivity'));
-                if ($lastActivityTimestamp < strtotime('-30 days')) {
+                $lastActivityTimestamp = Carbon::parse($submission->getData('dateLastActivity'))->endOfDay();
+                $comparingTimestamp = Carbon::today()->endOfDay()->subDays(30);
+                if ($comparingTimestamp->gt($lastActivityTimestamp)) {
                     /** @var WorkflowStageDAO $workflowStageDao */
                     $workflowStageDao = DAORegistry::getDAO('WorkflowStageDAO');
                     $outstanding[$submissionId] = __(
@@ -155,8 +166,11 @@ class EditorialReminder extends BaseJob
             ->from($context->getContactEmail(), $context->getLocalizedName(Locale::getLocale()))
             ->recipients([$editor])
             ->subject($emailTemplate->getLocalizedData('subject'))
-            ->body($emailTemplate->getLocalizedData('body'))
-            ->allowUnsubscribe($notification);
+            ->body($emailTemplate->getLocalizedData('body'));
+        
+        if ($notification) {
+            $mailable->allowUnsubscribe($notification);
+        }
 
         Mail::send($mailable);
 

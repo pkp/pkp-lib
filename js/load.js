10768506d5fe454843e789de9e5b64d97aa29e80
Blesilda Ramirez <ramirezblesilda@gmail.com>
2025-02-16 00:36:19 -0500

    pkp/pkp-lib#9717 Register v-strip-unsafe-html directive

--- js/load.js
+++ js/load.js
@@ -33,6 +33,9 @@ import * as useFetch from '@/composables/useFetch.js';
 import * as useFetchPaginated from '@/composables/useFetchPaginated.js';
 import * as useUrl from '@/composables/useUrl.js';
 
+// Directives
+import {stripUnsafeHtml} from '@/directives/stripUnsafeHtml';
+
 // Global components of UI Library
 import Badge from '@/components/Badge/Badge.vue';
 import Dropdown from '@/components/Dropdown/Dropdown.vue';
@@ -123,6 +126,7 @@ import UserAccessManager from '@/managers/UserAccessManager/UserAccessManager.vu
 // Helper for initializing and tracking Vue controllers
 import VueRegistry from './classes/VueRegistry.js';
 
+VueRegistry.registerDirective('strip-unsafe-html', stripUnsafeHtml);
 
 // Register global components
 VueRegistry.registerComponent('Badge', Badge);
@@ -280,6 +284,12 @@ function pkpCreateVueApp(createAppArgs) {
 	const allGlobalComponents = VueRegistry.getAllComponents();
 	Object.keys(allGlobalComponents).forEach((componentName) => {
 		vueApp.component(componentName, allGlobalComponents[componentName]);
+	});
+
+	// register all global directives
+	const allGlobalDirectives = VueRegistry.getAllDirectives();
+	Object.keys(allGlobalDirectives).forEach((directiveName) => {
+		vueApp.directive(directiveName, allGlobalDirectives[directiveName]);
 	});
 
 	return vueApp;

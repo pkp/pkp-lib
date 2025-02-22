cb23c4b92df8a5ea724740abe387808ee3e49bc6
Touhidur Rahman <abir.ruet@gmail.com>
2025-02-22 17:13:24 +0600

    pkp/pkp-lib#4787 moved migrations for 3.5.0

--- classes/migration/upgrade/v3_5_0/I4787_AddReviewSuggestionHelp.php
+++ classes/migration/upgrade/v3_5_0/I4787_AddReviewSuggestionHelp.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @file classes/migration/upgrade/v3_6_0/I4787_AddReviewSuggestionHelp.php
+ * @file classes/migration/upgrade/v3_5_0/I4787_AddReviewSuggestionHelp.php
  *
- * Copyright (c) 2014-2024 Simon Fraser University
- * Copyright (c) 2000-2024 John Willinsky
+ * Copyright (c) 2025 Simon Fraser University
+ * Copyright (c) 2025 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class I4787_AddReviewSuggestionHelp
@@ -12,7 +12,7 @@
  * @brief Add reviewer suggestion related default help text
  */
 
-namespace PKP\migration\upgrade\v3_6_0;
+namespace PKP\migration\upgrade\v3_5_0;
 
 use Illuminate\Support\Collection;
 use PKP\migration\upgrade\v3_4_0\I7191_InstallSubmissionHelpDefaults;

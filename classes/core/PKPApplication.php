17ccf0ef905c6f3252202cd2e3a4061577f21e75
Touhidur Rahman <abir.ruet@gmail.com>
2025-02-24 00:15:25 +0600

    pkp/pkp-lib#1660 moving app specific codes

--- classes/core/PKPApplication.php
+++ classes/core/PKPApplication.php
@@ -311,7 +311,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
     /**
      * Get the current application object
      */
-    public static function get(): self
+    public static function get(): static
     {
         return Registry::get('application');
     }

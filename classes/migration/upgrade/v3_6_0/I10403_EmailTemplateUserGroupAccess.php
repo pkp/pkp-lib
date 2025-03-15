e52d3229c244962f6d9d9d99f641f04d57eae573
Taslan Graham <gtaslan1@gmail.com>
2025-03-15 18:19:12 -0400

    pkp/pkp-lib#10571 Fix migration folder name

--- classes/migration/upgrade/v3_6_0/I10403_EmailTemplateUserGroupAccess.php
+++ classes/migration/upgrade/v3_6_0/I10403_EmailTemplateUserGroupAccess.php
@@ -3,8 +3,8 @@
 /**
  * @file classes/migration/upgrade/v3_6_0/I10403_EmailTemplateUserGroupAccess.php
  *
- * Copyright (c) 2014-2024 Simon Fraser University
- * Copyright (c) 2000-2024 John Willinsky
+ * Copyright (c) 2014-2025 Simon Fraser University
+ * Copyright (c) 2000-2025 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class I10403_EmailTemplateUserGroupAccess
@@ -12,7 +12,7 @@
  * @brief Adds the email_template_user_group_access table to allow email template access restriction
  */
 
-namespace PKP\migration\upgrade\V3_6_0;
+namespace PKP\migration\upgrade\v3_6_0;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
--- classes/migration/upgrade/v3_6_0/PreflightCheckMigration.php
+++ classes/migration/upgrade/v3_6_0/PreflightCheckMigration.php
@@ -3,8 +3,8 @@
 /**
  * @file classes/migration/upgrade/v3_6_0/PreflightCheckMigration.php
  *
- * Copyright (c) 2014-2024 Simon Fraser University
- * Copyright (c) 2000-2024 John Willinsky
+ * Copyright (c) 2014-2025 Simon Fraser University
+ * Copyright (c) 2000-2025 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class PreflightCheckMigration
@@ -12,7 +12,7 @@
  * @brief Check for common problems early in the upgrade process.
  */
 
-namespace PKP\migration\upgrade\V3_6_0;
+namespace PKP\migration\upgrade\v3_6_0;
 
 use PKP\db\DAORegistry;
 use Throwable;

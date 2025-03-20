b783ec53372aed9554d54baf3b8622e8a6d0a62a
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-03-20 09:27:20 +0100

    pkp/pkp-lib#10765 Refinment after QA feedback, updating some labels, order, width

--- classes/submission/Repository.php
+++ classes/submission/Repository.php
@@ -1092,7 +1092,7 @@ abstract class Repository
                         ->filterByStatus([PKPSubmission::STATUS_QUEUED]);
                     return new DashboardView(
                         $key,
-                        __('submission.dashboard.view.revisionsSubmitted'),
+                        in_array(Role::ROLE_ID_AUTHOR, $selectedRoleIds) ? __('submission.list.revisionsSubmitted')  : __('submission.dashboard.view.revisionsSubmitted'),
                         [Role::ROLE_ID_SITE_ADMIN, Role::ROLE_ID_MANAGER, Role::ROLE_ID_SUB_EDITOR, Role::ROLE_ID_ASSISTANT, Role::ROLE_ID_AUTHOR],
                         $canAccessUnassignedSubmission
                             ? $collector

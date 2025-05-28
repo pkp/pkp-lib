b3a6e34ad4ffb4fce68018252017ac349a6ce2a1
Jakob Ring <49451632+JakobRing@users.noreply.github.com>
2025-05-28 17:09:03 +0200

    add just the saveForLater-Hook (#11405)

--- classes/submission/reviewer/form/PKPReviewerReviewStep3Form.php
+++ classes/submission/reviewer/form/PKPReviewerReviewStep3Form.php
@@ -22,6 +22,7 @@ use APP\notification\NotificationManager;
 use APP\submission\Submission;
 use APP\template\TemplateManager;
 use Illuminate\Support\Facades\Mail;
+use PKP\plugins\Hook;
 use PKP\controllers\confirmationModal\linkAction\ViewReviewGuidelinesLinkAction;
 use PKP\core\Core;
 use PKP\core\PKPApplication;
@@ -291,6 +292,7 @@ class PKPReviewerReviewStep3Form extends ReviewerReviewForm
                 : (int)$this->getData('reviewerRecommendationId'),
         ]);
 
+        Hook::call(strtolower(get_class($this)) . '::saveForLater', [$this]);
         return true;
     }
 

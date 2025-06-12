d1d2b4e2f354535a53d29191d56c52c3af9390a7
Alec Smecher <alec@smecher.bc.ca>
2025-06-12 14:02:37 -0700

    pkp/pkp-lib#11387 Fix error creating version in OMP

--- classes/jats/Repository.php
+++ classes/jats/Repository.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file classes/jats/Repository.php
  *
@@ -22,7 +23,6 @@ use PKP\file\FileManager;
 use PKP\jats\exceptions\UnableToCreateJATSContentException;
 use PKP\submissionFile\SubmissionFile;
 use Throwable;
-
 
 class Repository
 {
@@ -60,7 +60,7 @@ class Repository
             ->getCollector()
             ->filterByFileStages([SubmissionFile::SUBMISSION_FILE_JATS])
             ->filterByAssoc(Application::ASSOC_TYPE_PUBLICATION, [$publicationId]);
-        
+
         if ($submissionId) {
             $submissionFileQuery = $submissionFileQuery->filterBySubmissionIds([$submissionId]);
         }
@@ -70,15 +70,15 @@ class Repository
             ->first();
 
         return new JatsFile(
-            $publicationId, 
-            $submissionId, 
-            $submissionFile, 
+            $publicationId,
+            $submissionId,
+            $submissionFile,
             $genres
         );
     }
 
     /**
-     * Returns the name of the file that will contain the default JATS content 
+     * Returns the name of the file that will contain the default JATS content
      */
     public function getDefaultJatsFileName(int $publicationId): string
     {
@@ -96,7 +96,7 @@ class Repository
         $submission = Repo::submission()->get($publication->getData('submissionId'));
 
         $context = app()->get('context')->get($submission->getData('contextId'));
-        $section = Repo::section()->get($submission->getSectionId());
+        $section = $submission->getSectionId() ? Repo::section()->get($submission->getSectionId()) : null;
 
         $issue = null;
         if ($publication->getData('issueId')) {
@@ -118,12 +118,11 @@ class Repository
     public function addJatsFile(
         string $fileTmpName,
         string $fileName,
-        int $publicationId, 
-        ?int $submissionId = null, 
+        int $publicationId,
+        ?int $submissionId = null,
         int $type = SubmissionFile::SUBMISSION_FILE_JATS,
         array $params = []
-    ): JatsFile
-    {
+    ): JatsFile {
         $publication = Repo::publication()->get($publicationId, $submissionId);
         $submission = Repo::submission()->get($publication->getData('submissionId'));
 
@@ -166,7 +165,7 @@ class Repository
         $params['name'][$primaryLocale] = $fileName;
 
         if (empty($params['genreId'])) {
-        
+
             [$firstGenre, $secondGenre] = [$genres->next(), $genres->next()];
             if ($firstGenre && !$secondGenre) {
                 $params['genreId'] = $firstGenre->getId();
@@ -186,7 +185,7 @@ class Repository
 
         if (!empty($errors)) {
             app()->get('file')->delete($fileId);
-            throw new Exception(''. implode(', ', $errors));
+            throw new Exception('' . implode(', ', $errors));
         }
 
         $submissionFile = Repo::submissionFile()
@@ -202,7 +201,7 @@ class Repository
     }
 
     /**
-     * Given a submission and a publication this function returns the JATS XML contents provided by the 
+     * Given a submission and a publication this function returns the JATS XML contents provided by the
      * submission/publication metadata
      *
      * @throws \PKP\jats\exceptions\UnableToCreateJATSContentException If the default JATS creation fails
@@ -230,9 +229,9 @@ class Repository
      *
      * Valid file stages should be passed through
      * the hook SubmissionFile::fileStages.
-     * @return array
      */
-    public function getFileStages(): array {
+    public function getFileStages(): array
+    {
         return [SubmissionFile::SUBMISSION_FILE_JATS];
     }
 }

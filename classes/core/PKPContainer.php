3e26545ceab358838deada035a0798fd8dd591bf
Touhidur Rahman <abir.ruet@gmail.com>
2025-06-26 10:42:52 +0600

    pkp/pkp-lib#9968 bind view facade

--- classes/core/PKPContainer.php
+++ classes/core/PKPContainer.php
@@ -338,6 +338,9 @@ class PKPContainer extends Container
                 \Illuminate\Contracts\Encryption\Encrypter::class,
                 \Illuminate\Contracts\Encryption\StringEncrypter::class,
             ],
+            'view' => [
+                \Illuminate\Support\Facades\View::class,
+            ],
         ] as $key => $aliases) {
             foreach ($aliases as $alias) {
                 $this->alias($key, $alias);

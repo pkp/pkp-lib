d119a96e18bbe35f9e372fac6dc551863c25f5ba
Touhidur Rahman <abir.ruet@gmail.com>
2025-08-04 18:08:32 +0600

    pkp/pkp-lib#9295 refactor to remove publish column and depend on new status as STATUS_READY_TO_PUBLISH

--- classes/template/PKPTemplateManager.php
+++ classes/template/PKPTemplateManager.php
@@ -916,6 +916,7 @@ class PKPTemplateManager extends Smarty
             'STATUS_PUBLISHED' => Submission::STATUS_PUBLISHED,
             'STATUS_DECLINED' => Submission::STATUS_DECLINED,
             'STATUS_SCHEDULED' => Submission::STATUS_SCHEDULED,
+            'STATUS_READY_TO_PUBLISH' => Submission::STATUS_READY_TO_PUBLISH,
         ]);
 
 

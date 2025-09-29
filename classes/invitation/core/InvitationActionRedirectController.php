36274a6074bc460b12f7d7257eae3b79b9036376
Touhidur Rahman <abir.ruet@gmail.com>
2025-09-29 15:17:57 +0600

    pkp/pkp-lib#11690 resolve invitation auto decline using confirmation landing page

--- classes/invitation/core/InvitationActionRedirectController.php
+++ classes/invitation/core/InvitationActionRedirectController.php
@@ -15,8 +15,11 @@
 namespace PKP\invitation\core;
 
 use APP\core\Request;
+use APP\template\TemplateManager;
 use Illuminate\Routing\Controller;
+use PKP\core\PKPApplication;
 use PKP\invitation\core\enums\InvitationAction;
+use PKP\invitation\core\enums\InvitationStatus;
 
 /**
  * @template TInvitation of Invitation
@@ -31,8 +34,43 @@ abstract class InvitationActionRedirectController extends Controller
         $this->invitation = $invitation;
     }
 
+    /**
+     * Redirect to accept invitation page
+     *
+     * @throws \Exception
+     */
+    public function declineHandle(Request $request): void
+    {
+        if ($this->invitation->getStatus() !== InvitationStatus::PENDING) {
+            throw new \Symfony\Component\HttpKernel\Exception\GoneHttpException();
+        }
+
+        $context = $request->getContext();
+
+        $templateMgr = TemplateManager::getManager($request);
+        $declineUrl = PKPApplication::get()->getDispatcher()->url(
+            PKPApplication::get()->getRequest(),
+            PKPApplication::ROUTE_PAGE,
+            $context->getData('urlPath'),
+            'invitation',
+            'confirmDecline',
+            null,
+            [
+                'id' => $request->getUserVar('id'),
+                'key' => $request->getUserVar('key'),
+            ]
+        );
+
+        $templateMgr->assign([
+            'declineUrl' => $declineUrl,
+            'pageComponent' => 'Page',
+        ]);
+
+        $templateMgr->display('invitation/declineInvitation.tpl');
+    }
+
     abstract public function preRedirectActions(InvitationAction $action): void;
 
     abstract public function acceptHandle(Request $request): void;
-    abstract public function declineHandle(Request $request): void;
+    abstract public function confirmDecline(Request $request): void;
 }

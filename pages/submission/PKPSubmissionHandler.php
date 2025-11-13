0780a99bdfc6f85ce5873d0aaa713fb5f2fcd5c3
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2025-11-13 13:47:29 +0100

    pkp/pkp-lib#12017 activeUsers scope

--- pages/submission/PKPSubmissionHandler.php
+++ pages/submission/PKPSubmissionHandler.php
@@ -615,12 +615,11 @@ abstract class PKPSubmissionHandler extends Handler
     {
         $request = Application::get()->getRequest();
         $isAdmin = $request->getUser()->hasRole([Role::ROLE_ID_SITE_ADMIN], \PKP\core\PKPApplication::SITE_CONTEXT_ID);
-        $query = UserGroup::query()->withContextIds([$context->getId()])->withUserIds([$user->getId()]);
+        $query = UserGroup::query()->withContextIds([$context->getId()])->withActiveUserIds([$user->getId()]);
 
         $userGroups = $isAdmin
             ? $query->withRoleIds([Role::ROLE_ID_MANAGER, Role::ROLE_ID_SITE_ADMIN])->get()
             : $query->withStageIds([WORKFLOW_STAGE_ID_SUBMISSION])
-                ->withUserUserGroupStatus(UserUserGroupStatus::STATUS_ACTIVE->value)
                 ->get(); // For non-admin users, query for the groups tht give them access to the submission stage
 
         // Users without a submitting role or access to submission stage can submit as an

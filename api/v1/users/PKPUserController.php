2d2fd383aff380163c16bdee61157abed3fb825d
Hafsa-Naeem <hafsanaeem321@hotmail.com>
2025-12-08 14:58:39 +0500

    pkp/pkplib#12100 fix(reviewers): array items for filtering

--- api/v1/users/PKPUserController.php
+++ api/v1/users/PKPUserController.php
@@ -249,8 +249,7 @@ class PKPUserController extends PKPBaseController
             'isSiteAdmin' => Validation::isSiteAdmin(),
         ];
 
-        $items = $map->summarizeManyReviewers($usersCollection, $options)->all();
-
+        $items = $map->summarizeManyReviewers($usersCollection, $options)->values()->all();
         return response()->json([
             'itemsMax' => $collector->getCount(),
             'items' => $items,

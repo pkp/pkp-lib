6805de7f6a47e10cc1b635290442c0b602f5d14b
Touhidur Rahman <abir.ruet@gmail.com>
2026-01-02 16:12:55 +0600

    pkp/pkp-lib#9345 use interface to define context aware job and removed param based reflection approach

---
+++ classes/queue/ContextAwareJob.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file classes/queue/ContextAwareJob.php
+ *
+ * Copyright (c) 2025 Simon Fraser University
+ * Copyright (c) 2025 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class ContextAwareJob
+ *
+ * @brief Abstraction to determine the context id for context aware jobs
+ */
+
+namespace PKP\queue;
+
+interface ContextAwareJob
+{
+    /**
+     * Get the context id of this context aware job
+     */
+    public function getContextId(): ?int;
+}

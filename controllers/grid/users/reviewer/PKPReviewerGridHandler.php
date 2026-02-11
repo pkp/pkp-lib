51493474b93287bcf3e0aac07a9a6d55a413e47d
Nicolas Boulay <NIBOU230@ulaval.ca>
2026-02-11 15:14:14 -0500

    pkp/pkp-lib#9921 Add a dropdown to select the email template when sending a reminder

---
+++ controllers/grid/users/reviewer/PKPReviewerGridHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file controllers/grid/users/reviewer/PKPReviewerGridHandler.php
+ *
+ * Make PKPReviewerGridHandler resolvable by PKPComponentRouter.
+ */
+
+require_once(PKP_LIB_PATH . '/classes/controllers/grid/users/reviewer/PKPReviewerGridHandler.php');

68ac8659ea805ecda1c2ad0035da9f8ac4255e5f
Nicolas Boulay <NIBOU230@ulaval.ca>
2026-02-17 11:20:45 -0500

    pkp/pkp-lib#9921 Added null checks, removed ambiguities and refact into methods

--- controllers/grid/users/reviewer/PKPReviewerGridHandler.php
+++ controllers/grid/users/reviewer/PKPReviewerGridHandler.php
@@ -3,6 +3,10 @@
 /**
  * @file controllers/grid/users/reviewer/PKPReviewerGridHandler.php
  *
+ * Copyright (c) 2014-2026 Simon Fraser University
+ * Copyright (c) 2000-2026 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ * 
  * Make PKPReviewerGridHandler resolvable by PKPComponentRouter.
  */
 

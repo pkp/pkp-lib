5497ae28356a101a500b07663cbdd1a56ae41593
Mariusz Sienkiewicz <mariusz.sienkiewicz.it@gmail.com>
2026-02-18 23:30:55 +0100

    Fix N+1 query performance issue in Validation::getAdministrationLevel (#12316)
    * Fix N+1 query issue in Validation::getAdministrationLevel (#12293)
    
    * Refactor Validation::getAdministrationLevel to use withWhereHas

--- classes/security/Validation.php
+++ classes/security/Validation.php
@@ -504,7 +504,7 @@ class Validation
 
         // single query to fetch user groups assigned to either user
         $allUserGroups = UserGroup::query()
-            ->whereHas('userUserGroups', function ($query) use ($administratorUserId, $administeredUserId) {
+            ->withWhereHas('userUserGroups', function ($query) use ($administratorUserId, $administeredUserId) {
                 $query->withUserIds([$administratorUserId, $administeredUserId])
                     ->withActive();
             })

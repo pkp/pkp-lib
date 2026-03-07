f113f4baf5e02e59da2e9ec5db8815e3a466aa5e
zemelten <zemelten7@gmail.com>
2026-03-07 17:47:22 +0300

    fix: Add authors to Statistics > Articles CSV export
    Adds author information as a new column in the CSV report downloaded
    from Statistics > Articles. Authors are displayed in comma separated
    format after the article title.
    
    Fixes #12223

--- api/v1/stats/publications/PKPStatsPublicationController.php
+++ api/v1/stats/publications/PKPStatsPublicationController.php
@@ -844,6 +844,8 @@ abstract class PKPStatsPublicationController extends PKPBaseController
         return [
             __('common.id'),
             __('common.title'),
+            __('submission.authors'),
+            __('publication.datePublished'),
             __('stats.total'),
             __('submission.abstractViews'),
             __('stats.fileViews'),
@@ -907,13 +909,18 @@ abstract class PKPStatsPublicationController extends PKPBaseController
         $galleyViews = $pdfViews + $htmlViews + $otherViews;
         $totalViews = $abstractViews + $galleyViews;
 
-        // Get submission title for display
+        // Get submission title, authors and publication date for display
         $submission = Repo::submission()->get($submissionId);
-        $submissionTitle = $submission->getCurrentPublication()->getLocalizedTitle();
+        $currentPublication = $submission->getCurrentPublication();
+        $submissionTitle = $currentPublication->getLocalizedTitle();
+        $authorsString = $currentPublication->getShortAuthorString() ?? '';
+        $datePublished = $currentPublication->getData('datePublished') ?? '';
 
         return [
             $submissionId,
             $submissionTitle,
+            $authorsString,
+            $datePublished,
             $totalViews,
             $abstractViews,
             $galleyViews,

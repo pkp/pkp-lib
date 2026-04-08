0ebe5ce47f240b7485e69a3019f24b4899e8d6e9
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2026-04-08 10:41:37 +0200

    pkp/pkp-lib#12540 Automatically copy user name in legacy forms to site locale if not available

--- controllers/grid/users/reviewer/form/CreateReviewerForm.php
+++ controllers/grid/users/reviewer/form/CreateReviewerForm.php
@@ -132,6 +132,19 @@ class CreateReviewerForm extends ReviewerForm
         }
 
         $this->readUserVars($inputData);
+
+        // Auto-populate site locale name fields from context primary if empty
+        $sitePrimaryLocale = Application::get()->getRequest()->getSite()->getPrimaryLocale();
+        $contextPrimaryLocale = $this->requiredLocale;
+        if ($sitePrimaryLocale !== $contextPrimaryLocale) {
+            foreach (['givenName', 'familyName', 'preferredPublicName'] as $field) {
+                $data = $this->getData($field);
+                if (empty($data[$sitePrimaryLocale]) && !empty($data[$contextPrimaryLocale])) {
+                    $data[$sitePrimaryLocale] = $data[$contextPrimaryLocale];
+                    $this->setData($field, $data);
+                }
+            }
+        }
     }
 
     /**

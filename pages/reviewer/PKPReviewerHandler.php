ddc219544494299eec2ff0f0c67c0c1cef076578
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-04-13 00:40:58 -0400

    pkp/pkp-lib#12534 Add submission file stages and review form element type constants

--- pages/reviewer/PKPReviewerHandler.php
+++ pages/reviewer/PKPReviewerHandler.php
@@ -32,10 +32,12 @@ use PKP\editorialTask\enums\EditorialTaskType;
 use PKP\facades\Locale;
 use PKP\log\event\PKPSubmissionEventLogEntry;
 use PKP\notification\Notification;
+use PKP\reviewForm\ReviewFormElement;
 use PKP\submission\reviewAssignment\ReviewAssignment;
 use PKP\submission\reviewer\form\PKPReviewerReviewStep3Form;
 use PKP\submission\reviewer\form\ReviewerReviewForm;
 use PKP\submission\reviewer\ReviewerAction;
+use PKP\submissionFile\SubmissionFile;
 
 class PKPReviewerHandler extends Handler
 {
@@ -101,6 +103,20 @@ class PKPReviewerHandler extends Handler
 
             // Editorial task log event types
             'SUBMISSION_LOG_TASK_NOTE_POSTED' => PKPSubmissionEventLogEntry::SUBMISSION_LOG_TASK_NOTE_POSTED,
+
+            // Submission file stages (needed for FileManager Vue component)
+            'SUBMISSION_FILE_REVIEW_FILE' => SubmissionFile::SUBMISSION_FILE_REVIEW_FILE,
+            'SUBMISSION_FILE_INTERNAL_REVIEW_FILE' => SubmissionFile::SUBMISSION_FILE_INTERNAL_REVIEW_FILE,
+            'SUBMISSION_FILE_REVIEW_ATTACHMENT' => SubmissionFile::SUBMISSION_FILE_REVIEW_ATTACHMENT,
+            'ASSOC_TYPE_REVIEW_ASSIGNMENT' => PKPApplication::ASSOC_TYPE_REVIEW_ASSIGNMENT,
+
+            // Review form element types
+            'REVIEW_FORM_ELEMENT_TYPE_SMALL_TEXT_FIELD' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_SMALL_TEXT_FIELD,
+            'REVIEW_FORM_ELEMENT_TYPE_TEXT_FIELD' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_TEXT_FIELD,
+            'REVIEW_FORM_ELEMENT_TYPE_TEXTAREA' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_TEXTAREA,
+            'REVIEW_FORM_ELEMENT_TYPE_CHECKBOXES' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_CHECKBOXES,
+            'REVIEW_FORM_ELEMENT_TYPE_RADIO_BUTTONS' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_RADIO_BUTTONS,
+            'REVIEW_FORM_ELEMENT_TYPE_DROP_DOWN_BOX' => ReviewFormElement::REVIEW_FORM_ELEMENT_TYPE_DROP_DOWN_BOX,
         ]);
 
         $templateMgr->assign([

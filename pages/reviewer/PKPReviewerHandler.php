f9995ae90ba812887f6530840a503e225b5a7efa
Blesilda Biazon <ramirezblesilda@gmail.com>
2026-07-28 23:15:23 -0400

    pkp/pkp-lib#12534 Remove unused js handlers for step3 form

--- pages/reviewer/PKPReviewerHandler.php
+++ pages/reviewer/PKPReviewerHandler.php
@@ -135,15 +135,6 @@ class PKPReviewerHandler extends Handler
             'submission' => $reviewSubmission,
         ]);
 
-        // Ensure Step 3 custom required validation script is loaded in the page header
-        if ((int)$reviewAssignment->getReviewFormId() > 0) {
-            $templateMgr->addJavaScript(
-                'reviewStep3Required',
-                $request->getBaseUrl() . '/lib/pkp/js/pages/reviewer/reviewStep3Required.js',
-                ['contexts' => ['backend']]
-            );
-        }
-
         $templateMgr->setState([
             'pageInitConfig' => [
                 'reviewRoundHistories' => $reviewRoundHistories,

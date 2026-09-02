1feb9b211bc2e07041afedc35455c38e4763d1bd
Dimitris Efstathiou <defstat@gmail.com>
2026-09-02 13:52:14 +0300

    pkp/pkp-lib#12807 Fix error 500 when someone other than the invited user is signed in.

---
+++ classes/invitation/core/exceptions/SignedInAsDifferentUserException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file classes/invitation/core/exceptions/SignedInAsDifferentUserException.php
+ *
+ * Copyright (c) 2026 Simon Fraser University
+ * Copyright (c) 2026 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class SignedInAsDifferentUserException
+ *
+ */
+
+namespace PKP\invitation\core\exceptions;
+
+use Exception;
+
+class SignedInAsDifferentUserException extends Exception
+{
+}

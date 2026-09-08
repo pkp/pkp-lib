c406f66e6fbe1aa1d7a1c15db5155e66952f333c
taslangraham <gtaslan1@gmail.com>
2026-09-08 15:03:01 -0500

    pkp/pkp-lib#13221 Implement crossref's citedBy feature

--- js/load_frontend.js
+++ js/load_frontend.js
@@ -43,6 +43,9 @@ import PkpCombobox from '@/frontend/components/PkpCombobox/PkpCombobox.vue';
 import PkpCiteBody from '@/frontend/components/PkpCite/PkpCiteBody.vue';
 import PkpCrossmarkButton from '@/frontend/components/PkpCrossmarkButton/PkpCrossmarkButton.vue';
 import PkpUsageChart from '@/frontend/components/PkpUsageChart/PkpUsageChart.vue';
+import PkpCitedBy from '@/frontend/components/PkpCitedBy/PkpCitedBy.vue';
+import PkpCitedByCount from '@/frontend/components/PkpCitedBy/PkpCitedByCount.vue';
+import PkpCitedByBody from '@/frontend/components/PkpCitedBy/PkpCitedByBody.vue';
 
 // Pinia stores
 import {usePkpModalStore} from '@/frontend/stores/pkpModalStore';
@@ -51,6 +54,7 @@ import {usePkpCommentsStore} from '@/frontend/components/PkpComments/usePkpComme
 import {usePkpOpenReviewStore} from '@/frontend/components/PkpOpenReview/usePkpOpenReviewStore';
 import {usePkpCiteStore} from '@/frontend/components/PkpCite/usePkpCiteStore';
 import {usePkpUsageChartStore} from '@/frontend/components/PkpUsageChart/usePkpUsageChartStore';
+import {usePkpCitedByStore} from '@/frontend/components/PkpCitedBy/usePkpCitedByStore';
 
 // PkpTab Components
 import PkpTabRoot from '@/frontend/components/PkpTab/PkpTabRoot.vue';
@@ -90,6 +94,7 @@ VueRegistry.registerStore('pkpComments', usePkpCommentsStore);
 VueRegistry.registerStore('pkpOpenReview', usePkpOpenReviewStore);
 VueRegistry.registerStore('pkpCite', usePkpCiteStore);
 VueRegistry.registerStore('pkpUsageChart', usePkpUsageChartStore);
+VueRegistry.registerStore('pkpCitedBy', usePkpCitedByStore);
 
 VueRegistry.registerComponent('PkpButton', PkpButton);
 VueRegistry.registerComponent('PkpDialog', PkpDialog);
@@ -106,6 +111,9 @@ VueRegistry.registerComponent('PkpCombobox', PkpCombobox);
 VueRegistry.registerComponent('PkpCiteBody', PkpCiteBody);
 VueRegistry.registerComponent('PkpCrossmarkButton', PkpCrossmarkButton);
 VueRegistry.registerComponent('PkpUsageChart', PkpUsageChart);
+VueRegistry.registerComponent('PkpCitedBy', PkpCitedBy);
+VueRegistry.registerComponent('PkpCitedByCount', PkpCitedByCount);
+VueRegistry.registerComponent('PkpCitedByBody', PkpCitedByBody);
 
 // Register PkpTab Components
 VueRegistry.registerComponent('PkpTabRoot', PkpTabRoot);

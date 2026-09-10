5108c48ca16db3b02a847ac7c2ef098d1420be0e
Diego Abadan <diego@lepidus.com.br>
2026-09-09 21:07:49 -0300

    Usa diretório temporário privado no PDF de avaliação

--- api/v1/reviews/PKPReviewController.php
+++ api/v1/reviews/PKPReviewController.php
@@ -343,7 +343,9 @@ class PKPReviewController extends PKPBaseController
         $submissionCommentsPrivate = $submissionCommentDao->getReviewerCommentsByReviewerId($submissionId, $reviewAssignment->getReviewerId(), $reviewId, false);
         $title = $submission->getCurrentPublication()->getLocalizedTitle(null, 'html');
         $cleanTitle = str_replace('&nbsp;', ' ', strip_tags($title));
+        $fileManager = new TemporaryFileManager();
         $mpdf = new Mpdf([
+            'tempDir' => $fileManager->getBasePath(),
             'default_font' => 'NotoSansSC',
             'mode' => '+aCJK',
             'autoScriptToLang' => true,
@@ -405,7 +407,6 @@ class PKPReviewController extends PKPBaseController
         $mpdf->WriteHTML($reviewHtml);
         $exportFileName = "submission_review_{$submissionId}-{$reviewId}.pdf";
         $pdfContent = $mpdf->Output($exportFileName, 'S');
-        $fileManager = new TemporaryFileManager();
         $tempFilename = $fileManager->getBasePath() . $exportFileName;
         $fileManager->writeFile($tempFilename, $pdfContent);
         $user = Application::get()->getRequest()->getUser();

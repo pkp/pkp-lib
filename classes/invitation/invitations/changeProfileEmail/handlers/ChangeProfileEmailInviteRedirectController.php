78d439c8b8f020f71dd6bb9ec6d73526935c851f
Dimitris Efstathiou <defstat@gmail.com>
2026-09-16 19:25:16 +0300

    pkp/pkp-lib#13181 Fix redirect to profile/contact after needed login

--- classes/invitation/invitations/changeProfileEmail/handlers/ChangeProfileEmailInviteRedirectController.php
+++ classes/invitation/invitations/changeProfileEmail/handlers/ChangeProfileEmailInviteRedirectController.php
@@ -46,9 +46,9 @@ class ChangeProfileEmailInviteRedirectController extends InvitationActionRedirec
             null,
             'user',
             'profile',
-            null,
-            null,
-            'contact'
+            [
+                'contact'
+            ]
         );
 
         $this->getInvitation()->finalize();
@@ -78,9 +78,9 @@ class ChangeProfileEmailInviteRedirectController extends InvitationActionRedirec
             null,
             'user',
             'profile',
-            null,
-            null,
-            'contact'
+            [
+                'contact'
+            ]
         );
 
         $this->getInvitation()->decline();

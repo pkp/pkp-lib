c1965ec08e437d1e2caff929c54c6d0d521b4076
Alec Smecher <alec@smecher.bc.ca>
2026-09-16 15:36:18 -0700

    pkp/pkp-lib#13335 Do not permit JS as an image type

--- classes/file/FileManager.php
+++ classes/file/FileManager.php
@@ -613,8 +613,6 @@ class FileManager
                 return '.mov';
             case 'video/mp4':
                 return '.mp4';
-            case 'text/javascript':
-                return '.js';
             case 'image/webp':
                 return '.webp';
             default:

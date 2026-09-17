9ce915e07e681a9c30dafcd56bbaf8cd62322688
Touhidur Rahman <abir.ruet@gmail.com>
2026-09-17 14:43:02 +0600

    pkp/pkp-lib#13348 Register Laravel session container aliases

--- classes/core/PKPContainer.php
+++ classes/core/PKPContainer.php
@@ -316,6 +316,13 @@ class PKPContainer extends Container
                 \Illuminate\Contracts\Routing\Registrar::class,
                 \Illuminate\Contracts\Routing\BindingRegistrar::class
             ],
+            'session' => [
+                \Illuminate\Session\SessionManager::class
+            ],
+            'session.store' => [
+                \Illuminate\Session\Store::class,
+                \Illuminate\Contracts\Session\Session::class
+            ],
             'url' => [
                 \Illuminate\Routing\UrlGenerator::class,
                 \Illuminate\Contracts\Routing\UrlGenerator::class

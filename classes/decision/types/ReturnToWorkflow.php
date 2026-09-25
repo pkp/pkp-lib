1ad4a14bb2c7a02445f7986596f7fd85ff6b1407
taslangraham <gtaslan1@gmail.com>
2026-09-25 17:29:40 -0500

    pkp/pkp-lib#12798 fix method signature

--- classes/decision/types/ReturnToWorkflow.php
+++ classes/decision/types/ReturnToWorkflow.php
@@ -72,7 +72,7 @@ class ReturnToWorkflow extends DecisionType
         return null;
     }
 
-    public function getLabel(?string $locale = null): string
+    public function getLabel(?string $locale = null, ?Submission $submission = null): string
     {
         return __('editor.submission.decision.returnToWorkflow', [], $locale);
     }
